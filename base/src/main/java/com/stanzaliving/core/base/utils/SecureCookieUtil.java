8739fe4fb0d346cacbd0499a8f8f258e43924e8f
Vaibhav Gupta <vaibhav.gupta@stanzaliving.com>
2019-10-25 14:38:53 +0530

    Added cookie sub domain handling

--- base/src/main/java/com/stanzaliving/core/base/utils/SecureCookieUtil.java
+++ base/src/main/java/com/stanzaliving/core/base/utils/SecureCookieUtil.java
@@ -6,6 +6,8 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.stanzaliving.core.base.constants.SecurityConstants;
 
 import lombok.experimental.UtilityClass;
@@ -21,6 +23,7 @@ public class SecureCookieUtil {
 			cookie.setSecure(false);
 			cookie.setHttpOnly(false);
 		} else {
+			cookie.setDomain(SecurityConstants.STANZA_DOMAIN);
 			cookie.setSecure(true);
 			cookie.setHttpOnly(true);
 		}
@@ -29,21 +32,30 @@ public class SecureCookieUtil {
 		return cookie;
 	}
 
-	public static Cookie expire(Cookie cookie) {
+	public static Cookie expire(Cookie cookie, boolean isLocalFrontEnd) {
 		cookie.setMaxAge(0);
 		cookie.setValue(null);
 		cookie.setPath("/");
+
+		if (!isLocalFrontEnd) {
+			cookie.setDomain(SecurityConstants.STANZA_DOMAIN);
+		}
+
 		return cookie;
 	}
 
 	public static void handleLogOutResponse(HttpServletRequest request, HttpServletResponse response) {
+
+		String frontEnv = request.getHeader(SecurityConstants.FRONT_ENVIRONMENT);
+		boolean isLocalFrontEnd = StringUtils.isNotBlank(frontEnv) && SecurityConstants.FRONT_ENVIRONMENT_LOCAL.equals(frontEnv);
+
 		Cookie[] cookies = request.getCookies();
 
 		if (cookies != null) {
 			for (Cookie cookie : cookies) {
 
 				if (SecurityConstants.TOKEN_HEADER_NAME.equals(cookie.getName())) {
-					response.addCookie(SecureCookieUtil.expire(cookie));
+					response.addCookie(SecureCookieUtil.expire(cookie, isLocalFrontEnd));
 				}
 			}
 		}
