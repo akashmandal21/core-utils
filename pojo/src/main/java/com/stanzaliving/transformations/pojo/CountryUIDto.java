3b5fbba4102d2bac497114c0e013c0a92a87fc47
Vaibhav Gupta <vaibhav.gupta@stanzaliving.com>
2019-10-25 12:20:45 +0530

    Added builder

--- pojo/src/main/java/com/stanzaliving/transformations/pojo/CountryUIDto.java
+++ pojo/src/main/java/com/stanzaliving/transformations/pojo/CountryUIDto.java
@@ -2,27 +2,33 @@ package com.stanzaliving.transformations.pojo;
 
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@Builder
 @EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class CountryUIDto {
 
-	private String uuid;
-	
 	private long id;
-	
+
+	private String uuid;
+
 	@NotNull
 	private String countryName;
-	
+
 	@NotNull
 	private String currency;
-	
+
 	@NotNull
 	private String localization;
 
