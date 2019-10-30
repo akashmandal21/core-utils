8149fcfdcc714c46845361eacb94284eaf1fea39
Naveen Kumar <naveen.kumar@stanzaliving.com>
2019-10-30 18:31:34 +0530

    Added dto for Estate

---
+++ pojo/src/main/java/com/stanzaliving/core/estate/dto/EstateDto.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.stanzaliving.core.estate.dto;
+
+import com.stanzaliving.core.base.common.dto.AbstractDto;
+import com.stanzaliving.core.user.dto.Address;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author naveen.kumar
+ *
+ * @date 30-Oct-2019
+ *
+ **/
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstateDto extends AbstractDto {
+
+	private String estateName;
+
+	private Address address;
+
+	private Long cityId;
+
+	private String cityName;
+
+	private Long micromarketId;
+
+	private String micromarketName;
+
+}
