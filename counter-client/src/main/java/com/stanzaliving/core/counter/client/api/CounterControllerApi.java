b92b151471aac46500a3fe804f738e60240dcae1
Naveen Kumar <naveen.kumar@stanzaliving.com>
2019-11-03 19:36:19 +0530

    Added client for counter service

---
+++ counter-client/src/main/java/com/stanzaliving/core/counter/client/api/CounterControllerApi.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.stanzaliving.core.counter.client.api;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.stanzaliving.core.base.common.dto.ResponseDto;
+import com.stanzaliving.core.base.http.StanzaRestClient;
+
+/**
+ * @author naveen.kumar
+ *
+ * @date 03-Nov-2019
+ *
+ **/
+public class CounterControllerApi {
+
+	private StanzaRestClient restClient;
+
+	public CounterControllerApi(StanzaRestClient stanzaRestClient) {
+		this.restClient = stanzaRestClient;
+	}
+
+	public ResponseDto<Long> getCounterForKey(String key) {
+
+		if (StringUtils.isBlank(key)) {
+			throw new IllegalArgumentException("Counter Key cannot be blank");
+		}
+
+		Object postBody = null;
+
+		// create path and map variables
+		final Map<String, Object> uriVariables = new HashMap<>();
+		uriVariables.put("key", key);
+
+		String path = UriComponentsBuilder.fromPath("/get/{key}").buildAndExpand(uriVariables).toUriString();
+
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+		final HttpHeaders headerParams = new HttpHeaders();
+
+		final String[] accepts = {
+				"*/*"
+		};
+		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+		ParameterizedTypeReference<ResponseDto<Long>> returnType = new ParameterizedTypeReference<ResponseDto<Long>>() {
+		};
+		return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
+	}
+}
