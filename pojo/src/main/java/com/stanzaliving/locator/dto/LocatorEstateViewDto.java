b92a8cb7872614fa414d54bf21c748cf24ed8ea6
Ankit Jindal <vaibhav.gupta@stanzaliving.com>
2019-11-05 16:44:22 +0530

    Pojo for locator

---
+++ pojo/src/main/java/com/stanzaliving/locator/dto/LocatorEstateViewDto.java
@@ -0,0 +1,15 @@
+package com.stanzaliving.locator.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LocatorEstateViewDto {
+
+	private List<InstitutionDto> institutions;
+	
+	private List<Competitor> competitors;
+}
