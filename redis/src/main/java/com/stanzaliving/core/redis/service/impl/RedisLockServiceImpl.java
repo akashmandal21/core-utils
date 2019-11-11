1b9433db3dc8f274ef87ef8017740302984b5b98
Ankit Jindal <ankit.jindal@stanzaliving.com>
2019-11-11 13:30:11 +0530

    Redis Util Committed

---
+++ redis/src/main/java/com/stanzaliving/core/redis/service/impl/RedisLockServiceImpl.java
@@ -0,0 +1,35 @@
+package com.stanzaliving.core.redis.service.impl;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.stanzaliving.core.redis.service.RedisLockService;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class RedisLockServiceImpl implements RedisLockService {
+
+	@Autowired
+	private RedissonClient redissonClient;
+	
+	@Override
+	public RLock acquire(String lockName) {
+		
+		log.info("Acquiring Lock for ["+lockName+"]");
+		
+		return redissonClient.getLock(lockName);
+		
+	}
+
+	@Override
+	public void release(RLock rLock) {
+		
+		rLock.unlock();
+		
+	}
+
+}
