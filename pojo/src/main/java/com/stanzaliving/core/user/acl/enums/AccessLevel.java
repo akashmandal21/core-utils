ada79d6b78b500d92772d9cd5e33ab5d72d2a1a0
Naveen Kumar <naveen.kumar@stanzaliving.com>
2019-11-18 11:45:43 +0530

    Added People Requirement Dtos

---
+++ pojo/src/main/java/com/stanzaliving/core/user/acl/enums/AccessLevel.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.stanzaliving.core.user.acl.enums;
+
+/**
+ * @author naveen.kumar
+ *
+ * @date 14-Nov-2019
+ *
+ **/
+public enum AccessLevel {
+
+	COUNTRY,
+	REGION,
+	CITY,
+	MICROMARKET,
+	RESIDENCE
+}
