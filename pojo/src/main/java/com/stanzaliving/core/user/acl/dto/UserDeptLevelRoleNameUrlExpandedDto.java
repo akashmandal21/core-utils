e7a656cdb5c36b7c876ed851a972206911b09f5a
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2019-11-29 10:59:46 +0530

    adding data annotation

--- pojo/src/main/java/com/stanzaliving/core/user/acl/dto/UserDeptLevelRoleNameUrlExpandedDto.java
+++ pojo/src/main/java/com/stanzaliving/core/user/acl/dto/UserDeptLevelRoleNameUrlExpandedDto.java
@@ -2,9 +2,11 @@ package com.stanzaliving.core.user.acl.dto;
 
 import com.stanzaliving.core.base.enums.AccessLevel;
 import com.stanzaliving.core.base.enums.Department;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class UserDeptLevelRoleNameUrlExpandedDto {
 
     private String userUuid;
