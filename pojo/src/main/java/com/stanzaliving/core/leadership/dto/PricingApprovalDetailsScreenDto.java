0d3cc27a63d2a25730ad615ff0a06d3327c46966
Ankit Jindal <ankit.jindal@stanzaliving.com>
2019-12-03 11:41:41 +0530

    Deal Terms Added

--- pojo/src/main/java/com/stanzaliving/core/leadership/dto/PricingApprovalDetailsScreenDto.java
+++ pojo/src/main/java/com/stanzaliving/core/leadership/dto/PricingApprovalDetailsScreenDto.java
@@ -1,5 +1,7 @@
 package com.stanzaliving.core.leadership.dto;
 
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -14,5 +16,7 @@ public class PricingApprovalDetailsScreenDto {
 	private PricingViewEstateData pricingViewEstateData;
 	
 	private PricingApprovalScreenDto pricingApprovalScreenDto;
+	
+	private List<DealTermsDto> dealTermsDtos;
 	
 }
