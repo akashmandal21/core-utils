5a738a0fc2fcc44fd8ce7b36dadcea7a10c24a8a
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2019-12-05 15:07:07 +0530

    integrating transformation master

--- pojo/src/main/java/com/stanzaliving/core/user/acl/dto/UserDeptLevelRoleNameUrlExpandedDto.java
+++ pojo/src/main/java/com/stanzaliving/core/user/acl/dto/UserDeptLevelRoleNameUrlExpandedDto.java
@@ -2,13 +2,17 @@ package com.stanzaliving.core.user.acl.dto;
 
 import com.stanzaliving.core.base.enums.AccessLevel;
 import com.stanzaliving.core.base.enums.Department;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDeptLevelRoleNameUrlExpandedDto {
 
     private String userUuid;
