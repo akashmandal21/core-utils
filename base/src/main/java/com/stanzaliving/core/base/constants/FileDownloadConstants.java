5d63d4b3be7d9c07f77b65fbf80bf59eb018794c
Parul Mishra <parul.mishra@C02ZL33NLVCF.local>
2019-12-09 14:47:27 +0530

    add constants for file download

---
+++ base/src/main/java/com/stanzaliving/core/base/constants/FileDownloadConstants.java
@@ -0,0 +1,24 @@
+package com.stanzaliving.core.base.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class FileDownloadConstants {
+
+    public static final String ROLE_NAME = "RoleName";
+    public static final String SIGNOFF_REQUIREMENT = "SignOffRequirement";
+
+    public static final String CITY_NAME = "CityName";
+    public static final String MICROMARKET_NAME = "MicromarketName";
+    public static final String RESIDENCE_NAME = "ResidenceName";
+
+    public static final String NEW_LINE_SEPARATOR = "\n";
+
+    public static final String USER_NAME = "UserName";
+    public static final String NO_OF_BEDS = "# Beds";
+    public static final String SCORE = "Score";
+
+
+
+
+}
