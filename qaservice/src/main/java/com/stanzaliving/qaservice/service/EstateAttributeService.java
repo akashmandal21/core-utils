2569b90f76216af5557f7d271a887a490475a19e
RajKumarStanza <raj.singh@stanzaliving.com>
2019-12-09 17:45:15 +0530

    Get and save estate

--- qaservice/src/main/java/com/stanzaliving/qaservice/service/EstateAttributeService.java
+++ qaservice/src/main/java/com/stanzaliving/qaservice/service/EstateAttributeService.java
@@ -6,6 +6,7 @@ package com.stanzaliving.qaservice.service;
 import java.util.List;
 import java.util.Map;
 
+import com.stanzaliving.qaservice.entity.AnswerEntity;
 import com.stanzaliving.qaservice.entity.EstateAttributeEntity;
 
 /**
@@ -17,4 +18,8 @@ public interface EstateAttributeService {
 	List<EstateAttributeEntity> saveEstateAttributeList(List<EstateAttributeEntity> estateAttributeEntityList);
 	
 	Map<String, EstateAttributeEntity> getEstateAttributeMap(String estateId);
+
+	List<EstateAttributeEntity> getAndSaveEstateAttributeEntityListFromAnswer(AnswerEntity answer, String estateId,
+			String propertyId);
+		
 }
--- qaservice/src/main/java/com/stanzaliving/qaservice/service/impl/EstateAttributeServiceImpl.java
+++ qaservice/src/main/java/com/stanzaliving/qaservice/service/impl/EstateAttributeServiceImpl.java
@@ -3,6 +3,7 @@
  */
 package com.stanzaliving.qaservice.service.impl;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,7 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stanzaliving.core.base.exception.StanzaException;
+import com.stanzaliving.qaservice.entity.AnswerEntity;
 import com.stanzaliving.qaservice.entity.EstateAttributeEntity;
 import com.stanzaliving.qaservice.repository.EstateAttributeRepository;
 import com.stanzaliving.qaservice.service.EstateAttributeService;
@@ -28,8 +32,11 @@ import lombok.extern.slf4j.Slf4j;
 public class EstateAttributeServiceImpl implements EstateAttributeService {
 
 	@Autowired
-	private EstateAttributeRepository estateAttributeRepository;
+	private ObjectMapper objectMapper;
 	
+	@Autowired
+	private EstateAttributeRepository estateAttributeRepository;
+		
 	@Override
 	public List<EstateAttributeEntity> saveEstateAttributeList(List<EstateAttributeEntity> estateAttributeEntityList) {
 		
@@ -76,4 +83,26 @@ public class EstateAttributeServiceImpl implements EstateAttributeService {
 		
 		return estateAttributeMap;
 	}
+	
+	@Override
+	public List<EstateAttributeEntity> getAndSaveEstateAttributeEntityListFromAnswer(AnswerEntity answer, String estateId, String propertyId) {
+        Map<String, Object> answerMap = null;
+        List<EstateAttributeEntity> estateAttributeEntityList = new ArrayList<>();
+        
+        try {
+        	
+        	answerMap = objectMapper.readValue(answer.getAnswer(), new TypeReference<Map<String, Object>>() {});
+            
+            for (Map.Entry<String, Object> entry : answerMap.entrySet()) {
+                estateAttributeEntityList.add(new EstateAttributeEntity(estateId, propertyId, (String) entry.getKey(), objectMapper.writeValueAsString(entry.getValue())));
+            }
+            
+            this.saveEstateAttributeList(estateAttributeEntityList);
+            
+        } catch (IOException e) {
+            log.error("IO exception occured while converting answer to Map " + answer.getAnswer(), e);
+        }
+        
+        return estateAttributeEntityList;
+    }
 }
