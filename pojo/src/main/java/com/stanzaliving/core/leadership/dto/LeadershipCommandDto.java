40c4921c8caa80b318960b8ef30dde6bf9571072
Ankit Jindal <ankit.jindal@stanzaliving.com>
2019-12-12 13:14:40 +0530

    No args constructor added

--- pojo/src/main/java/com/stanzaliving/core/leadership/dto/LeadershipCommandDto.java
+++ pojo/src/main/java/com/stanzaliving/core/leadership/dto/LeadershipCommandDto.java
@@ -4,15 +4,13 @@ import javax.validation.constraints.NotNull;
 
 import com.stanzaliving.core.leadership.enums.LeadershipCommand;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Builder
 @Getter
 @ToString
-@AllArgsConstructor
+@NoArgsConstructor
 public class LeadershipCommandDto {
 
 	@NotNull(message = "Leadeship Command can not be null")
