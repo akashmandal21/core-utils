a3079d84c5f2b64fb1375ad48bf2351fa8e9c18e
Parul Mishra <parul.mishra@C02ZL33NLVCF.local>
2019-12-12 21:42:19 +0530

    dto for revoke role to user

--- user-client/src/main/java/com/stanzaliving/core/user/client/api/AclUserClientApi.java
+++ user-client/src/main/java/com/stanzaliving/core/user/client/api/AclUserClientApi.java
@@ -3,11 +3,11 @@
  */
 package com.stanzaliving.core.user.client.api;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
+import com.stanzaliving.core.base.common.dto.ResponseDto;
+import com.stanzaliving.core.base.constants.SecurityConstants;
+import com.stanzaliving.core.base.http.StanzaRestClient;
+import com.stanzaliving.core.user.acl.request.dto.AddUserDeptLevelRoleRequestDto;
+import com.stanzaliving.core.user.acl.request.dto.RevokeUserDeptLevelRoleRequestDto;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
@@ -17,10 +17,10 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.stanzaliving.core.base.common.dto.ResponseDto;
-import com.stanzaliving.core.base.constants.SecurityConstants;
-import com.stanzaliving.core.base.http.StanzaRestClient;
-import com.stanzaliving.core.user.acl.request.dto.AddUserDeptLevelRoleRequestDto;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author naveen.kumar
@@ -68,5 +68,40 @@ public class AclUserClientApi {
 		ParameterizedTypeReference<ResponseDto<Void>> returnType = new ParameterizedTypeReference<ResponseDto<Void>>() {
 		};
 		return restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
+	}
+
+	public ResponseDto<Void> revokeUserAclRole(String token, RevokeUserDeptLevelRoleRequestDto reokeUserDeptroleRequestDto) {
+
+		if (StringUtils.isBlank(token)) {
+			throw new IllegalArgumentException("Token missing for adding user roles");
+		}
+
+		if (Objects.isNull(reokeUserDeptroleRequestDto)) {
+			throw new IllegalArgumentException("Request is null for adding role for user");
+		}
+
+		Object postBody = reokeUserDeptroleRequestDto;
+
+		// create path and map variables
+		final Map<String, Object> uriVariables = new HashMap<>();
+
+		String path = UriComponentsBuilder.fromPath("/acl/user/revoke/department/level/levelEntityList").buildAndExpand(uriVariables).toUriString();
+
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+		String tokenCookie = SecurityConstants.TOKEN_HEADER_NAME + "=" + token;
+
+		final HttpHeaders headerParams = new HttpHeaders();
+		headerParams.add(SecurityConstants.COOKIE_HEADER_NAME, tokenCookie);
+
+		final String[] accepts = {
+				"*/*"
+		};
+		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+		ParameterizedTypeReference<ResponseDto<Void>> returnType = new ParameterizedTypeReference<ResponseDto<Void>>() {
+		};
+		return restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
+
 	}
 }
