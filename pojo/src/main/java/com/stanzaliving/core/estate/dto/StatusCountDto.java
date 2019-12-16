96f868491b4178307fd7b309a3c37773b3abccac
Ankit Jindal <ankit.jindal@stanzaliving.com>
2019-12-16 19:55:39 +0530

    Code for finding poercent also StatusCountDto changed

--- pojo/src/main/java/com/stanzaliving/core/estate/dto/StatusCountDto.java
+++ pojo/src/main/java/com/stanzaliving/core/estate/dto/StatusCountDto.java
@@ -1,13 +1,29 @@
 package com.stanzaliving.core.estate.dto;
 
+import com.stanzaliving.core.estate.enums.EstateStatus;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class StatusCountDto {
-    String name;
-    Long count;
+	
+	private EstateStatus estateStatus;
+	
+	private String name;
+    
+	private Long count;
+    
+	private Double percentage;
+    
+    public StatusCountDto(EstateStatus estateStatus,long count) {
+    	
+    	this.estateStatus = estateStatus ;
+    	this.count = count;
+    }
 }
