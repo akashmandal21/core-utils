6698463f4e1cd393f2e75b928aa77849d87fa7b2
RajKumarStanza <raj.singh@stanzaliving.com>
2019-12-16 21:44:57 +0530

    Boq screen item dto

---
+++ pojo/src/main/java/com/stanzaliving/transformations/pojo/BoqScreenItemDto.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.stanzaliving.transformations.pojo;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class BoqScreenItemDto {
+
+	private List<BoqCategoryItemDto> boqCategoryItemList;
+}
---
+++ pojo/src/main/java/com/stanzaliving/transformations/pojo/LineItemDto.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.stanzaliving.transformations.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class LineItemDto {
+
+	private String particular;
+	private String specifications;
+	private String type;
+}
