68317f03829921f821f8a94e66e07185fefac065
Ankit Jindal <ankit.jindal@stanzaliving.com>
2019-12-17 11:48:39 +0530

    Comments service dtos instroduced

---
+++ pojo/src/main/java/com/stanzaliving/core/commentsservice/dto/CommentsDto.java
@@ -0,0 +1,38 @@
+package com.stanzaliving.core.commentsservice.dto;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
+@Setter
+public class CommentsDto {
+
+	private long commentId;
+
+	private Date commentTime;
+
+	private String commentBy;
+
+	@NotNull(message = "Please enter comment text")
+	private String commentText;
+
+	@NotNull(message = "Please enter context id")
+	private String contextId;
+
+	@NotNull(message = "Please enter context type")
+	private String contextType;
+	
+	@NotNull(message = "Please enter service name")
+	private String serviceName;
+	
+	@NotNull(message = "Please enter sub context id")
+	private String subContextId;
+}
