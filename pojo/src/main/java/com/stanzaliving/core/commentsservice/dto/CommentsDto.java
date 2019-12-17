20c1f703639bb8acc136695df43b71b4724caff7
Ankit Jindal <ankit.jindal@stanzaliving.com>
2019-12-17 12:57:29 +0530

    Default constructor added

--- pojo/src/main/java/com/stanzaliving/core/commentsservice/dto/CommentsDto.java
+++ pojo/src/main/java/com/stanzaliving/core/commentsservice/dto/CommentsDto.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -15,6 +16,7 @@ import lombok.ToString;
 @ToString
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CommentsDto {
 
 	private long commentId;
