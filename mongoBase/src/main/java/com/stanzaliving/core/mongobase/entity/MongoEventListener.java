720ad67205700aef6175fd825c1b24c25a3d36fe
Ankit Khandelwal <ankit.khandelwal@C02ZL35DLVCF.local>
2019-12-18 15:32:10 +0530

    Corrected Dependencies

--- mongoBase/src/main/java/com/stanzaliving/core/mongobase/entity/MongoEventListener.java
+++ mongoBase/src/main/java/com/stanzaliving/core/mongobase/entity/MongoEventListener.java
@@ -1,23 +1,24 @@
 package com.stanzaliving.core.mongobase.entity;
 
-import com.stanzaliving.versioning.entity.VersioningEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
 
 import java.util.Date;
 import java.util.UUID;
 
-public class MongoEventListener extends AbstractMongoEventListener<Object> {
+public  class MongoEventListener extends AbstractMongoEventListener<Object> {
+
     @Autowired
     private MongoOperations mongoOperations;
 
     @Override
     public void onBeforeConvert(BeforeConvertEvent<Object> event) {
         Object source = event.getSource();
-        if ((source instanceof VersioningEntity)) {
-            VersioningEntity ver = (VersioningEntity)source;
+        if ((source instanceof AbstractMongoEntity)) {
+            AbstractMongoEntity ver = (AbstractMongoEntity) source;
             if(ver.getUuid() == null)
                 ver.setUuid(UUID.randomUUID().toString());
             if (ver.getCreatedAt() == null) {
