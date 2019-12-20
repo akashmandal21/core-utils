dc87cd4459327ac63fbe08937cced14d2123b13f
RajKumarStanza <raj.singh@stanzaliving.com>
2019-12-20 17:04:34 +0530

    Property create request dto

---
+++ pojo/src/main/java/com/stanzaliving/transformations/pojo/CreatePropertyRequestDto.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.stanzaliving.transformations.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class CreatePropertyRequestDto {
+
+	private String propertyName;
+	private String propertyId;
+	private String estateId;
+	private int bedCount;
+	private int roomCount;
+	private int area;
+	private long cityId;
+	private String cityName;
+	private long micromarketId;
+	private String micromarketName;
+	private double capexEstimate;
+	private double boqEstimate;
+	private String projectStatus;
+	private String projectManager;
+	private String brandName;
+	private String projectManagerAssigned;
+
+}
