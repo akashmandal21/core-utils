646271929d27bb8dcd9805c94aabcb4f2f54f742
RajKumarStanza <raj.singh@stanzaliving.com>
2019-12-26 10:32:48 +0530

    User device token request dto

---
+++ pojo/src/main/java/com/stanzaliving/pushnotification/enums/ServiceName.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.stanzaliving.pushnotification.enums;
+
+/**
+ * @author raj.kumar
+ *
+ */
+public enum ServiceName {
+
+	Estate,
+	Sfr,
+	Leadership
+}
--- pojo/src/main/java/com/stanzaliving/pushnotification/pojo/UserDeviceTokenRequestDto.java
+++ pojo/src/main/java/com/stanzaliving/pushnotification/pojo/UserDeviceTokenRequestDto.java
@@ -3,6 +3,9 @@
  */
 package com.stanzaliving.pushnotification.pojo;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import com.stanzaliving.pushnotification.enums.ServiceName;
 
 import lombok.Getter;
@@ -16,10 +19,11 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class PushNotificationRequestDto {
+public class UserDeviceTokenRequestDto {
 
-	private String estateId;
-	private String notificationTitle;
-	private String notificationMessage;
+	@NotEmpty(message = "Device token cannot be null")
+	private String deviceToken;
+	
+	@NotNull(message = "Service name should not be null")
 	private ServiceName serviceName;
 }
