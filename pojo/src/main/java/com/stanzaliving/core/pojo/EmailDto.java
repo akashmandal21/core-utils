ca44df882d41a30c559bb2f7a4b5c8d3b58223ae
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-01-06 20:13:38 +0530

    reverting removed Personalization

--- pojo/src/main/java/com/stanzaliving/core/pojo/EmailDto.java
+++ pojo/src/main/java/com/stanzaliving/core/pojo/EmailDto.java
@@ -6,6 +6,7 @@ package com.stanzaliving.core.pojo;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.sendgrid.Personalization;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -41,7 +42,8 @@ public class EmailDto {
 	private String templateId;
 	private Map<String, Object> emailData;
 	private Map<String, String> attachment;
-
+	private Personalization personalization;
+	
 	public String[] getTo() {
 		return removeWhiteSpaces(to);
 	}
