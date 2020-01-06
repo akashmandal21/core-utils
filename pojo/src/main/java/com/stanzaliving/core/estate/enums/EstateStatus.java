0460c30293da7c6bc364bc429f85316101dfd849
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-01-06 19:31:21 +0530

    Code for New Estate Status, BdDashboard Status Mapping pending

--- pojo/src/main/java/com/stanzaliving/core/estate/enums/EstateStatus.java
+++ pojo/src/main/java/com/stanzaliving/core/estate/enums/EstateStatus.java
@@ -19,25 +19,38 @@ import lombok.Getter;
 @Getter
 public enum EstateStatus {
 
+	//BD Level Status
 	DRAFT_IN_PROGRESS("Draft In Progress"),
-	SHORTLISTED("Shortlisted"),
-	REJECTED("Rejected"),
-	APPROVED_BY_CITY_HEAD("Shortlisted By City Head"),
-	REJECTED_BY_CITY_HEAD("Rejected By City Head"),
+	
+	//Level 1 Status
 	SENT_FOR_APPROVAL_TO_CITY_HEAD("Sent for approval to City Head"),
-	DOCUMENTS_NEEDED("Documents Needed"),
-	INFORMATION_NEEDED("Information Needed"),
-	SENT_BACK("Sent Back"),
+	APPROVED_BY_CITY_HEAD("Shortlisted By City Head"),
 	SENT_BACK_BY_CITY_HEAD("Sent Back By City Head"),
+	REJECTED("Rejected"),
+	
+	//Level 2 Status
 	SENT_FOR_APPROVAL("Sent for Approval"),
-	SENT_BACK_BY_LEADERSHIP("Sent Back By Leadership"),
-	APPROVED_BY_LEADERSHIP("Shortlisted By Leadership"),
+	SENT_BACK("Sent Back By National Head"),
+	APPROVED_BY_NATIONAL_HEAD("Approved By National Head"),
 	SENT_BACK_BY_TRANSFORMATION("Sent Back By Transformations"),
-	APPROVED_BY_TRNASFORMATION("Approved By Transformations"),
+	APPROVED_BY_TRANSFORMATION("Approved By Transformations"),
+	SHORTLISTED("Shortlisted"),
+	
+	//Level 3 Status
+	SENT_TO_CENTRAL_BD_TEAM("Sent To Central BD Team"),
+	SENT_BACK_BY_CENTRAL_BD_TEAM("Sent Back By Central BD Team"),
+	SENT_TO_LEGAL("Sent to Legal"),
+	SENT_BACK_BY_LEGAL("Sent Back By Legal"),
+	
+	//ATL Status
 	SIGNED_ATL_UPLOADED("Signed ATL Uploaded"),
 	ATL_UPLOAD_PENDING("ATL Upload Pending"),
-	SENT_TO_LEGAL("Sent to Legal"),
-	SENT_BACK_BY_LEGAL("Sent Back By Legal");
+	
+	//Leadership Status
+	SENT_BACK_BY_LEADERSHIP("Sent Back By Leadership"),
+	APPROVED_BY_LEADERSHIP("Shortlisted By Leadership");
+
+	
 
 	public static List<EstateStatus> NON_MODIFIABLE_ESTATE_STATUS;
 	
@@ -49,8 +62,6 @@ public enum EstateStatus {
 		estateStatusMap.put(DRAFT_IN_PROGRESS, EstateStatus.DRAFT_IN_PROGRESS.getName());
 		estateStatusMap.put(SHORTLISTED, EstateStatus.SHORTLISTED.getName());
 		estateStatusMap.put(REJECTED, EstateStatus.REJECTED.getName());
-		estateStatusMap.put(DOCUMENTS_NEEDED, EstateStatus.DOCUMENTS_NEEDED.getName());
-		estateStatusMap.put(INFORMATION_NEEDED, EstateStatus.INFORMATION_NEEDED.getName());
 		estateStatusMap.put(SENT_BACK, EstateStatus.SENT_BACK.getName());
 		estateStatusMap.put(SENT_FOR_APPROVAL, EstateStatus.SENT_FOR_APPROVAL.getName());
 		estateStatusMap.put(SENT_BACK_BY_CITY_HEAD, EstateStatus.SENT_BACK_BY_CITY_HEAD.getName());
