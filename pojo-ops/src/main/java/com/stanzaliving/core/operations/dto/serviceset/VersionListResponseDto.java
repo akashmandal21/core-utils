b87c613235fe326ade81006d5ff0afe4e10ef0ff
Parul Mishra <parul.mishra@C02ZL33NLVCF.local>
2020-01-07 15:41:12 +0530

    add dtos for listing versions

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/serviceset/VersionListResponseDto.java
@@ -0,0 +1,26 @@
+package com.stanzaliving.core.operations.dto.serviceset;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class VersionListResponseDto {
+
+    private String contextId;
+
+    private String version;
+
+    private String createdBy;
+
+    private Date createdAt;
+
+    private String uuid;
+
+}
--- version-client/src/main/java/com/stanzaliving/core/version/client/api/VersionClientApi.java
+++ version-client/src/main/java/com/stanzaliving/core/version/client/api/VersionClientApi.java
@@ -3,7 +3,10 @@ package com.stanzaliving.core.version.client.api;
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.constants.SecurityConstants;
 import com.stanzaliving.core.base.http.StanzaRestClient;
+import com.stanzaliving.core.operations.dto.serviceset.VersionListRequestDto;
+import com.stanzaliving.core.operations.dto.serviceset.VersionListResponseDto;
 import com.stanzaliving.versioning.dto.VersioningRequestDto;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -27,9 +30,8 @@ public class VersionClientApi {
 
     public ResponseDto<Void> insertVersion(String token, VersioningRequestDto versioningRequestDto) {
 
-        //TODO API HAS NO AUTHENTICATION
         /*if (StringUtils.isBlank(token)) {
-            throw new IllegalArgumentException("Token missing for adding user roles");
+            throw new IllegalArgumentException("Token missing for inserting versions");
         }*/
 
         if (Objects.isNull(versioningRequestDto)) {
@@ -60,6 +62,43 @@ public class VersionClientApi {
         return restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
     }
 
+    public ResponseDto<List<VersionListResponseDto>> getAllVersions(String token, VersionListRequestDto versioningRequestDto) {
+
+       /* if (StringUtils.isBlank(token)) {
+            throw new IllegalArgumentException("Token missing for getting all versions ");
+        }*/
+
+        if (Objects.isNull(versioningRequestDto)) {
+            throw new IllegalArgumentException("Request is null for listing data ");
+        }
+
+        Object postBody = null;
+
+        // create path and map variables
+        final Map<String, Object> uriVariables = new HashMap<>();
+
+        uriVariables.put("contextId", versioningRequestDto.getContextId());
+        uriVariables.put("contextName", versioningRequestDto.getContextName());
+        uriVariables.put("contextService", versioningRequestDto.getContextService());
+
+        String path = UriComponentsBuilder.fromPath("/documentversioning/get/all/{contextId}/{contextName}/{contextService}").buildAndExpand(uriVariables).toUriString();
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+        String tokenCookie = SecurityConstants.TOKEN_HEADER_NAME + "=" + token;
+
+        final HttpHeaders headerParams = new HttpHeaders();
+        headerParams.add(SecurityConstants.COOKIE_HEADER_NAME, tokenCookie);
+
+        final String[] accepts = {
+                "*/*"
+        };
+        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+        ParameterizedTypeReference<ResponseDto<List<VersionListResponseDto>>> returnType = new ParameterizedTypeReference<ResponseDto<List<VersionListResponseDto>>>() {
+        };
+        return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
+    }
 
 
 }
