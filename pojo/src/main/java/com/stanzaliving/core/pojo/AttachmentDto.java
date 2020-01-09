d1ab626c730601265513159b4d2b11cf320a0cb2
RajKumarStanza <raj.singh@stanzaliving.com>
2020-01-09 17:27:57 +0530

    Atl and attachment dto builder

--- pojo/src/main/java/com/stanzaliving/core/pojo/AttachmentDto.java
+++ pojo/src/main/java/com/stanzaliving/core/pojo/AttachmentDto.java
@@ -3,6 +3,7 @@
  */
 package com.stanzaliving.core.pojo;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,6 +15,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Builder
 public class AttachmentDto {
 
 	private String bucket;	
