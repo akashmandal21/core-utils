91a1d6796eaf9a387748002005937411ea71fb35
Debendra Dhinda <debendra.dhinda@stanzaliving.com>
2020-01-15 00:24:52 +0530

    Client added for adding text to existing pdf internal API

---
+++ pojo/src/main/java/com/stanzaliving/documentgenerator/dto/PdfStampingDataDto.java
@@ -0,0 +1,60 @@
+package com.stanzaliving.documentgenerator.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/***
+ * A DTO representation to stamp additional data on an existing PDF.
+ * 
+ * @author debendra.dhinda
+ * @since 14-Jan-2020
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PdfStampingDataDto implements Serializable {
+
+	
+	private static final long serialVersionUID = 189457L;
+
+	/**
+	 * The x-coordinate of the stamping text
+	 */
+	private float xCoordinate;
+
+	/**
+	 * The y-coordinate of the stamping text
+	 */
+	private float yCoordinate;
+
+	/**
+	 * The text to be append to the (x,y) coordinate
+	 */
+	@NotBlank(message = "The stamping text can't be empty.")
+	private String stampingText;
+
+	/**
+	 * The page number(0-based index) to which the text will be append.
+	 * <p>The page number starts from 1 and so on. If the specified page number doesn't exists,
+	 * will throw an error message.</p>
+	 */
+	private Integer pageNumber;
+
+	/**
+	 * Font size of the stamping text.
+	 * Default to 10.
+	 * */
+	@Default
+	private float stampingTextFontSize = 10f;
+}
