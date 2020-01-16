16e2c38a4297c740e8b0f681a6d63d95f468d449
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-01-16 13:07:14 +0530

    adding lombok annotations

--- pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/request/report/food/FoodReportRequestDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/request/report/food/FoodReportRequestDto.java
@@ -1,9 +1,17 @@
 package com.stanzaliving.core.operations.dto.request.report.food;
 
 import com.stanzaliving.core.operations.dto.request.report.ReportRequestDto;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
 public class FoodReportRequestDto extends ReportRequestDto {
 
     List<String> mealType;
