559e88ef94a00a3d935afc782563165d1b14e459
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-01-21 19:36:00 +0530

    first graph done

--- pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/report/ValueFormatPoint.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/report/ValueFormatPoint.java
@@ -3,18 +3,13 @@ package com.stanzaliving.core.operations.dto.report;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import java.util.List;
-
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-public class LineDto {
-    String key;
-    String value;
-    String accessLevel;
-    List<Object> xAxis;
-    List<Object> yAxis;
+public class ValueFormatPoint {
+    Object value;
+    String format;
 }
