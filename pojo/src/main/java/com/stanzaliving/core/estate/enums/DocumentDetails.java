5bb71258929bbfe8b0276c34fe40a5097d91d110
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-01-24 14:32:29 +0530

    More documents added

--- pojo/src/main/java/com/stanzaliving/core/estate/enums/DocumentDetails.java
+++ pojo/src/main/java/com/stanzaliving/core/estate/enums/DocumentDetails.java
@@ -7,12 +7,18 @@ import lombok.Getter;
 @Getter
 public enum DocumentDetails {
 
-	UTILITYBILLS("utilityBills","Asset list with exact count retained asset"),
+	assetListWithExactCountRetained("assetListWithExactCountRetained","Asset list with exact count retained asset"),
 	FIRESYSTEM("fireSystem","Fire System"),
 	FIRENOC("fireNOC", "Fire NOC"),
 	WARRANTYCARDFORWHITEGOODS("warrantyCardForWhiteGoods", "Warranty Card For White Goods(RO,AC,refrigerator,washing machine etc.)"),
 	ELECTRICALLOADSANCTIONLETTER("electricalLoadSanctionLetter","Electrical load sanction letter considering commercial usage"),
-	POLLUTIONCONTROLBOARDNOC("pollutionControlBoardNOC","DG - Pollution control board NOC/ Electrical inspector approval if applicable");
+	POLLUTIONCONTROLBOARDNOC("pollutionControlBoardNOC","DG - Pollution control board NOC/ Electrical inspector approval if applicable"),
+	LIFTLICENSEANDRENEWAL("liftLicenseAndRenewal","Lift license and renewal copy"),
+	lastSixMonthsElectricityBills("lastSixMonthsElectricityBills","Last six month's electricity bills"),
+	lastSixMonthsWaterBills("lastSixMonthsWaterBills","Last six months’ water bills"),
+	utilityBills("utilityBills","All previous months’ utility bills due clearance confirmation"),
+	warrantyCardForWaterAccessories("warrantyCardForWaterAccessories","Warranty Card for Heat pumps, geysers, water pumps")
+	;
 	
 	private String attributeName;
 	
