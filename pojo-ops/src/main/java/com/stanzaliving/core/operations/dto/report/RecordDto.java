5773f8a3808f7dfbd3621e227afebdc37b8b4aef
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-01-25 23:36:36 +0530

    sorting records

--- pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/report/RecordDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/report/RecordDto.java
@@ -1,11 +1,14 @@
 package com.stanzaliving.core.operations.dto.report;
 
+import com.stanzaliving.core.base.enums.AccessLevel;
+import com.stanzaliving.core.operations.constants.FoodReportConstants;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.apache.commons.lang3.StringUtils;
 
 @Getter
 @Setter
@@ -13,7 +16,7 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-public class RecordDto {
+public class RecordDto implements Comparable {
 
 	private String cityUuid;
 	private String cityName;
@@ -32,4 +35,33 @@ public class RecordDto {
 		this.accessLevel = recordDto.getAccessLevel();
 		this.daysConsidered = recordDto.getDaysConsidered();
 	}
+
+/**
+	 * @return a negative integer, zero, or a positive integer as this object
+	 * is less than, equal to, or greater than the specified object.
+	 */
+	@Override
+	public int compareTo(Object o) {
+		RecordDto recordDto = (RecordDto) o;
+		if (StringUtils.isNotEmpty(recordDto.getAccessLevel()) && FoodReportConstants.ALL_CITIES.equals(recordDto.getAccessLevel())) {
+			return 1;
+		}
+		if (StringUtils.isNotEmpty(this.getAccessLevel()) && FoodReportConstants.ALL_CITIES.equals(this.getAccessLevel())) {
+			return -1;
+		}
+
+		if (StringUtils.isNotEmpty(this.getAccessLevel()) && StringUtils.isNotEmpty(recordDto.getAccessLevel())) {
+			AccessLevel level = AccessLevel.valueOf(accessLevel);
+			switch (level) {
+				case CITY:
+					return this.getCityName().compareToIgnoreCase(recordDto.getCityName());
+				case MICROMARKET:
+					return this.getMicromarketName().compareToIgnoreCase(recordDto.getMicromarketName());
+				case RESIDENCE:
+					return this.getResidenceName().compareToIgnoreCase(recordDto.getResidenceName());
+			}
+		}
+		return 0;
+	}
+
 }
