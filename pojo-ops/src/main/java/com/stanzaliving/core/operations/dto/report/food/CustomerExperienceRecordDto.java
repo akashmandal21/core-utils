fbffc3224a6ce62bfed57b604e2462acdfdcc841
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-01-29 17:29:44 +0530

    using parallel stream

--- pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/report/food/CustomerExperienceRecordDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/report/food/CustomerExperienceRecordDto.java
@@ -27,18 +27,18 @@ public class CustomerExperienceRecordDto extends RecordDto {
     int mealOrdered;
     int numStudentsFeedback;
 
-//    BiConsumer<CustomerExperienceRecordDto, CustomerExperienceRecordDto> aggregatedRowForAllCity = (o1, o2) -> {
-//        o1.setSatisfied(o1.getSatisfied() + o2.getSatisfied());
-//        o1.setUnsatisfied(o1.getUnsatisfied() + o2.getUnsatisfied());
-//        o1.setShortageCount(o1.getShortageCount() + o2.getShortageCount());
-//        o1.setTotalCount(o1.getTotalCount() + o2.getTotalCount());
-//        o1.setForeignParticle(o1.getForeignParticle() + o2.getForeignParticle());
-//        o1.setRcAvgQualityRating(o1.getRcAvgQualityRating() + o2.getRcAvgQualityRating());
-//        o1.setSocialMediaComplaints(o1.getSocialMediaComplaints() + o2.getSocialMediaComplaints());
-//        o1.setOnTimeDelivery(o1.getOnTimeDelivery() + o2.getOnTimeDelivery());
-//        o1.setQuantityAdherence(o1.getQuantityAdherence() + o2.getQuantityAdherence());
-//        o1.setMenuAdherence(o1.getMenuAdherence() + o2.getMenuAdherence());
-//        o1.setMealOrdered(o1.getMealOrdered() + o2.getMealOrdered());
-//        o1.setNumStudentsFeedback(o1.getNumStudentsFeedback() + o2.getNumStudentsFeedback());
-//    };
+    BiConsumer<CustomerExperienceRecordDto, CustomerExperienceRecordDto> aggregatedRowForAllCity = (o1, o2) -> {
+        o1.setSatisfied(o1.getSatisfied() + o2.getSatisfied());
+        o1.setUnsatisfied(o1.getUnsatisfied() + o2.getUnsatisfied());
+        o1.setShortageCount(o1.getShortageCount() + o2.getShortageCount());
+        o1.setTotalCount(o1.getTotalCount() + o2.getTotalCount());
+        o1.setForeignParticle(o1.getForeignParticle() + o2.getForeignParticle());
+        o1.setRcAvgQualityRating(o1.getRcAvgQualityRating() + o2.getRcAvgQualityRating());
+        o1.setSocialMediaComplaints(o1.getSocialMediaComplaints() + o2.getSocialMediaComplaints());
+        o1.setOnTimeDelivery(o1.getOnTimeDelivery() + o2.getOnTimeDelivery());
+        o1.setQuantityAdherence(o1.getQuantityAdherence() + o2.getQuantityAdherence());
+        o1.setMenuAdherence(o1.getMenuAdherence() + o2.getMenuAdherence());
+        o1.setMealOrdered(o1.getMealOrdered() + o2.getMealOrdered());
+        o1.setNumStudentsFeedback(o1.getNumStudentsFeedback() + o2.getNumStudentsFeedback());
+    };
 }
--- pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/report/food/graph/ProcessAdherenceGraphRecordDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/report/food/graph/ProcessAdherenceGraphRecordDto.java
@@ -4,6 +4,14 @@ import com.stanzaliving.core.operations.dto.report.GraphRecordDto;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
 @Getter
 @Setter
 @ToString
@@ -12,11 +20,54 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 public class ProcessAdherenceGraphRecordDto extends GraphRecordDto {
 
-    Integer totalCount;
-    Integer menuAdherence;
-    Integer quantityAdherence;
-    Integer onTimeDelivery;
-    Integer orderedOnTime;
-    Integer menuCreated;
+    Integer totalCount = 0;
+    Integer menuAdherence = 0;
+    Integer quantityAdherence = 0;
+    Integer onTimeDelivery = 0;
+    Integer orderedOnTime = 0;
+    Integer menuCreated = 0;
 
+
+    public static BiConsumer<ProcessAdherenceGraphRecordDto, ProcessAdherenceGraphRecordDto> aggregatedRow = (o1, o2) -> {
+        o1.setTotalCount(o1.getTotalCount() + o2.getTotalCount());
+        o1.setMenuAdherence(o1.getMenuAdherence() + o2.getMenuAdherence());
+    };
+
+    public static Supplier<ProcessAdherenceGraphRecordDto> supplier = () -> ProcessAdherenceGraphRecordDto.builder().menuAdherence(0).totalCount(0).build();
+
+
+    public static Collector<ProcessAdherenceGraphRecordDto, ProcessAdherenceGraphRecordDto, ProcessAdherenceGraphRecordDto> aggregatedCollector =
+            new Collector<ProcessAdherenceGraphRecordDto, ProcessAdherenceGraphRecordDto, ProcessAdherenceGraphRecordDto>() {
+                @Override
+                public Supplier<ProcessAdherenceGraphRecordDto> supplier() {
+                    return ProcessAdherenceGraphRecordDto.supplier;
+                }
+
+                @Override
+                public BiConsumer<ProcessAdherenceGraphRecordDto, ProcessAdherenceGraphRecordDto> accumulator() {
+                    return (o1, o2) -> {
+                        o1.setTotalCount(o1.getTotalCount() + o2.getTotalCount());
+                        o1.setMenuAdherence(o1.getMenuAdherence() + o2.getMenuAdherence());
+                    };
+                }
+
+                @Override
+                public BinaryOperator<ProcessAdherenceGraphRecordDto> combiner() {
+                    return (o1, o2) -> {
+                        o1.setTotalCount(o1.getTotalCount() + o2.getTotalCount());
+                        o1.setMenuAdherence(o1.getMenuAdherence() + o2.getMenuAdherence());
+                        return o1;
+                    };
+                }
+
+                @Override
+                public Function<ProcessAdherenceGraphRecordDto, ProcessAdherenceGraphRecordDto> finisher() {
+                    return a -> a;
+                }
+
+                @Override
+                public Set<Characteristics> characteristics() {
+                    return Collections.emptySet();
+                }
+            };
 }
