dea3585e0d87a8b4d9b071c43e479374dd5d92cf
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-01-31 20:32:32 +0530

    Color code changed for leadership l1 approval

--- pojo/src/main/java/com/stanzaliving/core/leadership/enums/PricingStepStatus.java
+++ pojo/src/main/java/com/stanzaliving/core/leadership/enums/PricingStepStatus.java
@@ -22,7 +22,7 @@ public enum PricingStepStatus{
 					EstateStatus.REJECTED,EstateStatus.SENT_BACK_L1_BY_NATIONAL_HEAD),
 			"#bfbfbf"),
 	PENDING_RM_APPROVAL("Pending RM Approval", Arrays.asList(EstateStatus.SENT_FOR_APPROVAL_TO_CITY_HEAD), "#4183d7"),
-	PENDING_L1_APPROVAL("Pending L1 Andy Approval", Arrays.asList(EstateStatus.SENT_FOR_L1_APPROVAL_TO_LEADERSHIP), "#ffff7e"),
+	PENDING_L1_APPROVAL("Pending L1 Andy Approval", Arrays.asList(EstateStatus.SENT_FOR_L1_APPROVAL_TO_LEADERSHIP), "#806600"),
 	L1_APPROVED("Pending L2 Details", Arrays.asList(EstateStatus.APPROVED_L1_BY_LEADERSHIP,EstateStatus.SENT_BACK_BY_TRANSFORMATION,EstateStatus.SENT_BACK), "#6bb9f0"),
 	PENDING_NATIONAL_HEAD_APPROVAL("Pending BD Head Approval",
 			Arrays.asList(EstateStatus.SENT_FOR_APPROVAL, EstateStatus.APPROVED_BY_TRANSFORMATION,
