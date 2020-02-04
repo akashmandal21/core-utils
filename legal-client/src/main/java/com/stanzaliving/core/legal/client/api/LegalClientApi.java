c179c77cf7e7022b9c46d69e59046409ece9512d
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-02-04 07:46:05 +0530

    added sow pdf generation in legal client

--- legal-client/src/main/java/com/stanzaliving/core/legal/client/api/LegalClientApi.java
+++ legal-client/src/main/java/com/stanzaliving/core/legal/client/api/LegalClientApi.java
@@ -54,7 +54,39 @@ public class LegalClientApi {
         return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
 
     }
-    
+
+    public ResponseDto<String> getSOWATL(String estateUuid,String propertyUuid) {
+
+        if (Objects.isNull(estateUuid) || Objects.isNull(propertyUuid)) {
+            return null;
+        }
+
+        Object postBody = null;
+
+        // create path and map variables
+        final Map<String, Object> uriVariables = new HashMap<>();
+
+        uriVariables.put("estateUuid", estateUuid);
+        uriVariables.put("propertyUuid",propertyUuid);
+
+        String path = UriComponentsBuilder.fromPath("/internal/atlDetails/{estateUuid}/{propertyUuid}/getSOWDetails").buildAndExpand(uriVariables).toUriString();
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+
+        final HttpHeaders headerParams = new HttpHeaders();
+
+        final String[] accepts = {
+                "*/*"
+        };
+        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+        ParameterizedTypeReference<ResponseDto<String>> returnType = new ParameterizedTypeReference<ResponseDto<String>>() {
+        };
+        return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
+
+    }
+
     public ResponseDto<Boolean> checkAllDealTermApproved(String estateUuid) {
 
         if (Objects.isNull(estateUuid)) {
