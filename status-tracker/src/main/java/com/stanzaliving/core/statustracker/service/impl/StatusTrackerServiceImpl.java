c0679c52b5d36b6aeac66ca1aa96a5cc2945ba03
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-02-05 17:39:48 +0530

    Consumer code done

--- status-tracker/src/main/java/com/stanzaliving/core/statustracker/service/impl/StatusTrackerServiceImpl.java
+++ status-tracker/src/main/java/com/stanzaliving/core/statustracker/service/impl/StatusTrackerServiceImpl.java
@@ -51,6 +51,7 @@ public class StatusTrackerServiceImpl implements StatusTrackerService {
 		try {
 			statusTrackerEntity = statusTrackerDbService.save(statusTrackerEntity);	
 		}catch(Exception e) {
+			e.printStackTrace();
 			log.error("Got error while saving status");
 			
 			return false;
