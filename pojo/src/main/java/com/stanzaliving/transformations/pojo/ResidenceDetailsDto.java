a48a9eb143ec1cf6a8655ba590b47723377fc855
Nipun <nipunaggarwal16@gmail.com>
2020-02-06 11:57:29 +0530

    ResidenceDetailsDto.java & ResidenceMetadataDto.java added

---
+++ pojo/src/main/java/com/stanzaliving/transformations/pojo/ResidenceDetailsDto.java
@@ -0,0 +1,24 @@
+/**
+ * @author nipunaggarwal
+ *
+ */
+package com.stanzaliving.transformations.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author nipunaggarwal
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class ResidenceDetailsDto {
+
+	private ResidenceUIDto residenceUIDto;
+
+	private String address;
+
+}
---
+++ pojo/src/main/java/com/stanzaliving/transformations/pojo/ResidenceMetadataDto.java
@@ -0,0 +1,38 @@
+/**
+ * @author nipunaggarwal
+ *
+ */
+package com.stanzaliving.transformations.pojo;
+
+import com.stanzaliving.transformations.pojo.CityMetadataDto.CityMetadataDtoBuilder;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author nipunaggarwal
+ *
+ */
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResidenceMetadataDto {
+	
+	private long id;
+
+	private String uuid;
+	
+	private String residenceName;
+	
+	private String residenceType;
+	
+	private String residenceBrand;
+
+	private Long microMarketId;
+	
+}
