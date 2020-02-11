7a2ddd2d390b13dd8cbb94a46f06e90132e2ec15
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-02-11 17:38:54 +0530

    adding smr in summary

--- pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/response/report/food/summary/CostAndExperienceRecordResponseDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/response/report/food/summary/CostAndExperienceRecordResponseDto.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Map;
 
 import com.stanzaliving.core.base.enums.AccessLevel;
+import com.stanzaliving.core.base.utils.DateUtil;
+import com.stanzaliving.core.operations.constants.FoodReportConstants;
 import com.stanzaliving.core.operations.dto.report.RecordDto;
 import com.stanzaliving.core.operations.dto.report.food.FoodRatingBuckets;
 import com.stanzaliving.core.operations.dto.report.food.UserFoodRatingDto;
 import com.stanzaliving.core.operations.dto.report.food.summary.CostAndExperienceRecordDto;
 import com.stanzaliving.core.operations.dto.report.food.summary.DateLevelNumbersDto;
 import com.stanzaliving.core.operations.dto.report.food.summary.SummaryRecordDto;
+import com.stanzaliving.core.operations.dto.request.report.food.FoodReportRequestDto;
 import com.stanzaliving.core.operations.dto.response.report.FeElementDto;
 import com.stanzaliving.core.operations.enums.FeElementType;
 import com.stanzaliving.core.operations.utils.FoodReportUtil;
@@ -49,8 +52,10 @@ public class CostAndExperienceRecordResponseDto extends RecordDto {
 
 	private FeElementDto costPerMIR;
 
+	private FeElementDto smr;
+
 	public CostAndExperienceRecordResponseDto(
-			AccessLevel accessLevel, SummaryRecordDto summaryRecordDto, Map<String, DateLevelNumbersDto> dateLevelFieldsMap, Map<String, List<UserFoodRatingDto>> foodRatingMap) {
+			AccessLevel accessLevel, SummaryRecordDto summaryRecordDto, Map<String, DateLevelNumbersDto> dateLevelFieldsMap, Map<String, List<UserFoodRatingDto>> foodRatingMap, FoodReportRequestDto reportRequestDto) {
 
 		super(summaryRecordDto);
 
@@ -59,6 +64,11 @@ public class CostAndExperienceRecordResponseDto extends RecordDto {
 		Integer mir = FoodReportUtil.getMIRCount(accessLevel, summaryRecordDto, dateLevelFieldsMap);
 
 		FoodRatingBuckets ratingBuckets = FoodReportUtil.getFoodRatingBuckets(accessLevel, summaryRecordDto, foodRatingMap);
+
+		double activeMeals = FoodReportUtil.getActiveMealsCount(accessLevel, summaryRecordDto, dateLevelFieldsMap);
+		double residences = FoodReportUtil.getResidenceCount(accessLevel, summaryRecordDto, dateLevelFieldsMap);
+		int daysConsidered = DateUtil.getCountOfDates(reportRequestDto.getStartDate(), reportRequestDto.getEndDate());
+		int totalMeals = (int) ((activeMeals * daysConsidered * mir * FoodReportConstants.MAX_EXPECTED_PRESENT_PERCENT) / residences);
 
 		this.movedInResidence = new FeElementDto(mir, FeElementType.INTEGER);
 
@@ -73,6 +83,8 @@ public class CostAndExperienceRecordResponseDto extends RecordDto {
 		this.costUtilization = new FeElementDto(costAndExperienceRecordDto.getTotalMealCost(), summaryRecordDto.getCostEfficiency().getExpectedMealCost());
 
 		this.costPerMIR = new FeElementDto(costAndExperienceRecordDto.getTotalMealCost(), mir, false, FeElementType.CURRENCY_INTEGER);
+
+		this.smr = new FeElementDto(costAndExperienceRecordDto.getTotalStudentFeedback(), totalMeals, FeElementType.PERCENT_DOUBLE);
 	}
 
 }
