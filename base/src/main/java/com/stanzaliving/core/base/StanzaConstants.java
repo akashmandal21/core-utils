6a1ca6c208cc23249ce33663119ac594911080c3
Amit Kumar Gupta <amit.gupta@FVFXXEUFJ1WK.local>
2020-02-13 19:53:49 +0530

    log changes done

--- base/src/main/java/com/stanzaliving/core/base/StanzaConstants.java
+++ base/src/main/java/com/stanzaliving/core/base/StanzaConstants.java
@@ -28,4 +28,6 @@ public class StanzaConstants {
 	public static final String ORGANIZATION_NAME = "Stanza Living";
 	public static final String ORGANIZATION_DOMAIN = "https://www.stanzaliving.com";
 
+	public static final String GUID = "guid";
+	public static final String LUID = "luid";
 }
