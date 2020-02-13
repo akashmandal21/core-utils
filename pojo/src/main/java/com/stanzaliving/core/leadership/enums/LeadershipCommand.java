05995f68b5041129f42ac705121a505d0fa32c38
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-02-13 13:50:38 +0530

    Sandeep Barnches Done

--- pojo/src/main/java/com/stanzaliving/core/leadership/enums/LeadershipCommand.java
+++ pojo/src/main/java/com/stanzaliving/core/leadership/enums/LeadershipCommand.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum LeadershipCommand {
 
-	APPROVE("Approved"),REJECT("Rejected"),SENDBACK("Sent Back"),APPROVE_L1("L1 Approved"),SENDBACK_L1("L1 Sent Back");
+	APPROVE("Approved"),REJECT("Rejected"),SENDBACK("Sent Back"),APPROVE_L1("L1 Approved"),SENDBACK_L1("L1 Sent Back"),APPROVED_BY_SANDEEP("Sow Approved By Sandeep"),SENT_BACK_BY_SANDEEP("Sent Back By Sandeep");
 	
 	private String command;
 }
