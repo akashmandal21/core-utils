a1b0ac18bfac73379349cca1bff6086bede20dbe
RajKumarStanza <raj.singh@stanzaliving.com>
2020-02-24 13:53:32 +0530

    Added no args and all args constructor

--- pojo/src/main/java/com/stanzaliving/locator/dto/LocatorEstateViewDto.java
+++ pojo/src/main/java/com/stanzaliving/locator/dto/LocatorEstateViewDto.java
@@ -3,10 +3,14 @@ package com.stanzaliving.locator.dto;
 import java.util.List;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
+@NoArgsConstructor
 public class LocatorEstateViewDto {
 
 	private List<InstitutionDto> institutions;
