994ee9290bc631edc665526b85335a4a0a3e132c
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-03-05 20:00:54 +0530

    updated enums

--- pojo/src/main/java/com/stanzaliving/item_master/enums/DimensionUnits.java
+++ pojo/src/main/java/com/stanzaliving/item_master/enums/DimensionUnits.java
@@ -1,7 +1,11 @@
 package com.stanzaliving.item_master.enums;
 
+import com.stanzaliving.transformations.enums.AreaOfUse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @AllArgsConstructor
 @Getter
@@ -14,4 +18,15 @@ public enum DimensionUnits {
     MILLIMETER("mm");
 
     private String dimUnit;
+
+    public static Map<String, DimensionUnits> dimensionUnitsMap = new HashMap<>();
+
+    static {
+
+        dimensionUnitsMap.put("inches", DimensionUnits.INCH);
+        dimensionUnitsMap.put("mts", DimensionUnits.METER);
+        dimensionUnitsMap.put("ft", DimensionUnits.FOOT);
+        dimensionUnitsMap.put("cm", DimensionUnits.CENTI);
+        dimensionUnitsMap.put("mm", DimensionUnits.MILLIMETER);
+    }
 }
