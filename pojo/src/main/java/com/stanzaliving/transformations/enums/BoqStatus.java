c0aa80e1cebb4778e0811fbf079f1f5a9eaaae4e
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-03-06 11:37:10 +0530

    Commited new status

--- pojo/src/main/java/com/stanzaliving/transformations/enums/BoqStatus.java
+++ pojo/src/main/java/com/stanzaliving/transformations/enums/BoqStatus.java
@@ -16,7 +16,9 @@ public enum BoqStatus {
 
 	NOT_STARTED("Not Started", "#e5e3e3",null,null),
 	DRAFT_IN_PROGRESS("Under Draft", "#e5e3e3",UserType.DESIGN_COORDINATOR,Department.DESIGN),
-	SEND_FOR_APPROVAL("Sent For Approval", "#FFC300",UserType.MANAGER,Department.TRANSFORMATIONS),
+	SEND_FOR_APPROVAL("Sent For Approval to Zonal Head", "#FFC300",UserType.ZONAL_HEAD,Department.DESIGN),
+	APPROVED_BY_ZH("Sent For Approval to Design Head", "#FFC300",UserType.NATIONAL_HEAD,Department.DESIGN),
+	APPROVED_BY_NH("Sent For Approval to Design Head", "#FFC300",UserType.MANAGER,Department.TRANSFORMATIONS),
 	SENT_BACK("Sent Back", "#d41111",UserType.DESIGN_COORDINATOR,Department.DESIGN),
 	APPROVED("Approved", "#77baa6",null,null);
 	
