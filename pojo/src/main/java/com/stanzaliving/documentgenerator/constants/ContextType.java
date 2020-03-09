9ddfe9179c1ad07bb52ecc17cb7f31f631017ded
Debendra Dhinda <debendra.dhinda@stanzaliving.com>
2020-03-09 12:37:45 +0530

    Context name for HOTODoc generation

--- pojo/src/main/java/com/stanzaliving/documentgenerator/constants/ContextType.java
+++ pojo/src/main/java/com/stanzaliving/documentgenerator/constants/ContextType.java
@@ -2,4 +2,6 @@ package com.stanzaliving.documentgenerator.constants;
 
 public class ContextType {
     public static final String LEGAL_ATL_DOC_GENERATION = "legalAtlDocGeneration";
+    
+    public static final String HOTO_DOC_GENERATION = "hotoDocGeneration";
 }
