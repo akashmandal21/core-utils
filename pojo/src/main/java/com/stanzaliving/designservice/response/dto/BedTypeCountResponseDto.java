b5ae6f5994798068806305a9bdfdc9290b2b7098
RajKumarStanza <raj.singh@stanzaliving.com>
2020-03-17 16:25:34 +0530

    Leadership bed type count response dto

---
+++ pojo/src/main/java/com/stanzaliving/designservice/response/dto/BedTypeCountResponseDto.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.stanzaliving.designservice.response.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class BedTypeCountResponseDto {
+
+	private int primaCount;
+	private int magnaCount;
+	private int summaCount;
+}
---
+++ pojo/src/main/java/com/stanzaliving/designservice/response/dto/LeadershipBedTypeCountResponseDto.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.stanzaliving.designservice.response.dto;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class LeadershipBedTypeCountResponseDto {
+
+	private BedTypeCountResponseDto totalBeds;
+	private BedTypeCountResponseDto totalRooms;
+	private BedTypeCountResponseDto areaBed;
+	Map<Integer, BedTypeCountResponseDto> bedTypeMap;
+}
