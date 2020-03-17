d12140b6fe2696844e78f1c4dcf0fa7a6f9bba65
RajKumarStanza <raj.singh@stanzaliving.com>
2020-03-17 08:14:57 +0530

    Leadership bed and floor type mapping

---
+++ pojo/src/main/java/com/stanzaliving/designservice/response/dto/BedTypeImagePathDto.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.stanzaliving.designservice.response.dto;
+
+import com.stanzaliving.designservice.enums.BedType;
+
+import lombok.Getter;
+import lombok.ToString;
+import lombok.Setter;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class BedTypeImagePathDto {
+
+	private BedType bedType;
+	private String filePath;
+}
---
+++ pojo/src/main/java/com/stanzaliving/designservice/response/dto/LeadershipFloorBedTypeDto.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.stanzaliving.designservice.response.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class LeadershipFloorBedTypeDto {
+
+	private List<Integer> floors;
+	private List<BedTypeImagePathDto> bedTypeImagePathList;
+}
