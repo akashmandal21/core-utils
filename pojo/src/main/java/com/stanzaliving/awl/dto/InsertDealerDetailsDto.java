520194e4b1da7f963a50e7cc37133a42dbbc79d3
Debendra Dhinda <debendra.dhinda@stanzaliving.com>
2020-03-20 11:00:25 +0530

    AWL integration initaited

---
+++ pojo/src/main/java/com/stanzaliving/awl/dto/InsertDealerDetailsDto.java
@@ -0,0 +1,57 @@
+package com.stanzaliving.awl.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/***
+ * An DTO representation to InsertDealerDetails of AWL 3rd party client.
+ * Check AWL integration
+ * http://test.eprontoglobal.com/Service1.svc/InsertDealerDetails
+ * */
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class InsertDealerDetailsDto implements Serializable {
+
+	private static final long serialVersionUID = 198734L;
+	@NotEmpty(message = "Client code can't be empty.")
+	private String Ccode;
+	@NotEmpty(message = "House name can't be empty.")
+	private String Dname;
+	@NotEmpty(message = "House city can't be empty.")
+	private String Dcity;
+	@NotEmpty(message = "Billing address can't be empty")
+	private String Baddress;
+	@NotEmpty(message = "Delivery address can't be empty.")
+	private String Daddress;
+	@NotEmpty(message = "PINCode can't be empty.")
+	private String Pincode;
+	@NotEmpty(message = "Mobile can't be empty")
+	private String Mob1;
+	@NotEmpty(message = "Mobile can't be empty. Please supply NA if not available")
+	private String mob2;
+	
+	@NotEmpty(message = "Email can't be empty. Please supply NA if not available")
+	private String Email;
+	@NotEmpty(message = "AWL CRM can't be empty. Please supply NA if not available")
+	private String Awlcrm;
+	
+	private int Awlcrmno;
+	private String Remarks;
+	@NotEmpty(message = "DCode can't be empty. Please supply NA if not available")
+	private String Dcode;
+	@NotEmpty(message = "Dealer GSTIN can't be empty. Please supply NA if not available")
+	private String Gstin;
+	@NotEmpty(message = "Dealer state can't be empty. Please supply NA if not available")
+	private String Dstate;
+
+}
