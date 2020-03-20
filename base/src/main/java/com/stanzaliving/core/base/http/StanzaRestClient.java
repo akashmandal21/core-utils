57752fbec174e9fc33bfd4b5917f5a11fdded0ef
Piyush Srivastava <jiitpiyush@gmail.com>
2020-03-20 18:56:18 +0530

    Multipart Request

--- base/src/main/java/com/stanzaliving/core/base/http/StanzaRestClient.java
+++ base/src/main/java/com/stanzaliving/core/base/http/StanzaRestClient.java
@@ -3,6 +3,12 @@
  */
 package com.stanzaliving.core.base.http;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -364,6 +370,48 @@ public class StanzaRestClient {
 			return b.toString();
 		} else {
 			return String.valueOf(param);
+		}
+	}
+
+
+
+	public Object request(String targetUrl, String parameters, String requestType, String contentType){
+		HttpURLConnection connection = null;
+		try {
+			//Create connection
+			URL url = new URL(targetUrl);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod(requestType);
+			connection.setRequestProperty("Content-Type", contentType);
+			connection.setRequestProperty("Content-Language", "en-US");
+
+			connection.setUseCaches(false);
+			connection.setDoOutput(true);
+
+			//Send request
+			DataOutputStream wr = new DataOutputStream(
+					connection.getOutputStream());
+			wr.writeBytes(parameters);
+			wr.close();
+
+			//Get Response
+			InputStream is = connection.getInputStream();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+			StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+			String line;
+			while ((line = rd.readLine()) != null) {
+				response.append(line);
+				response.append('\r');
+			}
+			rd.close();
+			return response.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			if (connection != null) {
+				connection.disconnect();
+			}
 		}
 	}
 
