d565197fbdab176e666ab1a99121fca2f53c4aa2
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-03-21 17:58:18 +0530

    dtos issue

--- pojo/src/main/java/com/stanzaliving/designservice/response/dto/BedTypeCountResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/designservice/response/dto/BedTypeCountResponseDto.java
@@ -16,8 +16,8 @@ import lombok.ToString;
 @ToString
 public class BedTypeCountResponseDto {
 
-	private int magnaBed;
-	private int primaBed;
-	private int summaBed;
+	private int primaCount;
+	private int magnaCount;
+	private int summaCount;
 	private int bed;
 }
