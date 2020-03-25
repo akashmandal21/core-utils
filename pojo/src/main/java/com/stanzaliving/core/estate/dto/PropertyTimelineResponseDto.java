14fee9c7f6a86ed9492269f08557ee705686e375
RajKumarStanza <raj.singh@stanzaliving.com>
2020-03-25 12:38:28 +0530

    Property timeline response dto

---
+++ pojo/src/main/java/com/stanzaliving/core/estate/dto/PropertyTimelineResponseDto.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.stanzaliving.core.estate.dto;
+
+import java.util.List;
+
+import com.stanzaliving.core.commentsservice.dto.CommentsDto;
+import com.stanzaliving.core.estate.enums.EstateStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class PropertyTimelineResponseDto {
+
+	private String estateStatusDesc;
+	private EstateStatus estateStatus;
+	private List<CommentsDto> comments;
+	private String updateBy;
+	
+}
