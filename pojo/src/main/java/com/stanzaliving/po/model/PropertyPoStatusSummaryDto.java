f7b46a7e5fd0c19faede27b069c9618242804103
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-03-28 19:36:42 +0530

    updated po status:

--- pojo/src/main/java/com/stanzaliving/po/model/PropertyPoStatusSummaryDto.java
+++ pojo/src/main/java/com/stanzaliving/po/model/PropertyPoStatusSummaryDto.java
@@ -5,11 +5,13 @@ import com.stanzaliving.po.enums.POSummaryStatus;
 import com.stanzaliving.po.enums.PoType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class PropertyPoStatusSummaryDto {
     PoType poType;
     POSummaryStatus poSummaryStatus;
