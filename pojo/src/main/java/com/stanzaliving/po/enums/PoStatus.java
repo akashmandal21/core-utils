7c0cd407b7aeec3a1fbe65805c22d6d3db8bfe25
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-03-30 08:43:41 +0530

    Work completed added in main flow

--- pojo/src/main/java/com/stanzaliving/po/enums/PoStatus.java
+++ pojo/src/main/java/com/stanzaliving/po/enums/PoStatus.java
@@ -10,6 +10,7 @@ public enum PoStatus {
 	IN_DRAFT("In Draft"),TH_APPROVAL_PENDING("Transformation Head Approval Pending"),TH_APPROVED("Approved By Transformation Head"),
 	SENT_BACK_BY_TH("Sent Back By Transformation Head"),LEADERSHIP_APPROVAL_PENDING("Leadership Approval Pending"),
 	LEADERSHIP_APPROVED("Approved By Leadership"),SENT_BACK_BY_LEADERSHIP("Sent Back By Leadership"),
+	WORK_COMPLETED("Work Completed"),
 	INVOICE_SUBMITTED("Invoice Submitted"), SENT_BACK_BY_FINANCE("Sent Back By Finance");
 	
 	private String status;
