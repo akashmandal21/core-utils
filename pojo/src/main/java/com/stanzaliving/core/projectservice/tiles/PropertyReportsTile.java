e257961080d250137f949fb48986bf85cac85f75
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-03-30 10:24:35 +0530

    added PO Tile

--- pojo/src/main/java/com/stanzaliving/core/projectservice/tiles/PropertyReportsTile.java
+++ pojo/src/main/java/com/stanzaliving/core/projectservice/tiles/PropertyReportsTile.java
@@ -20,8 +20,8 @@ public enum PropertyReportsTile {
     ATL("ATL",true,true,false,null,null,""),
     BED_COUNT_FINALIZATION("Bed Count Finalization",true,false,false,Department.DESIGN,PropertyUpdateEnum.DESIGN_BED_COUNT_FINALIZATION_COMPLETED,"bedcount"),
     GC_SCHEDULE("GC Schedule",true,true,true,Department.GC,PropertyUpdateEnum.PROJECT_SITE_ENGINEER_ASSIGNED,"gc"),
-    HOTO("Hoto",true,true,false,Department.PROJECTS,PropertyUpdateEnum.PROJECTS_HOTO_GENERATED,"hoto");
-
+    HOTO("Hoto",true,true,false,Department.PROJECTS,PropertyUpdateEnum.PROJECTS_HOTO_GENERATED,"hoto"),
+    PO("PO",false,false,true,null,null,"po");
     private String tileText;
     private boolean visibleToDesign;
     private boolean visibleToProjects;
