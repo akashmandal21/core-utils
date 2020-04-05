dd527ea3e1876ec0223e315c27e9de94ce37fa06
Naveen Kumar <naveen.kumar@stanzaliving.com>
2020-04-05 23:51:04 +0530

    Corrected returns

--- base/src/main/java/com/stanzaliving/core/base/utils/FileDownloadUtil.java
+++ base/src/main/java/com/stanzaliving/core/base/utils/FileDownloadUtil.java
@@ -21,7 +21,6 @@ import lombok.extern.log4j.Log4j2;
 public class FileDownloadUtil {
 
 	public byte[] getFileContentAsBytes(String fileUrl) {
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		try {
 
 			URL url = new URL(fileUrl);
@@ -29,15 +28,19 @@ public class FileDownloadUtil {
 			byte[] chunk = new byte[4096];
 			int bytesRead;
 
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			
 			InputStream stream = url.openStream();
 			while ((bytesRead = stream.read(chunk)) > 0) {
 				outputStream.write(chunk, 0, bytesRead);
 			}
 
+			return outputStream.toByteArray();
+
 		} catch (Exception e) {
 			log.error("Exception caught while reading content from url: " + fileUrl, e);
-			return null;
 		}
-		return outputStream.toByteArray();
+
+		return null;
 	}
 }
