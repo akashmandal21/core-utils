136f291791d03445718d69dc6cd2bae1a0786d7f
talk2debendra <debendra.dhinda@stanzaliving.com>
2020-04-13 17:40:05 +0530

    Generate BTS & LOI legal agreement doc type support

--- pojo/src/main/java/com/stanzaliving/documentgenerator/constants/ContextName.java
+++ pojo/src/main/java/com/stanzaliving/documentgenerator/constants/ContextName.java
@@ -13,5 +13,10 @@ public class ContextName {
     public static final String GC_SCHEDULE_CONTEXT_NAME = "gcSchedule";
 
     public static final String PO_DOCUMENT_CONTEXT_NAME = "poDocument";
+    
+    
+    public static final String LEGAL_LOI_DOC = "LOIDoc";
+    
+    public static final String LEGAL_BTS_DOC = "BTSDoc";
 
 }
