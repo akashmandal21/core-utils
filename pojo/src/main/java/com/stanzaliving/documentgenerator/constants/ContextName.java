123ad11d263318c068b4630283d417b9c004e4fe
talk2debendra <debendra.dhinda@stanzaliving.com>
2020-04-15 17:16:29 +0530

    LOI doc partitions and its logic to generate

--- pojo/src/main/java/com/stanzaliving/documentgenerator/constants/ContextName.java
+++ pojo/src/main/java/com/stanzaliving/documentgenerator/constants/ContextName.java
@@ -15,7 +15,10 @@ public class ContextName {
     public static final String PO_DOCUMENT_CONTEXT_NAME = "poDocument";
     
     
-    public static final String LEGAL_LOI_DOC = "LOIDoc";
+    public static final String LEGAL_LOI_DOC = "loiDocument";
+    public static final String LEGAL_LOI_DOC_ANNEXURE_A_B_C = "loiAnnexureA_B_C";
+    public static final String LEGAL_LOI_DOC_ANNEXURE_D_DILIGENCE_CHECKLIST = "loiAnnexureD_DigilinceChecklist";
+    public static final String LEGAL_LOI_DOC_ANNEXURE_E_DECLARATION_UNDERTAKING = "loiAnnexureE_DeclarationUndertaking";
     
     public static final String LEGAL_BTS_DOC = "BTSDoc";
 
