1c8e32a5259e15f9d885c45525ba526b1b03000f
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-04-16 15:16:28 +0530

    Missing fields added

--- pojo/src/main/java/com/stanzaliving/core/leadership/dto/ListingDto.java
+++ pojo/src/main/java/com/stanzaliving/core/leadership/dto/ListingDto.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 
 import com.stanzaliving.core.estate.enums.EstateStatus;
+import com.stanzaliving.core.user.dto.Address;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -21,12 +22,36 @@ import lombok.ToString;
 @ToString
 public class ListingDto {
 
+    private String createdBy;
+
+    private Date updatedAt;
+	
+    private String createdByName;
+
+    private String createdByNumber;
+    
+    private String pocNumber;
+    
+    private String pocName;
+    
 	private String estateUuid;
+	
+	private String estateCode;
 
 	private long estateId;
 	
 	private String estateName;
 
+    private Long cityId;
+
+    private Long micromarketId;
+
+    private Double lat;
+
+    private Double lon;
+
+    private Address address;
+	
 	private String cityName;
 
 	private String microMarketName;
