526f2e7c98f3a347d16f6f8d8b7f24f45e81bf0f
RajKumarStanza <raj.singh@stanzaliving.com>
2020-04-16 16:30:28 +0530

    Attribute datatype changed to integer

--- pojo/src/main/java/com/stanzaliving/designservice/response/dto/PropertyBedCountReponseDto.java
+++ pojo/src/main/java/com/stanzaliving/designservice/response/dto/PropertyBedCountReponseDto.java
@@ -18,7 +18,7 @@ import lombok.ToString;
 @ToString
 public class PropertyBedCountReponseDto {
 
-	private int noOfRooms;
+	private Integer noOfRooms;
 	private int bdBedCount;
 	private int primaBed;
 	private int magnaBed;
