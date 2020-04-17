78249884a1820d4501fd7dd8672ca4f5f5bb0994
Naveen Kumar <naveen.kumar@stanzaliving.com>
2020-04-17 16:19:34 +0530

    Added enum for food holiday and day types

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/enums/FoodDayType.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.stanzaliving.core.food.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author naveen.kumar
+ *
+ * @date 17-Apr-2020
+ *
+ **/
+@Getter
+@AllArgsConstructor
+public enum FoodDayType {
+
+	WEEK_DAY("Week Day"),
+	WEEK_END("Week End"),
+	HOLIDAY("Holiday");
+
+	private String dayName;
+}
