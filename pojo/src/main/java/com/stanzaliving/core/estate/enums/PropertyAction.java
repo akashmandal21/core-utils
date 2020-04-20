1e53de6c12daec657650ef0997ec8db4ddd6f8c0
RajKumarStanza <raj.singh@stanzaliving.com>
2020-04-20 14:21:31 +0530

    Property action enum

---
+++ pojo/src/main/java/com/stanzaliving/core/estate/enums/PropertyAction.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.stanzaliving.core.estate.enums;
+
+import lombok.Getter;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Getter
+public enum PropertyAction {
+
+	CALLBACK_PROPERTY_TO_UNDER_DRAFT("Callback Property to Under Draft"),
+	CALLBACK_PROPERTY_To_L1_APPROVED("Callback Property to L1 Approved"),
+	LOI_REQUEST("LOI Request"),
+	DROP_PROPERTY("Drop Property");
+
+	private String actionName;
+	
+	private PropertyAction(String actionName) {
+		this.actionName = actionName;
+	}
+}
