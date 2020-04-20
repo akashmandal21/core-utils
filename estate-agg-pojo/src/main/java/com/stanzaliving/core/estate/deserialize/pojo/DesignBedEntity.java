e3e73b242619c74fee44d6fdec1901e952fd3892
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-04-20 14:23:45 +0530

    moved deserialize pojos to core

---
+++ estate-agg-pojo/src/main/java/com/stanzaliving/core/estate/deserialize/pojo/DesignBedEntity.java
@@ -0,0 +1,82 @@
+package com.stanzaliving.core.estate.deserialize.pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.stanzaliving.designservice.enums.BedType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DesignBedEntity {
+
+    
+    @JsonProperty("id")
+    protected Long designId;
+
+    
+    @JsonProperty("created_at")
+    
+    protected Date createdAt;
+
+    
+    @JsonProperty("created_by")
+    protected String createdBy;
+
+    
+    @JsonProperty("updated_at")
+    
+    protected Date updatedAt;
+
+    
+    @JsonProperty("updated_by")
+    protected String updatedBy;
+
+    
+    @JsonProperty("status")
+    protected boolean status = true;
+
+    
+    @JsonProperty("estate_uuid")
+    String estateUuid;
+
+    
+    @JsonProperty("uuid")
+    private String designUuid;
+
+
+    
+    @JsonProperty("design_file_uuid")
+    private String designFileUuid;
+
+    
+    @JsonProperty("bed_type")
+    private BedType bedType;
+
+    
+    @JsonProperty("total_bed_count")
+    private Integer totalBedCount;
+
+    
+    @JsonProperty("design_file_path")
+    private String designFilePath;
+
+    
+    @JsonProperty("design_file_name")
+    private String designFileName;
+
+    
+    @JsonProperty("cad_file_path")
+    private String cadFilePath;
+
+    
+    @JsonProperty("cad_file_name")
+    private String cadFileName;
+
+    
+    @JsonProperty("reason")
+    private String reason;
+}
---
+++ estate-agg-pojo/src/main/java/com/stanzaliving/core/estate/deserialize/pojo/EstateStatusTrackerCDCInfo.java
@@ -0,0 +1,47 @@
+package com.stanzaliving.core.estate.deserialize.pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.stanzaliving.core.estate.enums.EstateStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstateStatusTrackerCDCInfo {
+
+    @JsonProperty("id")
+    protected Long id;
+
+    @JsonProperty("created_at")
+    protected Date createdAt;
+
+    @JsonProperty("created_by")
+    protected String createdBy;
+
+    @JsonProperty("updated_at")
+    protected Date updatedAt;
+
+    @JsonProperty("updated_by")
+    protected String updatedBy;
+
+    @JsonProperty("status")
+    protected boolean status = true;
+
+    @JsonProperty("uuid")
+    String statusTrackerUuid;
+
+    @JsonProperty("context_name")
+    private String contextName;
+
+    @JsonProperty("context_uuid")
+    private String estateUuid;
+
+    @JsonProperty("status_name")
+    private EstateStatus estateStatus;
+}
