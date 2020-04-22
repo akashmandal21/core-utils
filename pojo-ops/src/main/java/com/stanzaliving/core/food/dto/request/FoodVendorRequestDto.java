20d7ad79e38188761caa9a883a33a31e59dff114
hanish-kottamasu <hanish.kottamasu@stanzaliving.com>
2020-04-22 13:15:50 +0530

    added dtos for foodvendor and micromarketvendor

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/request/FoodVendorRequestDto.java
@@ -0,0 +1,34 @@
+package com.stanzaliving.core.food.dto.request;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodVendorRequestDto {
+
+	@NotBlank(message = "name Cannot be blank")
+	private String name;
+
+	private String cityId;
+
+	@NotBlank(message = "contactName Cannot be blank")
+	private String contactName;
+
+	@NotBlank(message = "contactNumber Cannot be blank")
+	private String contactNumber;
+
+	@NotBlank(message = "contactEmail Cannot be blank")
+	private String contactEmail;
+
+}
