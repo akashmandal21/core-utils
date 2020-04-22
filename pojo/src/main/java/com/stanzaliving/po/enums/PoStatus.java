d7008461bd60dd78c0b499f61e00d6645dbdf831
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-04-22 16:20:55 +0530

    updated po status

--- pojo/src/main/java/com/stanzaliving/po/enums/PoStatus.java
+++ pojo/src/main/java/com/stanzaliving/po/enums/PoStatus.java
@@ -7,11 +7,16 @@ import lombok.Getter;
 @Getter
 public enum PoStatus {
 
-	IN_DRAFT("In Draft",0),TH_APPROVAL_PENDING("Transformation Head Approval Pending",1),TH_APPROVED("Approved By Transformation Head",2),
-	SENT_BACK_BY_TH("Sent Back By Transformation Head",-1),LEADERSHIP_APPROVAL_PENDING("Leadership Approval Pending",3),
-	LEADERSHIP_APPROVED("Approved By Leadership",4),SENT_BACK_BY_LEADERSHIP("Sent Back By Leadership",-1),
+	IN_DRAFT("In Draft",0),
+	TH_APPROVAL_PENDING("Transformation Head Approval Pending",1),
+	TH_APPROVED("Approved By Transformation Head",2),
+	SENT_BACK_BY_TH("Sent Back By Transformation Head",-1),
+	LEADERSHIP_APPROVAL_PENDING("Leadership Approval Pending",3),
+	LEADERSHIP_APPROVED("Approved By Leadership",4),
+	SENT_BACK_BY_LEADERSHIP("Sent Back By Leadership",-1),
 	WORK_COMPLETED("Work Completed",5),
-	INVOICE_SUBMITTED("Invoice Submitted",6), SENT_BACK_BY_FINANCE("Sent Back By Finance",5);
+	INVOICE_SUBMITTED("Invoice Submitted",6),
+	SENT_BACK_BY_FINANCE("Sent Back By Finance",5);
 
 	private String status;
 	private Integer order;
