293165c7c6b96e58660cad44807ab4ba0e624259
RajKumarStanza <raj.singh@stanzaliving.com>
2020-04-24 16:21:18 +0530

    Property action response dto

---
+++ pojo/src/main/java/com/stanzaliving/core/estate/dto/EstatePropertyActionResponseDto.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.stanzaliving.core.estate.dto;
+
+
+import java.util.Date;
+
+import com.stanzaliving.core.estate.enums.PropertyAction;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class EstatePropertyActionResponseDto {
+
+	private String estateUuid;
+	private String reason;
+	private PropertyAction requestType;
+	private String requestTypeDesc;
+	private Date createdAt;
+}
--- pojo/src/main/java/com/stanzaliving/core/estate/enums/PropertyAction.java
+++ pojo/src/main/java/com/stanzaliving/core/estate/enums/PropertyAction.java
@@ -14,9 +14,9 @@ public enum PropertyAction {
 
 	CALLBACK_PROPERTY_TO_UNDER_DRAFT("Callback Property to Under Draft"),
 	CALLBACK_PROPERTY_TO_L1_APPROVED("Callback Property to L1 Approved"),
-	LOI_REQUEST("LOI Request"),
-	ATL_REQUEST("ATL Request"),
-	DROP_PROPERTY("Drop Property");
+	LOI_REQUEST("LOI Approval Request Raised"),
+	ATL_REQUEST("ATL Approval Request Raised"),
+	DROP_PROPERTY("Drop Property Request Raised");
 
 	private String actionName;
 	
