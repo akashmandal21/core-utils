ef4e5928d4ee6d768e2a8877b2b02de972d0f8e3
RajKumarStanza <raj.singh@stanzaliving.com>
2020-04-26 14:21:36 +0530

    Property action enum text changes

--- pojo/src/main/java/com/stanzaliving/core/estate/enums/PropertyAction.java
+++ pojo/src/main/java/com/stanzaliving/core/estate/enums/PropertyAction.java
@@ -15,8 +15,8 @@ public enum PropertyAction {
 	CALLBACK_PROPERTY_TO_UNDER_DRAFT("Callback Property to Under Draft" , 1, "Request Raised (Property To Under Draft)"),
 	CALLBACK_PROPERTY_TO_L1_APPROVED("Callback Property to L1 Approved", 2, "Request Raised (Property To L1)"),
 	LOI_REQUEST("LOI Request", 3, "Request Raised (LOI)"),
-	ATL_REQUEST("ATL Approval Request Raised", 4, "Request Raised (ATL)"),
-	DROP_PROPERTY("Drop Property Request Raised", 5, "Request Raised (Property Drop)");
+	ATL_REQUEST("ATL Request", 4, "Request Raised (ATL)"),
+	DROP_PROPERTY("Drop Request", 5, "Request Raised (Property Drop)");
 
 	private String actionName;
 	private String requestRaisedDesc;
