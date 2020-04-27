3752b9f3f98232ccaac77b520ca8eb17d8b7a38b
RajKumarStanza <raj.singh@stanzaliving.com>
2020-04-27 12:15:43 +0530

    Dto for approve and decline pending request dto

--- pojo/src/main/java/com/stanzaliving/core/estate/dto/PropertyDashboardActionDto.java
+++ pojo/src/main/java/com/stanzaliving/core/estate/dto/PropertyDashboardActionDto.java
@@ -3,9 +3,7 @@
  */
 package com.stanzaliving.core.estate.dto;
 
-
-import java.util.Date;
-
+import com.stanzaliving.core.estate.enums.EstateStatus;
 import com.stanzaliving.core.estate.enums.PropertyAction;
 
 import lombok.Getter;
@@ -19,12 +17,10 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class EstatePropertyActionResponseDto {
+public class PropertyDashboardActionDto {
 
 	private String uuid;
 	private String estateUuid;
-	private String reason;
-	private PropertyAction requestType;
-	private String requestTypeDesc;
-	private Date createdAt;
+	private PropertyAction requestAction;
+	private EstateStatus estateStatus;
 }
