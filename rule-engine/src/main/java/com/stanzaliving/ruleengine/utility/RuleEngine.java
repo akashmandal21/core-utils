c1dc352dc3572de4ff56c123174710971be54264
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-04-29 13:49:24 +0530

    Rule Conditions refactored

--- rule-engine/src/main/java/com/stanzaliving/ruleengine/utility/RuleEngine.java
+++ rule-engine/src/main/java/com/stanzaliving/ruleengine/utility/RuleEngine.java
@@ -2,13 +2,11 @@ package com.stanzaliving.ruleengine.utility;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stanzaliving.core.base.enums.RuleOperatorEnum;
-import com.stanzaliving.core.base.exception.StanzaException;
 import com.stanzaliving.ruleengine.dto.AddRuleRequestDto;
 import com.stanzaliving.ruleengine.dto.ConditionCombinationDto;
 import lombok.experimental.UtilityClass;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.util.*;
@@ -18,42 +16,6 @@ import java.util.*;
 public class RuleEngine {
 
 	private static Map<String, Integer> expressionVariables;
-
-
-
-	private void isValidCondition(ConditionCombinationDto combinationDto) {
-		if (CollectionUtils.isEmpty(combinationDto.getConditions())){
-			if (
-					StringUtils.isEmpty(combinationDto.getLeftOperand())
-							|| StringUtils.isEmpty(combinationDto.getRightOperand())
-							|| Objects.isNull(combinationDto.getOperator())
-			) {
-				throw new StanzaException("Invalid Conditions supplied. left operand , right operand and operator is required.");
-			}
-		} else {
-			if (!(
-					StringUtils.isEmpty(combinationDto.getLeftOperand())
-							&& StringUtils.isEmpty(combinationDto.getRightOperand())
-							&& Objects.isNull(combinationDto.getOperator())
-			)) {
-				throw new StanzaException("Invalid Conditions supplied. left operand , right operand and operator can not be used when condition combinations is being used.");
-			}
-
-			if (StringUtils.isEmpty(combinationDto.getAggregator()) || (combinationDto.getValue() == null)) {
-				throw new StanzaException("Aggregator and value are required with condition combination.");
-			}
-		}
-	}
-
-
-	public Boolean validateConditions(List<ConditionCombinationDto> combinationDtos){
-		for (ConditionCombinationDto combinationDto : combinationDtos) {
-			isValidCondition(combinationDto);
-			return validateConditions(combinationDto.getConditions());
-		}
-		return true;
-	}
-
 
 
 	private Boolean valid(ConditionCombinationDto combinationDto) {
