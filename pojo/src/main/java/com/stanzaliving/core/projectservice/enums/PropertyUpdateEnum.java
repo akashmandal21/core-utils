6ce1e8be81afa0316144b6817356f19384eba1f0
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-05-01 00:27:42 +0530

    updated grn changes

--- pojo/src/main/java/com/stanzaliving/core/projectservice/enums/PropertyUpdateEnum.java
+++ pojo/src/main/java/com/stanzaliving/core/projectservice/enums/PropertyUpdateEnum.java
@@ -83,6 +83,12 @@ public enum  PropertyUpdateEnum {
     PO_REJECTED("PO Rejected",false,NotificationTaskType.PO_REJECTED),
     PO_APPROVED("PO Approved",false,NotificationTaskType.PO_APPROVED),
 
+
+    //GRN SERVICE
+    GRN_BOI_ITEM_RECEIVED("BOI Item Receiving in GRN",false,NotificationTaskType.GRN_BOI_ITEM_RECEIVED),
+    GRN_BOI_ITEM_INSTALLATION("BOI Item Installation in GRN",false,NotificationTaskType.GRN_BOI_ITEM_INSTALLATION),
+    GRN_GC_ITEM_COMPLETION("GC Item completion in GRN",false,NotificationTaskType.GRN_GC_ITEM_COMPLETION),
+
     //FOR All other events where there are multiple complete signals. For e.g. Multiple POs/Invoices etc. Marker Event Only
     CHECK_COMPLETED_FROM_SOURCE_SERVICE("Check Completed Status from Source Service",false,null,null);
 
