c1b1b73b13b229f6f65d8f4c52df230074f3ac04
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-05-01 15:25:02 +0530

    New status added

--- pojo/src/main/java/com/stanzaliving/designservice/enums/PhoenixDesignStatus.java
+++ pojo/src/main/java/com/stanzaliving/designservice/enums/PhoenixDesignStatus.java
@@ -8,12 +8,27 @@ import java.util.Map;
 @Getter
 public enum PhoenixDesignStatus {
 
-	UNDER_DRAFT("Under Draft","#00B5CC"),
-	SENT_FINAL_BED_COUNT_TO_DESIGN_HEAD("Sent Final Bed Count To Design Head", "#00B5CC"),
-	FINAL_BED_COUNT_APPROVED("Final Bed Count Approved", "#019875"),
-	SENT_BACK_FINAL_BED_COUNT_BY_DESIGN_COORDINATOR("Sent Back Final Bed Count By Design Coordinator", "#BFBFBF"),
-	SENT_BACK_FINAL_BED_COUNT_BY_DESIGN_HEAD("Sent Back Final Bed Count By Design Head", "#BFBFBF");
+	IN_DRAFT("In draft","#e5e3e3"),
+	SUBMITTED("Submitted","#e5e3e3"),
+	TRANSFORMATIONS("Sent For Approval","#e5e3e3"),
+	APPROVED_BY_TRANSFORMATIONS("Approved By Transformations","#e5e3e3"),
+	SENT_BACK_BY_TRANSFORMATIONS("Sent Back By Transformations","#e5e3e3"),;
 
+	
+	/*
+	 * NOT_STARTED("Not Started","#e5e3e3"), IN_DRAFT("In draft","#e5e3e3"),
+	 * SUBMITTED("Submitted","#e5e3e3"), ZONAL_HEAD("Zonal Head","#e5e3e3"),
+	 * SENT_BACK_BY_ZONAL_HEAD("Sent back by zonal head","#e5e3e3"),
+	 * DESIGN_HEAD("Design head","#e5e3e3"),
+	 * SENT_BACK_BY_DESIGN_HEAD("Sent back by design head","#e5e3e3"),
+	 * TRANSFORMATIONS("Sent For Approval","#e5e3e3"),
+	 * APPROVED_BY_TRANSFORMATIONS("Approved By Transformations","#e5e3e3"),
+	 * SENT_BACK_BY_TRANSFORMATIONS("Sent back by tranformation","#e5e3e3"),
+	 * LEADERSHIP("Leadership","#e5e3e3"),
+	 * SENT_BACK_BY_LEADERSHIP("sent back by leadership","#e5e3e3");s
+	 */
+
+	
 	private String statusDescription;
 	private String colorCode;
 	private static Map<PhoenixDesignStatus, String> designStatusAndDescription = new HashMap<>();
@@ -27,7 +42,7 @@ public enum PhoenixDesignStatus {
 		
 		for(PhoenixDesignStatus record : PhoenixDesignStatus.values()) {
 			
-			if(record != PhoenixDesignStatus.UNDER_DRAFT)
+			if(record != PhoenixDesignStatus.IN_DRAFT)
 				designStatusAndDescription.put(record, record.getStatusDescription());
 		}
 		
