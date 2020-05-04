eae0307025294b834b1b9d3e6e58ca81bcdea3fa
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-05-04 23:12:47 +0530

    updated

--- pojo/src/main/java/com/stanzaliving/core/projectservice/enums/ProgressColor.java
+++ pojo/src/main/java/com/stanzaliving/core/projectservice/enums/ProgressColor.java
@@ -6,6 +6,6 @@ public class ProgressColor {
     public static final String DELAYED_COLOR = "#FA1F4B";
     public static final String ON_TRACK_COLOR = "#5EBBA0";
     public static final String AHEAD_COLOR = "#77baa6";
-    public static final String COMPLETED_COLOR = "#77baa6";
+    public static final String COMPLETED_COLOR = "#FFFFFF";
 
 }
