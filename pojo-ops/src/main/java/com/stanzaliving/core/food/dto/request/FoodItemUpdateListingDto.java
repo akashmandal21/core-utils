451a175a4a6e4dcd9123d04ed29460e02887df42
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-05-05 12:45:14 +0530

    Food Item Update Dtos

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/request/FoodItemUpdateListingDto.java
@@ -0,0 +1,42 @@
+package com.stanzaliving.core.food.dto.request;
+
+
+import com.stanzaliving.core.food.enums.FoodItemType;
+import com.stanzaliving.core.operations.enums.MealType;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author piyush srivastava "piyush.srivastava@stanzaliing.com"
+ *
+ * @date 04-May-2020
+ *
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodItemUpdateListingDto {
+	@NotNull(message = "item uuid is required.")
+	private String uuid;
+
+	private Set<MealType> meals;
+
+	private String textColor;
+
+	private String bgColor;
+
+	private Boolean quantifiable;
+
+	private FoodItemType foodItemType;
+
+	private Set<String> tagIds;
+}
