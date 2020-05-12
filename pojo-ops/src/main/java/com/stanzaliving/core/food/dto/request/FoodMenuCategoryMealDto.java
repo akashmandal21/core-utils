31ed56a5976099615199ab2b84f6bcdffcb3f78f
Naveen Kumar <naveen.kumar@stanzaliving.com>
2020-05-12 18:01:39 +0530

    removed incorrect import

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/request/FoodMenuCategoryMealDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/request/FoodMenuCategoryMealDto.java
@@ -1,9 +1,13 @@
 package com.stanzaliving.core.food.dto.request;
 
 import com.stanzaliving.core.operations.enums.MealType;
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
-import sun.jvm.hotspot.debugger.cdbg.EnumType;
 
 /**
  * @author piyush srivastava "piyush.srivastava@stanzaliving.com"
@@ -19,6 +23,7 @@ import sun.jvm.hotspot.debugger.cdbg.EnumType;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FoodMenuCategoryMealDto {
+
 	private String menuCategoryId;
 
 	private MealType mealType;
