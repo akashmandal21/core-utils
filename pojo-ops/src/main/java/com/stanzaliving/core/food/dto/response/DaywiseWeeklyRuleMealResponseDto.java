e777d70b622496cfd491af27dd2b6cca018849ea
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-05-12 19:59:22 +0530

    DTos

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/DaywiseWeeklyRuleMealResponseDto.java
@@ -0,0 +1,27 @@
+package com.stanzaliving.core.food.dto.response;
+
+import com.stanzaliving.core.rule.engine.dto.ConditionDto;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+/**
+ * @author piyush srivastava
+ *
+ * @date 12-May-2020
+ *
+ */
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DaywiseWeeklyRuleMealResponseDto extends ConditionDto {
+
+	private Boolean passed;
+
+	private List<WeeklyDaywiseMealResponseDto> daywiseMeals;
+}
