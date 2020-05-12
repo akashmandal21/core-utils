328254a90df4abcedc03fa100ba20f0dd69d996d
hanish-kottamasu <hanish.kottamasu@stanzaliving.com>
2020-05-12 14:21:03 +0530

    added constants

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/constants/FoodMenuPdfTemplateDataConstants.java
@@ -0,0 +1,15 @@
+package com.stanzaliving.core.food.constants;
+
+public class FoodMenuPdfTemplateDataConstants {
+
+	public static final String MICROMARKET_NAME = "micromarketName";
+
+	public static final String MENU_CATEGORY_NAME = "menuCategoryName";
+
+	public static final String MENU_WEEK = "menuWeek";
+
+	public static final String MEAL_COST_PER_DAY = "mealCostPerDay";
+
+	public static final String MEAL_LEVEL_ITEM_DTOS = "mealLevelItemDtos";
+
+}
