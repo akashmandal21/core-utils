30fd8181423384b1a7c8e9d3838f5c94364995b9
hanish-kottamasu <hanish.kottamasu@stanzaliving.com>
2020-05-12 23:24:52 +0530

    added dtos for food order remainder jobn

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/HostelIdNameDto.java
@@ -0,0 +1,21 @@
+package com.stanzaliving.core.food.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HostelIdNameDto {
+
+	private String id;
+
+	private String name;
+}
