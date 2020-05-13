70814dd2a560b9cef1ac0f6758c0536797a98312
Amit-K-G <amitgupta.retainer@stanzaliving.com>
2020-05-13 19:43:20 +0530

    po_cancellation

--- pojo/src/main/java/com/stanzaliving/core/projectservice/enums/PropertyUpdateEnum.java
+++ pojo/src/main/java/com/stanzaliving/core/projectservice/enums/PropertyUpdateEnum.java
@@ -84,7 +84,7 @@ public enum  PropertyUpdateEnum {
     PO_REJECTED("PO Rejected",false,NotificationTaskType.PO_REJECTED),
     PO_APPROVED("PO Approved",false,NotificationTaskType.PO_APPROVED),
     WAREHOUSE_PO_APPROVED("Warehouse PO Approved",false, NotificationTaskType.WAREHOUSE_PO_APPROVED),
-
+    PO_CANCELLED("PO Cancelled", false, NotificationTaskType.PO_CANCELLED),
 
     //GRN SERVICE
     GRN_BOI_ITEM_RECEIVED("BOI Item Receiving in GRN",false,NotificationTaskType.GRN_BOI_ITEM_RECEIVED),
