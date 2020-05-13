2e248443cb20b60e567b8da5ac64e0fb5c2fdcce
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-05-13 11:19:32 +0530

    collector service first commit

---
+++ pojo/src/main/java/com/stanzaliving/collector/enums/InvoiceType.java
@@ -0,0 +1,11 @@
+package com.stanzaliving.collector.enums;
+
+public enum InvoiceType {
+    RENTAL ,
+    SERVICES ,
+    BOOKING ,
+    VAS ,
+    PENALTY ,
+    ANNUAL_MAINTENANCE ,
+    SECURITY_DEPOSIT;
+}
