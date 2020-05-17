950af91a4c0cd409db8ae49683bf8e604f7e5af2
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-05-17 16:00:46 +0530

    reverted string to enum for existing pojos used in other services

--- pojo/src/main/java/com/stanzaliving/transformations/pojo/PropertyBoqStatusDto.java
+++ pojo/src/main/java/com/stanzaliving/transformations/pojo/PropertyBoqStatusDto.java
@@ -25,7 +25,7 @@ import lombok.ToString;
 public class PropertyBoqStatusDto {
 
 	private String propertyUuid;
-	private String boqStatus;
+	private BoqStatus boqStatus;
 	private String boqStatusDesc;
 	private String colorCode;
 }
