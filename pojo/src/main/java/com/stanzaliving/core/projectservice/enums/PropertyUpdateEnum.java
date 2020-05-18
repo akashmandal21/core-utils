deb8978658c3c22eec02cfa7120ad3d6a6e53fa7
Amit-K-G <amitgupta.retainer@stanzaliving.com>
2020-05-18 13:46:49 +0530

    short po changes

--- pojo/src/main/java/com/stanzaliving/core/projectservice/enums/PropertyUpdateEnum.java
+++ pojo/src/main/java/com/stanzaliving/core/projectservice/enums/PropertyUpdateEnum.java
@@ -86,6 +86,7 @@ public enum  PropertyUpdateEnum {
     PO_APPROVED("PO Approved",false,NotificationTaskType.PO_APPROVED),
     WAREHOUSE_PO_APPROVED("Warehouse PO Approved",false, NotificationTaskType.WAREHOUSE_PO_APPROVED),
     PO_CANCELLED("PO Cancelled", false, NotificationTaskType.PO_CANCELLED),
+    PO_PARTIALLY_CANCELLED("PO Partially Cancelled", false, NotificationTaskType.PO_PARTIALLY_CANCELLED),
 
     //GRN SERVICE
     GRN_BOI_ITEM_RECEIVED("BOI Item Receiving in GRN",false,NotificationTaskType.GRN_BOI_ITEM_RECEIVED),
