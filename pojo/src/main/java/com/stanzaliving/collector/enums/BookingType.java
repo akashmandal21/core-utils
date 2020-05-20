19d83d56776260b863423f7d1fd1e9bb750c362a
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-05-20 14:08:01 +0530

    adding map in bookingType enum

--- pojo/src/main/java/com/stanzaliving/collector/enums/BookingType.java
+++ pojo/src/main/java/com/stanzaliving/collector/enums/BookingType.java
@@ -12,6 +12,17 @@ public enum BookingType {
 
     NEW (0), RETENTION (1), ROYALTY (2), B2B (3), SUITES_BOOKING (4);
 
-    private int bookingTypeId;
+    private int id;
+
+
+    private static final Map<Integer, BookingType> map = new HashMap<>();
+
+    static {
+        for (BookingType bookingType : values()) map.put(bookingType.getId(), bookingType);
+    }
+
+    public static BookingType getBookingTypeById(int id) {
+        return map.get(id);
+    }
 
 }
