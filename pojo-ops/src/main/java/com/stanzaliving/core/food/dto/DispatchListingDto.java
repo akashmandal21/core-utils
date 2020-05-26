bbf5cf705590818ae82df6695816f7ab10767d70
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-05-26 17:46:21 +0530

    Pagination Request for Dispatch Listing Done

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/DispatchListingDto.java
@@ -0,0 +1,45 @@
+package com.stanzaliving.core.food.dto;
+
+import com.stanzaliving.core.operations.enums.MealType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Getter
+@Setter
+public class DispatchListingDto {
+
+	private String residenceName;
+	
+	private MealType mealType;
+	
+	private String mealTypeStr;
+	
+	private String mealDispatchTime;
+	
+	private String mealStartTime;
+	
+	private int vegMeals;
+	
+	private int nonVegMeals;
+	
+	private int staffMeals;
+	
+	private int vasVegMeals;
+	
+	private int vasNonVegMeals;
+	
+	private boolean printable;
+	
+	private String residenceMenuUuid;
+	
+	
+}
