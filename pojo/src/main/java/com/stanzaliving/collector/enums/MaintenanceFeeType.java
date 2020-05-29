41e0012f1a1db15c435c5dbec599933837728715
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-05-30 00:54:20 +0530

    enum correction

--- pojo/src/main/java/com/stanzaliving/collector/enums/MaintenanceFeeType.java
+++ pojo/src/main/java/com/stanzaliving/collector/enums/MaintenanceFeeType.java
@@ -3,9 +3,22 @@ package com.stanzaliving.collector.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @AllArgsConstructor
 public enum MaintenanceFeeType {
     ONE_TIME_UPFRONT(0),MONTHLY(1);
     int maintenanceFeeType;
+
+    private static final Map<Integer, MaintenanceFeeType> map = new HashMap<>();
+
+    static {
+        for (MaintenanceFeeType maintenanceFeeType : values()) map.put(maintenanceFeeType.getMaintenanceFeeType(), maintenanceFeeType);
+    }
+
+    public static MaintenanceFeeType getByMaintenanceFeeType(int id) {
+        return map.get(id);
+    }
 }
