013bb4cc058f3c06bca102b1b7789bced795bf6b
Naveen Kumar <naveen.kumar@stanzaliving.com>
2020-06-01 17:51:21 +0530

    Changed field type

--- pojo-ops/src/main/java/com/stanzaliving/core/food/constants/FoodEmailTemplateConstants.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/constants/FoodEmailTemplateConstants.java
@@ -3,13 +3,13 @@ package com.stanzaliving.core.food.constants;
 public class FoodEmailTemplateConstants {
 
 	public static final String MICROMARKET_NAME = "micromarketName";
-	
+
 	public static final String MICROMARKET_NAMES = "micromarketNames";
-	
+
 	public static final String MICROMARKET_MAP = "micromarketMap";
-	
+
 	public static final String CITY_DTOS = "cityDtos";
-	
+
 	public static final String DATE = "date";
 
 	public static final String MENU_CATEGORY_NAME = "menuCategoryName";
@@ -19,19 +19,20 @@ public class FoodEmailTemplateConstants {
 	public static final String MEAL_COST_PER_DAY = "mealCostPerDay";
 
 	public static final String MEAL_LEVEL_ITEM_DTOS = "mealLevelItemDtos";
-	
+
 	public static final String MENU_CATEGORY_NAMES = "categoryNames";
-	
+
 	public static final String APPROVAL_SUMMARY_MAP = "approvalSummaryMap";
-	
+
 	public static final String CENTRAL_FB_MANAGER_NAME = "centralFbManagerName";
-	
+
 	public static final String CENTRAL_FB_MANAGER_CONTACT_NO = "centralFbManagerContactNo";
-	
+
 	public static final String FB_MANAGER_CONTACT_NAMES = "fbManagerNames";
-	
+
 	public static final String FB_MANAGER_CONTACT_NUMBERS = "fbManagerContactNumbers";
-	
+
 	public static final String DASHBOARD_LINK = "dashboardLink";
-	
+
+	public static final String VENDOR_NAME = "vendorName";
 }
