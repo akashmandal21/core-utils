deadf6a9b86bc49abb50e1f48cc2c16ba08bdab6
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-06-02 20:23:54 +0530

    ccEmails Roles Added

--- pojo-ops/src/main/java/com/stanzaliving/core/operations/enums/FoodRoleType.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/enums/FoodRoleType.java
@@ -21,7 +21,14 @@ public enum FoodRoleType {
 	HEAD_CHEF("Head Chef", Department.FOOD_OPS, AccessLevel.COUNTRY),
 	CITY_CHEF_MANAGER("City Chef Manager", Department.FOOD_OPS, AccessLevel.CITY),
 	PURCHASE_MANAGER("Purchase Manager", Department.FOOD_OPS, AccessLevel.CITY),
-	STORE_MANAGER("Store Manager", Department.FOOD_OPS, AccessLevel.CITY);
+	STORE_MANAGER("Store Manager", Department.FOOD_OPS, AccessLevel.CITY),
+
+	EMAIL_CC_FOOD_ORDER_DETAILS("Email CC - Food Order Details", Department.OPS, AccessLevel.COUNTRY),
+	EMAIL_CC_FOOD_ORDER_REMINDER("Email CC - Food Order Reminder", Department.OPS, AccessLevel.COUNTRY),
+	EMAIL_CC_FOOD_MENU_CREATED("Email CC - Food Menu Created", Department.FOOD_OPS, AccessLevel.COUNTRY),
+	EMAIL_CC_FOOD_MENU_APPROVED("Email CC - Food Menu Approved", Department.FOOD_OPS, AccessLevel.COUNTRY),
+	EMAIL_CC_FOOD_MENU_REJECTED("Email CC - Food Menu Rejected", Department.FOOD_OPS, AccessLevel.COUNTRY),
+	EMAIL_CC_FOOD_MENU_CREATION_REMINDER("Email CC - Food Menu Creation Reminder", Department.FOOD_OPS, AccessLevel.COUNTRY);
 
 	private String roleType;
 
