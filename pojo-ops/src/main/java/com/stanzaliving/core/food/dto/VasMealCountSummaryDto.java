b9734c6509c6cf61df39309728c8b2c4b852cfa8
hanish-kottamasu <hanish.kottamasu@stanzaliving.com>
2020-06-02 16:41:10 +0530

    added vas order email details

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/VasMealCountSummaryDto.java
@@ -0,0 +1,32 @@
+package com.stanzaliving.core.food.dto;
+
+import java.util.List;
+
+import com.stanzaliving.core.food.dto.response.VasItemOrderDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VasMealCountSummaryDto {
+
+	private String mealName;
+
+	private Integer vegCount;
+
+	private Integer nonVegCount;
+
+	private List<VasItemOrderDto> vegItems;
+
+	private List<VasItemOrderDto> nonVegItems;
+
+}
