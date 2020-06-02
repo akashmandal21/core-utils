115b0d6bdbf01c1a2073c9bd87e3f51f9dd0393f
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-06-02 16:32:57 +0530

    removing underscore

--- pojo/src/main/java/com/stanzaliving/dashboard/constants/GraphConstants.java
+++ pojo/src/main/java/com/stanzaliving/dashboard/constants/GraphConstants.java
@@ -5,8 +5,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class GraphConstants {
 
-    public static final String RUN_RATE_Y_LABEL_RUPEE = "AMOUNT_PENDING (INR)";
-    public static final String RUN_RATE_Y_LABEL_DOLLAR = "AMOUNT_PENDING (USD)";
+    public static final String RUN_RATE_Y_LABEL_RUPEE = "AMOUNT PENDING (INR)";
+    public static final String RUN_RATE_Y_LABEL_DOLLAR = "AMOUNT PENDING (USD)";
     public static final String GRAPH_X_TYPE = "{value}";
     public static final String RUN_RATE_Y_TYPE_RUPEE = "{value}L";
     public static final String RUN_RATE_Y_TYPE_DOLLAR = "{value}k";
