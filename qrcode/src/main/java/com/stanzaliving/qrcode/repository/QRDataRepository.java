f6068aa471f8043bf73ff4eed797a82326c0e270
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-06-03 12:29:21 +0530

    User Id Check for pickup history

--- qrcode/src/main/java/com/stanzaliving/qrcode/repository/QRDataRepository.java
+++ qrcode/src/main/java/com/stanzaliving/qrcode/repository/QRDataRepository.java
@@ -14,8 +14,6 @@ public interface QRDataRepository extends AbstractJpaRepository<QRData, Long> {
 	
 	List<QRData> findByCreatedByAndQrContextTypeIn(String userUuid, List<QRContextType> contextType, Pageable page);
 
-	List<QRData> findByUserIdAndQrContextTypeIn(String userUuid, List<QRContextType> contextType, Pageable page);
-	
 	List<QRData> findByQrContextTypeIn( List<QRContextType> contextType, Pageable page);
 
 
