e13cfc7968ce712e5cb99bb200ac0735c5e90566
vaibhav-badole <vaibhav.badole@stanzaliving.com>
2020-06-04 11:53:31 +0530

    added builder

--- kafka/src/main/java/com/stanzaliving/core/kafka/dto/KafkaDTO.java
+++ kafka/src/main/java/com/stanzaliving/core/kafka/dto/KafkaDTO.java
@@ -8,10 +8,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @ToString
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class KafkaDTO {
--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/FoodMenuFeedbackDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/FoodMenuFeedbackDto.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @ToString
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class FoodMenuFeedbackDto {
