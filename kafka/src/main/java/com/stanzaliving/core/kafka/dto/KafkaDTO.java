58bcfc60676abce1e6e09fbb14a8606568cf3bfb
vaibhav-badole <vaibhav.badole@stanzaliving.com>
2020-06-04 12:03:52 +0530

    removed builder

--- kafka/src/main/java/com/stanzaliving/core/kafka/dto/KafkaDTO.java
+++ kafka/src/main/java/com/stanzaliving/core/kafka/dto/KafkaDTO.java
@@ -8,12 +8,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @ToString
-@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class KafkaDTO {
