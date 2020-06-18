6acc0a8863d3d36e8c7fc85a030c965a0067bf3e
Naveen Kumar <naveen.kumar@stanzaliving.com>
2020-06-18 20:09:22 +0530

    Corrected package structure

--- estate-agg-pojo/src/main/java/com/stanzaliving/core/phoenix/embeddedInfos/GCScheduleInfo.java
+++ estate-agg-pojo/src/main/java/com/stanzaliving/core/phoenix/embeddedInfos/GCScheduleInfo.java
@@ -1,4 +1,4 @@
-package com.stanzaliving.core.phoenix.embeddedinfos;
+package com.stanzaliving.core.phoenix.embeddedInfos;
 
 import com.stanzaliving.core.gcservice.enums.GcScheduleStatus;
 import lombok.AllArgsConstructor;
