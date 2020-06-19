e170245e2f384a668c8d5d27bb53fa8018f8df48
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-06-19 17:52:31 +0530

    FIrst commit for wanda

--- pojo/src/main/java/com/stanzaliving/core/user/acl/dto/AclUserDto.java
+++ pojo/src/main/java/com/stanzaliving/core/user/acl/dto/AclUserDto.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 import com.stanzaliving.core.user.dto.UserProfileDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @ToString(callSuper = true)
 @SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AclUserDto extends UserProfileDto {
     private List<UserDeptLevelRoleNameUrlExpandedDto> acl;
 }
