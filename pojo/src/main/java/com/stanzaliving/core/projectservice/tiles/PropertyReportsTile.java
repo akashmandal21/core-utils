1f506aa6bb1157039c99997359655c8e290cebb7
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-06-21 09:51:41 +0530

    changed service mix tile link

--- pojo/src/main/java/com/stanzaliving/core/projectservice/tiles/PropertyReportsTile.java
+++ pojo/src/main/java/com/stanzaliving/core/projectservice/tiles/PropertyReportsTile.java
@@ -30,7 +30,7 @@ public enum PropertyReportsTile {
     PO("POs", true, true, true, Department.GC, PropertyUpdateEnum.CHECK_COMPLETED_FROM_SOURCE_SERVICE, "pos",null),
     GRN("GRN", true, true, true, Department.GC, PropertyUpdateEnum.CHECK_COMPLETED_FROM_SOURCE_SERVICE, "grn",null),
     HOTO("HOTO", true, true, false, Department.PROJECTS, PropertyUpdateEnum.PROJECTS_HOTO_GENERATED, "hoto",null),
-    SERVICE_MIX("Service Mix", true, false, false, Department.DESIGN, PropertyUpdateEnum.SERVICE_MIX_COMPLETED, "service_mix",
+    SERVICE_MIX("Service Mix", true, false, false, Department.DESIGN, PropertyUpdateEnum.SERVICE_MIX_COMPLETED, "servicemix",
             Arrays.asList(PropertyUpdateEnum.DESIGN_BED_COUNT_FINALIZATION_COMPLETED,PropertyUpdateEnum.SERVICE_MIX_COMPLETED));
 
     private String tileText;
