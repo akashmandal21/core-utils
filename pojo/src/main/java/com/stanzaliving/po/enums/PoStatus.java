1f0f4bafb9c728bf03c87e7b2b89c2eeedbdcbce
Amit-K-G <amitgupta.retainer@stanzaliving.com>
2020-06-26 16:55:04 +0530

    po type changed

--- pojo/src/main/java/com/stanzaliving/po/enums/PoStatus.java
+++ pojo/src/main/java/com/stanzaliving/po/enums/PoStatus.java
@@ -1,33 +1,38 @@
 package com.stanzaliving.po.enums;
 
+import com.stanzaliving.core.base.enums.Department;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
 public enum PoStatus {
 
+    IN_DRAFT("In Draft", 0, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.SALES)),
+    TH_APPROVAL_PENDING("Transformation Head Approval Pending", 1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
+    TH_APPROVED("Approved By Transformation Head", 2, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
+    SENT_BACK_BY_TH("Sent Back By Transformation Head", -1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
+    SALES_CT_APPROVAL_PENDING("Sales Central Team Approval Pending", 1, Arrays.asList(Department.SALES)),
+    SALES_CT_APPROVED("Approved By Sales Central Team", 2, Arrays.asList(Department.SALES)),
+    SENT_BACK_BY_SALES_CT("Sent Back By Sales Central Team", -1, Arrays.asList(Department.SALES)),
+    TH_PARTIAL_CANCELLATION_PENDING("Transformation Head Partial Cancellation Pending", 1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
+    TH_PARTIALLY_CANCELLED("Partially Cancelled By Transformation Head", -1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
+    TH_CANCELLATION_PENDING("Transformation Head Cancellation Pending", 1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
+    TH_CANCELLED("Cancelled By Transformation Head", -1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
+    WORK_COMPLETED("Work Completed", 3, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS, Department.SALES)),
+    INVOICE_SUBMITTED("Invoice Submitted", 4, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS, Department.SALES)),
+    PO_CLOSED("Po Closed", 5, Arrays.asList(Department.SALES)),
+    SENT_BACK_BY_FINANCE("Sent back by finance", 8, null);
 
-	IN_DRAFT("In Draft",0),
-	TH_APPROVAL_PENDING("Transformation Head Approval Pending",1),
-	TH_APPROVED("Approved By Transformation Head",2),
-	SENT_BACK_BY_TH("Sent Back By Transformation Head",-1),
-	LEADERSHIP_APPROVAL_PENDING("Leadership Approval Pending",-1), //REMOVED
-	LEADERSHIP_APPROVED("Approved By Leadership",-1), //REMOVED
-	SENT_BACK_BY_LEADERSHIP("Sent Back By Leadership",-1),//REMOVED
-	WORK_COMPLETED("Work Completed",3),
-	INVOICE_SUBMITTED("Invoice Submitted",4),
-	SENT_BACK_BY_FINANCE("Sent Back By Finance",-1),
-	TH_PARTIAL_CANCELLATION_PENDING("Transformation Head Partial Cancellation Pending",1),
-	TH_PARTIALLY_CANCELLED("Partially Cancelled By Transformation Head",-1),
-	TH_CANCELLATION_PENDING("Transformation Head Cancellation Pending",1),
-	TH_CANCELLED("Cancelled By Transformation Head",-1),
-	PO_CLOSED("Po Closed", 5),
-	SALES_CT_APPROVAL_PENDING("Sales Central Team Approval Pending",1),
-	SALES_CT_APPROVED("Approved By Sales Central Team",2);
+    private String status;
 
-	private String status;
-	private Integer order;
+    private Integer order;
+
+    private List<Department> departments;
+
 }
 
 
--- pojo/src/main/java/com/stanzaliving/po/enums/PoType.java
+++ pojo/src/main/java/com/stanzaliving/po/enums/PoType.java
@@ -1,24 +1,26 @@
 package com.stanzaliving.po.enums;
 
-import com.stanzaliving.core.base.enums.Department;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.Arrays;
-import java.util.List;
 
 @AllArgsConstructor
 @Getter
 @NoArgsConstructor
 public enum PoType {
 
-	GC("GC",Arrays.asList(Department.GC,Department.DESIGN)),
-	ASIS("As Is",Arrays.asList(Department.PROCUREMENT,Department.PROJECTS)),
-	BOI("Boi",Arrays.asList(Department.PROCUREMENT,Department.PROJECTS));
+	BOI("BOI"),
+	GC("GC"),
+	ASIS("As-Is"),
+	COLLATERAL("Collateral"),
+	BRANDING("Branding"),
+	TRANSIT_MEDIA("Transit Media"),
+	MERCHANDISE("Merchandise"),
+	MANPOWER("Manpower"),
+	MATERIAL_TRANSPORTATION("Material Transportation"),
+	FACILITATION_CHARGES("Facilitation Charges"),
+	OTHER("Others");
 	
 	private String type;
-	
-	private List<Department> departments;
 	
 }
