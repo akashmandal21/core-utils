1fed09196398c847d952eab1740e25555c27c71a
Amit-K-G <amitgupta.retainer@stanzaliving.com>
2020-06-27 17:09:51 +0530

    po status added

--- pojo/src/main/java/com/stanzaliving/po/enums/PoStatus.java
+++ pojo/src/main/java/com/stanzaliving/po/enums/PoStatus.java
@@ -11,14 +11,14 @@ import java.util.List;
 @Getter
 public enum PoStatus {
 
-    IN_DRAFT("In Draft", 0, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.SALES)),
-    TH_APPROVAL_PENDING("Transformation Head Approval Pending", 1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
-    TH_APPROVED("Approved By Transformation Head", 2, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
-    SENT_BACK_BY_TH("Sent Back By Transformation Head", -1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
-    TH_PARTIAL_CANCELLATION_PENDING("Transformation Head Partial Cancellation Pending", 1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
-    TH_PARTIALLY_CANCELLED("Partially Cancelled By Transformation Head", -1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
-    TH_CANCELLATION_PENDING("Transformation Head Cancellation Pending", 1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
-    TH_CANCELLED("Cancelled By Transformation Head", -1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS)),
+    IN_DRAFT("In Draft", 0, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.GC, Department.SALES)),
+    TH_APPROVAL_PENDING("Transformation Head Approval Pending", 1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.GC, Department.TRANSFORMATIONS)),
+    TH_APPROVED("Approved By Transformation Head", 2, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.GC, Department.TRANSFORMATIONS)),
+    SENT_BACK_BY_TH("Sent Back By Transformation Head", -1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.GC, Department.TRANSFORMATIONS)),
+    TH_PARTIAL_CANCELLATION_PENDING("Transformation Head Partial Cancellation Pending", 1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.GC, Department.TRANSFORMATIONS)),
+    TH_PARTIALLY_CANCELLED("Partially Cancelled By Transformation Head", -1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.GC, Department.TRANSFORMATIONS)),
+    TH_CANCELLATION_PENDING("Transformation Head Cancellation Pending", 1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.GC, Department.TRANSFORMATIONS)),
+    TH_CANCELLED("Cancelled By Transformation Head", -1, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.GC, Department.TRANSFORMATIONS)),
     SALES_CT_APPROVAL_PENDING("Sales Central Team Approval Pending", 1, Arrays.asList(Department.SALES)),
     SALES_CT_APPROVED("Approved By Sales Central Team", 2, Arrays.asList(Department.SALES)),
     SENT_BACK_BY_SALES_CT("Sent Back By Sales Central Team", -1, Arrays.asList(Department.SALES)),
@@ -26,8 +26,8 @@ public enum PoStatus {
     SALES_CT_PARTIALLY_CANCELLED("Partially Cancelled By Sales Central Team", -1, Arrays.asList(Department.SALES)),
     SALES_CT_CANCELLATION_PENDING("Sales Central Team Cancellation Pending", 1, Arrays.asList(Department.SALES)),
     SALES_CT_CANCELLED("Cancelled By Sales Central Team", -1, Arrays.asList(Department.SALES)),
-    WORK_COMPLETED("Work Completed", 3, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS, Department.SALES)),
-    INVOICE_SUBMITTED("Invoice Submitted", 4, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.TRANSFORMATIONS, Department.SALES, Department.FINANCE)),
+    WORK_COMPLETED("Work Completed", 3, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.GC, Department.TRANSFORMATIONS, Department.SALES)),
+    INVOICE_SUBMITTED("Invoice Submitted", 4, Arrays.asList(Department.DESIGN, Department.PROCUREMENT, Department.GC, Department.TRANSFORMATIONS, Department.SALES, Department.FINANCE)),
     PO_CLOSED("Po Closed", 5, Arrays.asList(Department.SALES)),
     SENT_BACK_BY_FINANCE("Sent back by finance", 8, Arrays.asList(Department.LEADERSHIP));
 
