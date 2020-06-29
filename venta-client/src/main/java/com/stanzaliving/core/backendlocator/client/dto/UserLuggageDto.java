db0980f549fe4b8b83883c1dd8adb8d35cc6e0ab
Animesh Bajpai <animesh.bajpai@stanzaliving.com>
2020-06-29 15:58:11 +0530

    core utils support for getting luggage

--- venta-client/src/main/java/com/stanzaliving/core/backendlocator/client/dto/UserLuggageDto.java
+++ venta-client/src/main/java/com/stanzaliving/core/backendlocator/client/dto/UserLuggageDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -12,4 +14,16 @@ import lombok.NoArgsConstructor;
 public class UserLuggageDto {
     private String studentId;
     private String flagType;
+
+    public int hashCode() {
+        return studentId.getBytes().length;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserLuggageDto that = (UserLuggageDto) o;
+        return Objects.equals(studentId, that.studentId);
+    }
 }
