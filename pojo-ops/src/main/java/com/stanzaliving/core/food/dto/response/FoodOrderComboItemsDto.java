1ad2b2c4853117bac467eac5fd39c1a0fe0956b4
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-06-30 23:51:50 +0530

    Food Order combo item Dto

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/FoodOrderComboItemsDto.java
@@ -0,0 +1,44 @@
+package com.stanzaliving.core.food.dto.response;
+
+import com.stanzaliving.core.food.enums.FoodItemBasePreference;
+import com.stanzaliving.core.food.enums.FoodItemType;
+import com.stanzaliving.core.operations.enums.MealType;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+/**
+ * @author piyush srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @date 30-June-2020
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodOrderComboItemsDto {
+	private String vendorId;
+
+	private LocalDate menuDate;
+
+	private MealType mealType;
+
+	private String residenceId;
+
+	private FoodItemBasePreference basePreference;
+
+	private String itemId;
+
+	private String itemName;
+
+	private FoodItemType itemType;
+
+	private Integer orderedQuantity;
+
+	private Double orderedWeight;
+}
