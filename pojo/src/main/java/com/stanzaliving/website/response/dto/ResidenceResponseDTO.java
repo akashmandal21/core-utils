5f4e6ed19e800c5712ea4255cc59df0725201998
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-07-01 17:58:11 +0530

    Change done for images values

--- pojo/src/main/java/com/stanzaliving/website/response/dto/ResidenceResponseDTO.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/ResidenceResponseDTO.java
@@ -14,6 +14,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
  * @author nipunaggarwal
@@ -24,6 +25,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class ResidenceResponseDTO {
 
 	private int residenceId;
