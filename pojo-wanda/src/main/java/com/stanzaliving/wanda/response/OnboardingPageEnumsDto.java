5ea47fb4daf3c783a88c6fe77f453764180fc2c6
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-07-01 16:58:16 +0530

    Endpoint to get enum details

---
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/response/OnboardingPageEnumsDto.java
@@ -0,0 +1,28 @@
+package com.stanzaliving.wanda.response;
+
+import java.util.List;
+
+import com.stanzaliving.core.user.enums.BloodGroup;
+import com.stanzaliving.core.user.enums.EnumListing;
+import com.stanzaliving.core.user.enums.Gender;
+import com.stanzaliving.core.user.enums.Nationality;
+import com.stanzaliving.wanda.enums.ResidentType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class OnboardingPageEnumsDto {
+
+	private List<EnumListing<BloodGroup>> bloodGroup;
+	
+	private List<EnumListing<Nationality>> nationaility;
+	
+	private List<EnumListing<Gender>> gender;
+	
+	private List<EnumListing<ResidentType>> residentTypes;
+	
+}
