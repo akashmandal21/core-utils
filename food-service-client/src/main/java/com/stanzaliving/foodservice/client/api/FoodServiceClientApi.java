59c9dda582c51890503c949c646d2387e7afc5f9
deepak agrawal <deepak.agrawal>
2020-07-01 16:32:42 +0530

    food changes

--- food-service-client/src/main/java/com/stanzaliving/foodservice/client/api/FoodServiceClientApi.java
+++ food-service-client/src/main/java/com/stanzaliving/foodservice/client/api/FoodServiceClientApi.java
@@ -15,6 +15,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.stanzaliving.core.base.common.dto.ListingDto;
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
+import com.stanzaliving.core.food.dto.request.FullCategoryDto;
 import com.stanzaliving.core.food.dto.response.FoodMenuCategoryBasicDetailsDto;
 
 import lombok.extern.log4j.Log4j2;
@@ -82,9 +83,9 @@ public class FoodServiceClientApi {
 
 	}
 
-	public List<ListingDto> getServiceMixEnabledFoodItemTags() {
+	public List<ListingDto> getFoodItemTags() {
 		ResponseDto<List<ListingDto>> responseDto = null;
-		String path = UriComponentsBuilder.fromPath("/internal/tag/get/serviceMixListing").build().toUriString();
+		String path = UriComponentsBuilder.fromPath("/internal/tag/get/listing").build().toUriString();
 
 		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
 
@@ -107,5 +108,58 @@ public class FoodServiceClientApi {
 		return (Objects.nonNull(responseDto) && Objects.nonNull(responseDto.getData())) ? responseDto.getData() : null;
 
 	}
+	
+	public FullCategoryDto getFullCategoryById(String id) {
+		ResponseDto<FullCategoryDto> responseDto = null;
+		String path = UriComponentsBuilder.fromPath("/internal/menu/category/getFullCategoryById").build().toUriString();
 
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+		queryParams.put("categoryId", Arrays.asList(id));
+		
+		final HttpHeaders headerParams = new HttpHeaders();
+
+		final String[] accepts = {
+				"*/*"
+		};
+		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+		ParameterizedTypeReference<ResponseDto<FullCategoryDto>> returnType = new ParameterizedTypeReference<ResponseDto<FullCategoryDto>>() {
+		};
+
+		try {
+			responseDto = restClient.invokeAPI(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType);
+		} catch (Exception e) {
+			log.error("exception while listing internet plans", e);
+		}
+
+		return (Objects.nonNull(responseDto) && Objects.nonNull(responseDto.getData())) ? responseDto.getData() : null;
+
+	}
+	
+	public FullCategoryDto getFullCategoryByName(String name) {
+		ResponseDto<FullCategoryDto> responseDto = null;
+		String path = UriComponentsBuilder.fromPath("/internal/menu/category/getFullCategoryByName").build().toUriString();
+
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+		queryParams.put("categoryName", Arrays.asList(name));
+		
+		final HttpHeaders headerParams = new HttpHeaders();
+
+		final String[] accepts = {
+				"*/*"
+		};
+		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+		ParameterizedTypeReference<ResponseDto<FullCategoryDto>> returnType = new ParameterizedTypeReference<ResponseDto<FullCategoryDto>>() {
+		};
+
+		try {
+			responseDto = restClient.invokeAPI(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType);
+		} catch (Exception e) {
+			log.error("exception while listing internet plans", e);
+		}
+
+		return (Objects.nonNull(responseDto) && Objects.nonNull(responseDto.getData())) ? responseDto.getData() : null;
+
+	}
 }
