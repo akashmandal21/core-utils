d9984ddec08b917c33c014823b6fbba5d0f412f9
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-07-02 20:08:51 +0530

    Change for contract commercials

--- base/src/main/java/com/stanzaliving/core/base/utils/NumberUtils.java
+++ base/src/main/java/com/stanzaliving/core/base/utils/NumberUtils.java
@@ -1,83 +1,104 @@
 package com.stanzaliving.core.base.utils;
 
-import lombok.experimental.UtilityClass;
-import org.apache.commons.lang3.StringUtils;
-
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+
+import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class NumberUtils {
 
-    public String getOrdinalNumber(Integer n) {
-        String suffix;
-        switch (n % 10) {
-            case 1:
-                suffix = "st";
-                break;
-            case 2:
-                suffix = "nd";
-                break;
-            case 3:
-                suffix = "rd";
-                break;
-            default:
-                suffix = "th";
-        }
-        if (n >= 11 && n <= 13) {
-            suffix = "th";
-        }
-        return n.toString() + suffix;
-    }
+	private NumberFormat india = null;
+	static {
+		Locale indiaLocale = new Locale("en", "IN");
 
-    public boolean parsableToLong(String number) {
-        if (StringUtils.isEmpty(number)) {
-            return false;
-        }
-        try {
-            Long.parseLong(number);
-            return true;
-        } catch (NumberFormatException ex) {
-            return false;
-        }
-    }
+		india = NumberFormat.getCurrencyInstance(indiaLocale);
 
-    public boolean parsableToInteger(String number) {
-        if (StringUtils.isEmpty(number)) {
-            return false;
-        }
-        try {
-            Integer.parseInt(number);
-            return true;
-        } catch (NumberFormatException ex) {
-            return false;
-        }
-    }
+	}
 
-    public boolean parseableToDouble(String number) {
-        if (StringUtils.isEmpty(number)) {
-            return false;
-        }
-        try {
-            Double.parseDouble(number);
-            return true;
-        } catch (NumberFormatException ex) {
-            return false;
-        }
-    }
+	public String getOrdinalNumber(Integer n) {
+		String suffix;
+		switch (n % 10) {
+		case 1:
+			suffix = "st";
+			break;
+		case 2:
+			suffix = "nd";
+			break;
+		case 3:
+			suffix = "rd";
+			break;
+		default:
+			suffix = "th";
+		}
+		if (n >= 11 && n <= 13) {
+			suffix = "th";
+		}
+		return n.toString() + suffix;
+	}
 
-    public static List<Object> convertBigIntegerAndBigDecimal(Object[] record) {
-        List<Object> updatedRecord = new ArrayList<>(record.length);
-        Object updatedObject;
+	public boolean parsableToLong(String number) {
+		if (StringUtils.isEmpty(number)) {
+			return false;
+		}
+		try {
+			Long.parseLong(number);
+			return true;
+		} catch (NumberFormatException ex) {
+			return false;
+		}
+	}
 
-        for (Object object : record) {
-            updatedObject = (null == object) ? object : object instanceof BigDecimal ? ((BigDecimal) object).doubleValue()
-                    : object instanceof BigInteger ? ((BigInteger) object).intValue() : object;
-            updatedRecord.add(updatedObject);
-        }
-        return updatedRecord;
-    }
+	public boolean parsableToInteger(String number) {
+		if (StringUtils.isEmpty(number)) {
+			return false;
+		}
+		try {
+			Integer.parseInt(number);
+			return true;
+		} catch (NumberFormatException ex) {
+			return false;
+		}
+	}
 
+	public boolean parseableToDouble(String number) {
+		if (StringUtils.isEmpty(number)) {
+			return false;
+		}
+		try {
+			Double.parseDouble(number);
+			return true;
+		} catch (NumberFormatException ex) {
+			return false;
+		}
+	}
+
+	public static List<Object> convertBigIntegerAndBigDecimal(Object[] record) {
+		List<Object> updatedRecord = new ArrayList<>(record.length);
+		Object updatedObject;
+
+		for (Object object : record) {
+			updatedObject = (null == object) ? object
+					: object instanceof BigDecimal ? ((BigDecimal) object).doubleValue()
+							: object instanceof BigInteger ? ((BigInteger) object).intValue() : object;
+			updatedRecord.add(updatedObject);
+		}
+		return updatedRecord;
+	}
+
+	public static String getIndianFormat(Double amount) {
+		
+		if(Objects.isNull(amount)) {
+			return null;
+		}
+		
+		return india.format(amount);
+	}
 }
---
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/response/CommercialDetailsResponseDto.java
@@ -0,0 +1,53 @@
+package com.stanzaliving.wanda.response;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.stanzaliving.wanda.venta.response.MonthlyFeeDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Setter
+public class CommercialDetailsResponseDto {
+
+	private LocalDate moveInDate;
+
+	private String moveInDateText;
+	
+	private String contractStartDate;
+
+	private String contractStartDateText;
+	
+	private String contractPeriod;
+	
+	private String contractPeriodText;
+	
+	private MonthlyFeeDto monthlyFeeDto;
+	
+	private String monthlyFeeText;
+	
+	private String securityDeposit;
+	
+	private String securityDepositText;
+	
+	private String maintenanceFee;
+	
+	private String maintenanceFeeText;
+
+	private String advanceFee;
+	
+	private String advanceFeeText;
+	
+	private List<String> advanceFeeDescription;
+	
+}
