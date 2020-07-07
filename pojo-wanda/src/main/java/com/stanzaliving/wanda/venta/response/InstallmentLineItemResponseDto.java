357e00d734dfd3d581c0cc15f56115143599cb31
chaman0302 <chaman.parauliya@stanzaliving.com>
2020-07-07 10:06:26 +0530

    add_some_dtos_for_payment_plan

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/response/InstallmentLineItemResponseDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/response/InstallmentLineItemResponseDto.java
@@ -1,18 +1,17 @@
 package com.stanzaliving.wanda.venta.response;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.HashMap;
-import java.util.List;
-
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PaymentPlanResponseDto {
-
-
+public class InstallmentLineItemResponseDto {
+    private String name;
+    private Double lineItemAmount;
+    private Double lineItemAmountPaid;
 }
