45a6cc1f86ba6490f34855efdc0977e7f60f1f6e
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-07-07 07:37:40 +0530

    adding security deposit

--- pojo/src/main/java/com/stanzaliving/collector/enums/ResidentTimelineType.java
+++ pojo/src/main/java/com/stanzaliving/collector/enums/ResidentTimelineType.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ResidentTimelineType {
-    PLAN_OF_ACTION(""), BOOKING_AMOUNT("Booking Amount"), PAYMENT("Payment"),
-    RENTAL_FEE("Rental Fee"), SERVICE_FEE("Service Fee"), FOOD_FEE("Food Fee"),
+    PLAN_OF_ACTION(""), BOOKING_AMOUNT("Booking Amount"), SECURITY_DEPOSIT("Security Deposit"),
+    PAYMENT("Payment"), RENTAL_FEE("Rental Fee"), SERVICE_FEE("Service Fee"), FOOD_FEE("Food Fee"),
     VAS_FEE("VAS Fee"), AMC_FEE("AMC_Fee"), PROCESSING_FEE("Processing Fee"),
     PENALTY("Penalty"), CREDIT_NOTE("Credit Note");
 
