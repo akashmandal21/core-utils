3e031dcc71a55c0866ee3ced47783282504fbfa9
vaibhav-badole <vaibhav.badole@stanzaliving.com>
2020-07-07 17:55:10 +0530

    added getter setter

--- pojo-ops/src/main/java/com/stanzaliving/operations/dto/servicemix/ResidenceFoodCategoryChangeDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/operations/dto/servicemix/ResidenceFoodCategoryChangeDto.java
@@ -2,6 +2,11 @@ package com.stanzaliving.operations.dto.servicemix;
 
 import java.time.LocalDate;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ResidenceFoodCategoryChangeDto {
 	String residenceId;
 	LocalDate fromDate;
