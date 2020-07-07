14bd9363f37b5e277a6e255414109945d2d884e2
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-07-07 22:50:09 +0530

    timeline view suggestions

--- pojo/src/main/java/com/stanzaliving/collector/enums/ResidentTimelineType.java
+++ pojo/src/main/java/com/stanzaliving/collector/enums/ResidentTimelineType.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ResidentTimelineType {
-    PLAN_OF_ACTION(""), BOOKING_AMOUNT("Booking Amount"), SECURITY_DEPOSIT("Security Deposit"),
+    PLAN_OF_ACTION(""), BOOKING_AMOUNT("Annual Maintenance Charges"), SECURITY_DEPOSIT("Security Deposit"),
     PAYMENT("Payment"), RENTAL_FEE("Rental Fee"), SERVICE_FEE("Service Fee"), FOOD_FEE("Food Fee"),
-    VAS_FEE("VAS Fee"), AMC_FEE("AMC_Fee"), PROCESSING_FEE("Processing Fee"),
+    VAS_FEE("VAS Fee"), AMC_FEE("AMC Fee"), PROCESSING_FEE("Staggered plan processing fee"),
     PENALTY("Penalty"), CREDIT_NOTE("Credit Note");
 
     private String responseString;
