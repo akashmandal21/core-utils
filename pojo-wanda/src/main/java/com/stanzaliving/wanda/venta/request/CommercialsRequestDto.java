a3175e39a60dfe157b4d39996af7f8e08e344c58
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-07-08 14:01:42 +0530

    Changes done for update commercials

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/request/CommercialsRequestDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/request/CommercialsRequestDto.java
@@ -5,13 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class CommercialsRequestDto {
     private String bookingUuid;
     private String moveInDate;
     private Integer contractId;
+    private String couponCode;
 }
