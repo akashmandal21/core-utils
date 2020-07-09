d33db70d3ecc9a1c51d2f1cb44a8ae0610c3779e
chaman0302 <chaman.parauliya@stanzaliving.com>
2020-07-09 15:29:14 +0530

    handle_error

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/request/InstallmentRequestDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/request/InstallmentRequestDto.java
@@ -12,5 +12,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class InstallmentRequestDto {
     private String bookingUuid;
-    private InstallmentTypeEnum InstallmentType;
+    private InstallmentTypeEnum installmentType;
 }
