168f5f980da21219b090dff1a715c8b225e09f81
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-07-11 17:04:53 +0530

    query dto

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/query/FoodOrderPackagedPaxCountDto.java
@@ -0,0 +1,37 @@
+package com.stanzaliving.core.food.dto.query;
+
+import com.stanzaliving.core.operations.enums.MealType;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+/**
+ * @author piyush srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @date 11-july-2020
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodOrderPackagedPaxCountDto {
+	private LocalDate menuDate;
+
+	private String vendorId;
+
+	private String residenceId;
+
+	private MealType mealType;
+
+	private Integer vegPaxCount;
+
+	private Integer nonVegPaxCount;
+
+	private Integer stanzaStaffVegPaxCount;
+
+	private Integer nonStanzaStaffVegPaxCount;
+}
