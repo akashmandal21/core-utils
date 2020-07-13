facd6cdb0111f1888c487f39cf4303e95004eec1
chaman0302 <chaman.parauliya@stanzaliving.com>
2020-07-13 20:41:52 +0530

    add_some_field_in ResidenceServiceDto

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/response/ResidenceServiceDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/response/ResidenceServiceDto.java
@@ -8,10 +8,11 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode(of = {"serviceId"})
 public class ResidenceServiceDto {
-    
 	private String serviceName;
     private Boolean optional;
     private Double price;
     private Boolean enabled;
     private Integer serviceId;
+    private String serviceImage;
+    private String description;
 }
