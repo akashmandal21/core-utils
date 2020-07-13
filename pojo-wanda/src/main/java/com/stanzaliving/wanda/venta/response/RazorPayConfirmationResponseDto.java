ca10db82bc6d4c30fcb0fba8764658e74a4999fd
Ankit Jindal <ankit.jindal@stanzaliving.com>
2020-07-13 17:22:16 +0530

    Razor Pay Confirmation

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/response/RazorPayConfirmationResponseDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/response/RazorPayConfirmationResponseDto.java
@@ -1,10 +1,14 @@
 package com.stanzaliving.wanda.venta.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RazorPayConfirmationResponseDto {
 
 	private String message;
