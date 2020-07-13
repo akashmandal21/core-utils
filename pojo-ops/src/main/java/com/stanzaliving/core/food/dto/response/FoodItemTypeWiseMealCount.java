3fc42f44c07e1a752b620216c19dfc1ec37e3e7b
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-07-13 15:27:16 +0530

    added dto

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/FoodItemTypeWiseMealCount.java
@@ -0,0 +1,25 @@
+package com.stanzaliving.core.food.dto.response;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author piyush srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @date 05-May-2020
+ *
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodItemTypeWiseMealCount {
+
+	private String dispatchItemUuid;
+	private String itemTypeName;
+
+	private Integer numberOfMeals;
+}
