942437ec8c66358bd64a28c0e41c79ab3654838c
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-07-16 23:03:06 +0530

    added order in multimenu: dispatch item type

--- pojo-ops/src/main/java/com/stanzaliving/core/food/enums/DispatchSummaryItemType.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/enums/DispatchSummaryItemType.java
@@ -7,13 +7,13 @@ import lombok.Getter;
 @Getter
 public enum DispatchSummaryItemType {
 
-	VEG("Veg Meals", "Veg"),
-	NONVEG("Non Veg Meals", "Non-Veg"),
-	STAFF("Non-SL Staff Meals", "Non-SL Staff"),
-	VAS_VEG("VAS Veg", "VAS Veg"),
-	VAS_NON_VEG("VAS Non-Veg", "VAS Non-Veg");
+	VEG("Veg Meals", "Veg",1),
+	NONVEG("Non Veg Meals", "Non-Veg",2),
+	STAFF("Non-SL Staff Meals", "Non-SL Staff",5),
+	VAS_VEG("VAS Veg", "VAS Veg",3),
+	VAS_NON_VEG("VAS Non-Veg", "VAS Non-Veg",4);
 
 	private String mealName;
 	private String labelName;
-
+	private Integer order;
 }
