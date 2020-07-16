fa226763138778e0720dea71c9ad8dd89cd36d81
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-07-16 20:53:52 +0530

    updated VAS name

--- pojo-ops/src/main/java/com/stanzaliving/core/food/enums/DispatchSummaryItemType.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/enums/DispatchSummaryItemType.java
@@ -10,8 +10,8 @@ public enum DispatchSummaryItemType {
 	VEG("Veg Meals", "Veg"), 
 	NONVEG("Non Veg Meals", "Non-Veg"), 
 	STAFF("Non-SL Staff Meals", "Non-SL Staff"),
-	VAS_VEG("Vas Veg", "Vas Veg"),
-	VAS_NON_VEG("Vas Non-Veg", "Vas Non-Veg");
+	VAS_VEG("VAS Veg", "VAS Veg"),
+	VAS_NON_VEG("VAS Non-Veg", "VAS Non-Veg");
 
 	private String mealName; 
 	private String labelName;
