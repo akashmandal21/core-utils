83c2688b11000342267662c87496bc3085c957fb
chaman0302 <chaman.parauliya@stanzaliving.com>
2020-07-23 10:20:06 +0530

    add referrral Dtos

---
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/response/ReferralCodeUnlockLevelResponseDto.java
@@ -0,0 +1,17 @@
+package com.stanzaliving.wanda.venta.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReferralCodeUnlockLevelResponseDto {
+    private Integer levelNo;
+    private Integer minCriteria;
+    private Integer maxCriteria;
+    private Double amount;
+}
