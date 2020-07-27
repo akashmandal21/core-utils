c4665c59750a7b23d58826b0547bbc8fed79d3ec
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-07-27 20:06:44 +0530

    updated rule dto

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/request/CategoryApprovalUpdatedRuleDto.java
@@ -0,0 +1,22 @@
+package com.stanzaliving.core.food.dto.request;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author piyush srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @date 27-July-2020
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryApprovalUpdatedRuleDto {
+	private String rule;
+
+	private boolean isUpdated;
+}
