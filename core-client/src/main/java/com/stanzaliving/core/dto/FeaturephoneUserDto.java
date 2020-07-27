b771200bd70030fe72dd58bb925711cc8982565e
vaibhav-badole <vaibhav.badole@stanzaliving.com>
2020-07-27 23:21:46 +0530

    added feature phone api

---
+++ core-client/src/main/java/com/stanzaliving/core/dto/FeaturephoneUserDto.java
@@ -0,0 +1,32 @@
+package com.stanzaliving.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeaturephoneUserDto {
+
+	private Integer userId;
+
+	private String name;
+
+	private String mobile;
+
+	private String userCode;
+
+	private String email;
+
+	private String profilePictureUrl;
+	
+	private String room;
+
+}
