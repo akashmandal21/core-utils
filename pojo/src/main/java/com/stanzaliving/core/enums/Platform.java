7aaf4650e40d8b4ded06d84efbac151fa0fe4c7a
himanirathi <himani_rathi39@yahoo.com>
2020-07-28 14:52:14 +0530

    code review

--- pojo/src/main/java/com/stanzaliving/core/enums/Platform.java
+++ pojo/src/main/java/com/stanzaliving/core/enums/Platform.java
@@ -3,9 +3,11 @@ package com.stanzaliving.core.enums;
 import java.util.HashMap;
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum Platform {
 	Android("android"), Ios("ios");
 	
@@ -17,10 +19,6 @@ public enum Platform {
 		for (Platform e : Platform.values()) {
 			byPlatform.put(e.getPlatform(), e);
 		}
-	}
-	
-	private Platform(String platform) {
-		this.platform = platform;
 	}
 	
 	public static boolean existsInPlatform(String platform) {
