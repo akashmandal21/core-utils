928b59613ed97b8c4cd0ca863e118af458006e5b
vaibhav-badole <vaibhav.badole@stanzaliving.com>
2020-08-03 16:19:30 +0530

    added add method

--- base/src/main/java/com/stanzaliving/core/base/utils/MathUtil.java
+++ base/src/main/java/com/stanzaliving/core/base/utils/MathUtil.java
@@ -1,5 +1,7 @@
 package com.stanzaliving.core.base.utils;
 
+import java.util.Objects;
+
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -22,5 +24,22 @@ public class MathUtil {
 
 		return sum / count;
 
+	}
+	
+	public Integer add(Integer a, Integer b) {
+
+		if (Objects.isNull(a) && Objects.isNull(b)) {
+			return null;
+		}
+
+		if (Objects.isNull(a) && Objects.nonNull(b)) {
+			return b;
+		}
+
+		if (Objects.isNull(b) && Objects.nonNull(a)) {
+			return a;
+		}
+
+		return a + b;
 	}
 }
