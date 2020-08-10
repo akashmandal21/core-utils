10028f61422778728602965357aee772d8f39a2e
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-08-10 20:00:20 +0530

    Food Item Name Map

--- pojo-ops/src/main/java/com/stanzaliving/core/food/enums/FoodItemType.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/enums/FoodItemType.java
@@ -3,6 +3,9 @@ package com.stanzaliving.core.food.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @AllArgsConstructor
 public enum FoodItemType {
@@ -12,5 +15,17 @@ public enum FoodItemType {
 	SPECIAL_VEG("Special-Veg");
 
 	private String itemType;
+
+	private static Map<String, FoodItemType> foodItemTypeMap = new HashMap<>();
+
+	static {
+		for (FoodItemType foodItemType : FoodItemType.values()) {
+			foodItemTypeMap.put(foodItemType.getItemType(), foodItemType);
+		}
+	}
+
+	public static Map<String, FoodItemType> getFoodItemTypeNameMap() {
+		return foodItemTypeMap;
+	}
 
 }
