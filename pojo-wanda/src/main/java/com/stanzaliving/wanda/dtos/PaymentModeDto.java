39ad24a7b002c8b6a4d495f30b9e8ef308b75f11
ankitjindal <ankit.jindal@stanzaliving.com>
2020-08-12 18:01:01 +0530

    Booking Uuid changed

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/dtos/PaymentModeDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/dtos/PaymentModeDto.java
@@ -5,6 +5,7 @@ import com.stanzaliving.wanda.enums.WandaPaymentMode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -13,6 +14,7 @@ import lombok.ToString;
 @Getter
 @ToString
 @Setter
+@NoArgsConstructor
 public class PaymentModeDto {
 
 	private WandaPaymentMode wandaPaymentMode;
