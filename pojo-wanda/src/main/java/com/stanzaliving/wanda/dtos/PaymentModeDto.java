970a5ce0712ff4537ec4b010fec8e21f9b3386d3
ankitjindal <ankit.jindal@stanzaliving.com>
2020-08-12 13:30:54 +0530

    Payment Mode changed

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/dtos/PaymentModeDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/dtos/PaymentModeDto.java
@@ -1,16 +1,20 @@
 package com.stanzaliving.wanda.dtos;
 
+import javax.persistence.SecondaryTable;
+
 import com.stanzaliving.wanda.enums.WandaPaymentMode;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
 @Builder
 @Getter
 @ToString
+@Setter
 public class PaymentModeDto {
 
 	private WandaPaymentMode wandaPaymentMode;
