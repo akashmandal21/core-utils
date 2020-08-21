c1822442474cb7ecfa488daa6c5c49c02dbd9391
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-08-21 20:41:33 +0530

    base consumer added toDto new Method

--- kafka/src/main/java/com/stanzaliving/core/kafka/consumer/BaseConsumer.java
+++ kafka/src/main/java/com/stanzaliving/core/kafka/consumer/BaseConsumer.java
@@ -10,6 +10,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.log4j.Log4j2;
 
+import java.util.Objects;
+
 @Log4j2
 public abstract class BaseConsumer<T> {
 
@@ -36,7 +38,38 @@ public abstract class BaseConsumer<T> {
 
 				log.info("BaseConsumer:: Retrived class: {}", clazz);
 
-				data = objectMapper.readValue(record.value(), clazz);
+				data = getDto(record, clazz);
+
+			} else {
+				log.warn("BaseConsumer:: Kafka Record Missing key");
+			}
+
+		} catch (Exception e) {
+			log.error("BaseConsumer:: Error casting Kafka record to desrired dto: ", e);
+		} finally {
+			MDC.remove(StanzaConstants.GUID);
+		}
+
+		return data;
+	}
+
+	protected T getDto(ConsumerRecord<String, String> record, Class clazz) {
+		record.headers().forEach(header -> {
+			if (StanzaConstants.MESSAGE_ID.equalsIgnoreCase(header.key()))
+				MDC.put(StanzaConstants.GUID, new String(header.value()));
+		});
+
+		T data = null;
+
+		log.info("BaseConsumer:: Received Payload {}", record);
+
+		try {
+
+			if (Objects.nonNull(clazz)) {
+
+				log.info("BaseConsumer:: Retrived class: {}", clazz);
+
+				data = (T) objectMapper.readValue(record.value(), clazz);
 
 				log.info("BaseConsumer:: Retrieved object: {}", data);
 
@@ -52,5 +85,4 @@ public abstract class BaseConsumer<T> {
 
 		return data;
 	}
-
 }
