419297de4708a576f6360f827483647ff1870de0
ankitjindal <ankit.jindal@stanzaliving.com>
2020-08-26 10:19:03 +0530

    Commit for Home screen due

---
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/response/PendingDuesDetailsResponseDto.java
@@ -0,0 +1,13 @@
+package com.stanzaliving.wanda.venta.response;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class PendingDuesDetailsResponseDto {
+    private Long totalAmountToBePaid;
+    private Long duesPaid;
+    private Long duesTillDate;
+    private Long currentOutstanding;
+}
