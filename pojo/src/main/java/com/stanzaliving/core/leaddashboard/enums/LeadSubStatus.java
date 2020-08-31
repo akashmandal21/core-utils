3a4c69345a99a787710a62232d029c3c0f354ed1
RajKumarStanza <raj.singh@stanzaliving.com>
2020-08-31 16:13:56 +0530

    Added colorcode with status name

--- pojo/src/main/java/com/stanzaliving/core/leaddashboard/enums/LeadSubStatus.java
+++ pojo/src/main/java/com/stanzaliving/core/leaddashboard/enums/LeadSubStatus.java
@@ -7,27 +7,28 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum LeadSubStatus {
 
-	NEW_LEAD("New Lead"),
-	CALLED("Called"),
-	CONNECTED("Connected"),
-	QUALIFIED("Qualified"),
-	PRE_QUALIFICATION_FOLLOW_UP("Pre Qualification Follow Up"),
-	RNR("RNR"),
+	NEW_LEAD("New Lead","#F45E58"),
+	CALLED("Called","#F45E58"),
+	CONNECTED("Connected","#F45E58"),
+	QUALIFIED("Qualified","#F45E58"),
+	PRE_QUALIFICATION_FOLLOW_UP("Pre Qualification Follow Up","#F45E58"),
+	RNR("RNR","#50514F"),
 	
-	DISQUALIFIED("Disqualified"),
+	DISQUALIFIED("Disqualified","#F45E58"),
 
-	PRE_VISIT_FOLLOW_UP("Pre Visit Follow Up"),
-	PRE_VISIT_SCHEDULED_FOLLOW_UP("Pre Visit 1 Scheduled Follow Up"),
-	PRE_VISIT_SCHEDULED_DROP("Pre Visit  Scheduled Drop"),
+	PRE_VISIT_FOLLOW_UP("Pre Visit Follow Up","#F45E58"),
+	PRE_VISIT_SCHEDULED_FOLLOW_UP("Pre Visit 1 Scheduled Follow Up","#EEC583"),
+	PRE_VISIT_SCHEDULED_DROP("Pre Visit  Scheduled Drop","#EEC583"),
 
-	POST_VISIT_1_DROP("Post Visit 1 Drop"),
-	POST_VISIT_1_FOLLOW_UP("Post Visit 1 Follow Up"),
-	POST_VISIT_1_FOLLOW_UP_COMPLETED("Post Visit 1 Follow Up Completed"),
+	POST_VISIT_1_DROP("Post Visit 1 Drop","#EEC583"),
+	POST_VISIT_1_FOLLOW_UP("Post Visit 1 Follow Up","#EEC583"),
+	POST_VISIT_1_FOLLOW_UP_COMPLETED("Post Visit 1 Follow Up Completed","#EEC583"),
 	
 
-	POST_VISIT_2_DROP("Post Visit 2 Drop"),
-	POST_VISIT_2_FOLLOW_UP("Post Visit 2 Follow Up"),
-	POST_VISIT_2_FOLLOW_UP_COMPLETED("Post Visit 2 Follow Up Completed");
+	POST_VISIT_2_DROP("Post Visit 2 Drop","#50514F"),
+	POST_VISIT_2_FOLLOW_UP("Post Visit 2 Follow Up","#50514F"),
+	POST_VISIT_2_FOLLOW_UP_COMPLETED("Post Visit 2 Follow Up Completed","#50514F");
 	
 	private String statusDescription;
+	private String colorCode;
 }
