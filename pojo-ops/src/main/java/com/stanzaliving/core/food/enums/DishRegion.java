3812e84ada9233ce7eaee09c693a7336da219404
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-09-01 19:49:21 +0530

    dish region listing

--- pojo-ops/src/main/java/com/stanzaliving/core/food/enums/DishRegion.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/enums/DishRegion.java
@@ -1,9 +1,12 @@
 package com.stanzaliving.core.food.enums;
 
+import com.stanzaliving.core.base.common.dto.ListingDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,13 +30,20 @@ public enum DishRegion {
 
 	private static Map<String, DishRegion> dishRegionNameMap = new HashMap<>();
 
+	private static List<ListingDto> dishRegionListing = new ArrayList<>();
+
 	static {
 		for(DishRegion dishRegion: DishRegion.values()) {
 			dishRegionNameMap.put(dishRegion.getRegionName(), dishRegion);
+			dishRegionListing.add(ListingDto.builder().id(dishRegion.name()).name(dishRegion.getRegionName()).build());
 		}
 	}
 
 	public static Map<String, DishRegion> getDishRegionNameMap() {
 		return dishRegionNameMap;
+	}
+
+	public static List<ListingDto> getDishRegionListing() {
+		return dishRegionListing;
 	}
 }
