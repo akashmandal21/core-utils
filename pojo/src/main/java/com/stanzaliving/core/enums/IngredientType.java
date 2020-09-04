619a1b66dbe9b7eb7efc72841acc128b40c5fe83
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-09-04 18:47:48 +0530

    ingredient types

--- pojo/src/main/java/com/stanzaliving/core/enums/IngredientType.java
+++ pojo/src/main/java/com/stanzaliving/core/enums/IngredientType.java
@@ -3,13 +3,28 @@ package com.stanzaliving.core.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @AllArgsConstructor
 public enum IngredientType {
 
 	RAW_INGREDIENT("Raw Ingredient"),
-	PROCESSED_INGREDIENT("Processed Ingredient");
+	SEMI_PROCESSED("Processed Ingredient"),
+	DIRECT_SALE("Direct Sale");
 
 	private String typeName;
 
+	private static Map<String, IngredientType> ingredientsTypeByStr = new HashMap<>();
+
+	static {
+		for (IngredientType ingredientType: IngredientType.values()) {
+			ingredientsTypeByStr.put(ingredientType.name(), ingredientType);
+		}
+	}
+
+	public static IngredientType getIngredientsByStr(String ingredientsStr) {
+		return ingredientsTypeByStr.get(ingredientsStr);
+	}
 }
