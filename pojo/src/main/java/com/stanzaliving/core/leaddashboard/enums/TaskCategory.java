46ad568955b498264e579ad900312b3788532123
RajKumarStanza <raj.singh@stanzaliving.com>
2020-09-07 15:15:23 +0530

    Task categeory color

--- pojo/src/main/java/com/stanzaliving/core/leaddashboard/enums/TaskCategory.java
+++ pojo/src/main/java/com/stanzaliving/core/leaddashboard/enums/TaskCategory.java
@@ -3,12 +3,19 @@
  */
 package com.stanzaliving.core.leaddashboard.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @author raj.kumar
  *
  */
+@Getter
+@AllArgsConstructor
 public enum TaskCategory {
 
-	TASK,
-	MEETING;
+	TASK("#58CCB6"),
+	MEETING("#007AFF");
+	
+	private String taskCategoryColor;
 }
