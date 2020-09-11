64bab9571b17e4b50bee011e2453340ac57af4c3
RajKumarStanza <raj.singh@stanzaliving.com>
2020-09-11 19:09:37 +0530

    Property visit key identifier

--- pojo/src/main/java/com/stanzaliving/core/leaddashboard/constants/LeadQuestionIdentifier.java
+++ pojo/src/main/java/com/stanzaliving/core/leaddashboard/constants/LeadQuestionIdentifier.java
@@ -22,4 +22,22 @@ public class LeadQuestionIdentifier {
     public static final String LEAD_UNREACHABLE_ANSWER = "Lead Unreachable (EOD)";
     
     public static final String DROPPED = "Dropped";
+    
+    public static final String PROPERTY_1 = "property1";
+    public static final String PROPERTY_2 = "property2";
+    public static final String PROPERTY_3 = "property3";
+    public static final String PROPERTY_4 = "property4";
+    public static final String PROPERTY_5 = "property5";
+
+    public static final String VISIT_1_DATE = "visit1Date";
+    public static final String VISIT_2_DATE = "visit2Date";
+    public static final String VISIT_3_DATE = "visit3Date";
+    public static final String VISIT_4_DATE = "visit4Date";
+    public static final String VISIT_5_DATE = "visit5Date";
+    
+    public static final String PROPERTY_1_VISITING = "visit1PropertyVisiting";
+    public static final String PROPERTY_2_VISITING = "visit2PropertyVisiting";
+    public static final String PROPERTY_3_VISITING = "visit3PropertyVisiting";
+    public static final String PROPERTY_4_VISITING = "visit4PropertyVisiting";
+    public static final String PROPERTY_5_VISITING = "visit5PropertyVisiting";
 }
