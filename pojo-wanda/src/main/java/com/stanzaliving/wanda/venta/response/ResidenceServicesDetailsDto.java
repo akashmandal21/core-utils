0dbe47eda2c9a5917090a1cc624ace56d1f37d8f
ankitjindal <ankit.jindal@stanzaliving.com>
2020-09-14 12:32:51 +0530

    services data added

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/response/ResidenceServicesDetailsDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/response/ResidenceServicesDetailsDto.java
@@ -4,14 +4,18 @@ import java.util.List;
 import java.util.Set;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@Setter
 public class ResidenceServicesDetailsDto {
 
 	private Set<ResidenceServiceDto> residenceServices;
     private List<ResidenceServiceDto> packagedServices;
     private Set<ResidenceServiceDto> allValueAddedServices;
     private Set<ResidenceServiceDto> preSelectedValueAddedServices;
+    private String packagedServicesText;
+    private String valueAddedServicesText;
 }
