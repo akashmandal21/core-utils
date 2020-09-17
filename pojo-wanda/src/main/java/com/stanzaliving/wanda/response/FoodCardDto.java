a85128e0778c8982a3ad604d93b8150809d6958c
ankitjindal <ankit.jindal@stanzaliving.com>
2020-09-17 12:47:21 +0530

    Food Info Response

---
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/response/FoodCardDto.java
@@ -0,0 +1,25 @@
+package com.stanzaliving.wanda.response;
+
+import com.stanzaliving.core.food.enums.FoodItemType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class FoodCardDto {
+
+	private FoodItemType foodItemType;
+	
+	private String foodItemImage;
+	
+	private String foodName;
+	
+	private String foodItems;
+}
