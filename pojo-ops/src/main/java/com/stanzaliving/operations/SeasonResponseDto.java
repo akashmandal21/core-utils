ba4887386bc2726189c1fb435666ed05e4e2d8da
chaman0302 <chaman.parauliya@stanzaliving.com>
2020-09-21 23:52:36 +0530

    add_dto

---
+++ pojo-ops/src/main/java/com/stanzaliving/operations/SeasonResponseDto.java
@@ -0,0 +1,16 @@
+package com.stanzaliving.operations;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SeasonResponseDto {
+    private LocalDate fromDate;
+    private LocalDate toDate;
+    private String seasonName;
+}
