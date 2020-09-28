411adfa66ec877df27988461080965375186921c
Naveen Kumar <naveen.kumar@stanzaliving.com>
2020-09-28 17:54:27 +0530

    Refactor

--- pojo/src/main/java/com/stanzaliving/transformations/enums/UnitType.java
+++ pojo/src/main/java/com/stanzaliving/transformations/enums/UnitType.java
@@ -7,30 +7,27 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum UnitType {
 
-    NUMBER("nos."),
-    METERS("mtrs"),
-    CUBIT_FEET("cu.ft."),
-    SQUARE_FEET("sq.ft."),
-    KILOGRAM("kg"),
-    RFT("Running Feet"),
-    LS("Lump sum"),
-    PER_BED("Per Bed"),
-    MANDAYS("Mandays"),
-    LTS("ltr"),
-    PULSE("Pulse"),
-    FLAT_RATE("Flat Rate"),
-    PERCENTAGE("Percentage"),
-    LITERS("Ltrs")
-    ;
+	NUMBER("nos."),
+	METERS("mtrs"),
+	CUBIT_FEET("cu.ft."),
+	SQUARE_FEET("sq.ft."),
+	KILOGRAM("kg"),
+	RFT("Running Feet"),
+	LS("Lump sum"),
+	PER_BED("Per Bed"),
+	MANDAYS("Mandays"),
+	LTS("ltr"),
+	PULSE("Pulse"),
+	FLAT_RATE("Flat Rate"),
+	PERCENTAGE("Percentage"),
+	LITERS("Ltrs");
 
-    private String unitName;
-    UnitType(String unitName) {
-        this.unitName = unitName;
-    }
+	private String unitName;
 
-	public static Map<String, UnitType> unitByNameMap = new HashMap<>();
+	private static Map<String, UnitType> unitByNameMap = new HashMap<>();
 
 	static {
 
@@ -39,10 +36,8 @@ public enum UnitType {
 		}
 	}
 
-
-
-    public static UnitType getUnitTypeByName(String unitName) {
-        return unitByNameMap.get(unitName);
-    }
+	public static UnitType getUnitTypeByName(String unitName) {
+		return unitByNameMap.get(unitName);
+	}
 
 }
