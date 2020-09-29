55cb51a1bb17f101e62ef5803ab107a3f6821c31
chaman0302 <chaman.parauliya@stanzaliving.com>
2020-09-29 19:41:10 +0530

    change_current_status_string to_enum

--- pojo-ops/src/main/java/com/stanzaliving/core/opscalculator/dto/AnnualUnderWrittenResponseDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/opscalculator/dto/AnnualUnderWrittenResponseDto.java
@@ -24,7 +24,7 @@ public class AnnualUnderWrittenResponseDto extends AnnualUnderWrittenDto {
 
     private String residenceUuid;
 
-    private String currentStatus;               //get
+    private UnderWrittenStatus currentStatus;               //get
 
     private UnderWrittenStatus finalStatus;     //post
 
