7ae6ae747f11cb1fce9d39e8cfb91dd038b9f55a
Naveen Kumar <naveen.kumar@stanzaliving.com>
2020-09-30 16:57:11 +0530

    Refactor

--- pojo-ops/src/main/java/com/stanzaliving/core/utility/constants/MetricsType.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/utility/constants/MetricsType.java
@@ -1,18 +1,18 @@
 package com.stanzaliving.core.utility.constants;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum MetricsType {
-    DAILY_AVG_CONSUMPTION("Daily Average Consumption"),
-    TOTAL_CONSUMPTION("Total Consumption"),
-    PROCESS_ADHERENCE("Process Adherence"),
-    BUDGET_UTILIZATION("Budget Utilization"),
-    TOTAL_EXPENSE("Total Expense");
-    private final String name;
 
-    MetricsType(String name) {
-        this.name = name;
-    }
+	DAILY_AVG_CONSUMPTION("Daily Average Consumption"),
+	TOTAL_CONSUMPTION("Total Consumption"),
+	PROCESS_ADHERENCE("Process Adherence"),
+	BUDGET_UTILIZATION("Budget Utilization"),
+	TOTAL_EXPENSE("Total Expense");
 
-    public String getName() {
-        return name;
-    }
+	private String name;
+
 }
