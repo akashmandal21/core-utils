2cdd1f86fe90b748d01f9b0ab96bd6f23004b769
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-09-30 15:19:19 +0530

    vas type enum listing

--- pojo-ops/src/main/java/com/stanzaliving/core/food/enums/VasType.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/enums/VasType.java
@@ -1,7 +1,11 @@
 package com.stanzaliving.core.food.enums;
 
+import com.stanzaliving.core.user.enums.EnumListing;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
@@ -19,4 +23,19 @@ public enum VasType {
 	ADD_ON("Add On");
 
 	private String vasTypeName;
+
+	private static final List<EnumListing<VasType>> enumListing = new ArrayList<>();
+
+	static {
+		for (VasType vasType : VasType.values()) {
+			EnumListing<VasType> listing = EnumListing.of(vasType, vasType.getVasTypeName());
+
+			enumListing.add(listing);
+		}
+	}
+
+	public static List<EnumListing<VasType>> getEnumListing() {
+		return enumListing;
+	}
+
 }
