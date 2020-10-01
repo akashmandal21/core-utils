c7dc78caf614712bf9114d8044be8a28b2711398
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-10-01 08:41:53 +0530

    added gst view

---
+++ pojo/src/main/java/com/stanzaliving/transformations/projections/StanzaGstView.java
@@ -0,0 +1,15 @@
+package com.stanzaliving.transformations.projections;
+
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class StanzaGstView {
+    private String gstin;
+    private String gstStateId;
+    private String gstState;
+    private String address;
+}
