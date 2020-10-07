70f4da6385c0cb89498db87dbd28d22498658a91
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-10-08 01:20:04 +0530

    vas menu get api

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/VasMenuItemResponseDto.java
@@ -0,0 +1,21 @@
+package com.stanzaliving.core.food.dto.response;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @version 1.0
+ *
+ * @since 07-Oct-2020
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VasMenuItemResponseDto {
+}
--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/VasMenuResponseDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/VasMenuResponseDto.java
@@ -3,15 +3,23 @@ package com.stanzaliving.core.food.dto.response;
 import java.util.List;
 import java.util.Map;
 
-import com.stanzaliving.core.food.dto.VasItemDto;
 import com.stanzaliving.core.operations.enums.MealType;
 
+import com.stanzaliving.core.user.enums.EnumListing;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @version 1.0
+ *
+ * @since 07-Oct-2020
+ */
 
 @Getter
 @Setter
@@ -20,17 +28,9 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VasMenuResponseDto {
+	private EnumListing<MealType> meal;
 
-	private Map<MealType, List<VasItemDto>> menuMap;
-	
-	private Map<MealType, String> mealMapByMealType;
-	
-	private Integer maxItemsAllowed;
-	
-	private String orderWindowTime;
-	
-	private boolean orderWindowClosed; 
-	
-	private boolean oldOrderAvailable;
-	
+	private List<ItemCategoryCountDto> categoryCount;
+
+	List<VasMenuItemResponseDto> items;
 }
