4594254968428671d48f3e484b776dbbbd190021
RajKumarStanza <raj.singh@stanzaliving.com>
2020-10-07 16:43:02 +0530

    Lead detail upddate client

--- recon-client/src/main/java/com/stanzaliving/core/recon/client/api/ReconClientApi.java
+++ recon-client/src/main/java/com/stanzaliving/core/recon/client/api/ReconClientApi.java
@@ -14,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
+import com.stanzaliving.core.leaddashboard.dto.LeadDetailRequestDto;
 import com.stanzaliving.core.leaddashboard.dto.LeadVisitHistoryDto;
 
 import lombok.extern.log4j.Log4j2;
@@ -52,6 +53,30 @@ public class ReconClientApi {
 		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
 		
 		postBody = leadVisitHistory;
+		ParameterizedTypeReference<ResponseDto<String>> returnType = new ParameterizedTypeReference<ResponseDto<String>>() {
+		};
+		
+		return restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
+	}
+	
+	public ResponseDto<String> updateLeadDetail(List<LeadDetailRequestDto> leadDetailRequestDto) {
+		Object postBody = null;
+
+		// create path and map variables
+		final Map<String, Object> uriVariables = new HashMap<>();
+
+		String path = UriComponentsBuilder.fromPath("/lead/capture/detail").buildAndExpand(uriVariables).toUriString();
+
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+		
+		final HttpHeaders headerParams = new HttpHeaders();
+
+		final String[] accepts = {
+				"*/*"
+		};
+		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+		
+		postBody = leadDetailRequestDto;
 		ParameterizedTypeReference<ResponseDto<String>> returnType = new ParameterizedTypeReference<ResponseDto<String>>() {
 		};
 		
