4391f6c9660add55bea2a674a38a8fc7cdd5987d
ashutoshchandra-stanzaliving <ashutoshchandra.retainer@stanzaliving.com>
2020-10-13 17:46:49 +0530

    " cafe menu dto"

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/cafe/dto/CafeMenuDto.java
@@ -0,0 +1,36 @@
+package com.stanzaliving.core.cafe.dto;
+
+import com.stanzaliving.core.base.common.dto.AbstractDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CafeMenuDto extends AbstractDto{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String cafeId;
+	
+	private String cafeName;
+	
+	private String itemId;
+	
+	private String itemName;
+	
+	private double itemPrice;
+	
+	private int availableInventoryCount;	
+	
+	private boolean enabled;
+}
