937aeec343089ac37ab2298a2d82d7d034944b80
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-10-15 14:01:00 +0530

    packaging response in get api

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/CityPackagingMaterialResponseDto.java
@@ -0,0 +1,29 @@
+package com.stanzaliving.core.food.dto.response;
+
+import com.stanzaliving.core.base.common.dto.ListingDto;
+import com.stanzaliving.core.food.enums.PackagingMaterial;
+import com.stanzaliving.core.user.enums.EnumListing;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @since 15-Oct-2020
+ *
+ * @version 1.0
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CityPackagingMaterialResponseDto {
+
+	private ListingDto city;
+
+	private EnumListing<PackagingMaterial> material;
+
+}
---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/VasMasterPackagingResponseDto.java
@@ -0,0 +1,36 @@
+package com.stanzaliving.core.food.dto.response;
+
+import com.stanzaliving.core.food.dto.request.CityPackagingMaterialDto;
+import com.stanzaliving.core.food.enums.PackagingMaterial;
+import com.stanzaliving.core.food.enums.VasItemState;
+import com.stanzaliving.core.user.enums.EnumListing;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @since 15-Oct-2020
+ *
+ * @version 1.0
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VasMasterPackagingResponseDto {
+	private String packagingId;
+
+	private EnumListing<VasItemState> itemState;
+
+	private EnumListing<PackagingMaterial> masterMaterial;
+
+	private List<CityPackagingMaterialResponseDto> cityMaterial;
+}
