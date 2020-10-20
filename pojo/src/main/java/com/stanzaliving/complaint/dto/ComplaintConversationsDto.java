3ad7ea98b4804c025862e635189142cefe4dc859
Amit-K-G <amitgupta.retainer@stanzaliving.com>
2020-10-20 18:40:22 +0530

    complaint conversations dto added

---
+++ pojo/src/main/java/com/stanzaliving/complaint/dto/ComplaintConversationsDto.java
@@ -0,0 +1,39 @@
+package com.stanzaliving.complaint.dto;
+
+import com.stanzaliving.complaint.enums.ComplaintConversationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ComplaintConversationsDto {
+
+    private ComplaintConversationType complaintConversationType;
+
+    private String body;
+
+    private String bodyText;
+
+    private String attachmentName;
+
+    private String attachmentType;
+
+    private String attachmentPath;
+
+    private String attachmentBucket;
+
+    private Date createdAt;
+
+    private String createdAtText;
+
+}
