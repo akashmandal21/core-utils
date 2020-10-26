44c78725b5e1b576fb441bfb417d9c35f1eae166
Kapil.Dev <kapil.dev@stanzaliving.com>
2020-10-26 23:53:42 +0530

    Added electicity service common files in coreutils

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/electricity/constants/MeterType.java
@@ -0,0 +1,30 @@
+package com.stanzaliving.core.electricity.constants;
+
+import com.stanzaliving.core.user.enums.EnumListing;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@AllArgsConstructor
+public enum MeterType {
+
+    IOT("IOT"),
+    NON_IOT("Non IOT");
+
+    private String meterType;
+
+    private static List<EnumListing<MeterType>> meterTypes = new ArrayList<>();
+    static {
+        for(MeterType metertype: MeterType.values()){
+            meterTypes.add(EnumListing.of(metertype, metertype.getMeterType()));
+        }
+    }
+
+    public static  List<EnumListing<MeterType>> getMeterTypes(){
+        return meterTypes;
+    }
+}
