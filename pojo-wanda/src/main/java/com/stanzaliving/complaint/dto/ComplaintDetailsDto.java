c2eab53acc104fdf6e110a1384db56373e4239df
ankitjindal <ankit.jindal@stanzaliving.com>
2020-10-26 17:58:24 +0530

    Change for complaint details

--- pojo-wanda/src/main/java/com/stanzaliving/complaint/dto/ComplaintDetailsDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/complaint/dto/ComplaintDetailsDto.java
@@ -1,12 +1,21 @@
 package com.stanzaliving.complaint.dto;
 
-import java.util.TreeSet;
+import java.util.List;
 
 import com.stanzaliving.core.complaint.dto.ComplaintConversationsDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ComplaintDetailsDto {
 
-	private TreeSet<ComplaintConversationsDto> complaintConversationsDtos;
+	private List<ComplaintConversationsDto> complaintConversationsDtos;
 	
 	private ComplaintMetaDataDto complaintMetaDataDto;
 	
