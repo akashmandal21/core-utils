cf5f9336a4e4f17d0ee7e336c3a6be8bbef05a4f
ashutoshchandra-stanzaliving <ashutoshchandra.retainer@stanzaliving.com>
2020-10-28 02:38:53 +0530

    slot feature added

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/cafe/dto/ApplicableChargesDto.java
@@ -0,0 +1,34 @@
+package com.stanzaliving.core.cafe.dto;
+
+import com.stanzaliving.core.base.common.dto.AbstractDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApplicableChargesDto extends AbstractDto{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String chargesType;
+	
+	private double value;
+	
+	private String gst;
+	
+	private boolean mandatory;
+	
+	
+	
+}
