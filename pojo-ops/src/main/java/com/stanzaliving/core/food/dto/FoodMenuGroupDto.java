031e43464a5a24699fec18e9d6f784d4a11a5328
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2020-11-06 12:30:30 +0530

    central & vendor auto approved keys in dto

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/FoodMenuGroupDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/FoodMenuGroupDto.java
@@ -61,6 +61,10 @@ public class FoodMenuGroupDto extends AbstractDto {
 
 	private boolean autoSubmitted;
 
+	private boolean centralFAndBAutoApproved;
+
+	private boolean vendorAutoApproved;
+
 	private boolean comboAllowed;
 	
 	private boolean editingAllowed;
