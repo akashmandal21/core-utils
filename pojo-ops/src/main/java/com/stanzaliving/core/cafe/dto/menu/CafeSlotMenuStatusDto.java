d472083ffa05095eb6a3e4f2ad6b7e753c21ca12
ketan.yadav <ketan.yadav@stanzaliving.com>
2020-11-06 18:46:07 +0530

    serving menu changes

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/cafe/dto/menu/CafeSlotMenuStatusDto.java
@@ -0,0 +1,15 @@
+package com.stanzaliving.core.cafe.dto.menu;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CafeSlotMenuStatusDto {
+    private boolean active;
+    private Integer itemCount;
+}
