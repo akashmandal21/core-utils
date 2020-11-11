3f3a5f7dee077ef3b1ff9a4b9e420162e3e7572b
Naveen Kumar <naveen.kumar@stanzaliving.com>
2020-11-11 17:12:49 +0530

    Added dto for reading reminder

--- pojo-ops/src/main/java/com/stanzaliving/core/electricity/dto/MeterDtoCount.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/electricity/dto/MeterDtoCount.java
@@ -1,13 +1,16 @@
 package com.stanzaliving.core.electricity.dto;
 
+import java.util.List;
+
 import com.stanzaliving.core.base.common.dto.AbstractDto;
 import com.stanzaliving.core.base.common.dto.ListingCountDto;
-import com.stanzaliving.core.base.common.dto.ListingDto;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
 
-import java.util.List;
-import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
