d7b63dfbe6439bfa10fb2773b360142baeb36f37
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-11-11 14:16:35 +0530

    updated type

--- pojo/src/main/java/com/stanzaliving/core/grsi/dto/GrsiUpdateDto.java
+++ pojo/src/main/java/com/stanzaliving/core/grsi/dto/GrsiUpdateDto.java
@@ -1,15 +1,19 @@
 package com.stanzaliving.core.grsi.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class GrsiUpdateDto {
 
     private String potoUuid;
-    private Double grsiPct;
+    private BigDecimal grsiPct;
     private boolean grsiCompleted;
 }
