2e0aaaea24df1cd5f8c2e6e37c6c4b6a71fa6846
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-11-11 15:54:19 +0530

    added builder annotation

--- pojo/src/main/java/com/stanzaliving/core/grsi/dto/GrsiUpdateDto.java
+++ pojo/src/main/java/com/stanzaliving/core/grsi/dto/GrsiUpdateDto.java
@@ -1,12 +1,14 @@
 package com.stanzaliving.core.grsi.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class GrsiUpdateDto {
 
     private String potoUuid;
