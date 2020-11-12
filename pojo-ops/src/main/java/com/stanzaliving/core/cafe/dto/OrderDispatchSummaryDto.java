c10a272f7cff57a9c1d9919ee6bdb222835c2669
ashutoshchandra-stanzaliving <ashutoshchandra.retainer@stanzaliving.com>
2020-11-12 10:42:43 +0530

    dispatch dtos

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/cafe/dto/OrderDispatchSummaryDto.java
@@ -0,0 +1,35 @@
+package com.stanzaliving.core.cafe.dto;
+
+import com.stanzaliving.core.cafe.order.constants.OrderType;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class OrderDispatchSummaryDto {
+
+    private OrderType orderType;
+
+    private String orderId;
+
+    private String cafeId;
+
+    private String residenceId;
+
+    private String slotId;
+
+    private Integer rcCode;
+
+    private boolean orderStatus;
+
+    private Long hotbagsDispatchCount;
+
+    private Long orderPackedCount;
+
+    private Long packetDispatchCount;
+
+}
