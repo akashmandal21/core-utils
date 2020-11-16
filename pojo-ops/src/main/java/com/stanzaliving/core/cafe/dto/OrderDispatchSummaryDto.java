4d0b75ed3fa2f1a534f2e0e03b5c6f4d5ac99fe0
ashutoshchandra-stanzaliving <ashutoshchandra.retainer@stanzaliving.com>
2020-11-16 16:48:05 +0530

    fields added in summary Dto

--- pojo-ops/src/main/java/com/stanzaliving/core/cafe/dto/OrderDispatchSummaryDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/cafe/dto/OrderDispatchSummaryDto.java
@@ -1,5 +1,9 @@
 package com.stanzaliving.core.cafe.dto;
 
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotBlank;
+
 import com.stanzaliving.core.cafe.order.constants.OrderType;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -16,13 +20,21 @@ public class OrderDispatchSummaryDto {
 
     private String orderId;
 
+    @NotBlank(message = "Empty Cafe Id")
     private String cafeId;
 
     private String residenceId;
 
+    @NotBlank(message = "Empty Slot Id")
     private String slotId;
 
     private Integer rcCode;
+    
+    private LocalDateTime rcGeneratedAt;
+    
+    private boolean receivingStatus;
+    
+    private LocalDateTime rcReceivedAt;
 
     private boolean orderStatus;
 
@@ -31,5 +43,7 @@ public class OrderDispatchSummaryDto {
     private Long orderPackedCount;
 
     private Long packetDispatchCount;
+
+	
 
 }
