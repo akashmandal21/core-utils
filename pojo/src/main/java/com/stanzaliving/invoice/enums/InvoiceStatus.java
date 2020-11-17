138f8160cebdc8d45a057f85db2200614843d766
Amit-K-G <amitgupta.retainer@stanzaliving.com>
2020-11-17 11:53:47 +0530

    invoice status modified

--- pojo/src/main/java/com/stanzaliving/invoice/enums/InvoiceStatus.java
+++ pojo/src/main/java/com/stanzaliving/invoice/enums/InvoiceStatus.java
@@ -3,23 +3,44 @@ package com.stanzaliving.invoice.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @AllArgsConstructor
 @Getter
 public enum InvoiceStatus {
 
-    L1_APPROVAL_DUE("L1 Approval Due", "#FFB701"),
-    L2_APPROVAL_DUE("L2 Approval Due", "#FFB701"),
-    L3_APPROVAL_DUE("L3 Approval Due", "#FFB701"),
-    L1_REJECTED("L1 Rejected", "#F55F71"),
-    L2_REJECTED("L2 Rejected", "#F55F71"),
-    L3_REJECTED("L3 Rejected", "#F55F71"),
-    PAYMENT_PENDING("Payment Pending", null),
-    PARTIALLY_PAID("Partially Paid", null),
-    FULLY_PAID("Fully Paid", null);
+    IN_DRAFT("In Draft", null, -1),
+    L1_APPROVAL_DUE("L1 Approval Due", "#FFB701", 0),
+    L2_APPROVAL_DUE("L2 Approval Due", "#FFB701", 1),
+    L3_APPROVAL_DUE("L3 Approval Due", "#FFB701", 2),
+    L1_REJECTED("L1 Rejected", "#F55F71", -1),
+    L2_REJECTED("L2 Rejected", "#F55F71", -2),
+    L3_REJECTED("L3 Rejected", "#F55F71", -3),
+    PAYMENT_PENDING("Payment Pending", null, 3),
+    PARTIALLY_PAID("Partially Paid", null, 4),
+    FULLY_PAID("Fully Paid", null, 4);
 
     private String statusText;
 
     private String statusColor;
+
+    private Integer order;
+
+    public static Set<InvoiceStatus> approvalCycle = new HashSet<>(5);
+
+    public static Set<InvoiceStatus> approvalRejects = new HashSet<>(5);
+
+    public static Set<InvoiceStatus> paymentCycle = new HashSet<>(5);
+
+    static {
+
+        approvalCycle.addAll(Arrays.asList(L1_APPROVAL_DUE, L2_APPROVAL_DUE, L3_APPROVAL_DUE));
+        approvalRejects.addAll(Arrays.asList(L1_REJECTED, L2_REJECTED, L3_REJECTED));
+        paymentCycle.addAll(Arrays.asList(PAYMENT_PENDING, PARTIALLY_PAID));
+
+    }
 
 }
 
