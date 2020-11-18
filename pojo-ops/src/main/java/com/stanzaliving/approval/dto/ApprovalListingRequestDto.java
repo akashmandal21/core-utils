291b6202ec64f7e825d23aa154aad5a30ac80859
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-11-18 16:44:10 +0530

    using request dto

---
+++ pojo-ops/src/main/java/com/stanzaliving/approval/dto/ApprovalListingRequestDto.java
@@ -0,0 +1,24 @@
+package com.stanzaliving.approval.dto;
+
+import com.stanzaliving.approval.enums.ApprovalEntityType;
+import com.stanzaliving.core.opscalculator.enums.ApprovalStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApprovalListingRequestDto {
+    @Builder.Default
+    private List<String> residenceUuids = new ArrayList<>();
+    private int page;
+    private int limit;
+    private String roleName;
+    private ApprovalEntityType approvalEntityType;
+}
