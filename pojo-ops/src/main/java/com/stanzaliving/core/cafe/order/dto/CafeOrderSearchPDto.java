6a4eb98ba48e2b101eb518a869ab00dac7b2f9a2
ketan.yadav <ketan.yadav@stanzaliving.com>
2020-11-27 19:33:46 +0530

    date specification changes

--- pojo-ops/src/main/java/com/stanzaliving/core/cafe/order/dto/CafeOrderSearchPDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/cafe/order/dto/CafeOrderSearchPDto.java
@@ -1,14 +1,11 @@
 package com.stanzaliving.core.cafe.order.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.stanzaliving.core.cafe.order.constants.OrderSearchFilter;
 import com.stanzaliving.core.cafe.order.constants.OrderSearchStatus;
 import com.stanzaliving.core.cafe.order.constants.OrderType;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 @Getter
@@ -21,10 +18,7 @@ public class CafeOrderSearchPDto {
     private String orderId;
     private OrderSearchStatus searchStatus;
     private OrderType orderType;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDate orderDate;
     private String residenceId;
-    private LocalTime slotTime;
     private List<String> slotIds;
     private OrderSearchFilter filter;
     private String searchText;
