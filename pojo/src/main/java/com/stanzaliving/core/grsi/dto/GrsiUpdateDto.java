d911fe5033d32c14de354d2d5e7503cf5b30be61
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-11-27 16:14:04 +0530

    updayed

--- pojo/src/main/java/com/stanzaliving/core/grsi/dto/GrsiUpdateDto.java
+++ pojo/src/main/java/com/stanzaliving/core/grsi/dto/GrsiUpdateDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,4 +17,5 @@ public class GrsiUpdateDto {
     private String potoUuid;
     private BigDecimal grsiPct;
     private boolean grsiCompleted;
+    private LocalDateTime updatedAt;
 }
