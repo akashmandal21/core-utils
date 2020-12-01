dbb8f7f42d5c5caa3635814477f83bcc0ce65ebb
ketan.yadav <ketan.yadav@stanzaliving.com>
2020-12-01 14:55:46 +0530

    enum listing changes

--- pojo-ops/src/main/java/com/stanzaliving/core/cafe/order/constants/RefundMode.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/cafe/order/constants/RefundMode.java
@@ -1,7 +1,11 @@
 package com.stanzaliving.core.cafe.order.constants;
 
+import com.stanzaliving.core.user.enums.EnumListing;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -10,4 +14,17 @@ public enum RefundMode {
     ORIGINAL("Original");
 
     private final String mode;
+
+    private static final List<EnumListing<RefundMode>> enumListing = new ArrayList<>();
+
+    public static List<EnumListing<RefundMode>> getEnumListing() {
+        return enumListing;
+    }
+
+    static {
+        for (RefundMode curOption : RefundMode.values()) {
+            EnumListing<RefundMode> listing = EnumListing.of(curOption, curOption.getMode());
+            enumListing.add(listing);
+        }
+    }
 }
