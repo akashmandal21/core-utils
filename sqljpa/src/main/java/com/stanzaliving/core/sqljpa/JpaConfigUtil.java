c77df8da09518bb36bde8521963c66b2f9c1f8b8
ankitjindal <ankit.jindal@stanzaliving.com>
2020-12-02 12:25:50 +0530

    Log added

--- sqljpa/src/main/java/com/stanzaliving/core/sqljpa/JpaConfigUtil.java
+++ sqljpa/src/main/java/com/stanzaliving/core/sqljpa/JpaConfigUtil.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * 
@@ -18,11 +19,14 @@ import lombok.experimental.UtilityClass;
  * @date 30-Sep-2019
  */
 @UtilityClass
+@Log4j2
 public class JpaConfigUtil {
 
 	public static void configureDbCrendentials(
 			ComboPooledDataSource dataSource, String driverClassName, String dbUrl, String dbName, String dbPassword) throws PropertyVetoException {
 
+		log.info("db credentials are {} {}",dbUrl,dbName);
+		
 		dataSource.setDriverClass(driverClassName);
 		dataSource.setJdbcUrl(dbUrl);
 		dataSource.setUser(dbName);
