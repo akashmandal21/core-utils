34cb35c7f16759af572debf2f4945166c40bf9ce
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-12-02 12:17:38 +0530

    minor fix

--- transformation-client/src/main/java/com/stanzaliving/core/transformation/client/api/InternalDataControllerApi.java
+++ transformation-client/src/main/java/com/stanzaliving/core/transformation/client/api/InternalDataControllerApi.java
@@ -530,9 +530,9 @@ public class InternalDataControllerApi {
 
 		// create path and map variables
 		final Map<String, Object> uriVariables = new HashMap<>();
+		uriVariables.put("accessLevel", accessLevel);
 
 		String path = UriComponentsBuilder.fromPath("/internal/locations/all/").buildAndExpand(uriVariables).toUriString();
-		uriVariables.put("accessLevel", accessLevel);
 
 		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
 
