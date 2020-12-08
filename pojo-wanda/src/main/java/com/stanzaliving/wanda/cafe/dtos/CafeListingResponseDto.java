fa87af33d02e5bc7d753639d889545fc693556aa
ankitjindal <ankit.jindal@stanzaliving.com>
2020-12-08 11:38:21 +0530

    Cafe Listing Api

---
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/cafe/dtos/CafeListingResponseDto.java
@@ -0,0 +1,25 @@
+package com.stanzaliving.wanda.cafe.dtos;
+
+import java.util.List;
+
+import com.stanzaliving.core.cafe.enums.SlotType;
+import com.stanzaliving.core.cafe.order.constants.ServingMode;
+import com.stanzaliving.core.cafe.order.dto.CafeRDto;
+import com.stanzaliving.core.user.enums.EnumListing;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class CafeListingResponseDto {
+
+	private List<CafeRDto> cafeRDtos;
+	
+	private List<EnumListing<ServingMode>> servingModes;
+	
+	private List<EnumListing<SlotType>> slotTypes;;
+	
+}
