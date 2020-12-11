4e0f3520095de9a4c9a3077b14913fbb872e2815
jalaj gupta <jalaj.gupta@stanzaliving.com>
2020-12-11 16:05:43 +0530

    whatsp broker calling
    whatsp broker calling

--- venta-client/src/main/java/com/stanzaliving/core/backendlocator/client/api/VentaClientApi.java
+++ venta-client/src/main/java/com/stanzaliving/core/backendlocator/client/api/VentaClientApi.java
@@ -19,6 +19,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.stanzaliving.core.backendlocator.client.dto.ResidenceGstDto;
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
+import com.stanzaliving.core.user.dto.UserProfileDto;
 import com.stanzaliving.venta.BedCountDetailsDto;
 
 import lombok.extern.log4j.Log4j2;
@@ -88,4 +89,25 @@ public class VentaClientApi {
 
 	}
 
+	public HashMap<String, Object> getReferralCodeByUser(String userUuid) {
+
+		Object postBody = null;
+
+		final Map<String, Object> uriVariables = new HashMap<>();
+		//uriVariables.put("userUuid", userUuid);
+
+		String path = UriComponentsBuilder.fromPath("/broker").buildAndExpand().toUriString();
+
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+		queryParams.add("brokerMobile", userUuid);
+
+		final HttpHeaders headerParams = new HttpHeaders();
+
+		final String[] accepts = { "*/*" };
+		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+		ParameterizedTypeReference<HashMap<String, Object>> returnType = new ParameterizedTypeReference<HashMap<String, Object>>() {
+		};
+		return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
+	}
 }
