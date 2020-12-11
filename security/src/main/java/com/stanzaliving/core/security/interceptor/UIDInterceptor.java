f078c3e8c29fd5999be252b319589ec1263680b7
ankitjindal <ankit.jindal@stanzaliving.com>
2020-12-11 10:42:57 +0530

    Log Added for headers

--- security/src/main/java/com/stanzaliving/core/security/interceptor/UIDInterceptor.java
+++ security/src/main/java/com/stanzaliving/core/security/interceptor/UIDInterceptor.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.UUID;
 
 @Log4j2
@@ -29,7 +30,17 @@ public class UIDInterceptor extends HandlerInterceptorAdapter {
 		MDC.put(StanzaConstants.QUERY_STRING, request.getQueryString());
 
 		log.info("RequestReceived URI {} QueryString {} AppVersion {}", request.getRequestURI(), request.getQueryString(), request.getHeader("appversion"));
-
+		
+		Enumeration<String> headers = request.getHeaderNames();
+		
+		while (headers.hasMoreElements()) {
+			
+			String headerName = (String) headers.nextElement();
+			
+			log.info("header {} value is {}",request.getHeader(headerName));
+			
+		}
+		
 		request.setAttribute(StanzaConstants.GUID, guid);
 		request.setAttribute(StanzaConstants.LUID, luid);
 
