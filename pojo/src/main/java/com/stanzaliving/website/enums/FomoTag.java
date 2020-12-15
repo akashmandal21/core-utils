0b21e7ef0e54f511aaa9d37e4a73d30770c86736
Mohit Gupta <mohitgupta.retainer@stanzaliving.com>
2020-12-15 19:32:28 +0530

    fomoTag none added

--- pojo/src/main/java/com/stanzaliving/website/enums/FomoTag.java
+++ pojo/src/main/java/com/stanzaliving/website/enums/FomoTag.java
@@ -10,7 +10,8 @@ public enum FomoTag {
 	ALMOSTFULL("Almost Full", "#283264"), 
 	TOPSELLING("Top Selling", "#645A87"),
 	FILLINGFAST("Filling Fast", "#FAB432"),
-	SOLDOUT("Sold Out", "#F05A78");
+	SOLDOUT("Sold Out", "#F05A78"),
+	NONE("None","#7D7A7A");
 
 	public String fomoTagName;
 	public String fomoTagColour;
