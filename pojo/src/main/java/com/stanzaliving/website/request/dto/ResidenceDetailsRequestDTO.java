8892e8776cb9182017d1cad17e98a894b0ffd0a6
Mohit Gupta <mohitgupta.retainer@stanzaliving.com>
2020-12-17 13:10:40 +0530

    issue resolved

--- pojo/src/main/java/com/stanzaliving/website/request/dto/ResidenceDetailsRequestDTO.java
+++ pojo/src/main/java/com/stanzaliving/website/request/dto/ResidenceDetailsRequestDTO.java
@@ -31,7 +31,8 @@ public class ResidenceDetailsRequestDTO {
 	private Integer preBookingAmount;
 	private int priorityOrder;
 	private boolean virtualTour;
-	private String cardCTA;
+	private int cardCTAId;
+	private String cardCTAName;
 	private FomoTag fomoTag;
 	private String virtualTourURL;
 
