ada9c4f1234b250d6b6654f0a6ca8efcf369fa2e
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2020-12-28 21:30:42 +0530

    added enum handling

--- generic-validation-core/src/main/java/com/stanzaliving/core/generic/validation/enums/CompanyType.java
+++ generic-validation-core/src/main/java/com/stanzaliving/core/generic/validation/enums/CompanyType.java
@@ -3,6 +3,9 @@ package com.stanzaliving.core.generic.validation.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @AllArgsConstructor
 @Getter
 public enum CompanyType {
@@ -13,4 +16,17 @@ public enum CompanyType {
     PUB_LTD("Public Limited Company");
 
     private String companyTypeText;
+
+    private static Map<String,CompanyType> companyTypeMap = new HashMap<>();
+    static {
+        companyTypeMap.put("Sole Proprietorship",INDIVIDUAL);
+        companyTypeMap.put("Partnership Firm",PARTNERSHIP);
+        companyTypeMap.put("Limited Liability Partnership",LTD_LIABILITY_PARTNERSHIP);
+        companyTypeMap.put("Private Limited Company",PVT_LTD);
+        companyTypeMap.put("Public Limited Company",PUB_LTD);
+    }
+
+    public static CompanyType getCompanyType(String name){
+        return companyTypeMap.get(name);
+    }
 }
