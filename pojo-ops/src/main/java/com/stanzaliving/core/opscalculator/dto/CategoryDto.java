a3c68f55c20799e96818182f55cdd6eb3c6b843d
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2020-12-29 12:10:02 +0530

    commenting code

--- pojo-ops/src/main/java/com/stanzaliving/core/opscalculator/dto/CategoryDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/opscalculator/dto/CategoryDto.java
@@ -21,25 +21,25 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "categoryName", visible = true)
-@JsonSubTypes({
-		@JsonSubTypes.Type(value = AuditCategoryDto.class, name = "audit"),
-//		@JsonSubTypes.Type(value = BuildMaintenanceCategoryDto.class, name = "FOOD_FEEDBACK"),
-		@JsonSubTypes.Type(value = EquipmentRentalCategoryDto.class, name = "equipment"),
-		@JsonSubTypes.Type(value = FoodCategoryDto.class, name = "food"),
-		@JsonSubTypes.Type(value = HouseKeepingCategoryDto.class, name = "hk"),
-		@JsonSubTypes.Type(value = InternetCategoryDto.class, name = "internet"),
-		@JsonSubTypes.Type(value = LaundryCostCategoryDto.class, name = "laundry"),
-		@JsonSubTypes.Type(value = OeCategoryDto.class, name = "oe"),
-		@JsonSubTypes.Type(value = PersonnelCategoryDto.class, name = "personnel"),
-		@JsonSubTypes.Type(value = RcCategoryDto.class, name = "rc"),
-//		@JsonSubTypes.Type(value = RepairAndMaintenanceCategoryDto.class, name = "repair"),
-		@JsonSubTypes.Type(value = RepairCategoryDto.class, name = "repair"),
-		@JsonSubTypes.Type(value = SecurityCategoryDto.class, name = "security"),
-		@JsonSubTypes.Type(value = SubscriptionAndSuppliesCategoryDto.class, name = "subscription"),
-		@JsonSubTypes.Type(value = TransportCategoryDto.class, name = "transport"),
-		@JsonSubTypes.Type(value = UtilitiesCategoryDto.class, name = "utilities")
-})
+//@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "categoryName", visible = true)
+//@JsonSubTypes({
+//		@JsonSubTypes.Type(value = AuditCategoryDto.class, name = "audit"),
+////		@JsonSubTypes.Type(value = BuildMaintenanceCategoryDto.class, name = "FOOD_FEEDBACK"),
+//		@JsonSubTypes.Type(value = EquipmentRentalCategoryDto.class, name = "equipment"),
+//		@JsonSubTypes.Type(value = FoodCategoryDto.class, name = "food"),
+//		@JsonSubTypes.Type(value = HouseKeepingCategoryDto.class, name = "hk"),
+//		@JsonSubTypes.Type(value = InternetCategoryDto.class, name = "internet"),
+//		@JsonSubTypes.Type(value = LaundryCostCategoryDto.class, name = "laundry"),
+//		@JsonSubTypes.Type(value = OeCategoryDto.class, name = "oe"),
+//		@JsonSubTypes.Type(value = PersonnelCategoryDto.class, name = "personnel"),
+//		@JsonSubTypes.Type(value = RcCategoryDto.class, name = "rc"),
+////		@JsonSubTypes.Type(value = RepairAndMaintenanceCategoryDto.class, name = "repair"),
+//		@JsonSubTypes.Type(value = RepairCategoryDto.class, name = "repair"),
+//		@JsonSubTypes.Type(value = SecurityCategoryDto.class, name = "security"),
+//		@JsonSubTypes.Type(value = SubscriptionAndSuppliesCategoryDto.class, name = "subscription"),
+//		@JsonSubTypes.Type(value = TransportCategoryDto.class, name = "transport"),
+//		@JsonSubTypes.Type(value = UtilitiesCategoryDto.class, name = "utilities")
+//})
 public abstract class CategoryDto {
 
 	private String categoryName;
