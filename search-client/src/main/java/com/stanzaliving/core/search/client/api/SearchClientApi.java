c95bea72fe20aaebff646006b81aa62452bd8bf9
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-01-10 01:12:46 +0530

    search client - method for dish master search

--- search-client/src/main/java/com/stanzaliving/core/search/client/api/SearchClientApi.java
+++ search-client/src/main/java/com/stanzaliving/core/search/client/api/SearchClientApi.java
@@ -1,8 +1,14 @@
 package com.stanzaliving.core.search.client.api;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.stanzaliving.core.base.common.dto.PageResponse;
 import com.stanzaliving.core.base.common.dto.ResponseDto;
+import com.stanzaliving.core.base.exception.PreconditionFailedException;
 import com.stanzaliving.core.base.http.StanzaRestClient;
+import com.stanzaliving.core.food.dto.FoodItemSearchDto;
+import com.stanzaliving.core.utilservice.utils.ReflectionUtils;
+import com.stanzaliving.search.food.index.dto.dishmaster.DishMasterSearchIndexDto;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
@@ -10,9 +16,12 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
@@ -22,6 +31,7 @@ import java.util.Map;
  * @since 07-Jan-2021
  */
 
+@Log4j2
 public class SearchClientApi {
 
 	private final StanzaRestClient restClient;
@@ -30,15 +40,11 @@ public class SearchClientApi {
 		this.restClient = stanzaRestClient;
 	}
 
-	public ResponseDto<Void> searchDishMaster() {
+	public PageResponse<DishMasterSearchIndexDto> searchDishMaster(FoodItemSearchDto searchDto) {
 
-		Object postBody = null;
+		String path = UriComponentsBuilder.fromPath("search/dish/master").build().toUriString();
 
-		final Map<String, Object> uriVariables = new HashMap<>();
-
-		String path = UriComponentsBuilder.fromPath("").buildAndExpand(uriVariables).toUriString();
-
-		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+		final MultiValueMap<String, String> queryParams = convertToQueryParams(searchDto);
 
 		final HttpHeaders headerParams = new HttpHeaders();
 
@@ -46,10 +52,51 @@ public class SearchClientApi {
 
 		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
 
-		TypeReference<ResponseDto<Void>> returnType = new TypeReference<ResponseDto<Void>>() {
-		};
-		return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
+		TypeReference<ResponseDto<PageResponse<DishMasterSearchIndexDto>>> returnType = new TypeReference<ResponseDto<PageResponse<DishMasterSearchIndexDto>>>() {};
 
+		ResponseDto<PageResponse<DishMasterSearchIndexDto>> responseDto = new ResponseDto<>();
+
+		try {
+
+			responseDto = restClient.request(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType, MediaType.APPLICATION_JSON);
+
+		} catch (Exception e) {
+
+		}
+
+		if (!responseDto.isStatus()) {
+
+			throw new PreconditionFailedException(responseDto.getMessage());
+
+		}
+
+		return responseDto.getData();
+	}
+
+	private MultiValueMap<String, String> convertToQueryParams(FoodItemSearchDto searchDto) {
+
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+
+		Field[] fields = searchDto.getClass().getFields();
+
+		if (fields != null) {
+
+			Arrays.stream(fields).forEach(field -> {
+
+				try {
+					Object value = field.get(searchDto);
+
+					if (Objects.nonNull(value)) {
+						map.add(field.getName(), value.toString());
+					}
+
+				} catch (IllegalAccessException e) {
+					log.error(e);
+				}
+			});
+		}
+
+		return map;
 	}
 
 }
