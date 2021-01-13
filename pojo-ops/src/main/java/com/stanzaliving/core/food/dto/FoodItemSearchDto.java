3c27000e86bec6bba51099e6efde53a581a3fdc3
Naveen Kumar <naveen.kumar@stanzaliving.com>
2021-01-13 17:07:11 +0530

    Added recipeExists filter

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/FoodItemSearchDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/FoodItemSearchDto.java
@@ -1,16 +1,20 @@
 package com.stanzaliving.core.food.dto;
 
+import java.util.Collection;
+
 import com.stanzaliving.core.base.common.dto.AbstractDto;
 import com.stanzaliving.core.dto.PageAndSortDto;
 import com.stanzaliving.core.food.enums.FoodItemBackgroundColor;
 import com.stanzaliving.core.food.enums.FoodItemType;
 import com.stanzaliving.core.food.enums.RecipeType;
 import com.stanzaliving.core.operations.enums.MealType;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
 
-import java.util.Collection;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 /**
  * @author piyush srivastava "piyush.srivastava@stanzaliving.com"
@@ -25,6 +29,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FoodItemSearchDto extends AbstractDto {
+
+	private static final long serialVersionUID = -1699086756827155062L;
 
 	private Collection<String> itemIds;
 
@@ -41,16 +47,18 @@ public class FoodItemSearchDto extends AbstractDto {
 	private FoodItemBackgroundColor bgColor;
 
 	private String category;
-	
+
 	private MealType mealType;
 
 	private PageAndSortDto pageDto;
-	
+
 	private String subCategory;
 
 	private RecipeType recipeType;
 
 	private Collection<RecipeType> recipeTypeList;
-	
+
 	private Boolean dataComplete;
+
+	private Boolean recipeExists;
 }
