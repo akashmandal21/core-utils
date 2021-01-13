ef83518a3cb7e76bc09046336770ca82f7f33902
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2021-01-14 02:51:26 +0530

    removing extra dateformat

--- base/src/main/java/com/stanzaliving/core/base/enums/DateFormat.java
+++ base/src/main/java/com/stanzaliving/core/base/enums/DateFormat.java
@@ -36,7 +36,6 @@ public enum DateFormat {
 	MMMM_YYYY("MMMM, yyyy"),//June, 2020
 	MMM_YYYY("MMM, yyyy"),
 	MMM_YY("MMM, yy"),
-	MMM_YY2("MMMyy"),
 	DD_MMM_YYYY_H_MM_A("dd MMM yyyy h:mm a"),
 	H_MM_A("h:mm a"),
 	FRESHDESK_DATE_FORMAT("yyyy-MM-dd'T'HH:mm:ssXXX"),
