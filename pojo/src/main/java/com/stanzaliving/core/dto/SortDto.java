042f73fc0a2a1a2398b822d8ca7f4104f92b8c67
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-01-16 01:30:53 +0530

    sort dto

---
+++ pojo/src/main/java/com/stanzaliving/core/dto/SortDto.java
@@ -0,0 +1,15 @@
+package com.stanzaliving.core.dto;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @version 2.0
+ *
+ * @since 16-Jan-2021
+ */
+
+public class SortDto {
+	private String sortBy;
+
+	private String sortOrder;
+}
