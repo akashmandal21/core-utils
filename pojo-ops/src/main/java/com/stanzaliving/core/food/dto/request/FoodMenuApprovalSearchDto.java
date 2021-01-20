cad82226875aa2d705e0fa1490ab2eb5db2196b4
ketan.yadav <ketan.yadav@stanzaliving.com>
2021-01-20 22:46:06 +0530

    search api addde

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/request/FoodMenuApprovalSearchDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/request/FoodMenuApprovalSearchDto.java
@@ -6,14 +6,6 @@ package com.stanzaliving.core.food.dto.request;
 import com.stanzaliving.approval.enums.ApprovalStatus;
 import lombok.*;
 
-import java.time.LocalDate;
-
-/**
- * @author naveen.kumar
- *
- * @date 06-May-2020
- *
- **/
 @Getter
 @Setter
 @ToString
