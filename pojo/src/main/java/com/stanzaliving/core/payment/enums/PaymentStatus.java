fff92aabd77a0d292de6672bd26c2e2181b6fc78
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-01-25 15:31:38 +0530

    rwfund failed

--- pojo/src/main/java/com/stanzaliving/core/payment/enums/PaymentStatus.java
+++ pojo/src/main/java/com/stanzaliving/core/payment/enums/PaymentStatus.java
@@ -17,4 +17,8 @@ public enum PaymentStatus {
 	public static boolean isFailed(PaymentStatus status) {
 		return Objects.nonNull(status) && FAILED.equals(status);
 	}
+
+	public static boolean isRefundFailed(PaymentStatus status) {
+		return Objects.nonNull(status) && REFUND_FAILED.equals(status);
+	}
 }
