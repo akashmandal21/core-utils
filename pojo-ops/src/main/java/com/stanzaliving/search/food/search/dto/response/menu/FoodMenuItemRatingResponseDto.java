e5e63991ad117876576faf2b3bd6cd344c37af0d
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-01-27 00:44:27 +0530

    rating summary response dtos

---
+++ pojo-ops/src/main/java/com/stanzaliving/search/food/search/dto/response/menu/FoodMenuItemRatingResponseDto.java
@@ -0,0 +1,34 @@
+package com.stanzaliving.search.food.search.dto.response.menu;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @version 1.0
+ *
+ * @since 26-Jan-2021
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoodMenuItemRatingResponseDto {
+	private MicromarketIdRatingDto micromarket;
+
+	private List<ResidenceIdRatingDto> residences;
+
+	private List<ResidenceMealRatingDto> residenceMeals;
+
+	private List<MicromarketItemRatingDto> items;
+}
