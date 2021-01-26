341ca860dfd68cde20f90a54cdd08f9f746c7b20
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-01-27 01:36:43 +0530

    request aggregation dto

---
+++ pojo-ops/src/main/java/com/stanzaliving/search/food/search/dto/request/MenuItemAggregateRequestDto.java
@@ -0,0 +1,39 @@
+package com.stanzaliving.search.food.search.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @version 1.0
+ *
+ * @since 26-Jan-2021
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuItemAggregateRequestDto {
+
+	private String micromarketId;
+
+	private List<String> itemIds;
+
+	@NotNull(message = "fromDate is mandatory")
+	private LocalDate fromDate;
+
+	@NotNull(message = "toDate is mandatory")
+	private LocalDate toDate;
+}
