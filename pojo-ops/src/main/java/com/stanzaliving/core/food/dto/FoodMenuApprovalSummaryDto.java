c8676f0707436377749a6403c5984790c68f8315
ketan.yadav <ketan.yadav@stanzaliving.com>
2021-01-27 15:54:36 +0530

    summary changes

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/FoodMenuApprovalSummaryDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/FoodMenuApprovalSummaryDto.java
@@ -1,6 +1,7 @@
 package com.stanzaliving.core.food.dto;
 
 import com.stanzaliving.approval.dto.ApprovalListingDto;
+import com.stanzaliving.core.food.enums.FoodMenuStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,18 +16,18 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FoodMenuApprovalSummaryDto {
-
 	private String cityName;
-
 	private String micromarketName;
-
 	private String categoryName;
-
+	private String menuCategoryId;
 	private boolean autoCreated;
-
 	private boolean chefAutoApproved;
-
 	private boolean vendorAutoApproved;
-	
 	private ApprovalListingDto approvalSummary;
+	private String residenceName;
+	private FoodMenuStatus menuStatus;
+	private boolean menuApproved;
+	private Integer mir;
+	private Double price;
+	private String boxOption;
 }
