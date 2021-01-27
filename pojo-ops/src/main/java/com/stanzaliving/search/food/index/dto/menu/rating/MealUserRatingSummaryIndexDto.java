bab178f0fb1a94d6f433533e3f026bc5cbe38a20
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-01-27 23:59:46 +0530

    meal user rating summary search index dto

---
+++ pojo-ops/src/main/java/com/stanzaliving/search/food/index/dto/menu/rating/MealUserRatingSummaryIndexDto.java
@@ -0,0 +1,47 @@
+package com.stanzaliving.search.food.index.dto.menu.rating;
+
+import com.stanzaliving.core.base.common.dto.AbstractDto;
+import com.stanzaliving.core.operations.enums.MealType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @version 2.0
+ *
+ * @since 27-Jan-2021
+ */
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MealUserRatingSummaryIndexDto extends AbstractDto {
+
+	private String residenceFoodMenuId;
+
+	private String cityId;
+
+	private String microMarketId;
+
+	private String menuCategoryId;
+
+	private String residenceId;
+
+	private LocalDate menuDate;
+
+	private MealType mealType;
+
+	private String userId;
+
+	private float rating;
+}
