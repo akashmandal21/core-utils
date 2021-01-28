b3cab8b8ba201103468d8518cd9af0bbc494b4e3
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-01-29 03:03:23 +0530

    menu category consumption

---
+++ pojo-ops/src/main/java/com/stanzaliving/search/food/search/dto/response/menu/consumption/MenuCategoryConsumptionDto.java
@@ -0,0 +1,22 @@
+package com.stanzaliving.search.food.search.dto.response.menu.consumption;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @version 1.0
+ *
+ * @since 28-Jan-2021
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuCategoryConsumptionDto extends ConsumptionPercentageDto {
+	private String menuCategoryId;
+}
