e45008dc5ea3667a000418a225ef44cd314cd8f0
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2021-01-29 20:20:08 +0530

    adding constructor

--- pojo-ops/src/main/java/com/stanzaliving/approval/dto/ApprovalResponseDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/approval/dto/ApprovalResponseDto.java
@@ -2,14 +2,18 @@ package com.stanzaliving.approval.dto;
 
 import com.stanzaliving.approval.enums.ApprovalEntityType;
 import com.stanzaliving.approval.enums.ApprovalStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApprovalResponseDto {
     @NotBlank(message = "entityUuid cannot be blank")
     String entityUuid;
