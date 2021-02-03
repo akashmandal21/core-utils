1637f4067dd5a8236cf1f8d5d97d9b6e8ecf11dc
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-02-03 13:39:24 +0530

    generate qr code service

---
+++ qrcode/src/main/java/com/stanzaliving/qrcode/service/impl/GenerateQrServiceImpl.java
@@ -0,0 +1,53 @@
+package com.stanzaliving.qrcode.service.impl;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.stanza.qr.generator.QRGeneratorUtility;
+import com.stanzaliving.core.amazons3.service.S3UploadService;
+import com.stanzaliving.qrcode.dto.GenerateQrResponseDto;
+import com.stanzaliving.qrcode.service.GenerateQrService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @version 1.0
+ *
+ * @since 03-Feb-2021
+ */
+
+@Log4j2
+@Service
+public class GenerateQrServiceImpl implements GenerateQrService {
+
+	@Autowired
+	private S3UploadService s3UploadService;
+
+	@Override
+	public GenerateQrResponseDto generateQrCode(String s3Bucket, String filePath, String fileName, AmazonS3 s3Client) throws IOException {
+
+		String qrContent = ((Long) System.nanoTime()).toString();
+
+		log.info("Generating qr code with content: {}", qrContent);
+
+		BufferedImage image = QRGeneratorUtility.generateQRImageUsingLong(qrContent);
+
+		File outputFile = new File("/tmp/" + fileName + ".jpg");
+
+		ImageIO.write(image, "jpg", outputFile);
+
+		String outputFilePath = s3UploadService.upload(s3Bucket, filePath, fileName, outputFile, MediaType.IMAGE_JPEG_VALUE, s3Client, false);
+
+		return GenerateQrResponseDto.builder()
+				.qrContent(qrContent)
+				.outputFile(outputFilePath)
+				.build();
+	}
+}
