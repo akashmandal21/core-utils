c8b6a2ecb2b07c8d983c2e4502079f87bb27f165
Rahul Tiwary <rahul.kumar1@stanzaliving.com>
2021-02-08 13:12:06 +0530

    code for email check

--- base/src/main/java/com/stanzaliving/core/base/utils/EmailValidatorUtil.java
+++ base/src/main/java/com/stanzaliving/core/base/utils/EmailValidatorUtil.java
@@ -1,12 +1,13 @@
 package com.stanzaliving.core.base.utils;
 
+import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.EmailValidator;
 
-
+@UtilityClass
 public class EmailValidatorUtil {
 
-	public static boolean isValidEmail(String email) {
+	public boolean isValidEmail(String email) {
 		if (StringUtils.isBlank(email)) {
 			return false;
 		}
