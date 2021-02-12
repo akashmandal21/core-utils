7e75d9157d3510e5306cff71a8142c66ccc2a1da
kapil-stanza <kapil.dev@stanzaliving.com>
2021-02-12 17:35:40 +0530

    Added Dto for student mapping

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/ServiceMixDetailsDto.java
@@ -0,0 +1,21 @@
+package com.stanzaliving.core.operations.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class ServiceMixDetailsDto {
+
+	private String serviceMixUuid;
+	private String serviceMixName;
+
+}
