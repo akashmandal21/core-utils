df653c1b3fd532dd3b5990a7dbf40e86d2d5b9e7
ketan.yadav <ketan.yadav@stanzaliving.com>
2021-02-12 13:28:12 +0530

    changs

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/CityLevelFoodSummaryDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/CityLevelFoodSummaryDto.java
@@ -3,6 +3,7 @@ package com.stanzaliving.core.food.dto;
 import java.util.List;
 import java.util.Map;
 
+import com.stanzaliving.approval.dto.ApprovalListingDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,5 +29,22 @@ public class CityLevelFoodSummaryDto {
 	private List<StatusCountDto> statusWiseCount;
 	//city->mm->category
 	private Map<String,CityLevelFoodSummaryDto> childData;
+
+	private String requestDateTimeLabel;
+	private String l1ActionDateTimeLabel;
+	private String l2ActionDateTimeLabel;
+	private String l3ActionDateTimeLabel;
+	private String l4ActionDateTimeLabel;
+	private String l5ActionDateTimeLabel;
+	private String approvalStatus;
+	private Double price;
+	private String boxOption;
+	private ApprovalListingDto approvalSummary;
+	private List<String> residences;
+	private String residenceName;
+	private String menuStatus;
+	private boolean menuApproved;
+	private String categoryName;
+	private String categoryMealWiseName;
 
 }
