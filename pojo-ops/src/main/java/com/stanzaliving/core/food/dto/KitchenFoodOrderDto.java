256f55e796200065a3c872848e5cfe406b87729e
pooja2093 <pooja.gautam.iiit@gmail.com>
2021-02-12 17:48:55 +0530

    added to

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/KitchenFoodOrderDto.java
@@ -0,0 +1,20 @@
+package com.stanzaliving.core.food.dto;
+
+public class KitchenFoodOrderDto {
+
+	private String vendorId;
+
+	private String mealType;
+
+	private String orderDate;
+
+	private KitchenOrderDto breakfast;
+
+	private KitchenOrderDto lunch;
+
+	private KitchenOrderDto snacks;
+
+	private KitchenOrderDto dinner;
+
+
+}
---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/KitchenOrderDto.java
@@ -0,0 +1,8 @@
+package com.stanzaliving.core.food.dto;
+
+public class KitchenOrderDto {
+
+	private Integer stanzaStaffCount;
+	private Integer nonStanzaStaffCount;
+
+}
