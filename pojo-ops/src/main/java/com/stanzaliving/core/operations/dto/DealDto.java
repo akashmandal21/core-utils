9370f7b935e46463b6b68e2439d5f1bb9eca9a93
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-02-16 01:30:51 +0530

    Update DealDto.java

--- pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/DealDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/DealDto.java
@@ -29,32 +29,32 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DealDto {
-	
+
 	private String uuid;
 
 	private String residenceUuid;
-	
+
 	private String dealName;
 
 	private DealType dealType;
 
-    private DealStatus dealStatus;
-	
+	private DealStatus dealStatus;
+
 	private boolean mgDeal;
-	
+
 	@Builder.Default
 	private Integer mgCount = 0;
 
 	private String pocName;
-	
+
 	private String pocContact;
-	
+
 	private String pocEmail;
 
 	private LocalDate dealFromDate;
-	
+
 	private LocalDate dealToDate;
-	
+
 	private LocalDate dealCreatedAtVenta;
 
 	private String statementOfWorkDocumentUrl;
