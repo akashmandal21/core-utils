43ef8164185d09d915ab895179032559a64a0268
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-02-16 10:39:48 +0530

    Update ServiceMixSerializableDto.java

--- pojo-ops/src/main/java/com/stanzaliving/operations/dto/servicemix/ServiceMixSerializableDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/operations/dto/servicemix/ServiceMixSerializableDto.java
@@ -17,28 +17,28 @@ import lombok.ToString;
 @AllArgsConstructor
 public class ServiceMixSerializableDto {
 	private String uuid;
-	
+
 	private String residenceId;
 	private String residenceName;
-	
+
 	private String cityId;
 	private String cityName;
-	
+
 	private String micromarketId;
 	private String micromarketName;
-	
+
 	private String versionName;
-	
+
 	private Long version;
-	
+
 	private LocalDate fromDate;
-	
+
 	private boolean scheduled;
-	
+
 	private String dealId;
 	private String dealName;
-	
+
 	private DealCategory dealCategory;
-	
-    private Config config = new Config();
+
+	private Config config = new Config();
 }
