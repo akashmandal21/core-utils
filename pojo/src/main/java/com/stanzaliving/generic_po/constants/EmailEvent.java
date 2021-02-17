99578aaac9ef645455882ec8db09fb777f2d9186
Amit-K-G <amitgupta.retainer@stanzaliving.com>
2021-02-17 18:20:17 +0530

    rate master email changes

---
+++ pojo/src/main/java/com/stanzaliving/generic_po/constants/EmailEvent.java
@@ -0,0 +1,9 @@
+package com.stanzaliving.generic_po.constants;
+
+public interface EmailEvent {
+
+    String ITEM_ADDED = "ITEM_ADDED";
+    String ITEM_RATE_SUBMITTED = "ITEM_RATE_SUBMITTED";
+    String ITEM_RATE_APPROVED_OR_REJECTED = "ITEM_RATE_APPROVED_OR_REJECTED";
+
+}
