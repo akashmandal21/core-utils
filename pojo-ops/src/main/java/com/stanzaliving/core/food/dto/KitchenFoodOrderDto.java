f8c60bb28dd57b85014eab0b43d4967b36170785
pooja2093 <pooja.gautam.iiit@gmail.com>
2021-02-17 12:00:34 +0530

    draft

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/KitchenFoodOrderDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/KitchenFoodOrderDto.java
@@ -1,12 +1,22 @@
 package com.stanzaliving.core.food.dto;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class KitchenFoodOrderDto {
 
 	private String vendorId;
 
 	private String mealType;
 
-	private String orderDate;
+	private LocalDate orderDate;
 
 	private KitchenOrderDto breakfast;
 
--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/KitchenOrderDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/KitchenOrderDto.java
@@ -1,5 +1,13 @@
 package com.stanzaliving.core.food.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class KitchenOrderDto {
 
 	private Integer stanzaStaffCount;
