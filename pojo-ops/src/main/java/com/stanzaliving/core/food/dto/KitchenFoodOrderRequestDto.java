03a8b2cc2692c847cd8dcf918e3c6f0ec1b31f51
pooja2093 <pooja.gautam.iiit@gmail.com>
2021-02-19 11:47:40 +0530

    added validation

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/KitchenFoodOrderRequestDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/KitchenFoodOrderRequestDto.java
@@ -2,6 +2,8 @@ package com.stanzaliving.core.food.dto;
 
 import lombok.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
@@ -14,10 +16,14 @@ import java.util.List;
 @AllArgsConstructor
 public class KitchenFoodOrderRequestDto {
 
+	@NotBlank(message = "Vendor Id Cannot be blank")
 	private String vendorId;
 
+	@NotBlank(message = "orderDate Id Cannot be blank")
 	private LocalDate orderDate;
 
+	@NotBlank(message = "meals Cannot be blank")
+	@Valid
 	private List<KitchenOrderMealRequestDto> meals;
 
 }
--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/KitchenOrderMealRequestDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/KitchenOrderMealRequestDto.java
@@ -3,6 +3,7 @@ package com.stanzaliving.core.food.dto;
 import com.stanzaliving.core.operations.enums.MealType;
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
@@ -13,7 +14,10 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class KitchenOrderMealRequestDto {
 
+	@NotNull(message = "stanzaStaffCount cannot be null")
 	private Integer stanzaStaffCount;
+
+	@NotNull(message = "nonStanzaStaffCount cannot be null")
 	private Integer nonStanzaStaffCount;
 	private MealType mealType;
 
