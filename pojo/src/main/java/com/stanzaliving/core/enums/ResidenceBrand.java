dc8b86485be246e2b421613b9c3cc9de4336f042
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-03-03 11:28:20 +0530

    Update ResidenceBrand.java

--- pojo/src/main/java/com/stanzaliving/core/enums/ResidenceBrand.java
+++ pojo/src/main/java/com/stanzaliving/core/enums/ResidenceBrand.java
@@ -3,6 +3,11 @@
  */
 package com.stanzaliving.core.enums;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.stanzaliving.core.user.enums.EnumListing;
+
 /**
  * @author naveen.kumar
  *
@@ -13,5 +18,17 @@ public enum ResidenceBrand {
 
 	PRIMA,
 	MAGNA,
-	SUMMA
+	SUMMA;
+	
+		private static List<EnumListing<ResidenceBrand>> brandNameListing= new ArrayList<>();
+		
+		static {
+			brandNameListing.add(EnumListing.of(ResidenceBrand.PRIMA, ResidenceBrand.PRIMA.name()));
+			brandNameListing.add(EnumListing.of(ResidenceBrand.MAGNA, ResidenceBrand.MAGNA.name()));
+			brandNameListing.add(EnumListing.of(ResidenceBrand.SUMMA, ResidenceBrand.SUMMA.name()));
+		}
+		
+		public static List<EnumListing<ResidenceBrand>> getResidenceBrandNameListing() {
+			return brandNameListing;
+		}
 }
