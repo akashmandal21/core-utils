e1e030bb8d02e08d1b0b9c24392d55b09bb8aa3a
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-03-03 18:59:55 +0530

    EstateGender Updated

--- pojo/src/main/java/com/stanzaliving/core/enums/ResidenceBrand.java
+++ pojo/src/main/java/com/stanzaliving/core/enums/ResidenceBrand.java
@@ -8,24 +8,31 @@ import java.util.List;
 
 import com.stanzaliving.core.user.enums.EnumListing;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @author naveen.kumar
  *
  * @date 27-Dec-2019
  *
  **/
+@Getter
+@AllArgsConstructor
 public enum ResidenceBrand {
 
-	PRIMA, 
-	MAGNA, 
-	SUMMA;
+	PRIMA("Prima"), 
+	MAGNA("Magna"), 
+	SUMMA("Summa");
+	
+	String brandName;
 	
 	private static List<EnumListing<ResidenceBrand>> brandNameListing= new ArrayList<>();
 	
 	static {
-		brandNameListing.add(EnumListing.of(ResidenceBrand.PRIMA, ResidenceBrand.PRIMA.name()));
-		brandNameListing.add(EnumListing.of(ResidenceBrand.MAGNA, ResidenceBrand.MAGNA.name()));
-		brandNameListing.add(EnumListing.of(ResidenceBrand.SUMMA, ResidenceBrand.SUMMA.name()));
+		brandNameListing.add(EnumListing.of(ResidenceBrand.PRIMA, ResidenceBrand.PRIMA.getBrandName()));
+		brandNameListing.add(EnumListing.of(ResidenceBrand.MAGNA, ResidenceBrand.MAGNA.getBrandName()));
+		brandNameListing.add(EnumListing.of(ResidenceBrand.SUMMA, ResidenceBrand.SUMMA.getBrandName()));
 	}
 	
 	public static List<EnumListing<ResidenceBrand>> getResidenceBrandNameListing() {
