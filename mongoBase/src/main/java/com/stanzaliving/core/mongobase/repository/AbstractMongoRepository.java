73cfe542d5775f2fce721f1f2fda495cacff2228
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2021-03-04 20:03:05 +0530

    removed uuid method:

--- mongoBase/src/main/java/com/stanzaliving/core/mongobase/repository/AbstractMongoRepository.java
+++ mongoBase/src/main/java/com/stanzaliving/core/mongobase/repository/AbstractMongoRepository.java
@@ -13,6 +13,6 @@ public interface AbstractMongoRepository<T extends AbstractMongoEntity, I extend
 {
 
     T findFirstByUuid(String uuid);
-    List<T> findByUuidIn(Collection<String> uuids)
+    List<T> findByUuidIn(Collection<String> uuids);
 
 }
