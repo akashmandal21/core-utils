f682dc9dfdf04015833bad9502931c0387378ecd
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-03-09 14:37:04 +0530

    WebsiteVasDetailResidenceController added

---
+++ pojo/src/main/java/com/stanzaliving/website/constants/Constants.java
@@ -0,0 +1,20 @@
+package com.stanzaliving.website.constants;
+
+public class Constants {
+
+	public static final String GUID = "guid"; // globally unique identifier
+	
+	public static final String LUID = "luid"; // locally unique identifier
+
+	public static final String RESIDENCE_TYPE_HEADER = "X-residenceType";
+	
+	public static final String STANZA_SESSION = "stanza_session";
+	
+	public static final String USER_IP_HEADER = "x-forwarded-for";
+	
+	public static final String IMS_DEFAULT_BEARER_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtdWRpdEBjcnVzaGNhbXBhaWducy5jb20iLCJzY29wZXMiOlsiTkFUSU9OQUwgQURNSU4iXSwiaXNzIjoiIiwiaWF0IjoxNTg5NzkwNjc2LCJleHAiOjE2MjEzMzA2NzZ9.m1c3t7l8TJJ9tyeNVuTrT-xX6sei2U99DC8ItjgfbDgiQe_K2kFHgF7ORmSIUTm_kS4JB9UV9oFEfJqBEt5W4w";
+	
+	public static final int DEFAULT_PRE_BOOKING_AMOUNT = 1000;
+	
+	public static final String RUPEE_SYMBOL = "\u20B9";
+}
