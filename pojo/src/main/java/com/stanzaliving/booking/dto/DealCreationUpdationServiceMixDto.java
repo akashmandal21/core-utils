5911360e874cfe09235058593e5629fcebe11569
Rahul.Tiwary <rahul.tiwary@C2VZP1S3MD6N.local>
2021-03-11 18:25:43 +0530

    code for service mix

--- pojo/src/main/java/com/stanzaliving/booking/dto/DealCreationUpdationServiceMixDto.java
+++ pojo/src/main/java/com/stanzaliving/booking/dto/DealCreationUpdationServiceMixDto.java
@@ -1,12 +1,16 @@
 package com.stanzaliving.booking.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.stanzaliving.booking.DealType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -15,13 +19,15 @@ import java.util.List;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class DealCreationUpdationServiceMixDto {
-    
+
+    @NotNull(message = "Deal startdate is mandatory")
+    private LocalDate dealStartDate;
+    @NotNull(message = "Deal enddate is mandatory")
+    private LocalDate dealEndDate;
     private String dealName;
     private String dealUuid;
-    private String dealType;
-    private Timestamp dealStartDate;
-    private Timestamp dealEndDate;
-    private Timestamp dealCreatedDate;
+    private DealType dealType;
+    private Date dealCreatedDate;
     private String dealStatus;
     private String workDocumentUrl;
     private List<DealResidenceBedMapping> residenceBedMapping;
