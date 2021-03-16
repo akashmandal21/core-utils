78b048ab9158833d2e86794a256cdf59b935d4d2
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-03-16 18:53:50 +0530

    residence/pricerange api added

---
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/CityPriceRangeDto.java
@@ -0,0 +1,21 @@
+package com.stanzaliving.website.response.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CityPriceRangeDto {
+
+	Integer minPrice;
+	
+	Integer maxPrice;
+}
