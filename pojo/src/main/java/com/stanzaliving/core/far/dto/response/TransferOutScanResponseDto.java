25cbae2f6da4b78787b37b87e953bbec9ed208ac
shobhit <mittalshobhit7@gmail.com>
2021-03-18 15:31:46 +0530

    dto

---
+++ pojo/src/main/java/com/stanzaliving/core/far/dto/response/TransferOutScanResponseDto.java
@@ -0,0 +1,18 @@
+package com.stanzaliving.core.far.dto.response;
+
+import com.stanzaliving.core.far.enums.QrCodeStatus;
+import com.stanzaliving.core.far.enums.TransferOutScanStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransferOutScanResponseDto {
+    TransferOutScanStatus transferOutScanStatus;
+}
