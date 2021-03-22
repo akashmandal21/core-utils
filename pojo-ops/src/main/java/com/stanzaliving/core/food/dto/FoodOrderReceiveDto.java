c1dd52456bfb897ed6ea17173180e5006d0a8527
ketan.yadav <ketan.yadav@stanzaliving.com>
2021-03-22 12:29:12 +0530

    time format changes

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/FoodOrderReceiveDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/FoodOrderReceiveDto.java
@@ -30,7 +30,7 @@ public class FoodOrderReceiveDto {
 
 	private LocalDate date;
 
-	@JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss",timezone = "Asia/Kolkata")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "Asia/Kolkata")
 	private Date receivedTime;
 
 	private String orderId;
