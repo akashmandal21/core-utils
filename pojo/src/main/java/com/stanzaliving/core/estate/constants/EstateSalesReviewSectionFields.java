a9bd2b2f58f97110dfbe50029e6a3357ff55c8ae
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2021-03-26 02:23:31 +0530

    added names

---
+++ pojo/src/main/java/com/stanzaliving/core/estate/constants/EstateSalesReviewSectionFields.java
@@ -0,0 +1,54 @@
+package com.stanzaliving.core.estate.constants;
+
+import java.util.List;
+
+import com.stanzaliving.core.generic.dto.UIKeyValue;
+
+public class EstateSalesReviewSectionFields {
+
+	public static final String OCCUPANCY_TYPE = "occupancy";
+	
+	public static final String REMARKS = "remarks";
+	
+	public static final String VAS="vas";
+		
+	public static final String PRICE_PER_MONTH = "pricePerMonth";
+	
+	public static final String SELECTED_OPTIONS = "selectedOption";
+	
+	public static final String INPUT_VALUE = "inputValue";
+	
+	public static final String DURATION = "duration";
+	
+	public static final String IS_AVAILABLE = "isAvailable";
+	
+	public static final String BREAKFAST_DAYS = "breakfastDays";
+	
+	public static final String LUNCH_DAYS = "lunchDays";
+	public static final String SNACK_DAYS = "snackDays";
+	public static final String DINNER_DAYS = "dinnerDays";
+	public static final String AMOUNT_TYPE = "amountType";
+	public static final String PER_UNIT = "perUnit";
+	
+	public static final String ROOM_AMENETIES_OPTIONS = "roomAmenitiesOptions";
+	public static final String OCCUPANCY_OPTIONS = "occupancyOptions";
+	public static final String AVAILABLE_SUBBRANDS = "availableSubBrands";
+	public static final String COPY_TEMPLATE_OPTIONS = "copyTemplateOptions";
+	public static final String TEMPLATE_SUBBRAND_OPTIONS = "templateSubBrandOptions";
+	public static final String COMMON_AMENITIES_OPTIONS = "commonAmenitiesOptions";
+	public static final String SECURITY_OPTIONS = "securityOptions";
+	public static final String TIME_PER_OPTIONS = "timePerOptions";
+	public static final String INTERNET_USAGE_OPTIONS= "internetUsageOptions";
+	public static final String INTERNET_TIME_OPTIONS = "internetTimeOptions";
+	public static final String METER_CHARGES_OPTIONS = "meterChargesOptions";
+	public static final String MEAL_CHARGES_OPTIONS = "mealChargesOptions";
+	public static final String DEPOSIT_TYPE_OPTIONS = "depositTypeOptions";
+	public static final String LAYOUT_TYPE_OPTIONS = "layoutTypeOptions";
+	public static final String AMOUNT_TYPE_OPTIONS = "amountTypeOptions";
+	public static final String KEY_POSITIVES = "keyPositives";
+	public static final String KEY_NEGATIVES = "keyNegatives";
+	public static final String SUBBRANDS = "availableSubBrands";
+
+	public static final String GLOBAL_UUID="GLOBAL";
+	
+}
