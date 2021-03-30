39de73a934868cd66015fcae3bb56da80ba77418
shobhit <mittalshobhit7@gmail.com>
2021-03-30 12:27:24 +0530

    dto

--- pojo/src/main/java/com/stanzaliving/core/far/enums/AssetStatus.java
+++ pojo/src/main/java/com/stanzaliving/core/far/enums/AssetStatus.java
@@ -2,7 +2,7 @@ package com.stanzaliving.core.far.enums;
 
 public enum AssetStatus {
     MAPPED_TO_QR("Mapped To QR"),
-    READYFOR_TRANSFER_OUT("Ready For Transfer Out"), TRANSERRED_OUT("Transferred Out"), READYFOR_TRANSFER_IN("Ready For Transfer In"), TRANSERRED_IN("Transferred In"),
+    READYFOR_TRANSFER_OUT("Ready For Transfer Out"), TRANSFERRED_OUT("Transferred Out"), READYFOR_TRANSFER_IN("Ready For Transfer In"), TRANSFERRED_IN("Transferred In"),
     HOTO_INPROGRESS("HOTO In Progress"), HOTO_REJECTED("HOTO Rejected"), HOTO_DONE("HOTO Done"),
     DISCARDED("Discarded"),
     VENDOR_CONFIRMATION_DUE("Vendor Confirmation Due"), VENDOR_REJECTED_RETURN("Vendor Rejected Return"), RETURNED("Returned");
