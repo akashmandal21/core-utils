4d2dbd50951e2ee0f900e59b78413e0391426b96
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-03-30 16:22:06 +0530

    Dto moved in core.operations.dto package

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/operations/dto/ServiceMixReminderDto.java
@@ -0,0 +1,34 @@
+package com.stanzaliving.core.operations.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceMixReminderDto {
+
+	private String cityId;
+
+	private String cityName;
+
+	private String microMarketId;
+
+	private String microMarketName;
+
+	private String residenceId;
+
+	private String residenceName;
+
+	private String sessionStartDate;
+
+}
