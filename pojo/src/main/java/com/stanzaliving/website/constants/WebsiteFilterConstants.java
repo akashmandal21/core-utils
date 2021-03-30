05948281419994faabd7fb6adfc4cef29f87ed80
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-03-30 15:18:39 +0530

    strategy pattern

--- pojo/src/main/java/com/stanzaliving/website/constants/WebsiteFilterConstants.java
+++ pojo/src/main/java/com/stanzaliving/website/constants/WebsiteFilterConstants.java
@@ -1,41 +1,41 @@
 package com.stanzaliving.website.constants;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class WebsiteFilterConstants {
 
-	// done
-	public static final String FILTER_NAME = "Filters";
+	public final String FILTER_NAME = "Filters";
 
-	public static final Integer FILTER_SORT_BY_SEQUENCE_ID = 1;
-	public static final String FILTER_SORT_BY_NAME = "Sort By";
+	public final Integer FILTER_SORT_BY_SEQUENCE_ID = 1;
+	public final String FILTER_SORT_BY_NAME = "Sort By";
 	
-	public static final String LOWEST_PRICE_FIRST = "Lowest Price First";
-	public static final String HIGHEST_PRICE_FIRST = "Highest Price First";
-	public static final String ASC = "asc";
-	public static final String DESC = "desc";
+	public final String LOWEST_PRICE_FIRST = "Lowest Price First";
+	public final String HIGHEST_PRICE_FIRST = "Highest Price First";
+	public final String ASC = "asc";
+	public final String DESC = "desc";
 
-	public static final Integer FILTER_LOCALITY_SEQUENCE_ID = 2;
-	public static final String FILTER_LOCALITY_NAME = "Locality";
+	public final Integer FILTER_LOCALITY_SEQUENCE_ID = 2;
+	public final String FILTER_LOCALITY_NAME = "Locality";
 
-	public static final Integer FILTER_PRICE_SEQUENCE_ID = 3;
-	public static final String FILTER_PRICE_NAME = "Price";
+	public final Integer FILTER_PRICE_SEQUENCE_ID = 3;
+	public final String FILTER_PRICE_NAME = "Price";
 
-	public static final Integer FILTER_GENDER_SEQUENCE_ID = 4;
-	public static final String FILTER_GENDER_NAME = "Gender";
+	public final Integer FILTER_GENDER_SEQUENCE_ID = 4;
+	public final String FILTER_GENDER_NAME = "Gender";
 
-	public static final Integer FILTER_RESIDENCE_TYPE_SEQUENCE_ID = 5;
-	public static final String FILTER_RESIDENCE_TYPE_NAME = "Residence Type";
+	public final Integer FILTER_RESIDENCE_TYPE_SEQUENCE_ID = 5;
+	public final String FILTER_RESIDENCE_TYPE_NAME = "Residence Type";
 	
-	public static final String FILTER_STUDENT = "Student";
-	public static final String FILTER_WORKING_PROFESSIONAL = "Working Professional";
+	public final String FILTER_STUDENT = "Student";
+	public final String FILTER_WORKING_PROFESSIONAL = "Working Professional";
 
-	public static final Integer FILTER_OCCUPANCY_SEQUENCE_ID = 6;
-	public static final String FILTER_OCCUPANCY_NAME = "Occupancy";
-	public static final Integer FILTER_OCCUPANCY_DORM_SEQUENCE_ID = 5;
-	public static final String FILTER_OCCUPANCY_DORM_NAME = "Dorm Style";
+	public final Integer FILTER_OCCUPANCY_SEQUENCE_ID = 6;
+	public final String FILTER_OCCUPANCY_NAME = "Occupancy";
+	
+	public final Integer FILTER_AMENITIES_SEQUENCE_ID = 7;
+	public final String FILTER_AMENITIES_NAME = "Amenities";
 
-	public static final Integer FILTER_AMENITIES_SEQUENCE_ID = 7;
-	public static final String FILTER_AMENITIES_NAME = "Amenities";
-
-	public static final Integer FILTER_SERVICES_SEQUENCE_ID = 8;
-	public static final String FILTER_SERVICES_NAME = "Services";
+	public final Integer FILTER_SERVICES_SEQUENCE_ID = 8;
+	public final String FILTER_SERVICES_NAME = "Services";
 }
