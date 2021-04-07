1585574dab09d23c529830b8813993d5e19caebd
jalaj gupta <jalaj.gupta@stanzaliving.com>
2021-04-07 16:39:37 +0530

    removed getter setter and user lambok
    removed getter setter and user lambok

--- venta-client/src/main/java/com/stanzaliving/core/backendlocator/client/api/UserDTO.java
+++ venta-client/src/main/java/com/stanzaliving/core/backendlocator/client/api/UserDTO.java
@@ -2,6 +2,9 @@ package com.stanzaliving.core.backendlocator.client.api;
 import java.util.HashSet;
 import java.util.Set;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
 
 	private String username;
@@ -22,86 +25,6 @@ public class UserDTO {
 
 	private String phone;
 
-
 	private boolean enabled = true;
 
-	public String getPassword() {
-		return this.password;
-	}
-
-	public String getUsername() {
-		return this.username;
-	}
-
-	public boolean isEnabled() {
-		return enabled;
-	}
-
-	public Set<String> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<String> roles) {
-		this.roles = roles;
-	}
-
-	public Set<String> getPermissions() {
-		return permissions;
-	}
-
-	public void setPermissions(Set<String> permissions) {
-		this.permissions = permissions;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTeam() {
-		return team;
-	}
-
-	public void setTeam(String team) {
-		this.team = team;
-	}
-
-	public String getLevel() {
-		return level;
-	}
-
-	public void setLevel(String level) {
-		this.level = level;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
 }
