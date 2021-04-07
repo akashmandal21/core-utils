3e3662c4f7f8d963ee19dbbfb6cbff3a41e9c6f4
dexter <dhruvmathur671@gmail.com>
2021-04-07 09:50:08 +0530

    added dto's and enum for finance details

---
+++ pojo/src/main/java/com/stanzaliving/invoice/enums/Vertical.java
@@ -0,0 +1,17 @@
+package com.stanzaliving.invoice.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author dhruv.mathur
+ * @date 06/04/21
+ */
+@Getter
+@AllArgsConstructor
+public enum Vertical {
+
+    OPEX("OP");
+
+    private String value;
+}
