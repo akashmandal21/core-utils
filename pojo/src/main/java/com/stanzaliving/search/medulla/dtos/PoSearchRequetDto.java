05f89aa9730d30845ebaef0ad0eceaa093a54234
shobhit <mittalshobhit7@gmail.com>
2021-04-11 17:18:33 +0530

    dto

--- pojo/src/main/java/com/stanzaliving/search/medulla/dtos/PoSearchRequetDto.java
+++ pojo/src/main/java/com/stanzaliving/search/medulla/dtos/PoSearchRequetDto.java
@@ -3,10 +3,14 @@ package com.stanzaliving.search.medulla.dtos;
 
 import com.stanzaliving.core.base.enums.Department;
 import com.stanzaliving.core.dto.PageAndSortDto;
+import com.stanzaliving.core.far.enums.AssetType;
 import com.stanzaliving.core.generic.dto.UIKeyValue;
-import com.stanzaliving.core.po.generic.enums.GenericPOTOStatus;
 import com.stanzaliving.core.po.generic.enums.GenericPOType;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
@@ -30,4 +34,5 @@ public class PoSearchRequetDto {
     private String listingType;
     private PageAndSortDto pageDto;
     private String[] fieldsNeeded;
+    private AssetType assetType;
 }
