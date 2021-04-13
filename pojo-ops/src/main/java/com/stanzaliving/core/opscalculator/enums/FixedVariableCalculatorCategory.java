9e0e8de0b45fadbe7aeb7b0d4711b4b6d6b4873e
vaibhav gupta <vaibhav.gupta@stanzaliving.com>
2021-04-13 17:31:33 +0530

    category fix

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/opscalculator/enums/FixedVariableCalculatorCategory.java
@@ -0,0 +1,35 @@
+/**
+ * @author nipunaggarwal
+ *
+ */
+package com.stanzaliving.core.opscalculator.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum FixedVariableCalculatorCategory {
+
+	AUDIT,
+	DG,
+	ELECTRICITY,
+	EQUIPMENT,
+	FOOD,
+	HK,
+	INTERNET,
+	LAUNDRY,
+	OE,
+	PERSONNEL,
+	RC,
+	REPAIR,
+	BUILDING,
+	SECURITY,
+	SUBSCRIPTION,
+	TRANSPORT,
+	UNIFORM_COSTS,
+	UTILITIES,
+	UNDER_WRITTEN,
+	WATER;
+
+}
