605c9111c3b308529c68fffee062a53272cbf513
RajKumarStanza <raj.singh@stanzaliving.com>
2021-04-14 14:13:38 +0530

    User detail repsonse dto

---
+++ pojo/src/main/java/com/stanzaliving/core/bookingservice/dto/UserDetailResponseDto.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.stanzaliving.core.bookingservice.dto;
+
+import lombok.Data;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Data
+public class UserDetailResponseDto {
+
+	private String name;
+	private String userCode;
+	private String phone;
+	private String email;
+	
+}
