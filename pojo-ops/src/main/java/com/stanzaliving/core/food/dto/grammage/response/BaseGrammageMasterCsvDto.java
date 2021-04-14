087b41797a2d1716108d6ad4712fa40ee81dbfb2
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-04-14 14:39:00 +0530

    csv dto

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/grammage/response/BaseGrammageMasterCsvDto.java
@@ -0,0 +1,47 @@
+package com.stanzaliving.core.food.dto.grammage.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @since 14-Apr-2021
+ *
+ * @version 1.0
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseGrammageMasterCsvDto {
+
+	private String subCategoryId;
+
+	private Integer breakfast;
+
+	private Integer lunch;
+
+	private Integer eveningSnacks;
+
+	private Integer dinner;
+
+	private Integer breakfastCombo;
+
+	private Integer lunchCombo;
+
+	private Integer eveningSnacksCombo;
+
+	private Integer dinnerCombo;
+
+	private Boolean additive;
+
+	private Boolean override;
+}
