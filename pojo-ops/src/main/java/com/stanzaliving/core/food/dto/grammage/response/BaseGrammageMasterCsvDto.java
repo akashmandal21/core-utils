c08c607f1ab9e56df940b913ce1039983da5c14d
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-04-14 17:39:04 +0530

    csv bind by name to csv dto

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/grammage/response/BaseGrammageMasterCsvDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/grammage/response/BaseGrammageMasterCsvDto.java
@@ -1,5 +1,6 @@
 package com.stanzaliving.core.food.dto.grammage.response;
 
+import com.opencsv.bean.CsvBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,29 +24,42 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 public class BaseGrammageMasterCsvDto {
 
+	@CsvBindByName(column = "SubCategoryId")
 	private String subCategoryId;
 
+	@CsvBindByName(column = "Category" )
 	private String category;
 
+	@CsvBindByName(column = "SubCategory" )
 	private String subCategory;
 
+	@CsvBindByName(column = "Breakfast" )
 	private Integer breakfast;
 
+	@CsvBindByName(column = "Lunch" )
 	private Integer lunch;
 
+	@CsvBindByName(column = "Evening Snacks" )
 	private Integer eveningSnacks;
 
+	@CsvBindByName(column = "Dinner" )
 	private Integer dinner;
 
+	@CsvBindByName(column = "Breakfast Combo" )
 	private Integer breakfastCombo;
 
+	@CsvBindByName(column = "Lunch Combo" )
 	private Integer lunchCombo;
 
+	@CsvBindByName(column = "Evening Snacks Combo" )
 	private Integer eveningSnacksCombo;
 
+	@CsvBindByName(column = "Dinner Combo" )
 	private Integer dinnerCombo;
 
+	@CsvBindByName(column = "Additive" )
 	private Boolean additive;
 
+	@CsvBindByName(column = "Override" )
 	private Boolean override;
 }
