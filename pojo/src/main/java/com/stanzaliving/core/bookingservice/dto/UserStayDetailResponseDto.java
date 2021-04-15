97f1b84327037c3ea0de67492d565fcf58f1895e
RajKumarStanza <raj.singh@stanzaliving.com>
2021-04-15 17:55:07 +0530

    User stay detail dto

---
+++ pojo/src/main/java/com/stanzaliving/core/bookingservice/dto/UserStayDetailResponseDto.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.stanzaliving.core.bookingservice.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Data
+public class UserStayDetailResponseDto {
+
+	private String entityName;
+	private String roomNumber;
+	private String occupancy;
+	private String paymentTerm;
+	private Date contractStartDate;
+	private Date contractEndDate;
+	private String contractMonths;
+	private double dues;
+	
+}
