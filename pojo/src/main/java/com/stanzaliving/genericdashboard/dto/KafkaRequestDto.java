530d465501e7cd02df28ffe36f8011e0abfbc065
nishant.nineleaps <nishant.venugopal@nineleaps.com>
2021-04-15 14:31:11 +0530

    Adds kafka request dto

---
+++ pojo/src/main/java/com/stanzaliving/genericdashboard/dto/KafkaRequestDto.java
@@ -0,0 +1,26 @@
+package com.stanzaliving.genericdashboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KafkaRequestDto {
+
+    String userId;
+
+    String eventName;
+
+    String eventCategory;
+
+    Date eventTime;
+
+    Map<String , String> attributes;
+}
