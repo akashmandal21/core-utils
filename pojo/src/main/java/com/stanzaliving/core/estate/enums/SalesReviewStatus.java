d5b190b0ef70911ae38401739ef24210cf3f6f24
Mohammad Sameer Khan <sameerkhan.retainer@stanzaliving.com>
2021-04-16 11:57:43 +0530

    updated enum

--- pojo/src/main/java/com/stanzaliving/core/estate/enums/SalesReviewStatus.java
+++ pojo/src/main/java/com/stanzaliving/core/estate/enums/SalesReviewStatus.java
@@ -9,10 +9,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public enum SalesReviewStatus {
 
-	IN_DRAFT("In-Draft"),
-	SUBMITTED("Sales Review Submitted"),
-	APPROVED("Sales Review Approved"),
-	SENT_BACK("Sent Back");
+	IN_DRAFT("In-Draft", "#E6E9EA", "#7A7D7E"),
+	SUBMITTED("Sales Review Submitted", "#DDF2F5", "#5FC4F5"),
+	APPROVED("Sales Review Approved", "#B8E0FF", "#5FC4F5"),
+	SENT_BACK("Sent Back", "#FFF1F1", "#F55F71"),
+	PROPERTY_DROPPED("Property Dropped", "#F0F2F2", "#7A7D7E");
 	
 	private String statusText;
+	
+	private String bgColor;
+	
+	private String textColor;
 }
