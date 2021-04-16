37edff69011d00401609f709a762d1d456c95fd3
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-04-17 02:40:19 +0530

    code fixed

---
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/ApartmentDetailedResponseDto.java
@@ -0,0 +1,54 @@
+package com.stanzaliving.website.response.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApartmentDetailedResponseDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer apartmentId;
+	
+	private String propertyCode;
+	
+	private Integer roomCount;
+	
+	private String apartmentName;
+	
+	private String apartmentSlug;
+	
+	private String gender;
+	
+	private String pricingPlan;
+	
+	private Integer startingPrice;
+	
+	private Integer preBookingAmount;
+	
+	private String preBookingMode;
+	
+	private String googleMapLink;
+	
+	private String videoLink;
+	
+	private String virtualTourImage;
+	
+	@Builder.Default
+	private Set<FacilityResponseDTO> facilities = new HashSet<>(0);
+	
+	@Builder.Default
+	private Set<FeatureResponseDTO> features = new HashSet<>(0);
+	
+	@Builder.Default
+	private Set<ImageResponseDTO> images = new HashSet<>(0);
+}
