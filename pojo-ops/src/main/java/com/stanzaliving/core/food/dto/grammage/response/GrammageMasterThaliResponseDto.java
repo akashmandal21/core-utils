21a641ec19c5f8dc14758332813bf0c92e488bf3
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-04-17 22:01:08 +0530

    grammage-response-dto

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/grammage/response/GrammageMasterThaliResponseDto.java
@@ -0,0 +1,24 @@
+package com.stanzaliving.core.food.dto.grammage.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author piyush.srivastava "piyush.srivastava@stanzaliving.com"
+ *
+ * @since 16-Apr-2021
+ *
+ * @version 1.0
+ */
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+public class GrammageMasterThaliResponseDto {
+	//TODO add fields
+}
