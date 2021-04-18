a3ff6d77a4e19971374d96bc9f4c6f36db61e602
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2021-04-18 20:01:03 +0530

    upfated

--- generic-validation-core/src/main/java/com/stanzaliving/core/generic/validation/utility/FieldDecoder.java
+++ generic-validation-core/src/main/java/com/stanzaliving/core/generic/validation/utility/FieldDecoder.java
@@ -52,7 +52,7 @@ public class FieldDecoder {
         try {
             FieldType fieldType = templateField.getFieldType();
             JsonNode data = uiSubmitField.getValue();
-            log.info("Value data  field type {} needed {}",fieldType,needed,data);
+            log.info("Value data  field type {} needed {} {}",fieldType,needed,data);
             if (Objects.nonNull(data) && (!data.isNull()))
             {
                 switch (fieldType) {
