fae29a598eb5101a092e01faad3552071f3ef41a
Santhosh3579 <82016500+Santhosh3579@users.noreply.github.com>
2021-04-19 15:54:58 +0530

    add StanzaCityPostDto

---
+++ pojo/src/main/java/com/stanzaliving/transformations/pojo/StanzaCityPostDto.java
@@ -0,0 +1,43 @@
+package com.stanzaliving.transformations.pojo;
+
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import com.stanzaliving.core.generic.dto.UIKeyValue;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StanzaCityPostDto {
+	@NotBlank
+	private String cityName;
+	@NotNull
+	private Boolean status;
+	@NotNull
+	private UIKeyValue zone;
+
+	
+}
+
+
+
+
+
+
+
+
+
+
