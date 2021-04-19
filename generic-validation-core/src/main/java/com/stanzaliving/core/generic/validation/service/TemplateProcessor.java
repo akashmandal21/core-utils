cf798258c711f7263fcdc0024d6a0fca2e1bd997
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2021-04-19 19:16:03 +0530

    updated

--- generic-validation-core/src/main/java/com/stanzaliving/core/generic/validation/service/TemplateProcessor.java
+++ generic-validation-core/src/main/java/com/stanzaliving/core/generic/validation/service/TemplateProcessor.java
@@ -264,7 +264,7 @@ public abstract class TemplateProcessor {
                                                                         Map<String,Field> fields, Object sourceClass,
                                                                         boolean allowSkipNewFields, Object baseObject){
 
-        log.info("Processing Template {} {}",templateName,data);
+        log.info("Processing Template {}",templateName);
 //        log.info("Source class {}",baseObject);
         final int currErrors = errorInfo.getNumErrors();
         Map<String, UiParentField> uiFieldMap = new LinkedHashMap<>();
@@ -311,7 +311,7 @@ public abstract class TemplateProcessor {
                             updateErrorInfo(errorInfo);
                         }
                     } else {
-                        log.info("Data Value {}",data.get(templateField.getFieldName()));
+//                        log.info("Data Value {}",data.get(templateField.getFieldName()));
                         uiField = ValueAdapters.getValue(data.get(templateField.getFieldName()), UiField.class,objectMapper);
                         uiField.setErrorMsg(null);
                         uiField.setErrorOccurred(false);
