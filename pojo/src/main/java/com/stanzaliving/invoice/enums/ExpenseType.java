66fe80fd2e36c8e1dc9fb2ad64c6ab9ad79dc244
ankit.khandelwal <ankit.khandelwal@stanzaliving.com>
2021-04-20 11:29:53 +0530

    updated

--- pojo/src/main/java/com/stanzaliving/invoice/enums/ExpenseType.java
+++ pojo/src/main/java/com/stanzaliving/invoice/enums/ExpenseType.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ExpenseType {
-    BS("OP"),
+    BS("BS"),
     DIRECT("Direct"),
     INDIRECT("Indirect"),
     PRIMARY("Primary");
