85b17b952c1c12940fd0dbbdd3dd5d46c4efe51d
kapil-stanza <kapil.dev@stanzaliving.com>
2021-04-21 16:45:12 +0530

    Added builder annotation

--- pojo/src/main/java/com/stanzaliving/genericdashboard/dto/KafkaTemplateDto.java
+++ pojo/src/main/java/com/stanzaliving/genericdashboard/dto/KafkaTemplateDto.java
@@ -2,10 +2,12 @@ package com.stanzaliving.genericdashboard.dto;
 
 import com.stanzaliving.genericdashboard.enums.EventDataType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
