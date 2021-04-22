ce3ca702dce03467d9b1461ee9cffd17e6fba43b
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-04-22 14:57:36 +0530

    code added

---
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/ApartmentShortResponseDto.java
@@ -0,0 +1,38 @@
+package com.stanzaliving.website.response.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApartmentShortResponseDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer apartmentId;
+	
+	private String propertyCode;
+	
+	private Long societyId;
+	
+	private Integer roomCount;
+	
+	private String apartmentName;
+	
+	private String apartmentSlug;
+	
+	private String pricingPlan;
+	
+	private Integer startingPrice;
+	
+	@Builder.Default
+	private Set<ImageResponseDTO> images = new HashSet<>();
+}
