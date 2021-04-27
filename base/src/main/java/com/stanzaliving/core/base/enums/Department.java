197a1962cfad95f7d9978d82a474872ec019021b
Nipun <nipunaggarwal16@gmail.com>
2021-04-27 13:07:56 +0530

    departments and departmentMap added in Department enum

--- base/src/main/java/com/stanzaliving/core/base/enums/Department.java
+++ base/src/main/java/com/stanzaliving/core/base/enums/Department.java
@@ -1,5 +1,9 @@
 package com.stanzaliving.core.base.enums;
 
+import java.util.Collection;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,32 +11,43 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Department {
 
-
-	TECH("Tech","TP","TCH"),
-	FINANCE("Finance","FN","FIN"),
-	HR("HR","HR","HRS"),
-	LEGAL("Legal","LG","LGL"),
-	SUPERADMIN("Superadmin",null,null),
-	LEADERSHIP("Leadership",null,null),
-	BUSINESS_DEVELOPMENT("Real Estate","RE","RET"),
-	OPS("Operations","OP","OPS"),
-	FOOD_OPS("Food Operations","FB","FNB"),
-	PROCUREMENT("Procurement",null,"TRN"),
-	DESIGN("Design",null,"TRN"),
-	PROJECTS("Projects",null,"TRN"),
-	TRANSFORMATIONS("Transformation","TN","TRN"),
-	SALES("Sales","SM","SNM"),
-	WEB("Web",null,null),
-	GC("GC Procurement",null,"TRN"),
-	WAREHOUSE("Warehouse",null,null),
-	CX("Customer Experience","CX","CXP"),
-	ADMIN("Admin","AD","ADM"),
-	MARKETING("Marketing","MK","MKT"),
-	SCM("Supply Chain Mgmt","SM","TRN"),
-	GROWTH_STRATEGY("Growth and Strategy","GS","GRS");
-
+	TECH("Tech", "TP", "TCH"),
+	FINANCE("Finance", "FN", "FIN"),
+	HR("HR", "HR", "HRS"),
+	LEGAL("Legal", "LG", "LGL"),
+	SUPERADMIN("Superadmin", null, null),
+	LEADERSHIP("Leadership", null, null),
+	BUSINESS_DEVELOPMENT("Real Estate", "RE", "RET"),
+	OPS("Operations", "OP", "OPS"),
+	FOOD_OPS("Food Operations", "FB", "FNB"),
+	PROCUREMENT("Procurement", null, "TRN"),
+	DESIGN("Design", null, "TRN"),
+	PROJECTS("Projects", null, "TRN"),
+	TRANSFORMATIONS("Transformation", "TN", "TRN"),
+	SALES("Sales", "SM", "SNM"),
+	WEB("Web", null, null),
+	GC("GC Procurement", null, "TRN"),
+	WAREHOUSE("Warehouse", null, null),
+	CX("Customer Experience", "CX", "CXP"),
+	ADMIN("Admin", "AD", "ADM"),
+	MARKETING("Marketing", "MK", "MKT"),
+	SCM("Supply Chain Mgmt", "SM", "TRN"),
+	GROWTH_STRATEGY("Growth and Strategy", "GS", "GRS");
 
 	public String departmentName;
 	public String shortCode;
 	public String genericPoShortCode;
+	public static Collection<Department> departments;
+	private static SortedMap<String, Department> departmentMap = new TreeMap<String, Department>();
+
+	static {
+		for (Department d : Department.values()) {
+			departmentMap.put(d.getDepartmentName(), d);
+		}
+	}
+
+	public static Collection<Department> getDepartments() {
+		return departmentMap.values();
+	}
+
 }
