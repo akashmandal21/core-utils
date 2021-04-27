f592d8e23fde5cc1181c1d0db20c013f928634ca
Santhosh3579 <82016500+Santhosh3579@users.noreply.github.com>
2021-04-27 19:47:01 +0530

    changes

---
+++ pojo/src/main/java/com/stanzaliving/transformations/pojo/UpdateStanzaCityZoneDto.java
@@ -0,0 +1,32 @@
+package com.stanzaliving.transformations.pojo;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import com.stanzaliving.core.generic.dto.UIKeyValue;
+import com.stanzaliving.transformations.pojo.AddStanzaClusterDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateStanzaCityZoneDto {
+	
+	
+	private UIKeyValue zone;
+	
+	private List<UIKeyValue> stanzaCities;
+}
