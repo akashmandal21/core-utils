d6f8b4ea38f6bd63ba0570d71b6b039d4aa96618
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-04-28 20:51:29 +0530

    Update MonthlyForcastCsvDto.java

--- pojo-ops/src/main/java/com/stanzaliving/core/opscalculator/dto/MonthlyForcastCsvDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/opscalculator/dto/MonthlyForcastCsvDto.java
@@ -1,11 +1,13 @@
 package com.stanzaliving.core.opscalculator.dto;
 
-import lombok.*;
-import lombok.experimental.SuperBuilder;
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
@@ -13,60 +15,136 @@ import java.util.List;
 @NoArgsConstructor
 @ToString(callSuper = true)
 public class MonthlyForcastCsvDto {
-    private String city;
-    private String microMarket;
-    private String residenceName;
-    private String month;
-    private Double bedOccupancy;
-    private Double roomOccupancy;
+	@CsvBindByPosition(position = 0)
+	@CsvBindByName(column = "City")
+	private String city;
+	
+	@CsvBindByPosition(position = 1)
+	@CsvBindByName(column = "MicroMarket")
+	private String microMarket;
+	
+	@CsvBindByPosition(position = 2)
+	@CsvBindByName(column = "residenceName")
+	private String residenceName;
+	
+	@CsvBindByPosition(position = 3)
+	@CsvBindByName(column = "Month")
+	private String month;
+	
+	@CsvBindByPosition(position = 4)
+	@CsvBindByName(column = "BedOccupancy")
+	private Double bedOccupancy;
+	
+	@CsvBindByPosition(position = 5)
+	@CsvBindByName(column = "RoomOccupancy")
+	private Double roomOccupancy;
    
+	@CsvBindByPosition(position = 6)
+	@CsvBindByName(column = "Food")
     private Double food;
+	
+	@CsvBindByPosition(position = 7)
+	@CsvBindByName(column = "FoodHolidays")
     private Double foodHolidays;
+	
+	@CsvBindByPosition(position = 8)
+	@CsvBindByName(column = "FoodUtil")
     private Double foodUtil;
+	
+	@CsvBindByPosition(position = 9)
+	@CsvBindByName(column = "FoodMenuPricePerDay")
     private Double foodMenuPricePerDay;
     
+	@CsvBindByPosition(position = 10)
+	@CsvBindByName(column = "Electricity")
     private Double electricity;
+	
+	@CsvBindByPosition(position = 11)
+	@CsvBindByName(column = "ElectricityFixedCharges")
     private Double electricityFixedCharges;
+	
+	@CsvBindByPosition(position = 12)
+	@CsvBindByName(column = "ElectricityVariableCharges")
     private Double electricityVariableCharges;
     
+	@CsvBindByPosition(position = 13)
+	@CsvBindByName(column = "Water")
     private Double water;
+	
+	@CsvBindByPosition(position = 14)
+	@CsvBindByName(column = "WaterFixedCharges")
     private Double waterFixedCharges;
+	
+	@CsvBindByPosition(position = 15)
+	@CsvBindByName(column = "WaterVariableCharges")
     private Double waterVariableCharges;
     
-    private Double dG;
+	@CsvBindByPosition(position = 16)
+	@CsvBindByName(column = "DG")
+    private Double dg;
     
+	@CsvBindByPosition(position = 17)
+	@CsvBindByName(column = "Housekeeping")
     private Double housekeeping;
+	
+	@CsvBindByPosition(position = 18)
+	@CsvBindByName(column = "HousekeepingHK8hrs")
     private Double housekeepingHK8hrs;
+    
+    @CsvBindByPosition(position = 19)
+	@CsvBindByName(column = "HousekeepingHK12hrs")
     private Double housekeepingHK12hrs;
+    
+    @CsvBindByPosition(position = 20)
+	@CsvBindByName(column = "HousekeepingHKCT")
     private Double housekeepingHKCT;
     
+    @CsvBindByPosition(position = 21)
+	@CsvBindByName(column = "Security")
     private Double security;
+    
+    @CsvBindByPosition(position = 22)
+	@CsvBindByName(column = "SecurityInHouseSecurity")
     private Double securityInHouseSecurity;
+    
+    @CsvBindByPosition(position = 23)
+	@CsvBindByName(column = "SecurityInSecurity")
     private Double securityInSecurity;
     
-    
+    @CsvBindByPosition(position = 24)
+	@CsvBindByName(column = "Personnel")
     private Double personnel;
+    
+    @CsvBindByPosition(position = 25)
+	@CsvBindByName(column = "PersonnelRC")
     private Double personnelRC;
+    
+    @CsvBindByPosition(position = 26)
+	@CsvBindByName(column = "PersonnelOE")
     private Double personnelOE;
     
+    @CsvBindByPosition(position = 27)
+	@CsvBindByName(column = "Internet")
     private Double internet;
     
+    @CsvBindByPosition(position = 28)
+	@CsvBindByName(column = "Laundry")
     private Double laundry;
     
+    @CsvBindByPosition(position = 29)
+	@CsvBindByName(column = "Supplies")
     private Double supplies;
+    
+    @CsvBindByPosition(position = 30)
+	@CsvBindByName(column = "SuppliesDTH")
     private Double suppliesDTH;
+    
+    @CsvBindByPosition(position = 31)
+	@CsvBindByName(column = "SuppliesGeneralSupplies")
     private Double suppliesGeneralSupplies;
-    private Double suppliesCleaningChemicals;
     
-    
-    
-    
-    
- 
-    
-    
-   
-    
-    
+    @CsvBindByPosition(position = 32)
+	@CsvBindByName(column = "SuppliesCleaningChemicals")
+    private Double suppliesCleaningChemicals; 
     
 }
