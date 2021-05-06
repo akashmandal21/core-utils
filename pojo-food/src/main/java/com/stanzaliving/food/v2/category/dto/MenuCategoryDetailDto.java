1536a60b86a86cbfcb422e1f5fe1a54ff3dd0aef
ketan.yadav <ketan.yadav@stanzaliving.com>
2021-05-06 15:14:13 +0530

    changes

--- pojo-food/src/main/java/com/stanzaliving/food/v2/category/dto/MenuCategoryDetailDto.java
+++ pojo-food/src/main/java/com/stanzaliving/food/v2/category/dto/MenuCategoryDetailDto.java
@@ -6,7 +6,6 @@ import com.stanzaliving.core.base.common.dto.ListingDto;
 import com.stanzaliving.core.food.enums.FoodServeType;
 import com.stanzaliving.core.food.enums.MenuType;
 import com.stanzaliving.food.v2.category.constant.CategoryStatus;
-import com.stanzaliving.food.v2.common.dto.ResidenceRDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,32 +20,33 @@ import java.util.List;
 @SuperBuilder
 @NoArgsConstructor
 public class MenuCategoryDetailDto extends MenuCategoryDTO {
-    private String versionId;
-    private MenuType menuType;
-    private Double marginPercentage;
-    private Double utilityCost;
-    private List<MenuCategoryMealDto> mealData;
-    private CategoryStatus categoryStatus;
-    private FoodServeType foodServeType;
-    private Integer maxWeeklyCombo;
-    private boolean sampleMenuDefined;
-    private boolean grammageDefined;
-    private List<ListingDto> demographicTags;
+	private String versionId;
+	private MenuType menuType;
+	private Double marginPercentage;
+	private Double utilityCost;
+	private List<MenuCategoryMealDto> mealData;
+	private CategoryStatus categoryStatus;
+	private FoodServeType foodServeType;
+	private Integer maxWeeklyCombo;
+	private boolean sampleMenuDefined;
+	private boolean grammageDefined;
+	private List<ListingDto> demographicTags;
 
-    //derived information
-    private Double mealPrice;
-    private int totalMealInWeek;
-    private List<String> draftVersions;
-    private String mealWiseName;
-    private String fullName;
-    private List<MenuCategoryResidenceRDto> residences;
-    private int noOfResidenceServed;
+	//derived information
+	private Double mealPrice;
+	private int totalMealInWeek;
+	private List<String> draftVersions;
+	private String mealWiseName;
+	private String fullName;
+	private List<MenuCategoryResidenceRDto> residences;
+	private int noOfResidenceServed;
 
 
-    //approvalData
-    private ApprovalStatus approvalStatus;
-    private List<String> pendingApprovers;
+	//approvalData
+	private ApprovalStatus approvalStatus;
+	private List<String> pendingApprovers;
 
-    private CompositionRuleV2Dto compositionRules;
-    private List<DayWiseSampleMenuDto> dayWiseMenu;
+	private CompositionRuleV2Dto compositionRules;
+	private List<DayWiseSampleMenuDto> dayWiseMenu;
+	private List<MealWisePriceDto> mealWisePrice;
 }
