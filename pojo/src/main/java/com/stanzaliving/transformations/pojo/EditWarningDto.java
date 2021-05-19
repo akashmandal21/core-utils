8a62fcc53da19b24adf3321129a6d3a52f70346e
dhruv-ninleaps <dhruv.mathur@nineleaps.com>
2021-05-19 23:10:24 +0530

    updated dto

--- pojo/src/main/java/com/stanzaliving/transformations/pojo/EditWarningDto.java
+++ pojo/src/main/java/com/stanzaliving/transformations/pojo/EditWarningDto.java
@@ -15,7 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EditWarningDto {
-    private String name;
+    private List<String> name;
     private List<String> previousData;
     private List<String> newData;
 }
