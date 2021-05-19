c3cb4ae7cab3f66adb5f39ef725e7dd0508248c0
Mohammad Sameer Khan <sameerkhan.retainer@stanzaliving.com>
2021-05-19 23:08:51 +0530

    done

--- pojo/src/main/java/com/stanzaliving/core/estate/enums/SalesReviewStatus.java
+++ pojo/src/main/java/com/stanzaliving/core/estate/enums/SalesReviewStatus.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 public enum SalesReviewStatus {
 
 	IN_DRAFT("In-Draft", "#E6E9EA", "#7A7D7E"),
-	SUBMITTED("Sales Review Submitted", "#DDF2F5", "#5FC4F5"),
+	SUBMITTED("Sales Review Submitted", "#FFEAB6", "#FFB701"),
 	APPROVED("Sales Review Approved", "#B8E0FF", "#5FC4F5"),
 	SENT_BACK("Sent Back", "#FFF1F1", "#F55F71"),
 	PROPERTY_DROPPED("Property Dropped", "#F0F2F2", "#7A7D7E");
