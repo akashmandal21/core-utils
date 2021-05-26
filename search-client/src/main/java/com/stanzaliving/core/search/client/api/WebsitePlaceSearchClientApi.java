af5f69dd52154085fd6c7f27db4227099d45e904
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-05-26 23:12:35 +0530

    Create WebsitePlaceSearchClientApi.java

---
+++ search-client/src/main/java/com/stanzaliving/core/search/client/api/WebsitePlaceSearchClientApi.java
@@ -0,0 +1,68 @@
+package com.stanzaliving.core.search.client.api;
+
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.stanzaliving.core.base.common.dto.ResponseDto;
+import com.stanzaliving.core.base.exception.ApiValidationException;
+import com.stanzaliving.core.base.exception.PreconditionFailedException;
+import com.stanzaliving.core.base.http.StanzaRestClient;
+import com.stanzaliving.website.elasticsearch.index.dto.WebsitePlaceIndexDto;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class WebsitePlaceSearchClientApi {
+
+	private final StanzaRestClient restClient;
+
+	public WebsitePlaceSearchClientApi(StanzaRestClient stanzaRestClient) {
+		this.restClient = stanzaRestClient;
+	}
+
+	public List<WebsitePlaceIndexDto> searchWebsitePlace(String place) {
+
+		String path = UriComponentsBuilder.fromPath("/internal/website/place/search").build().toUriString();
+
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+		
+		queryParams.add("place", place);
+
+		final HttpHeaders headerParams = new HttpHeaders();
+
+		final String[] accepts = {"*/*"};
+
+		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+		TypeReference<ResponseDto<List<WebsitePlaceIndexDto>>> returnType = new TypeReference<ResponseDto<List<WebsitePlaceIndexDto>>>() {};
+
+		ResponseDto<List<WebsitePlaceIndexDto>> responseDto = new ResponseDto<>();
+
+		try {
+
+			responseDto = restClient.request(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType, MediaType.APPLICATION_JSON);
+
+		} catch (Exception e) {
+
+			log.error("Error while searching for website place", e);
+
+			throw new ApiValidationException("Some error occurred. Please try again after some time.");
+		}
+
+		if (!responseDto.isStatus()) {
+
+			throw new PreconditionFailedException(responseDto.getMessage());
+
+		}
+
+		return responseDto.getData();
+	}
+}
+
