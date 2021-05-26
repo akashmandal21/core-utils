29b585176a0ce8f2557f3fe6a6c35a3453399fa8
Naveen Kumar <naveen.kumar@stanzaliving.com>
2021-05-26 20:10:33 +0530

    updated event names

--- pojo/src/main/java/com/stanzaliving/genericdashboard/enums/FoodEventName.java
+++ pojo/src/main/java/com/stanzaliving/genericdashboard/enums/FoodEventName.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum FoodEventCategory {
+public enum FoodEventName {
+
 	FEEDBACK_GIVEN_FOR_CAFE_VAS_ORDER("Feedback given for Cafe (VAS) Order"),
 	CAFE_VAS_ORDER_PLACED("Cafe (VAS) Order Placed"),
 	CAFE_VAS_ORDER_QR_SCANNED("Cafe (VAS) Order QR Scanned"),
@@ -13,9 +14,9 @@ public enum FoodEventCategory {
 	FEEDBACK_GIVEN_FOR_REGULAR_MEAL("Feedback given for regular meal"),
 	CAFE_ORDER_MARKED_READY("Cafe Order Marked as ready"),
 	MEAL_PREFERENCE_SUBMITTED("Meal Preference Submitted"),
-	CAFE_ORDER_PARTIALLY_CANCELLED_BY_CAFE("Cafe Order partially cancelled (by cafe)"),
+	CAFE_ORDER_PARTIALLY_CANCELLED_BY_CAFE("Cafe Order partially cancelled)"),
 	RC_RECEIVED_REGULAR_MEALS_IN_JARVIS_APP("RC received the Regular meals in Jarvis app"),
 	RC_RECEIVED_BATCH_CAFE_ORDER_IN_JARVIS_APP("RC received the Batch Cafe Orders in Jarvis app");
 
-	String foodEventCategoryValue;
+	String eventName;
 }
