58a2357c136a4119a01313564fa4a026b49a108c
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-05-28 03:11:33 +0530

    Update WebsiteDistanceConstants.java

--- pojo/src/main/java/com/stanzaliving/website/constants/WebsiteDistanceConstants.java
+++ pojo/src/main/java/com/stanzaliving/website/constants/WebsiteDistanceConstants.java
@@ -3,7 +3,7 @@ package com.stanzaliving.website.constants;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
-public class WebsiteElasticsearchConstants {
+public class WebsiteDistanceConstants {
 
 	public final double MINIMUM_DISTANCE_VALUE = 0.1;
 }
