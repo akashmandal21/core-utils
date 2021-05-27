cb25f88c5ef0d59f86b44c7be67d93ca33128da3
KarthikSJhingade <karthik.jhingade@nineleaps.com>
2021-05-27 14:51:03 +0530

    Karthik | Moved the DTOs' from grn service to pojo/core/oracle/integration

---
+++ pojo/src/main/java/com/stanzaliving/core/oracle/integration/OracleItemDetailsDto.java
@@ -0,0 +1,25 @@
+package com.stanzaliving.core.oracle.integration;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OracleItemDetailsDto {
+
+    private String item;
+    private boolean receive;
+    private BigDecimal quantity=BigDecimal.ZERO;
+    private BigDecimal quantityReceived=BigDecimal.ZERO;
+    private BigDecimal quantityReturned=BigDecimal.ZERO;
+    private String returnReason;
+    private BigDecimal quantityInstalled=BigDecimal.ZERO;
+    private BigDecimal quantityRejected=BigDecimal.ZERO;
+    private String rejectionReason;
+}
