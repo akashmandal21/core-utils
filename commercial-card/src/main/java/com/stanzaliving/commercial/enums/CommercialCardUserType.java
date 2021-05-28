a953be52c6292db4473b9fd210f28da7d6ff7826
malhotravanshika <vanshikha.malhotra@nineleaps.com>
2021-05-28 15:37:21 +0530

    Adds enum package for commercial-card

---
+++ commercial-card/src/main/java/com/stanzaliving/commercial/enums/CommercialCardUserType.java
@@ -0,0 +1,11 @@
+package com.stanzaliving.commercial.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CommercialCardUserType {
+    SCHOLAR,
+    SUITES
+}
