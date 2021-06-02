13e4b943f6f31fd7fee82fb89efef24aef390fa4
Shanthi-rajendran-22 <shanthi.r@nineleaps.com>
2021-06-03 00:57:08 +0530

    Approval color code and filter date format updates

--- pojo-generic-approval/src/main/java/com/stanzaliving/approve/enums/ApprovalStatus.java
+++ pojo-generic-approval/src/main/java/com/stanzaliving/approve/enums/ApprovalStatus.java
@@ -13,18 +13,18 @@ import java.util.stream.Collectors;
 @Getter
 @AllArgsConstructor
 public enum ApprovalStatus {
-    PENDING_L1_APPROVAL("Pending L1 Approval",1, ColorCode.LIGHT_GRAYISH_PINK.getColorCode(), ColorCode.VERY_DARK_GRAYISH_CYAN.getColorCode(), ColorCode.GRAYISH_RED.getColorCode(), "Successfully Submitted for L1 Approval"),
+    PENDING_L1_APPROVAL("Pending L1 Approval",1, ColorCode.LIGHT_GRAYISH_PINK.getColorCode(), ColorCode.DARK_GRAYISH_BLUE.getColorCode(), ColorCode.GRAYISH_RED.getColorCode(), "Successfully Submitted for L1 Approval"),
     L1_REJECTED("L1 Rejected",1, ColorCode.VERY_PALE_MOSTLY_WHITE_RED.getColorCode(), ColorCode.SOFT_RED.getColorCode(), ColorCode.GRAYISH_RED.getColorCode(), "Successfully Rejected"),
-    PENDING_L2_APPROVAL("Pending L2 Approval",2, ColorCode.LIGHT_GRAYISH_ORANGE.getColorCode(), ColorCode.VERY_DARK_GRAYISH_CYAN.getColorCode(), ColorCode.VERY_SOFT_ORANGE.getColorCode(),"Successfully Submitted for L2 Approval"),
+    PENDING_L2_APPROVAL("Pending L2 Approval",2, ColorCode.LIGHT_GRAYISH_ORANGE.getColorCode(), ColorCode.DARK_GRAYISH_BLUE.getColorCode(), ColorCode.VERY_SOFT_ORANGE.getColorCode(),"Successfully Submitted for L2 Approval"),
     L2_REJECTED("L2 Rejected",2, ColorCode.VERY_PALE_MOSTLY_WHITE_RED.getColorCode(), ColorCode.SOFT_RED.getColorCode(), ColorCode.VERY_SOFT_ORANGE.getColorCode(),"Successfully Rejected"),
 
-    PENDING_L3_APPROVAL("Pending L3 Approval", 3, ColorCode.VERY_PALE_MOSTLY_WHITE_VIOLET.getColorCode(), ColorCode.VERY_DARK_GRAYISH_CYAN.getColorCode(), ColorCode.STRONG_VIOLET.getColorCode(), "Successfully Submitted for L3 Approval"),
+    PENDING_L3_APPROVAL("Pending L3 Approval", 3, ColorCode.VERY_PALE_MOSTLY_WHITE_VIOLET.getColorCode(), ColorCode.DARK_GRAYISH_BLUE.getColorCode(), ColorCode.STRONG_VIOLET.getColorCode(), "Successfully Submitted for L3 Approval"),
     L3_REJECTED("L3 Rejected", 3, ColorCode.VERY_PALE_MOSTLY_WHITE_RED.getColorCode(), ColorCode.SOFT_RED.getColorCode(), ColorCode.STRONG_VIOLET.getColorCode(), "Successfully Rejected"),
 
-    PENDING_L4_APPROVAL("Pending L4 Approval", 4, ColorCode.VERY_PALE_MOSTLY_WHITE_BLUE.getColorCode(), ColorCode.VERY_DARK_GRAYISH_CYAN.getColorCode(), ColorCode.VERY_LIGHT_BLUE.getColorCode(),"Successfully Submitted for L4 Approval"),
+    PENDING_L4_APPROVAL("Pending L4 Approval", 4, ColorCode.VERY_PALE_MOSTLY_WHITE_BLUE.getColorCode(), ColorCode.DARK_GRAYISH_BLUE.getColorCode(), ColorCode.VERY_LIGHT_BLUE.getColorCode(),"Successfully Submitted for L4 Approval"),
     L4_REJECTED("L4 Rejected", 4, ColorCode.VERY_PALE_MOSTLY_WHITE_RED.getColorCode(), ColorCode.SOFT_RED.getColorCode(), ColorCode.VERY_LIGHT_BLUE.getColorCode(),"Successfully Rejected"),
 
-    PENDING_L5_APPROVAL("Pending L5 Approval", 5, ColorCode.PALE_ORANGE.getColorCode(), ColorCode.VERY_DARK_GRAYISH_CYAN.getColorCode(), ColorCode.VIVID_ORANGE.getColorCode(), "Successfully Submitted for L5 Approval"),
+    PENDING_L5_APPROVAL("Pending L5 Approval", 5, ColorCode.PALE_ORANGE.getColorCode(), ColorCode.DARK_GRAYISH_BLUE.getColorCode(), ColorCode.VIVID_ORANGE.getColorCode(), "Successfully Submitted for L5 Approval"),
     L5_REJECTED("L5 Rejected", 5, ColorCode.VERY_PALE_MOSTLY_WHITE_RED.getColorCode(), ColorCode.SOFT_RED.getColorCode(), ColorCode.VIVID_ORANGE.getColorCode(),"Successfully Rejected"),
 
     APPROVED("Approved", 6, ColorCode.SOFT_GREEN.getColorCode(), ColorCode.GREEN_CYAN.getColorCode(), ColorCode.GREEN_CYAN.getColorCode(),"Successfully Approved"),        //color code need to be updated with product
