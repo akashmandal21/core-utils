6ed753e991f315207e8f779a24b8ea21f0973059
anjali <anjali.garg@nineleaps.com>
2021-06-02 12:40:08 +0530

    notifications

---
+++ pojo/src/main/java/com/stanzaliving/core/dto/KycDocumentDTO.java
@@ -0,0 +1,39 @@
+package com.stanzaliving.core.dto;
+
+import com.stanzaliving.core.enums.ApprovalStatus;
+import com.stanzaliving.core.enums.DocumentSide;
+import com.stanzaliving.core.enums.DocumentTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KycDocumentDTO {
+    private String documentUuid;
+
+    private DocumentTypeEnum documentType;
+
+    private DocumentSide documentSide;
+
+    private String filePath;
+
+    private String fileName;
+
+    private String fileUrl;
+
+    private String metadata;
+
+    private String residentUuid;
+
+    private String bookingUuid;
+
+    private boolean isVerified;
+
+    private ApprovalStatus approvalStatus;
+
+    private String rejectionReason;
+}
