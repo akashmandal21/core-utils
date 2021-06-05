6eff564dc8863c9c79f7d5c287b86ac3c3bfe4d0
aayush07raj <aayush.raj@nineleaps.com>
2021-06-05 16:51:13 +0530

    added residentId & removed attributes from searchDTO

--- pojo/src/main/java/com/stanzaliving/ventaInvoice/dto/SearchDTO.java
+++ pojo/src/main/java/com/stanzaliving/ventaInvoice/dto/SearchDTO.java
@@ -1,23 +1,12 @@
 package com.stanzaliving.ventaInvoice.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.stanzaliving.ventaInvoice.enums.DocumentType;
 import lombok.*;
-
-import java.time.LocalDate;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchDTO {
-    private Integer documentId;
-    private String referenceId;
-    @JsonFormat(pattern ="yyyy-MM-dd")
-    private LocalDate issueDate;
-    private Double amount;
-    private DocumentType type;
-    private BillToDto billTo;
-    private CategoryDto category;
-    private InvoiceDurationDto duration;
+    private String stanzaDocumentId;
+    private String residentId;
 }
