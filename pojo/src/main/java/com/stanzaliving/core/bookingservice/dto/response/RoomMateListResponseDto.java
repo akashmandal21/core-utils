a3ac3dabd545ff313e3ed282b4f058f120c71f83
Mohit Gupta <mohitgupta.retainer@stanzaliving.com>
2021-06-07 09:54:37 +0530

    dto

---
+++ pojo/src/main/java/com/stanzaliving/core/bookingservice/dto/response/RoomMateListResponseDto.java
@@ -0,0 +1,16 @@
+package com.stanzaliving.core.bookingservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoomMateListResponseDto {
+
+	private String name;
+	private String profilePic;
+}
