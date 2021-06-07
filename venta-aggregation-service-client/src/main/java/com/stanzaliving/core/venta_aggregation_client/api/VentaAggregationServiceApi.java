605fc0fa309254340da817ef1c87a8575442ae1f
RajKumarStanza <raj.singh@stanzaliving.com>
2021-06-07 19:44:45 +0530

    Residence aggregation response handling

--- venta-aggregation-service-client/src/main/java/com/stanzaliving/core/venta_aggregation_client/api/VentaAggregationServiceApi.java
+++ venta-aggregation-service-client/src/main/java/com/stanzaliving/core/venta_aggregation_client/api/VentaAggregationServiceApi.java
@@ -1,5 +1,6 @@
 package com.stanzaliving.core.venta_aggregation_client.api;
 
+import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
 import com.stanzaliving.core.ventaaggregationservice.dto.ResidenceAggregationEntityDto;
 import lombok.extern.log4j.Log4j2;
@@ -30,17 +31,19 @@ public class VentaAggregationServiceApi {
      * @param residenceUuid->Unique ID corresponding to residence
      * @return Aggregated Residence data on aggregation service
      */
-    public ResidenceAggregationEntityDto getAggregatedResidenceInformation(String residenceUuid) {
+    public ResponseDto<ResidenceAggregationEntityDto> getAggregatedResidenceInformation(String residenceUuid) {
         log.info("Aggregation Residence Controller::Processing to get residence information on basis of residenceUuid {}", residenceUuid);
-        Map<String, Object> uriVariables = new HashMap();
+        Map<String, Object> uriVariables = new HashMap<>();
         uriVariables.put("residenceUuid", residenceUuid);
+        
         String path = UriComponentsBuilder.fromPath("/internal/residence/{residenceUuid}")
                 .buildAndExpand(uriVariables).toUriString();
-        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap();
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
         HttpHeaders headerParams = new HttpHeaders();
         String[] accepts = new String[]{"*/*"};
         List<MediaType> accept = this.restClient.selectHeaderAccept(accepts);
-        ParameterizedTypeReference<ResidenceAggregationEntityDto> returnType = new ParameterizedTypeReference<ResidenceAggregationEntityDto>() {};
+        
+        ParameterizedTypeReference<ResponseDto<ResidenceAggregationEntityDto>> returnType = new ParameterizedTypeReference<ResponseDto<ResidenceAggregationEntityDto>>() {};
         try {
             log.info("Executing Api for getting residence Info with Url {}", path);
             return this.restClient.invokeAPI(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType);
