ce258b305462a48f11c4e4ecc627c2595cb6c922
jalaj gupta <jalaj.gupta@stanzaliving.com>
2021-06-09 18:35:01 +0530

    venta client  for location
    venta client  for location

--- venta-client/src/main/java/com/stanzaliving/core/backendlocator/client/api/VentaClientApi.java
+++ venta-client/src/main/java/com/stanzaliving/core/backendlocator/client/api/VentaClientApi.java
@@ -293,4 +293,24 @@ public class VentaClientApi {
 		return restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
 	}
 
+	public String getLocation(String house) {
+
+		Object postBody = null;
+
+		final Map<String, Object> uriVariables = new HashMap<>();
+		String path = UriComponentsBuilder.fromPath("/house/location").buildAndExpand(uriVariables).toUriString();
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+		queryParams.add("house", house);
+		final HttpHeaders headerParams = new HttpHeaders();
+
+		final String[] accepts = { "*/*" };
+		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+		ParameterizedTypeReference<String> returnType = new ParameterizedTypeReference<String>() {
+		};
+
+		return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
+	}
+
 }
