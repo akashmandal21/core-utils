5e62235939a645cbf79e8b59e1147934c4de8b16
kapil-stanza <kapil.dev@stanzaliving.com>
2021-06-10 13:03:53 +0530

    added responsee dto

---
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/food/response/DemographicsResponseDto.java
@@ -0,0 +1,22 @@
+package com.stanzaliving.wanda.food.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Builder
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class DemographicsResponseDto {
+
+	private String residenceName;
+	private String microMarketName;
+	private Integer mirCount;
+	List<FoodRegionPreference> foodRegionPreferenceList;
+}
