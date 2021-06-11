30b53c65ae2f74b62ac6d76b415e7b84c328721a
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-06-11 19:20:09 +0530

    Revert "Update ImageTags.java"
    This reverts commit 35b0b0fbabd436146392396d0f8a37e1496adba7.

--- pojo/src/main/java/com/stanzaliving/website/enums/ImageTags.java
+++ pojo/src/main/java/com/stanzaliving/website/enums/ImageTags.java
@@ -1,13 +1,11 @@
 package com.stanzaliving.website.enums;
 
-import java.util.Comparator;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum ImageTags implements Comparator<ImageTags> {
+public enum ImageTags {
 	COMMONAREA("Common Area"),
 	DININGAREA("Dining Area"),
 	KITCHEN("Kitchen"),
@@ -19,17 +17,4 @@ public enum ImageTags implements Comparator<ImageTags> {
 	OTHERS("Others");
 	
 	public String imageTagName;
-
-	@Override
-	public int compare(ImageTags tag1, ImageTags tag2) {
-
-		if (tag1.equals(ImageTags.OTHERS)) {
-			return 1;
-		}
-		if (tag2.equals(ImageTags.OTHERS)) {
-			return -1;
-		}
-
-		return tag1.compareTo(tag2);
-	}
 }
