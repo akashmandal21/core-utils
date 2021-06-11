35b0b0fbabd436146392396d0f8a37e1496adba7
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-06-11 18:33:07 +0530

    Update ImageTags.java

--- pojo/src/main/java/com/stanzaliving/website/enums/ImageTags.java
+++ pojo/src/main/java/com/stanzaliving/website/enums/ImageTags.java
@@ -1,11 +1,13 @@
 package com.stanzaliving.website.enums;
 
+import java.util.Comparator;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum ImageTags {
+public enum ImageTags implements Comparator<ImageTags> {
 	COMMONAREA("Common Area"),
 	DININGAREA("Dining Area"),
 	KITCHEN("Kitchen"),
@@ -17,4 +19,17 @@ public enum ImageTags {
 	OTHERS("Others");
 	
 	public String imageTagName;
+
+	@Override
+	public int compare(ImageTags tag1, ImageTags tag2) {
+
+		if (tag1.equals(ImageTags.OTHERS)) {
+			return 1;
+		}
+		if (tag2.equals(ImageTags.OTHERS)) {
+			return -1;
+		}
+
+		return tag1.compareTo(tag2);
+	}
 }
