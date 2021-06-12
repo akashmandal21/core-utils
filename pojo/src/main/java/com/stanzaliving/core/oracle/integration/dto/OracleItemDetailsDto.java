91e8dbcbb07844760a31f3df282d934b9270b3f9
ankitAtStanza <58777336+ankitAtStanza@users.noreply.github.com>
2021-06-12 14:57:08 +0530

    added builder defaults

--- pojo/src/main/java/com/stanzaliving/core/oracle/integration/dto/OracleItemDetailsDto.java
+++ pojo/src/main/java/com/stanzaliving/core/oracle/integration/dto/OracleItemDetailsDto.java
@@ -15,11 +15,16 @@ public class OracleItemDetailsDto {
 
     private String item;
     private boolean receive;
+    @Builder.Default
     private BigDecimal quantity=BigDecimal.ZERO;
+    @Builder.Default
     private BigDecimal quantityReceived=BigDecimal.ZERO;
+    @Builder.Default
     private BigDecimal quantityReturned=BigDecimal.ZERO;
     private String returnReason;
+    @Builder.Default
     private BigDecimal quantityInstalled=BigDecimal.ZERO;
+    @Builder.Default
     private BigDecimal quantityRejected=BigDecimal.ZERO;
     private String rejectionReason;
 }
