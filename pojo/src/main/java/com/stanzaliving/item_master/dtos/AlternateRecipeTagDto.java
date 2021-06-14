d2a27f2fb0d028f1ad6b1320d13c736059b511c2
pradhumna1 <pradhumna1@nineleaps.com>
2021-06-14 14:53:03 +0530

    Alternate recipe tag dto

---
+++ pojo/src/main/java/com/stanzaliving/item_master/dtos/AlternateRecipeTagDto.java
@@ -0,0 +1,25 @@
+package com.stanzaliving.item_master.dtos;
+
+import com.stanzaliving.core.generic.dto.UIKeyValue;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlternateRecipeTagDto {
+
+    private String recipeTagUuid;
+    private UIKeyValue currentRecipeTag;
+    private UIKeyValue currentRecipeUom;
+    private List<UIKeyValue> alternateRecipeTagOptions;
+    private UIKeyValue alternateRecipeTag;
+    private Boolean errorOccurred;
+    private String errorMessage;
+    private List<ItemsMapped> items;
+}
