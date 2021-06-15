7b4c3f4a780c03a41daa5cb144e6e92a4b30a474
nihalasati <nihal.asati@nineleaps.com>
2021-06-15 13:57:50 +0530

    Bug Fix:Updated User List DTO validations

--- pojo/src/main/java/com/stanzaliving/community/request/AddEventDTO.java
+++ pojo/src/main/java/com/stanzaliving/community/request/AddEventDTO.java
@@ -18,6 +18,9 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddEventDTO {
+
+    private String eventID;
+
     @NotEmpty(message = "Event Status Should not be empty",
             groups = {DraftValidations.class, ApprovalValidation.class})
     private String eventStatus;
--- pojo/src/main/java/com/stanzaliving/community/request/UserListDTO.java
+++ pojo/src/main/java/com/stanzaliving/community/request/UserListDTO.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserListDTO {
-    @NotNull(message = "Please select User List Type",groups = {DraftValidations.class, ApprovalValidation.class})
+    @NotNull(message = "Please select User List Type",groups = {ApprovalValidation.class})
     private UserList userListType;
 
     private EventLocationDTO specificLocation;
