f94e9269c3108affed8207bd7023b87ca6d8bdfd
kapil-stanza <kapil.dev@stanzaliving.com>
2021-06-17 15:09:59 +0530

    removed date

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/food/request/DemographicsDataRequestDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/food/request/DemographicsDataRequestDto.java
@@ -1,14 +1,10 @@
 package com.stanzaliving.wanda.food.request;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.stanzaliving.core.base.localdate.Java8LocalDateStdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-
-import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -18,8 +14,5 @@ import java.time.LocalDate;
 public class DemographicsDataRequestDto extends DemographicsRequestDto {
 
 	private String vendorId;
-
-	@JsonSerialize(using = Java8LocalDateStdSerializer.class)
-	private LocalDate date;
 
 }
