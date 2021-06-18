39e8019dd2e2590ed98532d73a5fa1164a98b418
anjali <anjali.garg@nineleaps.com>
2021-06-18 16:47:49 +0530

    getResidenceByName

--- pojo/src/main/java/com/stanzaliving/core/dto/BookingInventoryDTO.java
+++ pojo/src/main/java/com/stanzaliving/core/dto/BookingInventoryDTO.java
@@ -40,4 +40,6 @@ public class BookingInventoryDTO {
     private String residenceName;
 
     private boolean isAlfredEnabled = Boolean.FALSE;
+
+    private Date bookingDate;
 }
--- transformation-client/src/main/java/com/stanzaliving/core/transformation/client/api/InternalDataControllerApi.java
+++ transformation-client/src/main/java/com/stanzaliving/core/transformation/client/api/InternalDataControllerApi.java
@@ -879,4 +879,32 @@ public class InternalDataControllerApi {
 		}
 		return null;
 	}
+
+	public ResponseDto<ResidenceDto> getResidenceDetailsByResidenceName(String residenceName) {
+
+		Object postBody = null;
+
+		// create path and map variables
+		final Map<String, Object> uriVariables = new HashMap<>();
+		uriVariables.put("residenceName", residenceName);
+		String path =
+				UriComponentsBuilder.fromPath("/residence/get")
+						.buildAndExpand(uriVariables)
+						.toUriString();
+
+		final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+		queryParams.put("residenceName", Arrays.asList(residenceName));
+
+		final HttpHeaders headerParams = new HttpHeaders();
+
+		final String[] accepts = {"*/*"};
+
+		final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+		ParameterizedTypeReference<ResponseDto<ResidenceDto>> returnType =
+				new ParameterizedTypeReference<ResponseDto<ResidenceDto>>() {};
+
+		return restClient.invokeAPI(
+				path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
+	}
 }
