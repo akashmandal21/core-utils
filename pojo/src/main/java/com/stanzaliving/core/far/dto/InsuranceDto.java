46f690f96bf4a4ff8f58bec43b7ea2657cc0785e
shobhit <mittalshobhit7@gmail.com>
2021-06-19 13:17:52 +0530

    dto

--- pojo/src/main/java/com/stanzaliving/core/far/dto/InsuranceDto.java
+++ pojo/src/main/java/com/stanzaliving/core/far/dto/InsuranceDto.java
@@ -6,11 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.collections.CollectionUtils;
 
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 @Builder
@@ -28,4 +30,10 @@ public class InsuranceDto {
     private BigDecimal insuredValue;
     private BigDecimal insuranceAmount;
     private List<String> insuranceDocumentPathList; // where the uploaded doc. by user is stored
+
+    public boolean isEmpty() {
+        return Objects.isNull(this.insuranceDocumentNumber) && Objects.isNull(this.insuranceType) && Objects.isNull(this.insuranceCompany)
+                && Objects.isNull(this.contactNumber) && Objects.isNull(this.insuranceStartDate) && Objects.isNull(this.insuranceEndDate)
+                && Objects.isNull(this.insuredValue) && Objects.isNull(this.insuranceAmount) && CollectionUtils.isEmpty(this.insuranceDocumentPathList);
+    }
 }
