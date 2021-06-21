761cc2825b26e7ed7d10ba6411c1d3c6feaa4158
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-06-21 19:44:52 +0530

    Update ReviewCmsListingResponseDto.java

--- pojo/src/main/java/com/stanzaliving/website/response/dto/ReviewCmsListingResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/ReviewCmsListingResponseDto.java
@@ -3,6 +3,7 @@ package com.stanzaliving.website.response.dto;
 import java.util.Date;
 
 import com.stanzaliving.core.enums.PropertyEntityType;
+import com.stanzaliving.core.user.enums.EnumListing;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,7 +30,7 @@ public class ReviewCmsListingResponseDto {
 	
 	private String reviewText;
 	
-	private PropertyEntityType reviewType;
+	private EnumListing<PropertyEntityType> reviewType;
 	
 	private Date reviewDate;
 	
