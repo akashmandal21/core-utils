9264e0647fdb261d29ca1fa047c8227504c278c6
Shanthi-rajendran-22 <shanthi.r@nineleaps.com>
2021-06-21 09:31:46 +0530

    Monthly breakup

--- commercial-card/src/main/java/com/stanzaliving/core/commercialcode/client/api/CommercialDataControllerApi.java
+++ commercial-card/src/main/java/com/stanzaliving/core/commercialcode/client/api/CommercialDataControllerApi.java
@@ -2,10 +2,7 @@ package com.stanzaliving.core.commercialcode.client.api;
 
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
-import com.stanzaliving.core.commercialcode.dto.CommercialCardBookingDto;
-import com.stanzaliving.core.commercialcode.dto.CommercialCardDto;
-import com.stanzaliving.core.commercialcode.dto.CommercialCardListDto;
-import com.stanzaliving.core.commercialcode.dto.CommercialCardUsageDto;
+import com.stanzaliving.core.commercialcode.dto.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
@@ -105,6 +102,33 @@ public class CommercialDataControllerApi {
                 new ParameterizedTypeReference<ResponseDto<CommercialCardUsageDto>>() {};
 
         return this.restClient.invokeAPI(path, HttpMethod.PUT, queryParams, postBody, headerParams, accept, returnType);
+    }
+
+    public ResponseDto<CommercialCardResponseDto> getCardByUuid(String token, String commercialCardUuid) {
+
+        log.info("Commercial-code-Data-Controller::Processing to retrieve commercial card information {}", commercialCardUuid);
+
+        Map<String, Object> uriVariables = new HashMap();
+
+        uriVariables.put("uuid", commercialCardUuid);
+
+        String path = UriComponentsBuilder.fromPath("/api/v1/commercial-card/{uuid}").buildAndExpand(uriVariables).toUriString();
+
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap();
+
+        HttpHeaders headerParams = new HttpHeaders();
+
+        headerParams.add("Cookie", "token=" + token);
+
+        String[] accepts = new String[]{"*/*"};
+
+        List<MediaType> accept = this.restClient.selectHeaderAccept(accepts);
+
+        ParameterizedTypeReference<ResponseDto<CommercialCardResponseDto>> returnType =
+                new ParameterizedTypeReference<ResponseDto<CommercialCardResponseDto>>() {};
+
+        return this.restClient.invokeAPI(path, HttpMethod.GET
+                , queryParams, null, headerParams, accept, returnType);
     }
 
 }
