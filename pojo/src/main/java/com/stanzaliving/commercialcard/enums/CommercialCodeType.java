7160def4ba6991a1c826f3b336be5582ab34546d
malhotravanshika <vanshikha.malhotra@nineleaps.com>
2021-06-21 11:58:09 +0530

    Adds collection fee type enum

--- pojo/src/main/java/com/stanzaliving/commercialcard/enums/CommercialCodeType.java
+++ pojo/src/main/java/com/stanzaliving/commercialcard/enums/CommercialCodeType.java
@@ -3,6 +3,9 @@ package com.stanzaliving.commercialcard.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @AllArgsConstructor
 @Getter
 public enum CommercialCodeType {
@@ -15,4 +18,12 @@ public enum CommercialCodeType {
 
 
     String name;
+    public static Set<String> getAllCriteria() {
+        Set<String> criteria = new HashSet<>();
+
+        criteria.add(CommercialCodeType.CONTRACTTENURE.getName());
+        criteria.add(CommercialCodeType.LOCKIN.getName());
+
+        return criteria;
+    }
 }
---
+++ pojo/src/main/java/com/stanzaliving/commercialcard/enums/FeeCollectionStrategy.java
@@ -0,0 +1,13 @@
+package com.stanzaliving.commercialcard.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum FeeCollectionStrategy {
+    ONE_TIME_UPFRONT("ONE TIME UPFRONT"),
+    MONTHLY_PAYMENTS("MONTHLY PAYMENT");
+
+    String strategy;
+}
