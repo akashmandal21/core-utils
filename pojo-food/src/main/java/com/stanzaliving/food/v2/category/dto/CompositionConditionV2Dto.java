251e38ccb3953826b7242d3a77127cf09dacca9c
ketan.yadav <ketan.yadav@stanzaliving.com>
2021-06-21 10:00:38 +0530

    condition type addded

--- pojo-food/src/main/java/com/stanzaliving/food/v2/category/dto/CompositionConditionV2Dto.java
+++ pojo-food/src/main/java/com/stanzaliving/food/v2/category/dto/CompositionConditionV2Dto.java
@@ -2,6 +2,7 @@ package com.stanzaliving.food.v2.category.dto;
 
 import com.stanzaliving.core.operations.enums.MealType;
 import com.stanzaliving.core.rule.engine.dto.ConditionDto;
+import com.stanzaliving.food.v2.category.constant.CompositionConditionType;
 import com.stanzaliving.food.v2.category.constant.RuleDuration;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -21,6 +22,8 @@ public class CompositionConditionV2Dto extends ConditionDto {
 	
 	@NotNull(message = "Rule Condition is Empty")
 	private RuleDuration condition;
+	
+	private CompositionConditionType type;
 	
 	@Builder.Default
 	private Boolean passed = true;
