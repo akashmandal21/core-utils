f85f9ad7e90ff351f7b234d55effe73543ad3dfe
Mohit Gupta <mohitgupta.retainer@stanzaliving.com>
2021-06-21 12:19:14 +0530

    changes in payment plan

--- pojo/src/main/java/com/stanzaliving/core/commercialcode/dto/response/CommercialCardResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/core/commercialcode/dto/response/CommercialCardResponseDto.java
@@ -4,14 +4,18 @@ import com.stanzaliving.commercialcard.enums.CommercialCardStatus;
 import com.stanzaliving.core.commercialcode.dto.request.CommercialCardRequestDto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@Builder
 public class CommercialCardResponseDto extends CommercialCardRequestDto {
     private CommercialCardStatus cardStatus;
 }
