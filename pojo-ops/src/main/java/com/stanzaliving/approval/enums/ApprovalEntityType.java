dc780c534686f6da53293fd9cb9468748b37719e
Piyush Srivastava <piyush.srivastava@stanzaliving.com>
2021-06-24 17:45:05 +0530

    comma removed

--- pojo-ops/src/main/java/com/stanzaliving/approval/enums/ApprovalEntityType.java
+++ pojo-ops/src/main/java/com/stanzaliving/approval/enums/ApprovalEntityType.java
@@ -14,8 +14,7 @@ public enum ApprovalEntityType {
 	MONTHLY_UNDERWRITTEN("Monthly Underwritten", "underwritten", "underwritten"),
 	FOOD_MENU_CATEGORY("Food Menu Category", "foodMenuCategory", "foodMenuCategory"),
 	FOOD_MENU_CATEGORY_V2("Food Menu Category", "FOOD_SERVICE", "foodMenuCategoryV2"),
-	WEEKLY_MENU("Weekly Menu", "FOOD_SERVICE", "WEEKLY_MENU"),
-	;
+	WEEKLY_MENU("Weekly Menu", "FOOD_SERVICE", "WEEKLY_MENU");
 
 	private final String value;
 	private final String commentServiceName;
