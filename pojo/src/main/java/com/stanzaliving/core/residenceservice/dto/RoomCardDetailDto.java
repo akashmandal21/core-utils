c48ae0a957a4a089db64aaa1a3e2b6f2c53a7642
Rahul.Tiwary <rahul.kumar1@stanzaliving.com>
2021-06-28 19:05:59 +0530

    code chnage

--- pojo/src/main/java/com/stanzaliving/core/residenceservice/dto/RoomCardDetailDto.java
+++ pojo/src/main/java/com/stanzaliving/core/residenceservice/dto/RoomCardDetailDto.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class RoomCardDetailDto {
+public class RoomCardDetailDto implements Serializable {
 
     Double blendedPrice;
     Integer totalRooms;
