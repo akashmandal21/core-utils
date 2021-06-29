bb9d77cc2ce72a5d7b756874c5eb1d7039b86fc1
Bhupinder <bhupinderpal.kaur@nineleaps.com>
2021-06-29 11:51:59 +0530

    Dtos added to send property details to legal

---
+++ pojo/src/main/java/com/stanzaliving/estate_v2/dto/LegalPropertyDto.java
@@ -0,0 +1,29 @@
+package com.stanzaliving.estate_v2.dto;
+
+import com.stanzaliving.core.dto.AbstractMongoDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class LegalPropertyDto extends AbstractMongoDto{
+
+    private String propertyName;
+    private String propertyType;
+    private String city;
+    private String microMarket;
+    private String status;
+    private String propertyTypeUUid;
+
+}
