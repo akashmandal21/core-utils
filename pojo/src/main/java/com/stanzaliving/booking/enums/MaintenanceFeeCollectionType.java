880e984454bdf2d3836c291bc648637802bb304a
Rahul.Tiwary <rahul.kumar1@stanzaliving.com>
2021-06-30 12:43:03 +0530

    code for inventory uuid

--- pojo/src/main/java/com/stanzaliving/booking/enums/MaintenanceFeeCollectionType.java
+++ pojo/src/main/java/com/stanzaliving/booking/enums/MaintenanceFeeCollectionType.java
@@ -1,6 +1,10 @@
 package com.stanzaliving.booking.enums;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
--- pojo/src/main/java/com/stanzaliving/booking/enums/PaymentPlanType.java
+++ pojo/src/main/java/com/stanzaliving/booking/enums/PaymentPlanType.java
@@ -1,5 +1,9 @@
 package com.stanzaliving.booking.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
