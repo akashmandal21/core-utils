3f55e2d53ea747b8b792240da99984ee82cb8f4a
Naveen Kumar <naveen.kumar@stanzaliving.com>
2021-07-01 16:36:22 +0530

    added daywise cogs details dto

--- pojo-food/src/main/java/com/stanzaliving/food/v2/menu/dto/MenuSearchRDto.java
+++ pojo-food/src/main/java/com/stanzaliving/food/v2/menu/dto/MenuSearchRDto.java
@@ -24,6 +24,7 @@ import java.util.List;
 @SuperBuilder
 @NoArgsConstructor
 public class MenuSearchRDto extends AbstractDto {
+	private static final long serialVersionUID = 726075754816567584L;
 	private String vendorId;
 	private String cityId;
 	private String cityName;
