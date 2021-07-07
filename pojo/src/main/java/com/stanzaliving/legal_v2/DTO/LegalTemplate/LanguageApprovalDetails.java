64c7eaf47512e007ae9eda93fe31e2aece0cdfac
RamaKrishnaRaju <krishna.raju@nineleaps.com>
2021-07-07 17:05:50 +0530

    Added DTO for language approaval

---
+++ pojo/src/main/java/com/stanzaliving/legal_v2/DTO/LegalTemplate/LanguageApprovalDetails.java
@@ -0,0 +1,20 @@
+package com.stanzaliving.legal_v2.DTO.LegalTemplate;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LanguageApprovalDetails {
+
+
+    String documentURL;
+
+    List<DeviationDTO> deviationDTOList;
+
+}
