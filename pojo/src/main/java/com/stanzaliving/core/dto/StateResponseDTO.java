d06f2d82f8a4dd6140bb1a89c474e1c4814d642c
anjali <anjali.garg@nineleaps.com>
2021-07-12 13:19:12 +0530

    getter, setters removed

--- pojo/src/main/java/com/stanzaliving/core/dto/StateResponseDTO.java
+++ pojo/src/main/java/com/stanzaliving/core/dto/StateResponseDTO.java
@@ -9,20 +9,4 @@ import lombok.NoArgsConstructor;
 public class StateResponseDTO {
     private int stateId;
     private String name;
-
-    public int getStateId() {
-        return stateId;
-    }
-
-    public void setStateId(int stateId) {
-        this.stateId = stateId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
