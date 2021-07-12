47c7afc04d1c7f721db2c2114955a2cb28ab9910
nihalasati <nihal.asati@nineleaps.com>
2021-07-12 12:13:39 +0530

    EventFilterDTO: More Filters Field added

--- pojo/src/main/java/com/stanzaliving/community/request/EventFilterDTO.java
+++ pojo/src/main/java/com/stanzaliving/community/request/EventFilterDTO.java
@@ -2,6 +2,7 @@ package com.stanzaliving.community.request;
 
 import lombok.*;
 
+import java.util.Date;
 import java.util.List;
 
 @Builder
@@ -12,5 +13,12 @@ public class EventFilterDTO {
     private List<String> eventType;
     private List<String> eventCategory;
     private List<String> status;
+    private String venueType;
+    private List<String> eventOrganizer;
+    private UserListDTO userList;
+    private Date initialStartDate;
+    private Date initialEndDate;
+    private Date finalStartDate;
+    private Date finalEndDate;
 }
 
