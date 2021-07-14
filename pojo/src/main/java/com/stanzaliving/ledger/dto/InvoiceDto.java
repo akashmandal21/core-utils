0988c5c3b35c047ec98bf2d27f634bd32fffc9c2
Saima15 <saima.siddiqui@nineleaps.com>
2021-07-14 16:54:10 +0530

    Added field in invoice dto

--- pojo/src/main/java/com/stanzaliving/ledger/dto/InvoiceDto.java
+++ pojo/src/main/java/com/stanzaliving/ledger/dto/InvoiceDto.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class InvoiceDto {
 
+    private String uuid;
     private String referenceUuid;
     private String referenceType;
     private String subCategoryName;
