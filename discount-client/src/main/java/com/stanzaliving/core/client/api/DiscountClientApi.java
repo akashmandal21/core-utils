519c6b3237eca3ef853194d32798ae1e6c44faf7
Mohit Gupta <mohitgupta.retainer@stanzaliving.com>
2021-07-19 18:31:21 +0530

    changes in strategy

--- discount-client/src/main/java/com/stanzaliving/core/client/api/DiscountClientApi.java
+++ discount-client/src/main/java/com/stanzaliving/core/client/api/DiscountClientApi.java
@@ -17,7 +17,7 @@ import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
 import com.stanzaliving.core.discount.dto.request.BookingDiscountStrategyDto;
 import com.stanzaliving.core.discount.dto.response.DiscountCodeListDto;
-import com.stanzaliving.core.discount.dto.response.DiscountStrategyResponseDto;
+import com.stanzaliving.core.discount.dto.response.DiscountStrategyListingResponseDto;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -61,19 +61,19 @@ public class DiscountClientApi {
 		return null;
 	}
 
-	public ResponseDto<DiscountStrategyResponseDto> getDiscountStrategyList(BookingDiscountStrategyDto requestDto) {
+	public ResponseDto<List<DiscountStrategyListingResponseDto>> getDiscountStrategyList(BookingDiscountStrategyDto requestDto) {
 
 		try {
 			Object postBody = requestDto;
 			log.info("Received request to get discount strategies {}", requestDto);
 			final Map<String, Object> uriVariables = new HashMap<>();
-			String path = UriComponentsBuilder.fromPath("/api/v1/discount/discount-strategy/get")
+			String path = UriComponentsBuilder.fromPath("/api/v1/discount-strategy/get")
 					.buildAndExpand(uriVariables).toUriString();
 			final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
 			HttpHeaders headerParams = new HttpHeaders();
 			final String[] accepts = { "*/*" };
 			final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
-			ParameterizedTypeReference<ResponseDto<DiscountStrategyResponseDto>> returnType = new ParameterizedTypeReference<ResponseDto<DiscountStrategyResponseDto>>() {
+			ParameterizedTypeReference<ResponseDto<List<DiscountStrategyListingResponseDto>>> returnType = new ParameterizedTypeReference<ResponseDto<List<DiscountStrategyListingResponseDto>>>() {
 			};
 
 			return restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
