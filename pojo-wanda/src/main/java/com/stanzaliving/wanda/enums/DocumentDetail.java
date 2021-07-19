130a9e8e9dcd4df9d7b05a5c4f6c9207e4da3629
Shanthi-rajendran-22 <shanthi.r@nineleaps.com>
2021-07-19 17:10:01 +0530

    Document order | KYCListing

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/enums/DocumentDetail.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/enums/DocumentDetail.java
@@ -10,20 +10,55 @@ import java.util.Map;
 @AllArgsConstructor
 public enum DocumentDetail {//todo: update
 
-    PASSPORT("Passport","", "Address Proof","https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/description.svg"),
-    PAN("PAN Card", "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/contacts.svg","Id Proof","https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/badge.svg"),
-    REFERRAL_PAN("Referral PAN Card","https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/contacts.svg", "Id Proof","https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/badge.svg"),
-    ADHAAR_CARD("Aadhaar Card","fingerprint","Address Proof","https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/description.svg"),
-    DRIVING_LICENSE("Driving License","","Address Proof","https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/description.svg"),
-    OTHERS("Others", "","Others",""),
-    POLICE_VERIFICATION("Police Verification","https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/local-police.svg","Police Verification","https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/local-police.svg"),
-    VISA("VISA","","Address Proof",""),
-    SELFIE("Resident Image", "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/add-photo-alternate.svg","Resident Image","https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/add-photo-alternate.svg");
+    PASSPORT("Passport","", "Address Proof",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/description.svg",
+            3),
+    PAN("PAN Card",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/contacts.svg",
+            "Id Proof",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/badge.svg",
+            2),
+    REFERRAL_PAN("Referral PAN Card",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/contacts.svg",
+            "Id Proof",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/badge.svg",
+            2),
+    ADHAAR_CARD("Aadhaar Card",
+            "fingerprint",
+            "Address Proof",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/description.svg",
+            3),
+    DRIVING_LICENSE("Driving License",
+            "",
+            "Address Proof",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/description.svg",
+            3),
+    OTHERS("Others",
+            "",
+            "Others",
+            "",
+            4),
+    POLICE_VERIFICATION("Police Verification",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/local-police.svg",
+            "Police Verification",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/local-police.svg",
+            5),
+    VISA("VISA",
+            "",
+            "Address Proof",
+            "",
+            3),
+    SELFIE("Resident Image",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/add-photo-alternate.svg",
+            "Resident Image",
+            "https://res.cloudinary.com/stanza-living/image/upload/v1625048060/sigma-app/add-photo-alternate.svg",
+            1);
 
     private final String displayName;
     private final String documentIcon;
     private final String category;
     private final String categoryIcon;
+    private final Integer order;
 
     private static Map<String, DocumentDetail> documentDetailMap = new HashMap<>();
 
