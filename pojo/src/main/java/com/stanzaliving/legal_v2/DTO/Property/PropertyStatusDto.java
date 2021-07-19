71c995251389e099801cbbed629259fdc42fa57e
Bhupinder <bhupinderpal.kaur@nineleaps.com>
2021-07-19 12:37:54 +0530

    PropertyStatusDto added to fetch status from camunda

---
+++ pojo/src/main/java/com/stanzaliving/legal_v2/DTO/Property/PropertyStatusDto.java
@@ -0,0 +1,21 @@
+package com.stanzaliving.legal_v2.DTO.Property;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PropertyStatusDto {
+
+    private String propertyId;
+    private String propertyStatus;
+
+}
