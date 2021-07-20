ceb2555539f60c6ed057c7b72d006e47f1abeef3
AdityaTomar7 <aditya.tomar@nineleaps.com>
2021-07-20 15:30:13 +0530

    Adding changes for resident apis

--- pojo/src/main/java/com/stanzaliving/core/security/enums/StatusCard.java
+++ pojo/src/main/java/com/stanzaliving/core/security/enums/StatusCard.java
@@ -1,15 +1,24 @@
 package com.stanzaliving.core.security.enums;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Getter
 public enum StatusCard {
-    PRESENT,
-    PENDING,
-    NOT_PRESENT,
-    NODAL_ACTION_TAKEN,
-    PENDING_ACTION;
+    PRESENT("PRESENT"),
+    PENDING("PENDING"),
+    NOT_PRESENT("NOT PRESENT"),
+    NODAL_ACTION_TAKEN("NODAL ACTION TAKEN"),
+    PENDING_ACTION("PENDING ACTION");
+
+    private final String label;
+
+    private StatusCard(String label){
+        this.label = label;
+    }
 
     public static List<StatusCard> getPresentPendingStatusList() {
         return new ArrayList<>(Arrays.asList(StatusCard.PENDING, StatusCard.PRESENT));
