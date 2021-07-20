94e8e33652ea324987f256d668f1ccc5bd83ca68
Rahul.Tiwary <rahul.kumar1@stanzaliving.com>
2021-07-20 17:36:06 +0530

    code chnages

---
+++ pojo/src/main/java/com/stanzaliving/booking/dto/response/InventoryResponseOccupancyDto.java
@@ -0,0 +1,35 @@
+package com.stanzaliving.booking.dto.response;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class InventoryResponseOccupancyDto {
+
+    private String bookingUuid;
+
+    private String referenceType;
+
+    private String residenceUuid;
+
+    private String roomUuid;
+
+    private String roomNumber;
+
+    private String inventoryUuid;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private Integer beds;
+
+    private Integer occupancy;
+
+    private boolean selfConverted;
+}
