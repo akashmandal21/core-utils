e87c214a47438dcff224ca21bec1d91e7767cc2b
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-07-22 11:15:52 +0530

    Update GrammageClientApi.java

--- grammage-client/src/main/java/com/stanzaliving/core/grammage/client/api/GrammageClientApi.java
+++ grammage-client/src/main/java/com/stanzaliving/core/grammage/client/api/GrammageClientApi.java
@@ -1,11 +1,6 @@
 package com.stanzaliving.core.grammage.client.api;
 
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
@@ -17,7 +12,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
-import com.stanzaliving.food.v2.grammage.response.MenuOptionGrammage;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -63,53 +57,6 @@ public class GrammageClientApi {
 		}
 
 	}
-	
-	public Boolean isGrammageDefined(String versionId) {
-        String path = UriComponentsBuilder.fromPath("/internal/category/grammage/exists").build().toUriString();
-        final HttpHeaders headerParams = new HttpHeaders();
-        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
-        queryParams.add("versionId", versionId);
-        final String[] accepts = {
-                "*/*"
-        };
-        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
-        ParameterizedTypeReference<ResponseDto<Boolean>> returnType = new ParameterizedTypeReference<ResponseDto<Boolean>>() {
-        };
-
-        try {
-            ResponseDto<Boolean> responseDto = restClient.invokeAPI(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType);
-            return Objects.nonNull(responseDto) && responseDto.isStatus() && Objects.nonNull(responseDto.getData()) && responseDto.getData();
-        }catch (Exception e) {
-            log.error("Error while checking isGrammageDefined", e);
-            return null;
-        }
-    }
-	
-	
-	public Map<String, Map<LocalDate, Map<String, MenuOptionGrammage>>> calculateGrammage(String versionId, Map<String, Map<LocalDate, Map<String, Collection<String>>>> optionWiseItemMap) {
-		ResponseDto<Map<String, Map<LocalDate, Map<String, MenuOptionGrammage>>>> responseDto = null;
-		String path = UriComponentsBuilder.fromPath("/internal/category/grammage/calculateGrammage").build().toUriString();
-        final HttpHeaders headerParams = new HttpHeaders();
-        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
-        queryParams.add("versionId", versionId);
-        queryParams.add("optionWiseItemMap", optionWiseItemMap.toString());
-        final String[] accepts = {
-                "*/*"
-        };
-        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
-        ParameterizedTypeReference<ResponseDto<Map<String, Map<LocalDate, Map<String, MenuOptionGrammage>>>>> returnType = new ParameterizedTypeReference<ResponseDto<Map<String, Map<LocalDate, Map<String, MenuOptionGrammage>>>>>() {
-        };
-
-        try {
-             responseDto = restClient.invokeAPI(path, HttpMethod.POST, queryParams, null, headerParams, accept, returnType);            
-        }catch (Exception e) {
-            log.error("Error while calculateGrammage ", e);
-            return null;
-        }
-        return (Objects.nonNull(responseDto) && Objects.nonNull(responseDto.getData())) ? responseDto.getData()
-				: new HashMap<>();
-    }
-	
 
 }
 
