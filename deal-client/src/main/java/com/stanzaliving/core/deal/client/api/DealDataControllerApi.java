6b45156d9e0a85991385e2c4647a499fb76453b9
Abhishek-sopho <abhishek.kumar488@gmail.com>
2021-07-27 14:30:31 +0530

    packaged service working code

--- deal-client/src/main/java/com/stanzaliving/core/deal/client/api/DealDataControllerApi.java
+++ deal-client/src/main/java/com/stanzaliving/core/deal/client/api/DealDataControllerApi.java
@@ -1,11 +1,9 @@
 package com.stanzaliving.core.deal.client.api;
 
-import com.stanzaliving.booking.dto.BookingResponseDto;
-import com.stanzaliving.core.base.common.dto.ResponseDto;
-import com.stanzaliving.core.base.http.StanzaRestClient;
-import com.stanzaliving.core.deal.client.dto.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -14,9 +12,13 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.stanzaliving.core.base.common.dto.ResponseDto;
+import com.stanzaliving.core.base.http.StanzaRestClient;
+import com.stanzaliving.core.deal.client.dto.DealRoomsRequestDto;
+import com.stanzaliving.core.deal.client.dto.DealUserRequestDto;
+import com.stanzaliving.core.deal.client.dto.DealUserResponseDto;
+
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class DealDataControllerApi {
@@ -51,7 +53,7 @@ public class DealDataControllerApi {
         return restClient.invokeAPI(path, HttpMethod.PUT, queryParams, putBody, headerParams, accept, returnType);
     }
 
-	public List<String> getServiceMixUuidForDealAndResidence(String dealUuid, String residenceUuid) {
+	public ResponseDto<List<String>> getServiceMixUuidForDealAndResidence(String dealUuid, String residenceUuid) {
 
         Object postBody = null;
 
@@ -73,6 +75,30 @@ public class DealDataControllerApi {
         };
         return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
     
+	}
+
+	public ResponseDto<List<Integer>> getAllowedOccupanciesForDealAndResidence(DealRoomsRequestDto dealRoomsRequestDto) {
+
+			log.info("fetching rooms for ContactUuid : " + dealRoomsRequestDto.getDealUuid());
+
+			Object postBody = null;
+
+	        final Map<String, Object> uriVariables = new HashMap<>();
+	        
+	        String path = UriComponentsBuilder.fromPath("/internal/deal/allowed-occupancies/"+ dealRoomsRequestDto.getDealUuid() + "/" + dealRoomsRequestDto.getResidenceUuid()).buildAndExpand(uriVariables).toUriString();
+
+	        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+	        final HttpHeaders headerParams = new HttpHeaders();
+
+	        final String[] accepts = {
+	                "*/*"
+	        };
+	        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+	        ParameterizedTypeReference<ResponseDto<List<Integer>>> returnType = new ParameterizedTypeReference<ResponseDto<List<Integer>>>() {
+	        };
+	        return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
 	}
 
 }
