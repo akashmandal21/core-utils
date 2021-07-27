bff70af52de90682aa160fff8a64a3aac3fd0ccf
ShubhankarNineleaps <shubhankar.kotnala@nineleaps.com>
2021-07-27 13:05:19 +0530

    Shubhankar | Added meta fileds to TagDto

--- pojo/src/main/java/com/stanzaliving/support/dto/TagDto.java
+++ pojo/src/main/java/com/stanzaliving/support/dto/TagDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -13,4 +15,9 @@ public class TagDto {
     private String tag;
     private String uuid;
     private String ticketUuid;
+    private boolean active;
+    private String createdBy;
+    private String lastUpdatedBy;
+    private Date createdAt;
+    private Date lastUpdatedAt;
 }
