5dbdea9d0a468dec5003663bb6a352eaeb1ddd42
ketan.yadav <ketan.yadav@stanzaliving.com>
2021-07-28 13:10:03 +0530

    field added

--- pojo-food/src/main/java/com/stanzaliving/food/v2/category/dto/MealOptionDto.java
+++ pojo-food/src/main/java/com/stanzaliving/food/v2/category/dto/MealOptionDto.java
@@ -2,6 +2,7 @@ package com.stanzaliving.food.v2.category.dto;
 
 import com.stanzaliving.core.food.dto.MenuItemDto;
 import com.stanzaliving.core.food.enums.Action;
+import com.stanzaliving.core.food.enums.FoodServeType;
 import com.stanzaliving.food.v2.common.dto.OrderedListingDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,4 +26,5 @@ public class MealOptionDto extends OrderedListingDto {
 	private Double grammage;
 	private Double expectedGrammage;
 	private Boolean mealOfDay;
+	private FoodServeType foodServeType;
 }
