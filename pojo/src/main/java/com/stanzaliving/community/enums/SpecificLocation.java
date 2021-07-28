f46a6c91e72ab7fdb2792b7336b60d963caac254
nihalasati <nihal.asati@nineleaps.com>
2021-07-28 13:45:59 +0530

    Rename SpecificLocation Enum

--- pojo/src/main/java/com/stanzaliving/community/enums/SpecificLocation.java
+++ pojo/src/main/java/com/stanzaliving/community/enums/SpecificLocation.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum Specific_Location {
+public enum SpecificLocation {
     CITY("city"),
     MICRO_MARKET("micromarket"),
     RESIDENCE("residence"),
