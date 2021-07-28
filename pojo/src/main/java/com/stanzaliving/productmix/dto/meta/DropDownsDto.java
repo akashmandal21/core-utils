b12f5cf5a550a8f217c37c3199a8668dbc0f5ba0
vis-me07 <vishal.mehta@nineleaps.com>
2021-07-28 20:16:15 +0530

    Vishal | Added localdate to epoch and meta dto

---
+++ pojo/src/main/java/com/stanzaliving/productmix/dto/meta/DropDownsDto.java
@@ -0,0 +1,23 @@
+package com.stanzaliving.productmix.dto.meta;
+
+import com.stanzaliving.core.enums.ResidenceBrand;
+import com.stanzaliving.core.leaddashboard.enums.PropertyTypeEnum;
+import com.stanzaliving.productmix.dto.LabelValueDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DropDownsDto {
+    private List<LabelValueDto<PropertyTypeEnum>> propertyType;
+
+    private List<LabelValueDto<ResidenceBrand>> propertyBrand;
+
+    private List<LabelValueDto<String>> roomFeatures;
+}
