ad4c0867ebebae7e58f67ae10651fab439b33e5f
vis-me07 <vishal.mehta@nineleaps.com>
2021-07-28 19:27:13 +0530

    Vishal | Changed applicability duration from LocalDate to epoch in dto

--- pojo/src/main/java/com/stanzaliving/productmix/dto/ApplicableDurationDto.java
+++ pojo/src/main/java/com/stanzaliving/productmix/dto/ApplicableDurationDto.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -15,8 +14,8 @@ import java.time.LocalDate;
 public class ApplicableDurationDto {
 
     @NotNull(message = "Applicable from date cannot be null")
-    private LocalDate fromDate;
+    private Long fromDate;
 
     @NotNull(message = "Applicable to date cannot be null")
-    private LocalDate toDate;
+    private Long toDate;
 }
