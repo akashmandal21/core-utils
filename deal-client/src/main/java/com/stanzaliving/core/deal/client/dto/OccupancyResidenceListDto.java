dc8deaf31af001752872a15b1cd920f4d4a06aff
nishant.nineleaps <nishant.venugopal@nineleaps.com>
2021-07-29 15:35:47 +0530

    Changes return type occupancies

--- deal-client/src/main/java/com/stanzaliving/core/deal/client/dto/OccupancyResidenceListDto.java
+++ deal-client/src/main/java/com/stanzaliving/core/deal/client/dto/OccupancyResidenceListDto.java
@@ -13,7 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 public class OccupancyResidenceListDto {
 
-    List<String> occupancies;
+    List<Integer> occupancies;
 
     List<String> residences;
 }
