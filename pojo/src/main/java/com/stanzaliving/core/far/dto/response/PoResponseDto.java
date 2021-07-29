814726dbf506026717d9e92c4068af314a625b1b
shobhit <mittalshobhit7@gmail.com>
2021-07-29 14:08:29 +0530

    dto

---
+++ pojo/src/main/java/com/stanzaliving/core/far/dto/response/PoResponseDto.java
@@ -0,0 +1,21 @@
+package com.stanzaliving.core.far.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PoResponseDto {
+    private String label;
+    private String value;
+    private boolean isPo;
+}
