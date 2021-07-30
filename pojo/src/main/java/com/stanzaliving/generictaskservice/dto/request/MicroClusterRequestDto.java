a10f33b4de55c5b02769016431b80d48e0e29767
rajj5 <varun.r@robosoftin.com>
2021-07-30 17:49:38 +0530

    added Request and Response Dtos

---
+++ pojo/src/main/java/com/stanzaliving/generictaskservice/dto/request/MicroClusterRequestDto.java
@@ -0,0 +1,23 @@
+package com.stanzaliving.generictaskservice.dto.request;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MicroClusterRequestDto {
+
+    @NotBlank(message = "micro Cluster Name Cannot be blank")
+    private String microClusterName;
+
+    @NotNull(message = "residenceIds Cannot be blank")
+    private List<String> residenceIds;
+}
