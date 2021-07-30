7d22384a526038390c85ec3a5873013366a05a15
VikasST <vikas.st@robosoftin.com>
2021-07-30 17:30:08 +0530

    Added required shift dtos inside pojo

---
+++ pojo/src/main/java/com/stanzaliving/generictaskservice/dto/ShiftDto.java
@@ -0,0 +1,41 @@
+package com.stanzaliving.generictaskservice.dto;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * @author Vikas S T
+ * @date 27-Jul-21
+ **/
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShiftDto {
+
+    private String shiftUuid;
+
+    @NotBlank(message = "Module mandatory")
+    private String module;
+
+    @NotBlank(message = "Shift name mandatory")
+    private String shiftName;
+
+    @NotNull(message = "Shift category is mandatory")
+    private String shiftCategory;
+
+    @NotNull(message = "Duration is mandatory")
+    private Integer duration;
+
+    private List<String> tagsUuidList;
+
+    private List<TagResponseDto> tags;
+
+}
