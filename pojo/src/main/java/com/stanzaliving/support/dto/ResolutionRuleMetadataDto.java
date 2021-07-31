0ff35157d016bebc9d164549bb358f30469538a8
ShubhankarNineleaps <shubhankar.kotnala@nineleaps.com>
2021-07-31 07:47:53 +0530

    Shubhankar | Added & Refactored DTOs for Resolution flow

---
+++ pojo/src/main/java/com/stanzaliving/support/dto/ResolutionRuleMetadataDto.java
@@ -0,0 +1,29 @@
+package com.stanzaliving.support.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResolutionRuleMetadataDto {
+    private String complaintGroup;
+    private String complaintGroupUuid;
+    private String category;
+    private String categoryUuid;
+    private String subCategory;
+    private String subCategoryUuid;
+    private String createdBy;
+    private String lastUpdatedBy;
+    private Date createdAt;
+    private Date lastUpdatedAt;
+    private List<UserLevelRoleDto> resolutionLevelList;
+    private List<SubTicketTaskDto> subTicketTaskDtoList;
+    private boolean isResolutionRuleActive;
+}
