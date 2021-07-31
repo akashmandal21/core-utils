d7e4918a0f156d934e367020436ee157b0a47496
RamaKrishnaRaju <krishna.raju@nineleaps.com>
2021-07-31 21:11:30 +0530

    QR Code DTO's added

---
+++ pojo/src/main/java/com/stanzaliving/legal_v2/DTO/Document/ImageDetails.java
@@ -0,0 +1,17 @@
+package com.stanzaliving.legal_v2.DTO.Document;
+
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageDetails {
+
+    private int index;
+
+    private MultipartFile image;
+}
