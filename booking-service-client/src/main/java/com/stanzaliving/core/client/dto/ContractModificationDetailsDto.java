8d83d2383668fc8fb8e7a5cf5f9c58c3a58ed36b
nabila.shah <nabila.shah@stanzaliving.com>
2021-08-02 17:35:45 +0530

    venta-notification-client

---
+++ booking-service-client/src/main/java/com/stanzaliving/core/client/dto/ContractModificationDetailsDto.java
@@ -0,0 +1,27 @@
+package com.stanzaliving.core.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContractModificationDetailsDto {
+
+	private String studentId;
+	private String studentName;
+	private String contractStartDate;
+	private String contractEndDate;
+	private double monthlyFee;
+	private double amc;
+	private double securityDeposit;
+	private double advanceRentalMonths;
+	private double totalAmount;
+	private String addendumLink;
+	private String subContract1EndDate;
+	private String approvalUrl;
+	
+}
