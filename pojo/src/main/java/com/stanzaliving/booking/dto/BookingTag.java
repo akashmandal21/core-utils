3bd4fdfab0340f1aff8cace1b87012df771a12f7
Rahul.Tiwary <rahul.kumar1@stanzaliving.com>
2021-08-04 14:27:43 +0530

    Code

--- pojo/src/main/java/com/stanzaliving/booking/dto/BookingTag.java
+++ pojo/src/main/java/com/stanzaliving/booking/dto/BookingTag.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class BookingTag {
+public class BookingTag implements Serializable {
 
     private String name;
 
