05a5ff0bf959343102229984a4d94b0bfa67fafe
Mohit Gupta <mohitgupta.retainer@stanzaliving.com>
2021-08-04 18:29:48 +0530

    lock in issue

--- pojo/src/main/java/com/stanzaliving/core/residenceservice/dto/MoveInDateDto.java
+++ pojo/src/main/java/com/stanzaliving/core/residenceservice/dto/MoveInDateDto.java
@@ -1,11 +1,16 @@
 package com.stanzaliving.core.residenceservice.dto;
 
-import com.stanzaliving.core.residenceservice.enums.ResidentType;
-import lombok.*;
-
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.validation.constraints.NotBlank;
+
+import com.stanzaliving.booking.enums.BookingType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -13,8 +18,8 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MoveInDateDto {
-    @Enumerated(EnumType.STRING)
-    private ResidentType residentType;
-    @NotBlank(message = "Move-in date cannot be empty")
-    private String moveInDate;
+	@Enumerated(EnumType.STRING)
+	private BookingType residentType;
+	@NotBlank(message = "Move-in date cannot be empty")
+	private String moveInDate;
 }
