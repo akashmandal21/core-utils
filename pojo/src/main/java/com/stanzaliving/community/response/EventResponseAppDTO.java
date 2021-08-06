7a75c96f1b1f316baa0c5146aae061deddf94a86
nihalasati <nihal.asati@nineleaps.com>
2021-08-06 19:22:50 +0530

    Rename stickyNotes Field

--- pojo/src/main/java/com/stanzaliving/community/response/EventResponseAppDTO.java
+++ pojo/src/main/java/com/stanzaliving/community/response/EventResponseAppDTO.java
@@ -38,7 +38,7 @@ public class EventResponseAppDTO {
     private List<EventImagesResponseDTO> images;
     private UserListResponseDTO userList;
     private EventFaqResponseDTO faq;
-    private Map stickyNotesAndTags;
+    private Object stickyNotesAndTags;
     private Object isRegistrationOpen;
     private PageResponse<SimilarEventResponseDTO> similarEvents;
     private PageResponse<RegisteredUserResponseDTO> registeredUser;
