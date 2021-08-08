3932ae1f6584741b48940b079d753a06c6c46d68
RajKumarStanza <raj.singh@stanzaliving.com>
2021-08-08 22:26:44 +0530

    Added blended price dto

---
+++ pojo/src/main/java/com/stanzaliving/residenceservice/Dto/OccupancyBlendedPriceDto.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.stanzaliving.residenceservice.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author raj.kumar
+ *
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OccupancyBlendedPriceDto {
+
+	private Integer occupancy;
+	private double blendedPrice;
+}
