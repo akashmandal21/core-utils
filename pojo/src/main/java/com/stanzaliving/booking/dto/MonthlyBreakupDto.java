d1658ba9d2a0566fb5e71a7f5f327c8f0c4a7fc5
nabila.shah <nabila.shah@stanzaliving.com>
2021-08-12 19:02:29 +0530

    student mail

--- pojo/src/main/java/com/stanzaliving/booking/dto/MonthlyBreakupDto.java
+++ pojo/src/main/java/com/stanzaliving/booking/dto/MonthlyBreakupDto.java
@@ -19,8 +19,6 @@ public class MonthlyBreakupDto {
 
     private Double subContract2MonthlyRent;
     
-	private Double monthlyFee;
-
     private Double licenseFee;
 
     private Double cityHeadBuffer;
