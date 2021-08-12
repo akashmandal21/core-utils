ee3145346e3f58d8a72ab90ff39bd2a51c66c4a6
nabila.shah <nabila.shah@stanzaliving.com>
2021-08-12 20:43:47 +0530

    student mail

--- booking-service-client/src/main/java/com/stanzaliving/core/client/api/BookingDataControllerApi.java
+++ booking-service-client/src/main/java/com/stanzaliving/core/client/api/BookingDataControllerApi.java
@@ -300,15 +300,14 @@ public class BookingDataControllerApi {
         Object postBody = null;
 
         // create path and map variables
-        final Map<String, Object> uriVariables = new HashMap<>();
-
-        String path = UriComponentsBuilder.fromPath("/modify-contract/v1/modify/contract/details/{bookingUuid}").buildAndExpand(uriVariables).toUriString();
-
         final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
-
         queryParams.add("bookingUuid", bookingUuid);
 
         final HttpHeaders headerParams = new HttpHeaders();
+        // create path and map variables
+        final Map<String, Object> uriVariables = new HashMap<>();
+
+        String path = UriComponentsBuilder.fromPath("/modify-contract/v1/modify/contract/details/{bookingUuid}").buildAndExpand(uriVariables).toUriString();
 
         final String[] accepts = {
                 "*/*"
