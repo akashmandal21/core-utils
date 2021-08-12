93d70423097907dfc81c3ce43fb2505f1e5a024a
vishal1404 <vishal.jha@nineleaps.com>
2021-08-12 16:12:00 +0530

    Added ModulesDto

--- pojo/src/main/java/com/stanzaliving/estate_v2/enumeration/CamundaFlow.java
+++ pojo/src/main/java/com/stanzaliving/estate_v2/enumeration/CamundaFlow.java
@@ -1,5 +1,6 @@
 package com.stanzaliving.estate_v2.enumeration;
 
+import com.stanzaliving.estate_v2.dto.ModulesDto;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -20,12 +21,22 @@ public enum CamundaFlow {
         this.id = id;
     }
 
-    public static List<String> getAlphaModules() {
-       return new ArrayList<>(Arrays.asList("Initial Bed Test","Sales Review","Legal Deviations","Property Naming"));
+    public static List<ModulesDto> getAlphaModules() {
+        List<ModulesDto> modulesList=new ArrayList<>();
+        modulesList.add(new ModulesDto(Module.INITIAL_BED_TEST.getName(),Module.INITIAL_BED_TEST.getUuid()));
+        modulesList.add(new ModulesDto(Module.SALES_REVIEW.getName(),Module.SALES_REVIEW.getUuid()));
+        modulesList.add(new ModulesDto(Module.LEGAL_DEVIATION.getName(),Module.LEGAL_DEVIATION.getUuid()));
+        modulesList.add(new ModulesDto(Module.PROPERTY_NAMING.getName(),Module.PROPERTY_NAMING.getUuid()));
+        return modulesList;
+
     }
 
-    public static List<String> getBetaModules() {
-        return new ArrayList<>(Arrays.asList("Sales Review","Legal Deviations","Property Naming"));
+    public static List<ModulesDto> getBetaModules() {
+        List<ModulesDto> modulesList=new ArrayList<>();
+        modulesList.add(new ModulesDto(Module.SALES_REVIEW.getName(),Module.SALES_REVIEW.getUuid()));
+        modulesList.add(new ModulesDto(Module.LEGAL_DEVIATION.getName(),Module.LEGAL_DEVIATION.getUuid()));
+        modulesList.add(new ModulesDto(Module.PROPERTY_NAMING.getName(),Module.PROPERTY_NAMING.getUuid()));
+        return modulesList;
     }
 
     public static List<CamundaFlow> getCamundaFlow() {
