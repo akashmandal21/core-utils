8fcd1dc050e7c22638e552152408833aef5a5608
VishalNineleaps <vishalkumar.saini@nineleaps.com>
2021-08-16 15:25:57 +0530

    AuditFieldsCount API updated

--- pojo/src/main/java/com/stanzaliving/ventaAudit/dto/AuditFieldsCountResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/ventaAudit/dto/AuditFieldsCountResponseDto.java
@@ -1,5 +1,6 @@
 package com.stanzaliving.ventaAudit.dto;
 
+import com.stanzaliving.ventaAudit.enums.AuditStatus;
 import com.stanzaliving.ventaAudit.enums.DueDates;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,13 +13,9 @@ import java.util.Map;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AuditDetailsCountResponseDto {
-//  private DueDates dueDates;
-//  private Long count;
+public class AuditFieldsCountResponseDto {
   Map<DueDates,Long> dueDateCount;
   Map<String,Long> auditorCount;
-  Map<String,Long> auditStatusCount;
+  Map<AuditStatus,Long> auditStatusCount;
 
-  public void setAuditStatusCount(DueDates dueToday, Long count) {
-  }
 }
