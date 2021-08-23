85d063126b4f0155a28d778a760d355150170ed4
vishal1404 <vishal.jha@nineleaps.com>
2021-08-23 23:09:50 +0530

    Updated PropertyAction.

--- pojo/src/main/java/com/stanzaliving/estate_v2/enumeration/PropertyAction.java
+++ pojo/src/main/java/com/stanzaliving/estate_v2/enumeration/PropertyAction.java
@@ -11,27 +11,29 @@ import java.util.List;
 public enum PropertyAction {
 
     //Pg Flow
-    L1_NATIONAL_HEAD("L1 National Head", "f7aad022-0428-11ec-9a03-0242ac130003"),
-    L1_TO_LEADERSHIP("L1 Leadership", "f7aad2b6-0428-11ec-9a03-0242ac130003"),
-    L2_NATIONAL_HEAD("L2 National Head", "f7aad3a6-0428-11ec-9a03-0242ac130003"),
-    L2_TRANSFORMATION("L2 Transformation", "f7aad46e-0428-11ec-9a03-0242ac130003"),
-    L2_PROPERTY_NAMING("Property Naming", "f7aad68a-0428-11ec-9a03-0242ac130003"),
+    L1_NATIONAL_HEAD("L1 National Head", "f7aad022-0428-11ec-9a03-0242ac130003", "L1_NATIONAL_HEAD"),
+    L1_TO_LEADERSHIP("L1 Leadership", "f7aad2b6-0428-11ec-9a03-0242ac130003", "L1_TO_LEADERSHIP"),
+    L2_NATIONAL_HEAD("L2 National Head", "f7aad3a6-0428-11ec-9a03-0242ac130003", "L2_NATIONAL_HEAD"),
+    L2_TRANSFORMATION("L2 Transformation", "f7aad46e-0428-11ec-9a03-0242ac130003", "L2_TRANSFORMATION"),
+    L2_PROPERTY_NAMING("Property Naming", "f7aad68a-0428-11ec-9a03-0242ac130003", "L2_PROPERTY_NAMING"),
 
     //Apartment Flow
-    L1_ZH("L1 Zonal Head", "f7aad752-0428-11ec-9a03-0242ac130003"),
-    L1_SL("L1 SL", "f7aad810-0428-11ec-9a03-0242ac130003"),
-    L2_SL("L2 SL", "f7aad8ce-0428-11ec-9a03-0242ac130003"),
-    SENT_TO_LEGAL("Legal", "f7aad98c-0428-11ec-9a03-0242ac130003"),
-    ESTATE_LEADERSHIP("Leadership", "f7aadd88-0428-11ec-9a03-0242ac130003"),
-    LEGAL_DUE_DILIGENCE("Legal Due Diligence", "f7aade6e-0428-11ec-9a03-0242ac130003"),
-    SENT_TO_PROPERTY_NAMING("Property Naming", "f7aadf22-0428-11ec-9a03-0242ac130003");
+    L1_ZH("L1 Zonal Head", "f7aad752-0428-11ec-9a03-0242ac130003", "L1_ZH"),
+    L1_SL("L1 SL", "f7aad810-0428-11ec-9a03-0242ac130003", "L1_SL"),
+    L2_SL("L2 SL", "f7aad8ce-0428-11ec-9a03-0242ac130003", "L2_SL"),
+    SENT_TO_LEGAL("Legal", "f7aad98c-0428-11ec-9a03-0242ac130003", "SENT_TO_LEGAL"),
+    ESTATE_LEADERSHIP("Leadership", "f7aadd88-0428-11ec-9a03-0242ac130003", "ESTATE_LEADERSHIP"),
+    LEGAL_DUE_DILIGENCE("Legal Due Diligence", "f7aade6e-0428-11ec-9a03-0242ac130003", "LEGAL_DUE_DILIGENCE"),
+    SENT_TO_PROPERTY_NAMING("Property Naming", "f7aadf22-0428-11ec-9a03-0242ac130003", "SENT_TO_PROPERTY_NAMING");
 
     private final String name;
     private final String uuid;
+    private final String alias;
 
-    PropertyAction(String name, String uuid) {
+    PropertyAction(String name, String uuid, String alias) {
         this.name = name;
         this.uuid = uuid;
+        this.alias = alias;
     }
 
 
