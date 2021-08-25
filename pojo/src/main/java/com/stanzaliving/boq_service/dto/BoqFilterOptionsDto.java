28de2bf75b62b2c96db62bedb97d52b3b14e785f
akshitgupta915 <akshit.gupta@nineleaps.com>
2021-08-25 14:43:53 +0530

    Drop downs added in Bulk Add modal

--- pojo/src/main/java/com/stanzaliving/boq_service/dto/BoqFilterOptionsDto.java
+++ pojo/src/main/java/com/stanzaliving/boq_service/dto/BoqFilterOptionsDto.java
@@ -1,6 +1,8 @@
 package com.stanzaliving.boq_service.dto;
 
+import com.stanzaliving.core.boq_service.enums.BoqItemProvider;
 import com.stanzaliving.core.enums.PropertyEntityType;
+import com.stanzaliving.transformations.enums.AreaOfUse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,4 +18,6 @@ public class BoqFilterOptionsDto {
     private List<LabelValueDto<Long>> cities;
     private List<LabelValueDto<String>> microMarkets;
     private List<LabelValueDto<PropertyEntityType>> propertyTypes;
+    private List<LabelValueDto<BoqItemProvider>> doneBy;
+    private List<LabelValueDto<AreaOfUse>> usedByArea;
 }
