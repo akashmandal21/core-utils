18eff2a2e3cfec739fbea132bfb1b90d4dc6415e
pradeep.naik <pradeep.naik@robosoftin.com>
2021-08-25 14:58:54 +0530

    Update: Renamed DTO by removing PP attachment as prefix.

--- pojo/src/main/java/com/stanzaliving/projectplanningservice/dto/request/PlanningTemplateFilterRequestDto.java
+++ pojo/src/main/java/com/stanzaliving/projectplanningservice/dto/request/PlanningTemplateFilterRequestDto.java
@@ -1,11 +1,15 @@
 package com.stanzaliving.projectplanningservice.dto.request;
 
-import com.stanzaliving.core.base.common.dto.DateTimeRangeDto;
 import com.stanzaliving.core.base.common.dto.PaginationRequest;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
 
 /**
  * @author Pradeep Naik R
@@ -15,13 +19,21 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
-public class PPPlanningTemplateFilterRequestDto {
+public class PlanningTemplateFilterRequestDto implements Serializable {
+
+    private static final long serialVersionUID = 1635296868115644218L;
 
     private String ppPlanningTemplateName;
 
     private String addedBy;
 
-    private DateTimeRangeDto addedBetween;
+//    private DateTimeRangeDto addedBetween;
+
+//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:MM:SS")
+    private Date startDate;
+
+//    @DateTimeFormat(pattern = "yyyy-MM-dd HH:MM:SS")
+    private Date endDate;
 
     private String status;
 
