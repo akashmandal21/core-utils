550466704516f43aa17da4bdf5a473205ea62005
aadi1997 <aditya.arora@nineleaps.com>
2021-08-31 17:14:24 +0530

    added dto for RC activity

---
+++ pojo/src/main/java/com/stanzaliving/core/dto/RCActivityDTO.java
@@ -0,0 +1,29 @@
+package com.stanzaliving.core.dto;
+
+import com.stanzaliving.core.enums.ApprovalStatus;
+import com.stanzaliving.core.enums.DocumentTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RCActivityDTO {
+    DocumentTypeEnum documentType;
+
+    String BookingUuid;
+
+    Boolean isVerified;
+
+    ApprovalStatus approvalStatus;
+
+    String updatedBy;
+
+    LocalDate updatedAt;
+
+    String rejectionReason;
+}
