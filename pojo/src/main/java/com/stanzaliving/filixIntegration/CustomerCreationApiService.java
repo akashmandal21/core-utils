246f027f81e7ed984c6f7c12927ac1b6cbf52735
Saloni.Aggarwal <saloni.aggarwal@C02ZL349LVCF.local>
2021-09-01 18:33:47 +0530

    null check removed

--- pojo/src/main/java/com/stanzaliving/filixIntegration/CustomerCreationApiService.java
+++ pojo/src/main/java/com/stanzaliving/filixIntegration/CustomerCreationApiService.java
@@ -63,7 +63,7 @@ public class CustomerCreationApiService extends CustomerApiFactory {
             objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             CustomerApiDto customerApiDto = objectMapper.readValue(dataMap.get("data").toString(), CustomerApiDto.class);
             logger.info("customerApiDto "+customerApiDto);
-            if(null != customerApiDto && null != customerApiDto.getFilixBookingResponseDto()) {
+            if(null != customerApiDto) {
                 FilixUserDetailResponseDto filixUserDetailResponseDto=customerApiDto.getFilixUserDetailResponseDto();
                 FilixResidenceDetailsDto filixResidenceDetailsDto=customerApiDto.getFilixResidenceDetailsDto();
                 FilixBookingDto booking = customerApiDto.getBooking();
