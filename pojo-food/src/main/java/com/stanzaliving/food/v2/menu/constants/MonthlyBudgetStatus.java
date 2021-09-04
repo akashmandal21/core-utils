dc204034d9d93e7e7b77b9b3f0e5aae674e92e00
rohit.gupta <rohit.gupta@stanzaliving.com>
2021-09-04 15:59:22 +0530

    Adding the enums required

---
+++ pojo-food/src/main/java/com/stanzaliving/food/v2/menu/constants/MonthlyBudgetStatus.java
@@ -0,0 +1,39 @@
+package com.stanzaliving.food.v2.menu.constants;
+
+import com.stanzaliving.core.user.enums.EnumListing;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public enum MonthlyBudgetStatus {
+
+    DRAFT("In Draft"),
+    SUBMITTED("Submitted"),
+    APPROVED("Approved");
+    private final String status;
+
+    public static boolean showOnApprovalDashboard(MonthlyBudgetStatus monthlyBudgetStatus) {
+        return SUBMITTED.equals(monthlyBudgetStatus) || APPROVED.equals(monthlyBudgetStatus);
+    }
+
+    public static boolean isEditable(MonthlyBudgetStatus monthlyBudgetStatus) {
+        return monthlyBudgetStatus == DRAFT || monthlyBudgetStatus == APPROVED ;
+    }
+
+    private static final List<EnumListing<MonthlyBudgetStatus>> enumListing = new ArrayList<>();
+
+    static {
+        for (MonthlyBudgetStatus curStatus : MonthlyBudgetStatus.values()) {
+            enumListing.add(EnumListing.of(curStatus, curStatus.getStatus()));
+        }
+    }
+
+    public static List<EnumListing<MonthlyBudgetStatus>> getEnumListing() {
+        return enumListing;
+    }
+    
+}
---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/enums/SpecialEventType.java
@@ -0,0 +1,4 @@
+package com.stanzaliving.core.food.enums;
+
+public class SpecialEventType {
+}
