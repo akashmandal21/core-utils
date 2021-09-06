a42b18bf8822846fb4ad296585f62825dee86fe1
Shiva Charan K <sivacharan@nineleaps.com>
2021-09-07 02:37:02 +0530

    makes key value dto serializable

--- pojo/src/main/java/com/stanzaliving/estate_v2/dto/KeyValueDto.java
+++ pojo/src/main/java/com/stanzaliving/estate_v2/dto/KeyValueDto.java
@@ -2,12 +2,14 @@ package com.stanzaliving.estate_v2.dto;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Builder
-public class KeyValueDto {
+public class KeyValueDto implements Serializable {
     private String label;
     private String value;
 }
