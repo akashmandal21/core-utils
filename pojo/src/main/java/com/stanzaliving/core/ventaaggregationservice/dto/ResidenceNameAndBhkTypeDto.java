af0b0197c9fe9551670deb587e2904003b64b5f1
Saima15 <saima.siddiqui@nineleaps.com>
2021-09-06 14:47:19 +0530

    Added implements serializable

--- pojo/src/main/java/com/stanzaliving/core/ventaaggregationservice/dto/ResidenceNameAndBhkTypeDto.java
+++ pojo/src/main/java/com/stanzaliving/core/ventaaggregationservice/dto/ResidenceNameAndBhkTypeDto.java
@@ -2,12 +2,14 @@ package com.stanzaliving.core.ventaaggregationservice.dto;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResidenceNameAndBhkTypeDto {
+public class ResidenceNameAndBhkTypeDto implements Serializable {
     private Integer residenceBhkType;
     private String residenceName;
     private String residenceUuid;
