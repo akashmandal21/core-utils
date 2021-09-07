329a93d0c79c7e9e7083b1973231301b4b94b1f9
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-09-07 13:23:54 +0530

    Update EventsDemographicsDetailsDto.java

--- pojo-food/src/main/java/com/stanzaliving/food/v2/monthlybudgetplanner/dto/EventsDemographicsDetailsDto.java
+++ pojo-food/src/main/java/com/stanzaliving/food/v2/monthlybudgetplanner/dto/EventsDemographicsDetailsDto.java
@@ -1,13 +1,16 @@
 package com.stanzaliving.food.v2.monthlybudgetplanner.dto;
 
+import java.io.Serializable;
+import java.util.List;
+
+import com.stanzaliving.wanda.DemographicsOverallResponseDto;
+import com.stanzaliving.wanda.DemographicsResponseDto;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
-
-import java.io.Serializable;
-import java.util.List;
 
 @Getter
 @Setter
