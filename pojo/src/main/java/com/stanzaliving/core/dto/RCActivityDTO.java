49445b417769a2cc194c123a7836466aa3c007f8
aadi1997 <aditya.arora@nineleaps.com>
2021-09-08 19:31:02 +0530

    corrected camel case for rcDto

--- pojo/src/main/java/com/stanzaliving/core/dto/RCActivityDTO.java
+++ pojo/src/main/java/com/stanzaliving/core/dto/RCActivityDTO.java
@@ -19,7 +19,7 @@ import java.util.Date;
 public class RCActivityDTO {
    private DocumentTypeEnum documentType;
 
-    private String BookingUuid;
+    private String bookingUuid;
 
     private Boolean isVerified;
 
