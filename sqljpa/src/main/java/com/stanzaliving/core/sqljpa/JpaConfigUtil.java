1cc1c5995db13129522999919e1a196e72101fe5
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-09-09 17:21:25 +0530

    temp

--- sqljpa/src/main/java/com/stanzaliving/core/sqljpa/JpaConfigUtil.java
+++ sqljpa/src/main/java/com/stanzaliving/core/sqljpa/JpaConfigUtil.java
@@ -75,12 +75,12 @@ public class JpaConfigUtil {
 		properties.put("hibernate.order_inserts", hibernateOrderInserts);
 		properties.put("hibernate.order_updates", hibernateOrderUpdates);//PhysicalNamingStrategyStandardImpl
 		//properties.put("org.hibernate.envers.audit_table_suffix", "_aud");
-		/*
-		 * properties.put("hibernate.implicit_naming_strategy",
-		 * "org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy");
-		 * properties.put("hibernate.physical_naming_strategy",
-		 * "org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy");
-		 */
+		
+		  properties.put("hibernate.implicit_naming_strategy",
+		  "org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy");
+		  properties.put("hibernate.physical_naming_strategy",
+		  "org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy");
+		 
 		return properties;
 	}
 
