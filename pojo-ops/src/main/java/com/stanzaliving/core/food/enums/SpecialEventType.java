53628fefd3495adf243cba8425ee1a07676a47d1
rohit.gupta <rohit.gupta@stanzaliving.com>
2021-09-10 13:37:30 +0530

    Event type as values and label

--- pojo-ops/src/main/java/com/stanzaliving/core/food/enums/SpecialEventType.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/enums/SpecialEventType.java
@@ -1,11 +1,29 @@
 package com.stanzaliving.core.food.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum SpecialEventType {
 
-    EXAMS,
-    FESTIVAL,
-    NATIONAL_HOLIDAY,
-    SPECIAL_EVENT,
-    OTHER
+    EXAMS("Exams"),
+    FESTIVAL("Festival"),
+    NATIONAL_HOLIDAY("National Holiday"),
+    SPECIAL_EVENT("Special Event"),
+    OTHER("Other");
+    
+    private String eventTypeName;
+    
+    public static Map<String,String> eventTypeMap = new HashMap<>();
+    
+    static {
+        for(SpecialEventType eventType : SpecialEventType.values()) {
+            eventTypeMap.put(eventType.toString(),eventType.eventTypeName);
+        }
+    }
+    
+    SpecialEventType(String eventTypeName) {
+        this.eventTypeName = eventTypeName;
+    }
+    
 
 }
