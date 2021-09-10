d855c5b7e0db97190f96ff7baa956fa755d2387e
Santhosh3579 <82016500+Santhosh3579@users.noreply.github.com>
2021-09-10 16:50:10 +0530

    after pr review

--- erpinvoice-client/src/main/java/com/stanzaliving/core/erpinvoice/client/ErpInvoiceClient.java
+++ erpinvoice-client/src/main/java/com/stanzaliving/core/erpinvoice/client/ErpInvoiceClient.java
@@ -64,10 +64,11 @@ public class ErpInvoiceClient {
         return restClient.invokeAPI(path, HttpMethod.POST, queryParams, invoiceNumbers, headerParams, accept, returnType);
     }
 
-    public ResponseDto<Map<String, Integer>> getMaxReject(String po_to_uuid) {
+    public ResponseDto<Map<String, Integer>> getMaxRejectionCountForInstallableItems(String po_to_uuid) {
 
         final Map<String, Object> uriVariables = new HashMap<>();
-        uriVariables.put("po_to_uuid",po_to_uuid);
+        uriVariables.put("po_uuid",po_to_uuid);
+        uriVariables.put("property","maxRejection");
         final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
 
         final HttpHeaders headerParams = new HttpHeaders();
@@ -76,7 +77,7 @@ public class ErpInvoiceClient {
 
         final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
 
-        String path = UriComponentsBuilder.fromPath("/internal/get/maxRejection/{po_to_uuid}").buildAndExpand(uriVariables).toUriString();
+        String path = UriComponentsBuilder.fromPath("/internal/po/{po_uuid}/invoice/items/{property}/get").buildAndExpand(uriVariables).toUriString();
 
         ParameterizedTypeReference<ResponseDto<Map<String, Integer>>> returnType = new ParameterizedTypeReference<ResponseDto<Map<String, Integer>>>() {};
 
