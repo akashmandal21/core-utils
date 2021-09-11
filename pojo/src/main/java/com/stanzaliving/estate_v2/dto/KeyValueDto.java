229c8ae121fc935bb3f52f8a3f2eb985ba079e80
Shiva Charan K <sivacharan@nineleaps.com>
2021-09-11 16:45:03 +0530

    implements clone method in KeyValueDto

--- pojo/src/main/java/com/stanzaliving/estate_v2/dto/KeyValueDto.java
+++ pojo/src/main/java/com/stanzaliving/estate_v2/dto/KeyValueDto.java
@@ -9,7 +9,16 @@ import java.io.Serializable;
 @Getter
 @Setter
 @Builder
-public class KeyValueDto implements Serializable {
+public class KeyValueDto implements Serializable, Cloneable {
     private String label;
     private String value;
+
+    @Override
+    public KeyValueDto clone() {
+        try {
+            return (KeyValueDto) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
 }
