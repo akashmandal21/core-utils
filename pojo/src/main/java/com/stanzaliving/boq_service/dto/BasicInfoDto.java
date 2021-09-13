57d833f398bca362f0591d7debd73d5c6772b440
vis-me07 <vishal.mehta@nineleaps.com>
2021-09-13 19:05:25 +0530

    Vishal | Added DTOs for BOQ CRUD

---
+++ pojo/src/main/java/com/stanzaliving/boq_service/dto/BasicInfoDto.java
@@ -0,0 +1,32 @@
+package com.stanzaliving.boq_service.dto;
+
+import com.stanzaliving.core.boq_service.enums.BoqType;
+import com.stanzaliving.transformations.enums.BoqStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BasicInfoDto {
+    private String propertyUuid;
+
+    private String boqUuid;
+
+    private Boolean groupByAreaOfUse;
+
+    private LabelValueDto<String> houseName;
+
+    private String boqName;
+
+    private LabelValueDto<BoqType> boqType;
+
+    private LabelValueDto<BoqStatus> boqStatus;
+
+    private boolean canEdit;
+
+    private boolean canApprove;
+}
