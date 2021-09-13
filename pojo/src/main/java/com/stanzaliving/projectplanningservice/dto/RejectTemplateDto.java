6459e2538bdc3b93cfd38df5ad57881d06fb26f1
Ranjith K Jain <ranjithkjain@RanjithKJ-MBP.local>
2021-09-13 22:58:58 +0530

    Added DTO for Rejection Tag and Rejection Template

---
+++ pojo/src/main/java/com/stanzaliving/projectplanningservice/dto/RejectTemplateDto.java
@@ -0,0 +1,28 @@
+package com.stanzaliving.projectplanningservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class RejectTemplateDto {
+
+   private String planningTemplateID;
+
+    private Date selectDate;
+
+    private String remarks;
+
+    private List<String> tagList;
+}
