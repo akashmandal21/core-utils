d7ce7798536e03b359b59aeeaa67113b4acf180e
RajatSingh51 <rajat.singh@nineleaps.com>
2021-09-17 12:16:16 +0530

    Added migration dtos

--- pojo/src/main/java/com/stanzaliving/support/dto/ComplaintCategoryMigrationDto.java
+++ pojo/src/main/java/com/stanzaliving/support/dto/ComplaintCategoryMigrationDto.java
@@ -10,8 +10,31 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ComplaintCategoryMigrationDto {
 
+    @JsonSetter(value="uuid")
+    private String uuid;
 
+    @JsonSetter(value="name")
+    private String name;
 
+    @JsonSetter(value="eligibility")
+    private String eligibility;
 
+    @JsonSetter(value="complaint_type_uuid")
+    private String complaintTypeUuid;
+
+    @JsonSetter(value="created_at")
+    private String createdAt;
+
+    @JsonSetter(value="created_by")
+    private String createdBy;
+
+    @JsonSetter(value="status")
+    private String status;
+
+    @JsonSetter(value="updated_at")
+    private String updatedAt;
+
+    @JsonSetter(value="updated_by")
+    private String updatedBy;
 
 }
