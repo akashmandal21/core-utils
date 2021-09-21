7f034016af254e4affb5567a743ee433a5e250ed
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-09-21 14:49:29 +0530

    Update FaqCategoryDto.java

--- pojo/src/main/java/com/stanzaliving/website/dto/FaqCategoryDto.java
+++ pojo/src/main/java/com/stanzaliving/website/dto/FaqCategoryDto.java
@@ -2,6 +2,8 @@ package com.stanzaliving.website.dto;
 
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+
 import com.stanzaliving.core.base.common.dto.AbstractDto;
 
 import lombok.AllArgsConstructor;
@@ -20,8 +22,13 @@ public class FaqCategoryDto extends AbstractDto {
 	private static final long serialVersionUID = 1L;
 	
 	private String categoryName;
+	
+	@NotBlank(message = "Category Slug is Mandatory")
+	private String slug;
 
 	private Integer displayOrder;
 	
 	private List<FaqDto> faqDtos;
+	
+	private Integer totalFaqCount;
 }
