696a1b3ee561356885713867ef5222533675009d
malhotravanshika <vanshikha.malhotra@nineleaps.com>
2021-09-21 17:11:39 +0530

    refcator code

--- payment-client/src/main/java/com/stanzaliving/core/payment/client/api/RazorPayXControllerApi.java
+++ payment-client/src/main/java/com/stanzaliving/core/payment/client/api/RazorPayXControllerApi.java
@@ -3,12 +3,14 @@ package com.stanzaliving.core.payment.client.api;
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
 import com.stanzaliving.core.payment.dto.TransactionDto;
+import com.stanzaliving.ledger.dto.RefundRequest;
 import lombok.extern.log4j.Log4j2;
 import org.json.JSONObject;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -27,6 +29,7 @@ public class RazorPayXControllerApi {
 
     public ResponseDto<?> getResponseFromRazorpayX(String payoutId,
                                                    Boolean isRefund ) {
+        log.info("Initiate RazorpayXPayout Controller");
         Map<String, Object> uriVariables = new HashMap<>();
 
         String path = UriComponentsBuilder.fromPath("/payment/razorpayX/response")
@@ -51,5 +54,29 @@ public class RazorPayXControllerApi {
         }
         return null;
 
+    }
+
+    public ResponseEntity<HashMap<String, String>> initiateRazorpayXPayout(RefundRequest refundRequestDTO){
+        log.info("Initiate RazorpayXPayout Controller");
+
+        Object postBody = refundRequestDTO;
+
+        final Map<String, Object> uriVariables = new HashMap<>();
+
+        String path = UriComponentsBuilder.fromPath("/payment/razorpayX/initiate").buildAndExpand(uriVariables).toUriString();
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+        final HttpHeaders headerParams = new HttpHeaders();
+
+        final String[] accepts = {
+                "*/*"
+        };
+        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+        ParameterizedTypeReference<ResponseEntity<HashMap<String, String>>> returnType = new ParameterizedTypeReference<ResponseEntity<HashMap<String, String>>>() {
+        };
+
+        return restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
     }
 }
