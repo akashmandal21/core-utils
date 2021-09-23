fe6b266bfbefb255669d63ecb321e2c9ffea5634
Priyadarshini MB <priyadasrhini.mb@robosoftin.com>
2021-09-23 10:02:45 +0530

    Removed Array Initialization

--- pojo/src/main/java/com/stanzaliving/generictaskservice/dto/TaskCategoryDto.java
+++ pojo/src/main/java/com/stanzaliving/generictaskservice/dto/TaskCategoryDto.java
@@ -1,13 +1,11 @@
 package com.stanzaliving.generictaskservice.dto;
 
 import com.stanzaliving.core.base.common.dto.AbstractDto;
-import com.stanzaliving.generictaskservice.dto.request.TaskCategoryTypeDetailsRequestDto;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -34,6 +32,6 @@ public class TaskCategoryDto extends AbstractDto {
     private boolean overrideTask;
     private LocalTime startTime;
     private String autoCompletionEvent;
-    private List<String> tagsList = new ArrayList<String>();
+    private List<String> tagsList ;
 
 }
