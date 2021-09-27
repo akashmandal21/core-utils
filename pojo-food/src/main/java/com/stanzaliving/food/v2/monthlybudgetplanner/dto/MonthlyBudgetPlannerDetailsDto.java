8ccd65b4c97493c4f7eff8853089616b2485fcbb
kapil-stanza <kapil.dev@stanzaliving.com>
2021-09-27 11:05:23 +0530

    Added changes for table structure

--- pojo-food/src/main/java/com/stanzaliving/food/v2/monthlybudgetplanner/dto/MonthlyBudgetPlannerDetailsDto.java
+++ pojo-food/src/main/java/com/stanzaliving/food/v2/monthlybudgetplanner/dto/MonthlyBudgetPlannerDetailsDto.java
@@ -1,5 +1,26 @@
 package com.stanzaliving.food.v2.monthlybudgetplanner.dto;
 
-public class MonthlyBudgetPlannerDetailsDto {
+import com.stanzaliving.food.v2.menu.constants.MonthlyBudgetStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+public class MonthlyBudgetPlannerDetailsDto implements Serializable {
+
+	private static final long serialVersionUID = 9129236210318749280L;
+
+	private String budgetPlannerUuid;
+	private String budgetPlannerVersionUuid;
+	private MonthlyBudgetStatus monthlyBudgetStatus;
+	private LocalDate startDate;
 }
