340429dc2ed649756c4c4f6e9f86b8f183170a50
rahulsinghnineleaps <rahul.singh@nineleaps.com>
2021-09-29 11:06:42 +0530

    change in business hour dto

--- pojo/src/main/java/com/stanzaliving/support/dto/BusinessHourDto.java
+++ pojo/src/main/java/com/stanzaliving/support/dto/BusinessHourDto.java
@@ -1,14 +1,9 @@
 package com.stanzaliving.support.dto;
-
-
-import com.stanzaliving.support.enums.TicketType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.time.LocalTime;
 import java.util.List;
 
