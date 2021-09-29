771a44d2cd2486f96b3a4f2d0d031a7608c3da30
malhotravanshika <vanshikha.malhotra@nineleaps.com>
2021-09-29 13:15:14 +0530

    updated code for settle-ledger

--- payment-client/src/main/java/com/stanzaliving/core/payment/client/api/RazorPayXControllerApi.java
+++ payment-client/src/main/java/com/stanzaliving/core/payment/client/api/RazorPayXControllerApi.java
@@ -3,12 +3,14 @@ package com.stanzaliving.core.payment.client.api;
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
 import com.stanzaliving.core.payment.dto.TransactionDto;
+import com.stanzaliving.ledger.dto.RefundRequest;
 import lombok.extern.log4j.Log4j2;
 import org.json.JSONObject;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -25,15 +27,16 @@ public class RazorPayXControllerApi {
         this.restClient = restClient;
     }
 
-    public ResponseDto<?> getResponseFromRazorpayX(String payoutId,
+    public ResponseEntity<List<String>> getResponseFromRazorpayX(List<String> payoutIdList,
                                                    Boolean isRefund ) {
+        log.info("Initiate RazorpayXPayout Controller");
         Map<String, Object> uriVariables = new HashMap<>();
 
         String path = UriComponentsBuilder.fromPath("/payment/razorpayX/response")
                 .buildAndExpand(uriVariables).toUriString();
 
         final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
-        queryParams.add("payoutId", payoutId);
+        queryParams.add("payoutId", payoutIdList.toString());
         queryParams.add("isRefund", isRefund.toString());
 
          HttpHeaders headerParams = new HttpHeaders();
@@ -41,7 +44,7 @@ public class RazorPayXControllerApi {
          String[] accepts = { "*/*" };
          List<MediaType> accept = restClient.selectHeaderAccept(accepts);
 
-        ParameterizedTypeReference<ResponseDto<?>> returnType = new ParameterizedTypeReference<ResponseDto<?>>() {
+        ParameterizedTypeReference<ResponseEntity<List<String>>> returnType = new ParameterizedTypeReference<ResponseEntity<List<String>>>() {
         };
         try {
             return restClient.invokeAPI(path, HttpMethod.GET, queryParams, null, headerParams, accept,
@@ -51,5 +54,29 @@ public class RazorPayXControllerApi {
         }
         return null;
 
+    }
+
+    public ResponseDto<HashMap<String, String>> initiateRazorpayXPayout(RefundRequest refundRequestDTO){
+        log.info("Initiate RazorpayXPayout Controller");
+
+        Object postBody = refundRequestDTO;
+
+        final Map<String, Object> uriVariables = new HashMap<>();
+
+        String path = UriComponentsBuilder.fromPath("/payment/razorpayX/initiate").buildAndExpand(uriVariables).toUriString();
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+        final HttpHeaders headerParams = new HttpHeaders();
+
+        final String[] accepts = {
+                "*/*"
+        };
+        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+        ParameterizedTypeReference<ResponseDto<HashMap<String, String>>> returnType = new ParameterizedTypeReference<ResponseDto<HashMap<String, String>>>() {
+        };
+
+        return restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
     }
 }
