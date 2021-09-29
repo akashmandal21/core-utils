23b418ee64f33b80b201a9ace7b5c5cca19971ee
malhotravanshika <vanshikha.malhotra@nineleaps.com>
2021-09-29 19:05:34 +0530

    code fix

--- pojo/src/main/java/com/stanzaliving/core/payment/dto/RazorPayXPayoutDto.java
+++ pojo/src/main/java/com/stanzaliving/core/payment/dto/RazorPayXPayoutDto.java
@@ -2,16 +2,14 @@ package com.stanzaliving.core.payment.dto;
 
 import lombok.*;
 
-import java.util.List;
-
 @Getter
 @Setter
 @ToString
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class RazorPayXDto {
-    List<RazorPayXPayoutDto> payoutIds;
+public class RazorPayXPayoutDto {
+    String payoutId;
 
-    boolean isRefund;
+    String requestUniqueId;
 }
