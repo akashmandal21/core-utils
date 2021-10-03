70b18b6c294c4a2afdaf7a4882e0046a70ca4258
pradeep.naik <pradeep.naik@robosoftin.com>
2021-10-03 22:12:02 +0530

    Search filter request dto added for Search by taskName with pagination and OrderBy

---
+++ pojo/src/main/java/com/stanzaliving/generictaskservice/dto/request/TaskSearchFilterRequestDto.java
@@ -0,0 +1,33 @@
+package com.stanzaliving.generictaskservice.dto.request;
+
+import com.stanzaliving.core.base.common.dto.PageResponse;
+import com.stanzaliving.core.base.common.dto.PaginationRequest;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+/**
+ * @author Vikas S T
+ * @date 02-Sep-21
+ **/
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskSearchFilterRequestDto {
+
+    private String taskName;
+
+    private List<String> taskUuids;
+
+    private boolean taskTemplate;
+
+    private String orderBy;
+
+    private String orderByColumnName;
+
+    private PaginationRequest pageRequest;
+}
