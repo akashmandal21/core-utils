4037676037345cf8f472b44cb0f28e545219f9ef
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-10-07 11:30:30 +0530

    Create ElectricityBillType.java

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/electricity/constants/ElectricityBillType.java
@@ -0,0 +1,32 @@
+package com.stanzaliving.core.electricity.constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.stanzaliving.core.user.enums.EnumListing;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public enum ElectricityBillType {
+
+	MAIN("Main"), 
+	COMMON_AREA("Common area"), 
+	ROOM("Room");
+
+    private String billTypeName;
+
+    private static List<EnumListing<ElectricityBillType>> electricityBillTypes = new ArrayList<>();
+    static {
+        for(ElectricityBillType electricityBillType: ElectricityBillType.values()){
+        	electricityBillTypes.add(EnumListing.of(electricityBillType, electricityBillType.getBillTypeName()));
+        }
+    }
+
+    public static  List<EnumListing<ElectricityBillType>> getElectricityBillTypes(){
+        return electricityBillTypes;
+    }
+}
