7200c982a58290ccf5e01691b58da85e2fbc2f77
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-10-08 13:59:55 +0530

    ElectricityBillDto added

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/electricity/constants/ElectricityBillStatus.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.stanzaliving.core.electricity.constants;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.stanzaliving.core.user.enums.EnumListing;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author manish.pareek
+ *
+ * @date 08-OCT-2021
+ */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public enum ElectricityBillStatus {
+	UNDER_DRAFT("Under Draft", 3), 
+	SUBMITTED("Submitted", 2), 
+	CANCELLED("Cancelled", 4), 
+	APPROVED("Approved", 1),
+	REJECTED("Rejected", 5);
+
+   private String billStatus;
+   private int sequence;
+   
+   private static List<EnumListing<ElectricityBillStatus>> electricityBillStatusList = new ArrayList<>();
+   static {
+       for(ElectricityBillStatus electricityBillStatus: ElectricityBillStatus.values()){
+    	   electricityBillStatusList.add(EnumListing.of(electricityBillStatus, electricityBillStatus.getBillStatus()));
+       }
+   }
+
+   public static  List<EnumListing<ElectricityBillStatus>> getelectricityBillStatus(){
+       return electricityBillStatusList;
+   }
+
+}
---
+++ pojo-ops/src/main/java/com/stanzaliving/core/electricity/dto/ElectricityBillDto.java
@@ -0,0 +1,76 @@
+package com.stanzaliving.core.electricity.dto;
+
+import java.util.Date;
+import java.util.Map;
+
+import com.stanzaliving.approval.enums.ApprovalStatus;
+import com.stanzaliving.core.base.common.dto.AbstractDto;
+import com.stanzaliving.core.electricity.constants.ElectricityBillStatus;
+import com.stanzaliving.core.electricity.constants.ElectricityBillType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ElectricityBillDto extends AbstractDto {
+
+	private static final long serialVersionUID = 6801852116636272900L;
+
+	private String residenceId;
+
+	private String propertyId;
+
+	private String billNumber;
+
+	private String billId;
+
+	private ElectricityBillType electricityBillType;
+
+	private Date billStartDate;
+
+	private Date billEndDate;
+
+	private String billUrl;
+
+	private Double totalConsumptionCharges;
+
+	private Double gridConsumptionCharges;
+
+	private Double dgConsumptionCharges;
+
+	private Double otherCharges;
+
+	private Double penalties;
+
+	private Double arrears;
+
+	private Double totalUnits;
+
+	private Double dgUnits;
+
+	private Double gridUnits;
+
+	private Double dgUnitRate;
+
+	private Double gridUnitRate;
+
+	private Map<String, Object> config;
+
+	private Date addedAt;
+
+	private String addedBy;
+
+	private ElectricityBillStatus electricityBillStatus;
+
+	private ApprovalStatus approvalStatus;
+
+}
