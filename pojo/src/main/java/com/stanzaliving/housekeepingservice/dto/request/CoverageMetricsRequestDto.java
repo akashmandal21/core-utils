80cc34b95a07c006907e2f6256d551125275e5b3
Priyadarshini MB <priyadasrhini.mb@robosoftin.com>
2021-10-08 15:42:55 +0530

    Coverage metrics planned data count changes

--- pojo/src/main/java/com/stanzaliving/housekeepingservice/dto/request/CoverageMetricsRequestDto.java
+++ pojo/src/main/java/com/stanzaliving/housekeepingservice/dto/request/CoverageMetricsRequestDto.java
@@ -1,8 +1,9 @@
 package com.stanzaliving.housekeepingservice.dto.request;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalTime;
+import java.time.LocalDate;
 
 /**
  * @author Priyadarshini MB
@@ -18,5 +19,7 @@ public class CoverageMetricsRequestDto {
 
     private String planningTemplateUuid;
     private String microClusterUuid;
-    private LocalTime startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+    private String taskCategoryUuid;
 }
