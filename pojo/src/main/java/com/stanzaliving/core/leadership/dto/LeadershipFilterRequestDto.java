c000d4a74491d0a68e95d1fb06b7f0b954234dc7
vaibhavguptastanza <vaibhav.gupta@stanzaliving.com>
2021-10-11 17:19:26 +0530

    Update LeadershipFilterRequestDto.java

--- pojo/src/main/java/com/stanzaliving/core/leadership/dto/LeadershipFilterRequestDto.java
+++ pojo/src/main/java/com/stanzaliving/core/leadership/dto/LeadershipFilterRequestDto.java
@@ -23,7 +23,7 @@ import lombok.ToString;
 public class LeadershipFilterRequestDto {
 
 	private List<String> estateStatuses;
-	private List<String> competitorNameList;
+	private List<String> competition;
 	private BedCountRange bedCountRange;
 	private RentBedRange rentBedRange;
 	private String city;
