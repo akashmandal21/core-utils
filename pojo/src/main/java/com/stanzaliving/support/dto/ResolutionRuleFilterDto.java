df02738415a35fa41810dfac88d08296e8f1e297
denz08 <denis.sebastian@nineleaps.com>
2021-10-14 15:51:41 +0530

    Added Dto & Enum for Ticket Sla Color

--- pojo/src/main/java/com/stanzaliving/support/dto/ResolutionRuleFilterDto.java
+++ pojo/src/main/java/com/stanzaliving/support/dto/ResolutionRuleFilterDto.java
@@ -7,7 +7,6 @@ import lombok.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
---
+++ pojo/src/main/java/com/stanzaliving/support/enums/TicketSlaStatus.java
@@ -0,0 +1,11 @@
+package com.stanzaliving.support.enums;
+
+public enum TicketSlaStatus {
+    DUE_IN_X_DAYS,
+    DUE_IN_Y_HOURS,
+    NEARING_OR_BREACHED,
+    IN_TIME,
+    VIOLATED,
+    NOT_APPLICABLE,
+    NOT_DEFINED,
+}
