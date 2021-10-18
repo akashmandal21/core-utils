80480560b54aee48b32fa4a2e012da490abe3db4
rohit.gupta <rohit.gupta@stanzaliving.com>
2021-10-18 14:05:25 +0530

    CSV upload message refactoring

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/CsvUploadResponseDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/CsvUploadResponseDto.java
@@ -1,21 +1,22 @@
 package com.stanzaliving.core.food.dto.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @SuperBuilder
+@NoArgsConstructor
 public class CsvUploadResponseDto {
 
     private String csvUrl;
     private long totalCount;
     private long errorCount;
     private long successCount;
-    private String message;
     
-    public String getResultMessage() {
+    public String getMessage() {
         
         if(this.totalCount==0l) {
             return "No records found in the upload request";
