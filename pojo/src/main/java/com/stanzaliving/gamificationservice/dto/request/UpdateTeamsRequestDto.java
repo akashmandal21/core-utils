8cf6ec2d3d443641db8d98aa7e34ad88c02376fa
rajj5 <varun.r@robosoftin.com>
2021-10-18 19:16:26 +0530

    added UpdateTeamsRequestDto

---
+++ pojo/src/main/java/com/stanzaliving/gamificationservice/dto/request/UpdateTeamsRequestDto.java
@@ -0,0 +1,39 @@
+package com.stanzaliving.gamificationservice.dto.request;
+
+import com.stanzaliving.core.base.common.dto.AbstractDto;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateTeamsRequestDto extends AbstractDto {
+    @NotNull(
+            message = "Teams Id is mandatory to update"
+    )
+    private String uuid;
+
+    private String teamName;
+
+    private LocalDate dateOfJoining;
+
+    private List<String> users = new ArrayList<>();
+
+    private String location;
+
+    private String zone;
+
+    private String clusterName;
+
+    private String clusterManagerUserUuid;
+
+}
