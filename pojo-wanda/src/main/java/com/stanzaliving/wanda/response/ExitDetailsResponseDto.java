f6b745a7e11acad469263f8fe5a5b9a3cf4a1d5e
nishant.nineleaps <nishant.venugopal@nineleaps.com>
2021-10-20 14:16:16 +0530

    Updates response dto for exit get api

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/response/ExitDetailsResponseDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/response/ExitDetailsResponseDto.java
@@ -1,15 +1,16 @@
 package com.stanzaliving.wanda.response;
 
-import java.time.LocalDate;
-import java.util.List;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.stanzaliving.booking.dto.PaymentDetailsDto;
 import com.stanzaliving.wanda.dtos.BankDetailsDto;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 @Builder
 @Data
@@ -17,21 +18,45 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ExitDetailsResponseDto {
 
-	private String bookingUuid;
+    private String bookingUuid;
 
-	private LocalDate moveOutDate;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate moveOutDate;
 
-	private LocalDate actualMoveOutDate;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate actualMoveOutDate;
 
-	private Double dues;
+    private Double dues;
 
-	private String actualEndDateNote;
+    private String actualEndDateNote;
 
-	private String confirmationNote;
+    private String confirmationNote;
 
-	private List<PaymentDetailsDto> paymentDetails;
+    private List<PaymentDetailsDto> paymentDetails;
 
-	private boolean isEligibleForExitInitiation = true;
+    private boolean isEligibleForExitInitiation = true;
 
-	private BankDetailsDto bankDetails;
+    private BankDetailsDto bankDetails;
+
+    private String exitReason;
+
+    private String exitReasonUuid;
+
+    private String defaulterUuid;
+
+    private String defaulterReason;
+
+    private String roomKeyHandOverStatus;
+
+    private Date contractStartDate;
+
+    private Date contractEndDate;
+
+    private Integer noticePeriod;
+
+    private String zhComments;
+
+    private String exitReasonNote;
+
+    private String bookingStatus;
 }
