35909203982b09d4ffbdc0d4764647142165b7d5
denz08 <denis.sebastian@nineleaps.com>
2021-10-21 10:56:34 +0530

    Changes for Status

--- pojo/src/main/java/com/stanzaliving/support/enums/Status.java
+++ pojo/src/main/java/com/stanzaliving/support/enums/Status.java
@@ -18,7 +18,8 @@ public enum Status {
     PENDING("PENDING","ab0a8afe-8aa4-45ad-9544-130a5ee368ae","SYSTEM","SUB_TICKET"),
     DELAYED("DELAYED","cde42cc6-1b8e-4055-a26e-d4ead90cc195","USER","SUB_TICKET"),
     COMPLETED("COMPLETED","1ac402a2-3c70-4566-a1b5-d2c05a95ff0b","USER","SUB_TICKET"),
-    MERGED("MERGED","4cf7d95f-2138-4663-9220-a7c3dc3464ed","SYSTEM","TICKET");
+    MERGED("MERGED","4cf7d95f-2138-4663-9220-a7c3dc3464ed","SYSTEM","TICKET"),
+    NOT_DEFINED("NOT_DEFINED","f3833b2f-4a2d-3c70-8da3-a7c3dc3464ed","SYSTEM","TICKET");
 
     private final String value;
 
