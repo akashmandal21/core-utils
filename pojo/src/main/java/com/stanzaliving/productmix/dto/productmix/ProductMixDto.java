bafeaa30054bbecb7060b7356db89a0836af8c6c
Vishal Mehta <vishal.mehta@nineleaps.com>
2021-10-21 17:01:47 +0530

    Refactoring

--- pojo/src/main/java/com/stanzaliving/productmix/dto/productmix/ProductMixDto.java
+++ pojo/src/main/java/com/stanzaliving/productmix/dto/productmix/ProductMixDto.java
@@ -1,14 +1,9 @@
 package com.stanzaliving.productmix.dto.productmix;
 
-import com.stanzaliving.productmix.dto.LabelValueDto;
-import com.stanzaliving.productmix.dto.propertytemplate.ZonesDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.Map;
 
 @Data
 @AllArgsConstructor
