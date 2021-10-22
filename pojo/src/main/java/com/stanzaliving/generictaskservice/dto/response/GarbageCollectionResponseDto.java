e4815dc6848b73fda58efd83812c6058276d0ad3
VikasST <87382623+VikasST@users.noreply.github.com>
2021-10-22 17:49:28 +0530

    mock api dto for task attributes

---
+++ pojo/src/main/java/com/stanzaliving/generictaskservice/dto/response/GarbageCollectionResponseDto.java
@@ -0,0 +1,26 @@
+package com.stanzaliving.generictaskservice.dto.response;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * @author Vikas S T
+ * @date 22-Oct-21
+ **/
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GarbageCollectionResponseDto {
+    private String residenceName;
+    private String residenceUuid;
+    private boolean collectGarbage;
+    private LocalTime duration;
+    private LocalDate date;
+}
