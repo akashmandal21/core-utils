10dc40557ab873b3c3cab7a127a8ad3cf7f394bb
akshitgupta915 <akshit.gupta@nineleaps.com>
2021-10-27 16:47:11 +0530

    In productMixDetails Dto, removed bedCount and added uploadCsv

--- pojo/src/main/java/com/stanzaliving/productmix/dto/productmix/ProductMixDetails.java
+++ pojo/src/main/java/com/stanzaliving/productmix/dto/productmix/ProductMixDetails.java
@@ -34,9 +34,11 @@ public class ProductMixDetails {
     @Valid
     private ApplicableDurationDto applicableDuration;
 
+    @NotBlank(message = "No. of rooms cannot be blank")
     private int noOfRooms; //todo: validation
 
-    private int noOfBeds; //todo: validation
+    @NotBlank(message = "Upload CSV URL cannot be blank")
+    private String uploadCsv;
 
     @NotNull
     @Valid
