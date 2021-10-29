9bcbd6a2fdaa5028b3175f73a802b3a1f020abca
Keshav Kumar Singh <keshav.ks@robosoftin.com>
2021-10-29 15:16:04 +0530

    Added contest creation dto file

---
+++ pojo/src/main/java/com/stanzaliving/gamificationservice/dto/ContestDto.java
@@ -0,0 +1,50 @@
+package com.stanzaliving.gamificationservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.time.LocalDate;
+
+/**
+ * @author Keshav Singh
+ * @date 10/28/2021
+ **/
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@ToString
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ContestDto {
+
+	private String uuid;
+
+	private String description;
+
+	private String contestTitle;
+
+	private String createdBy;
+
+	private int duration;
+
+	private LocalDate startDate;
+
+	private LocalDate endDate;
+
+	private LocalDate dateOfJoining;
+
+	private String contestType;
+
+	private String contestTypeId;
+
+	private String location;
+
+	private String zone;
+
+	private String cluster;
+
+	private String pair;
+
+	private String status;
+}
