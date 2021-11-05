c6832cf5208268757a518ca7a4937b1ae1cf02bf
AnudeepAlevoor <anudeep@robosoftin.com>
2021-11-05 10:40:32 +0530

    Filter API for Added By for PP template and plan DTO review changes

--- pojo/src/main/java/com/stanzaliving/projectplanningservice/dto/PropertyFilterAddedByDto.java
+++ pojo/src/main/java/com/stanzaliving/projectplanningservice/dto/PropertyFilterAddedByDto.java
@@ -14,7 +14,7 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
-public class PropertyFilterAddedBtDto {
+public class PropertyFilterAddedByDto {
 
     private String userUuid;
 
