45a84c18f0449be82f41325ffbfb0587e164f170
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-11-11 16:26:00 +0530

    Create UnitsConsumptionDto.java

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/electricity/dto/UnitsConsumptionDto.java
@@ -0,0 +1,25 @@
+package com.stanzaliving.core.electricity.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnitsConsumptionDto {
+	private double consumptionAmount;
+	private double unitsConsumed;
+	private double unitRate;
+	private LocalDate consumptionUnitsStartDate;
+	private LocalDate consumptionUnitsEndDate;
+
+}
