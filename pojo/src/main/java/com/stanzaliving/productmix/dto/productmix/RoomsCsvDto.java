a700ca326c95e81e059f5addc7724837dcfd6641
akshitgupta915 <akshit.gupta@nineleaps.com>
2021-11-11 12:06:45 +0530

    Added validations in RoomCsvDto

--- pojo/src/main/java/com/stanzaliving/productmix/dto/productmix/RoomsCsvDto.java
+++ pojo/src/main/java/com/stanzaliving/productmix/dto/productmix/RoomsCsvDto.java
@@ -5,12 +5,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class RoomsCsvDto {
+
+    @NotBlank(message = "Product mix uuid cannot be blank")
     private String productMixUuid;
+
+    @NotBlank(message = "File path cannot be blank")
     private String filePath;
+
+    @NotNull(message = "Number of rooms cannot be null")
     private Integer noOfRooms;
 }
