80c96ff90470373939f78244fd47e36461eaa5e9
AnudeepAlevoor <anudeep@robosoftin.com>
2021-11-15 17:56:19 +0530

    Area common api updated

--- pojo/src/main/java/com/stanzaliving/housekeepingservice/enums/AreaCommonSortSpecs.java
+++ pojo/src/main/java/com/stanzaliving/housekeepingservice/enums/AreaCommonSortSpecs.java
@@ -8,6 +8,7 @@ public enum AreaCommonSortSpecs {
     AREA_NAME("areaName"),
     SIZE_IN_SQFT("sizeInSqFt"),
     CLEANING_TIME_IN_MINUTES("cleaningTimeInMinutes"),
+    AREA_TAG_NAME("areaTagName"),
     AREA_TAG_UUID("areaTagUuid");
     private String dbKey;
 
