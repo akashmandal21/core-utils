34866fbb88c38a5e96e619744b76d9b3d6e04bd7
rahulsinghnineleaps <rahul.singh@nineleaps.com>
2021-11-15 12:13:06 +0530

    mapping of status for support and wanda

--- pojo/src/main/java/com/stanzaliving/support/enums/Status.java
+++ pojo/src/main/java/com/stanzaliving/support/enums/Status.java
@@ -15,10 +15,10 @@ public enum Status {
     REOPEN("REOPEN","99269fe9-1e4a-4a2d-a0fb-070ca50541fa","SYSTEM","TICKET","Reopen"),
     CLOSED("CLOSED","277c107b-458f-4b51-9940-ed9c651ec292","SYSTEM","TICKET","Closed"),
     RESOLVED("RESOLVED","6c1c88a-d3f3-446d-8da3-1dcc92043ce4","USER","TICKET","Resolved"),
-    PENDING("PENDING","ab0a8afe-8aa4-45ad-9544-130a5ee368ae","SYSTEM","SUB_TICKET",""),
+    PENDING("PENDING","ab0a8afe-8aa4-45ad-9544-130a5ee368ae","SYSTEM","SUB_TICKET","Pending"),
     DELAYED("DELAYED","cde42cc6-1b8e-4055-a26e-d4ead90cc195","USER","SUB_TICKET",""),
     COMPLETED("COMPLETED","1ac402a2-3c70-4566-a1b5-d2c05a95ff0b","USER","SUB_TICKET",""),
-    MERGED("MERGED","4cf7d95f-2138-4663-9220-a7c3dc3464ed","SYSTEM","TICKET",""),
+    MERGED("MERGED","4cf7d95f-2138-4663-9220-a7c3dc3464ed","SYSTEM","TICKET","Closed"),
     NOT_DEFINED("NOT_DEFINED","f3833b2f-4a2d-3c70-8da3-a7c3dc3464ed","SYSTEM","TICKET","");
 
     private final String value;
@@ -30,6 +30,7 @@ public enum Status {
     private final String ticketType;
 
     private final String complaintStatus;
+
 
     private static final Map<String, Status> map = new HashMap<>();
 
