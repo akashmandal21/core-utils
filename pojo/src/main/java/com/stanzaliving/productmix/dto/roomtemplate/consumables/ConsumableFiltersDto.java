7351f6c201566a1451900b77f6858a4f863b704a
Vishal Mehta <vishal.mehta@nineleaps.com>
2021-11-16 18:00:17 +0530

    Added dto for filters in consumable modal

---
+++ pojo/src/main/java/com/stanzaliving/productmix/dto/roomtemplate/consumables/ConsumableFiltersDto.java
@@ -0,0 +1,17 @@
+package com.stanzaliving.productmix.dto.roomtemplate.consumables;
+
+import com.stanzaliving.productmix.dto.LabelValueDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ConsumableFiltersDto {
+    private List<LabelValueDto<String>> categories;
+}
