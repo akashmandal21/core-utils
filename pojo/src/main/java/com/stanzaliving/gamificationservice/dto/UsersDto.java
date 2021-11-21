40266f5ed4021c9c5487d0c72dea506968458e9b
Keshav Kumar Singh <keshav.ks@robosoftin.com>
2021-11-21 09:11:13 +0530

    Added user_dto for create users api

---
+++ pojo/src/main/java/com/stanzaliving/gamificationservice/dto/UsersDto.java
@@ -0,0 +1,28 @@
+package com.stanzaliving.gamificationservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.time.LocalDate;
+
+/**
+ * @author Keshav Singh
+ * @date 11/21/2021
+ **/
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@ToString
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class UsersDto {
+
+	private String uuid;
+
+	private String firstName;
+
+	private String lastName;
+
+	private LocalDate dateOfJoining;
+}
