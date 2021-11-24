f6e411e0a4495520bf4b306f79e0c630380876c7
Shubham-Mohanty <shubham.mohanty@nineleaps.com>
2021-11-24 23:42:01 +0530

    Removed createDocument client

--- invoice-service-client/src/main/java/com/stanzaliving/core/invoice_client/api/InvoiceServiceApi.java
+++ invoice-service-client/src/main/java/com/stanzaliving/core/invoice_client/api/InvoiceServiceApi.java
@@ -1,12 +1,7 @@
 package com.stanzaliving.core.invoice_client.api;
 
 import com.stanzaliving.core.base.common.dto.ResponseDto;
-import com.stanzaliving.core.base.constants.SecurityConstants;
-import com.stanzaliving.core.base.enums.Department;
-import com.stanzaliving.core.base.exception.StanzaException;
 import com.stanzaliving.core.base.http.StanzaRestClient;
-import com.stanzaliving.invoice.dto.InvoiceMaxApprovalLevelDto;
-import com.stanzaliving.ventaInvoice.dto.DocumentRequestDto;
 import com.stanzaliving.ventaInvoice.dto.DocumentResponseDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
@@ -124,36 +119,6 @@ public class InvoiceServiceApi {
             log.error("error while fetching the AR invoice details for booking uuid{}", bookingUuid,e);
             return null;
         }
-    }
-
-    public ResponseDto<DocumentResponseDto> createDocument(DocumentRequestDto documentRequestDto) {
-
-        Object postBody = null;
-
-        log.info("DocumentRequestDto is {} ", documentRequestDto);
-
-        final Map<String, Object> uriVariables = new HashMap<>();
-
-        String path = UriComponentsBuilder.fromPath("/internal/invoice/create").buildAndExpand(uriVariables).toUriString();
-
-        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
-
-        final HttpHeaders headerParams = new HttpHeaders();
-
-        final String[] accepts = {"*/*"};
-
-        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
-
-        ParameterizedTypeReference<ResponseDto<DocumentResponseDto>> returnType = new ParameterizedTypeReference<ResponseDto<DocumentResponseDto>>() {
-        };
-        postBody = documentRequestDto;
-
-        try {
-            return restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
-        } catch (Exception e) {
-            throw new StanzaException("error while creating the invoice/credit-note. Error is "+ e.getMessage());
-        }
-
     }
 
 }
