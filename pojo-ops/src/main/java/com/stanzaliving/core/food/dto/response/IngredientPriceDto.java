745e5f635c59a047022d0619f0f60e6026aef0a7
rohit.gupta <rohit.gupta@stanzaliving.com>
2021-11-25 16:27:54 +0530

    making price dto serializable

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/IngredientPriceDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/IngredientPriceDto.java
@@ -3,13 +3,15 @@ package com.stanzaliving.core.food.dto.response;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @ToString
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class IngredientPriceDto {
+public class IngredientPriceDto implements Serializable {
 	private String ingredientId;
 	private String cityId;
 	private String vendorId;
