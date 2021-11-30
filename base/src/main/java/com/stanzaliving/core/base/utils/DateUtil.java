0bf391188b4af4d41fef5015f7059fd62a503f05
rahulsinghnineleaps <rahul.singh@nineleaps.com>
2021-11-30 11:03:12 +0530

    added dto for booking service integration

--- base/src/main/java/com/stanzaliving/core/base/utils/DateUtil.java
+++ base/src/main/java/com/stanzaliving/core/base/utils/DateUtil.java
@@ -24,6 +24,9 @@ import static com.google.common.base.Preconditions.checkArgument;
 @UtilityClass
 public class DateUtil {
 
+    // added for booking service dto validation
+    public final String yyyy_MM_dd_FORMAT = "yyyy-MM-dd";
+
     public String formatIst(Date date, String format) {
         return Instant.ofEpochMilli(date.getTime()).atZone(StanzaConstants.IST_TIMEZONEID).format(DateTimeFormatter.ofPattern(format));
     }
--- booking-service-client/src/main/java/com/stanzaliving/core/client/api/BookingDataControllerApi.java
+++ booking-service-client/src/main/java/com/stanzaliving/core/client/api/BookingDataControllerApi.java
@@ -9,9 +9,8 @@ import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.exception.ApiValidationException;
 import com.stanzaliving.core.base.http.StanzaRestClient;
 import com.stanzaliving.core.bookingservice.dto.response.BookedPackageServiceDto;
+import com.stanzaliving.core.bookingservice.dto.response.BookingDetailDto;
 import com.stanzaliving.core.client.dto.*;
-import com.stanzaliving.core.dto.AddressResponseDTO;
-import com.stanzaliving.ventaAudit.dto.VentaNotificationDto;
 import com.stanzaliving.ledger.dto.UpcomingBookingsDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
@@ -22,7 +21,6 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -85,6 +83,32 @@ public class BookingDataControllerApi {
         };
         return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
     }
+
+    public ResponseDto<List<BookingDetailDto>> getBookingDetailsForResident(String residentId) {
+
+        Object postBody = null;
+
+        // create path and map variables
+        final Map<String, Object> uriVariables = new HashMap<>();
+
+        String path = UriComponentsBuilder.fromPath("/internal/booking-detail/resident/{residentId}").buildAndExpand(uriVariables).toUriString();
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+        queryParams.add("residentId", residentId);
+
+        final HttpHeaders headerParams = new HttpHeaders();
+
+        final String[] accepts = {
+                "*/*"
+        };
+        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+        ParameterizedTypeReference<ResponseDto<List<BookingDetailDto>>> returnType = new ParameterizedTypeReference<ResponseDto<List<BookingDetailDto>>>() {
+        };
+        return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
+    }
+
 
 
     public ResponseDto<ExpiredBookingsResponseDto> expireBooking(ExpiredBookingsDto expiredBookingsDto) {
