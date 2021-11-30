44683c8e1c93f11743d18d86ada824f8539eae4c
rajj5 <varun.r@robosoftin.com>
2021-11-30 17:50:53 +0530

    added HkStaffFilterDto

--- pojo/src/main/java/com/stanzaliving/housekeepingservice/dto/response/HkStaffResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/housekeepingservice/dto/response/HkStaffResponseDto.java
@@ -1,6 +1,7 @@
 package com.stanzaliving.housekeepingservice.dto.response;
 
 import lombok.*;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @Getter
 @Setter
@@ -8,18 +9,15 @@ import lombok.*;
 @Builder
 @ToString
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class HkStaffResponseDto {
+    protected String createdBy;
+    protected String updatedBy;
     private String uuid;
-
     private String vendor;
-
     private String employeeName;
-
     private String emailId;
-
     private String mobileNo;
-
     private String imageUrl;
-
     private String hkUuid;
 }
