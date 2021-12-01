a3b6debbe6f491e1f28bdafca3c4d636f31ea133
Keshav Kumar Singh <keshav.ks@robosoftin.com>
2021-12-01 14:25:51 +0530

    Added user profile dto file

---
+++ pojo/src/main/java/com/stanzaliving/gamificationservice/dto/UsersProfileDto.java
@@ -0,0 +1,29 @@
+package com.stanzaliving.gamificationservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Keshav Singh
+ * @date 11/25/2021
+ **/
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@ToString
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class UsersProfileDto {
+
+	private String uuid;
+
+	private String userId;
+
+	private List<String> activeContest = new ArrayList<>();
+
+	private List<String> badges = new ArrayList<>();
+}
