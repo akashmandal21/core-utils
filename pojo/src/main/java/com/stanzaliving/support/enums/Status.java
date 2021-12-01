2f7ec56556cba91294ac2978b5936321152ccb81
RajatSingh51 <rajat.singh@nineleaps.com>
2021-12-01 11:19:06 +0530

    changed text from reopen to reopened

--- pojo/src/main/java/com/stanzaliving/support/enums/Status.java
+++ pojo/src/main/java/com/stanzaliving/support/enums/Status.java
@@ -12,7 +12,7 @@ public enum Status {
     OPEN("OPEN","a11d5ed6-c694-496d-8fd5-e08d41a8065c","SYSTEM","TICKET","Open"),
     IN_PROGRESS("IN PROGRESS","1826ffdf-989a-4a2b-8435-e9e0b1f787ec","USER","TICKET","In Progress"),
     WAITING_ON_CUSTOMER("WAITING ON CUSTOMER","f3833b2f-93a3-4569-8840-8602da68ffb8","USER","TICKET","Waiting on Customer"),
-    REOPEN("REOPEN","99269fe9-1e4a-4a2d-a0fb-070ca50541fa","SYSTEM","TICKET","Reopen"),
+    REOPEN("REOPENED","99269fe9-1e4a-4a2d-a0fb-070ca50541fa","SYSTEM","TICKET","Reopen"),
     CLOSED("CLOSED","277c107b-458f-4b51-9940-ed9c651ec292","SYSTEM","TICKET","Closed"),
     RESOLVED("RESOLVED","6c1c88a-d3f3-446d-8da3-1dcc92043ce4","USER","TICKET","Resolved"),
     PENDING("PENDING","ab0a8afe-8aa4-45ad-9544-130a5ee368ae","SYSTEM","SUB_TICKET","Pending"),
