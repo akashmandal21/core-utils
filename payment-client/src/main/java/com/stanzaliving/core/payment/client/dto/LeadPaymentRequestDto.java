1809986fb2ba974441a394b2ae15ca0161ec1bd2
Abhishek-sopho <abhishek.kumar488@gmail.com>
2021-12-07 17:40:56 +0530

    request dto

--- payment-client/src/main/java/com/stanzaliving/core/payment/client/dto/LeadPaymentRequestDto.java
+++ payment-client/src/main/java/com/stanzaliving/core/payment/client/dto/LeadPaymentRequestDto.java
@@ -1,10 +1,16 @@
 package com.stanzaliving.core.payment.client.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LeadPaymentRequestDto {
 
     private String phone;
