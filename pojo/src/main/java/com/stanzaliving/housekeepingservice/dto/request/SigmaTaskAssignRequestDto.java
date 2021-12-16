3e6771b76b9b936055427e788d94a8e9a055b1a4
VikasST <87382623+VikasST@users.noreply.github.com>
2021-12-16 16:40:00 +0530

    beat plan response updated

--- pojo/src/main/java/com/stanzaliving/housekeepingservice/dto/request/SigmaTaskAssignRequestDto.java
+++ pojo/src/main/java/com/stanzaliving/housekeepingservice/dto/request/SigmaTaskAssignRequestDto.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author Vikas S T
  * @date 25-Nov-21
@@ -17,7 +19,18 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SigmaTaskAssignRequestDto {
+    @NotBlank(message = "Task uuid is mandatory")
     private String taskUuid;
+
+    @NotBlank(message = "Pseudo person uuid is mandatory")
     private String pseudoPersonUuid;
+
+    @NotBlank(message = "Hk beat person uuid is mandatory")
     private String hkBeatPersonUuid;
+
+    @NotBlank(message = "Micro cluster uuid is mandatory")
+    private String microClusterUuid;
+
+    @NotBlank(message = "Residences uuid is mandatory")
+    private String residenceUuid;
 }
