0f4bbc1fc86502b5f3a4e62cee3a5676fff47e94
pradeepnaikramesh <pradeep.naik@robosoftin.com>
2021-12-20 16:50:52 +0530

    Change Request updated

--- pojo/src/main/java/com/stanzaliving/projectplanningservice/dto/PredecessorTaskDto.java
+++ pojo/src/main/java/com/stanzaliving/projectplanningservice/dto/PredecessorTaskDto.java
@@ -1,5 +1,6 @@
 package com.stanzaliving.projectplanningservice.dto;
 
+import com.stanzaliving.projectplanningservice.enums.Duration;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,4 +15,8 @@ public class PredecessorTaskDto {
     private String taskUuid;
 
     private String taskName;
+
+    private int duration;
+
+    private Duration durationUnit;
 }
