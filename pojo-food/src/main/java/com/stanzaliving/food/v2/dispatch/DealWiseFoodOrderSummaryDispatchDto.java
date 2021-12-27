eb5239fbd8197044d149bdebdfabe55b791196e3
manishpareek <manishpareek.retainer@stanzaliving.com>
2021-12-27 20:34:38 +0530

    Update DealWiseFoodOrderSummaryDispatchDto.java

--- pojo-food/src/main/java/com/stanzaliving/food/v2/dispatch/DealWiseFoodOrderSummaryDispatchDto.java
+++ pojo-food/src/main/java/com/stanzaliving/food/v2/dispatch/DealWiseFoodOrderSummaryDispatchDto.java
@@ -1,14 +1,16 @@
 package com.stanzaliving.food.v2.dispatch;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import com.stanzaliving.core.operations.enums.DealCategory;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -21,6 +23,8 @@ public class DealWiseFoodOrderSummaryDispatchDto {
 	private String dealId;
 
 	private DealCategory dealCategory;
+	
+	private LocalDate menuDate;
 
 	private List<FoodOrderSummaryDispatchDto> foodOrderSummaryDispatchDtoList;
 
