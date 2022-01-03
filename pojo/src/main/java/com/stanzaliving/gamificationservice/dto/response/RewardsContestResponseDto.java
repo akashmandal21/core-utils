5f6357d9974644913142bfb7cdd82e43e95bd41c
rajj5 <varun.r@robosoftin.com>
2022-01-03 17:04:54 +0530

    updated dto

---
+++ pojo/src/main/java/com/stanzaliving/gamificationservice/dto/response/RewardsContestResponseDto.java
@@ -0,0 +1,66 @@
+package com.stanzaliving.gamificationservice.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.stanzaliving.gamificationservice.enums.ContestStatus;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@ToString
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class RewardsContestResponseDto {
+
+    private String uuid;
+
+    private String description;
+
+    private String contestTitle;
+
+    private String createdBy;
+
+    private int duration;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private LocalDate dateOfJoining;
+
+    private String contestType;
+
+    private List<String> location;
+
+    private List<String> zone;
+
+    private List<String> cluster;
+
+    private List<String> pair;
+
+    private ContestStatus contestStatus;
+
+    private String profilePhotoUrl;
+
+    private String durationUnit;
+
+    private boolean recurringContest;
+
+    private String contestMainBackgroundImg;
+
+    private String contestLogo;
+
+    private String contestMessage;
+
+    private String backGroundPhotoUrl;
+
+    private String rejectDescription;
+
+    private List<TeamsResponseDto> teamsResponseDtoList;
+
+}
