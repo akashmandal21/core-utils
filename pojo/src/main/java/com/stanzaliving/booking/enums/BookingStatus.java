d0ae4c8bf3459b74b529554a153973ec4370eb6e
Shanthi-rajendran-22 <shanthi.r@nineleaps.com>
2022-01-06 15:44:24 +0530

    Exit flow booking visibility updated

--- pojo/src/main/java/com/stanzaliving/booking/enums/BookingStatus.java
+++ pojo/src/main/java/com/stanzaliving/booking/enums/BookingStatus.java
@@ -255,6 +255,10 @@ public enum BookingStatus {
     public static Set<String> notInitiatedExitBookingStatuses(){
         Set<String> bookingStatus = new HashSet<>();
         bookingStatus.add(ONBOARDING_COMPLETED.getBookingStatus());
+        bookingStatus.add(STAY_FORFEITURE.getBookingStatus());
+        bookingStatus.add(RENT_DEFAULTER_PAID.getBookingStatus());
+        bookingStatus.add(RENT_DEFAULTER_UNPAID.getBookingStatus());
+        bookingStatus.add(CONTRACT_TERMINATED.getBookingStatus());
         return bookingStatus;
     }
 
@@ -262,6 +266,11 @@ public enum BookingStatus {
         Set<String> bookingStatus = new HashSet<>();
         bookingStatus.add(TRESSPASSER.getBookingStatus());
         bookingStatus.add(ONBOARDING_COMPLETED.getBookingStatus());
+        bookingStatus.add(CONTRACT_COMPLETED.getBookingStatus());
+        bookingStatus.add(STAY_FORFEITURE.getBookingStatus());
+        bookingStatus.add(RENT_DEFAULTER_PAID.getBookingStatus());
+        bookingStatus.add(RENT_DEFAULTER_UNPAID.getBookingStatus());
+        bookingStatus.add(CONTRACT_TERMINATED.getBookingStatus());
         return bookingStatus;
     }
 
