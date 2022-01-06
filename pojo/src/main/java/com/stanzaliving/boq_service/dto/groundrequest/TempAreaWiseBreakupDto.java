4e7c73ea96cf98c3a051896cef1b89f67753ac79
ankit.anand <ankit.anand@stanzaliving.com>
2022-01-06 19:09:02 +0530

    Temp Area Wise Breakup DTO

---
+++ pojo/src/main/java/com/stanzaliving/boq_service/dto/groundrequest/TempAreaWiseBreakupDto.java
@@ -0,0 +1,21 @@
+package com.stanzaliving.boq_service.dto.groundrequest;
+
+import com.stanzaliving.transformations.enums.AreaOfUse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TempAreaWiseBreakupDto {
+
+    private String masterItemUuid;
+    private AreaOfUse areaOfUse;
+    private BigDecimal quantity;
+
+}
