3b8f403195a870462c4205de865fcf86b1b85236
Keshav Kumar Singh <keshav.ks@robosoftin.com>
2022-01-10 14:11:08 +0530

    commit

---
+++ pojo/src/main/java/com/stanzaliving/gamificationservice/dto/mock/ProfileHistoryResponseDto.java
@@ -0,0 +1,24 @@
+package com.stanzaliving.gamificationservice.dto.mock;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileHistoryResponseDto {
+
+    @JsonProperty("history_view_all_quick_filter")
+    private List<FilterDto> historyViewAllQuickFilter;
+
+    @JsonProperty("user_contest_subtask_history_view_all")
+    private List<HistoryViewAllDto> userContestSubtaskHistoryViewAll;
+
+}
