cdb6d9e6a17005a4cb382391e986c7cebbe737f9
Vishal Mehta <vishal.mehta@nineleaps.com>
2022-01-10 11:29:00 +0530

    Implemented getKey method

--- pojo/src/main/java/com/stanzaliving/boq_service/enums/GroupByEnum.java
+++ pojo/src/main/java/com/stanzaliving/boq_service/enums/GroupByEnum.java
@@ -1,14 +1,28 @@
 package com.stanzaliving.boq_service.enums;
 
+import com.stanzaliving.boq_service.dto.PropertyBoqListingGroupByItem;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum GroupByEnum {
-    SUBCATEGORY ("Subcategory",  "getSubCategory"),
-    AREA_OF_USE ("Area of use", "getAOU");
+public enum GroupByEnum implements KeyInterface {
+    SUBCATEGORY ("Subcategory") {
+        @Override
+        public String getKey(PropertyBoqListingGroupByItem item) {
+            return item.getSubCategory();
+        }
+    },
+    AREA_OF_USE ("Area of use") {
+        @Override
+        public String getKey(PropertyBoqListingGroupByItem item) {
+            return item.getAOU();
+        }
+    };
 
     private final String label;
-    private final String  getterName;
+}
+
+interface KeyInterface {
+    String getKey(PropertyBoqListingGroupByItem item);
 }
