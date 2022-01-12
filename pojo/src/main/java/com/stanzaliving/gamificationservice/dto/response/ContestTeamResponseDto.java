f5d6b780f1cab5713734f1a507dc56f9e810b23e
Indira MR <indira.mr@rodosoftin.com>
2022-01-12 14:56:35 +0530

    teams for contests.

---
+++ pojo/src/main/java/com/stanzaliving/gamificationservice/dto/response/ContestTeamResponseDto.java
@@ -0,0 +1,25 @@
+package com.stanzaliving.gamificationservice.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ContestTeamResponseDto {
+
+    @JsonProperty("contest")
+    private List<String> contest;
+
+    @JsonProperty("teams")
+    private List<String> teams;
+
+}
