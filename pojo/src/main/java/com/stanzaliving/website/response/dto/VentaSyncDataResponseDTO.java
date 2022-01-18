cd6a6339d051eb4048bd7355c11e1e65bb967675
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-01-18 16:09:04 +0530

    code temp

--- pojo/src/main/java/com/stanzaliving/website/response/dto/VentaSyncDataResponseDTO.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/VentaSyncDataResponseDTO.java
@@ -1,5 +1,8 @@
 package com.stanzaliving.website.response.dto;
 
+import java.util.List;
+import java.util.Map;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,4 +20,14 @@ public class VentaSyncDataResponseDTO {
 	private String preBookingAmount;
 	private Integer residenceTypeId;
 	private String propertyCode;
+	
+//    private String residenceName;
+//    private Integer gender;
+//    private boolean enabled;
+    private List<Map<String,String>> occupancyPricingList;
+    private List<String> attributeNameList;
+    private List<String> consumableNameList;
+//    private Integer residenceTypeId;
+//    private String propertyCode;
+    private String residenceUuid;
 }
