88f83b0596ab35f7b7d6b1e9db1270b651a28872
Hitesh.Singh <hitesh.singh@C02ZL33HLVCF.local>
2022-01-25 15:58:52 +0530

    Mail Inbound Dto

---
+++ pojo/src/main/java/com/stanzaliving/core/mail/InboundMailInfoDto.java
@@ -0,0 +1,18 @@
+package com.stanzaliving.core.mail;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InboundMailInfoDto {
+
+    private String from;
+
+    private String body;
+
+}
