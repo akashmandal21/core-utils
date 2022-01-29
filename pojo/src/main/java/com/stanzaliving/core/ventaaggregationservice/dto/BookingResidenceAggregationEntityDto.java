3f5917336ddb6820aeab0ba0234c62c9a7955ccb
Rahul.Tiwary <rahul.kumar1@stanzaliving.com>
2022-01-30 03:54:27 +0530

    Code

--- pojo/src/main/java/com/stanzaliving/core/ventaaggregationservice/dto/BookingResidenceAggregationEntityDto.java
+++ pojo/src/main/java/com/stanzaliving/core/ventaaggregationservice/dto/BookingResidenceAggregationEntityDto.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include. NON_NULL)
 public class BookingResidenceAggregationEntityDto {
 
     private String residenceUuid;
@@ -27,14 +27,15 @@ public class BookingResidenceAggregationEntityDto {
     private Integer cityId;
 
     private String entity;
-    @Builder.Default
-    private Long totalBeds=0L;
-    @Builder.Default
-    private Long totalRooms=0L;
-    @Builder.Default
-    private Long totalApartments=0L;
+
+    private Long totalBeds;
+
+    private Long totalRooms;
+
+    private Long totalApartments;
 
     private List<String> residenceUuids;
+
 
     @Builder.Default
     private Integer liveRooms=0;
@@ -53,6 +54,7 @@ public class BookingResidenceAggregationEntityDto {
     @Builder.Default
     private Float blockedBeds=0F;
 
+    private boolean testHouse;
 }
 
 
