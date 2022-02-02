fee95f5f3752dcfd42c5cb8a5dcc9e41e4296e05
Ayushi.Sharma <ayushi.sharma@stanzaliving.com>
2022-02-02 16:09:17 +0530

    Added default BOQLabel to tag legacy boqs

--- pojo/src/main/java/com/stanzaliving/boq_service/enums/BoqLabel.java
+++ pojo/src/main/java/com/stanzaliving/boq_service/enums/BoqLabel.java
@@ -3,11 +3,22 @@ package com.stanzaliving.boq_service.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 @AllArgsConstructor
 public enum BoqLabel {
     CAPEX("Capex"),
-    OPEX("Opex");
+    OPEX("Opex"),
+    NA("NA"); // not to be used while labeling. (only used to tag legacy boqs)
 
-    private String boqLabelText;
+    private final String boqLabelText;
+
+    public static List<BoqLabel> getAllLabels() {
+        return Arrays.stream(BoqLabel.values())
+                .filter(boqLabel -> !boqLabel.equals(BoqLabel.NA))
+                .collect(Collectors.toList());
+    }
 }
