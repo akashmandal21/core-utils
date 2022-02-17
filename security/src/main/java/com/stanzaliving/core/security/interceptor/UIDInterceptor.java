841e6b704315ac821cca3711d0a1e607ffa29471
rohit.gupta <rohit.gupta@stanzaliving.com>
2022-02-17 09:19:40 +0530

    Request logging

--- security/src/main/java/com/stanzaliving/core/security/interceptor/UIDInterceptor.java
+++ security/src/main/java/com/stanzaliving/core/security/interceptor/UIDInterceptor.java
@@ -2,6 +2,7 @@ package com.stanzaliving.core.security.interceptor;
 
 import com.stanzaliving.core.base.http.interceptor.BaseUIDInterceptor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,8 @@ public class UIDInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+		log.info("Request URL %s",request.getRequestURI());
+		log.info("Remote URL %s, remote method %s and origin %s",request.getRemoteUser(),request.getMethod(),request.getHeader(HttpHeaders.ORIGIN));
 		return new BaseUIDInterceptor().preHandle(request, response, handler);
 	}
 
