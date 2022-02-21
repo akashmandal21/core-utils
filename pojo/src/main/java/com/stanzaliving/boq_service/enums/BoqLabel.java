abbc5c134209a358808872f2a28818d635259218
Nipun <nipunaggarwal16@gmail.com>
2022-02-21 20:34:20 +0530

    refactored boq labels enum

--- pojo/src/main/java/com/stanzaliving/boq_service/enums/BoqLabel.java
+++ pojo/src/main/java/com/stanzaliving/boq_service/enums/BoqLabel.java
@@ -1,49 +1,90 @@
 package com.stanzaliving.boq_service.enums;
 
-import javafx.scene.layout.BorderWidths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.ImmutableSet;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
 public enum BoqLabel {
-//    CAPEX("Capex", "expense1"),
-//    OPEX("Opex", "expense2"),
 
-    CAPEX_TRANSFORMATION("CAPEX-TRANSFORMATION", Arrays.asList("CAPEX","Rental")),
-    CAPEX_STRUCTURAL_CHANGE("CAPEX-STRUCTURAL CHANGE",Arrays.asList("CAPEX","Rental")),
-    CAPEX_MARGIN_ENHANCEMENT("CAPEX-MARGIN ENHANCEMENT",Arrays.asList("CAPEX","Rental")),
-    CAPEX_REDENSIFICATION("CAPEX-REDENSIFICATION",Arrays.asList("CAPEX","Rental")),
-    REFURBISHMENT("REFURBISHMENT",Arrays.asList("FFE")),
-    CAPEX_VAS("CAPEX-VAS",Arrays.asList("CAPEX VAS","Rental VAS")),
-    LL_DEBIT_LL_ASSET("LL DEBIT - LL ASSET",Arrays.asList("CAPEX LL Debit","Rental LL Debit")),
-    LL_DEBIT_SL_ASSET("LL DEBIT - SL ASSET",Arrays.asList("CAPEX LL Debit","Rental LL Debit")),
-    CONSULTANT_DEBIT("CONSULTANT DEBIT",Arrays.asList("Consultant Debit")),
-    CAPEX_LL_SOW_MISALIGNMENT("CAPEX - LL SOW MISALIGNMENT",Arrays.asList("CAPEX - LL SOW Misalignment")),
-    NA("NA", null); // not to be used while labeling. (only used to tag legacy boqs)
+	CAPEX_TRANSFORMATION("CAPEX-TRANSFORMATION", Arrays.asList("CAPEX", "Rental")),
+	CAPEX_STRUCTURAL_CHANGE("CAPEX-STRUCTURAL CHANGE", Arrays.asList("CAPEX", "Rental")),
+	CAPEX_MARGIN_ENHANCEMENT("CAPEX-MARGIN ENHANCEMENT", Arrays.asList("CAPEX", "Rental")),
+	CAPEX_REDENSIFICATION("CAPEX-REDENSIFICATION", Arrays.asList("CAPEX", "Rental")),
+	REFURBISHMENT("REFURBISHMENT", Arrays.asList("FFE")),
+	CAPEX_VAS("CAPEX-VAS", Arrays.asList("CAPEX VAS", "Rental VAS")),
+	LL_DEBIT_LL_ASSET("LL DEBIT - LL ASSET", Arrays.asList("CAPEX LL Debit", "Rental LL Debit")),
+	LL_DEBIT_SL_ASSET("LL DEBIT - SL ASSET", Arrays.asList("CAPEX LL Debit", "Rental LL Debit")),
+	CONSULTANT_DEBIT("CONSULTANT DEBIT", Arrays.asList("Consultant Debit")),
+	CAPEX_LL_SOW_MISALIGNMENT("CAPEX - LL SOW MISALIGNMENT", Arrays.asList("CAPEX - LL SOW Misalignment")),
+	NA("NA", Collections.emptyList()); // not to be used while labeling. (only used to tag legacy boqs)
 
-    private final String boqLabelText;
-    private final List<String> poExpenseType;
+	private final String boqLabelText;
+	private final List<String> poExpenseType;
 
-    public static List<BoqLabel> getAllLabels() {
-        return Arrays.stream(BoqLabel.values())
-                .filter(boqLabel -> !boqLabel.equals(BoqLabel.NA))
-                .collect(Collectors.toList());
-    }
+	private static final Map<BoqLabel, List<String>> boqLabelExpenseTypeMap = new HashMap<BoqLabel, List<String>>();
+	private static final Map<String, Set<BoqLabel>> expenseTypeBoqLabelMap = new HashMap<String, Set<BoqLabel>>();
 
-    public static List<BoqLabel>  getLabelsForExpenseType(String expenseType){
-        List<BoqLabel> boqList=new ArrayList<BoqLabel>();
-        boqList.add(BoqLabel.NA);
-        for(BoqLabel bl:BoqLabel.getAllLabels()){
-            if(bl.getPoExpenseType().contains(expenseType)){
-                boqList.add(bl);
-            }
-        }
-        return boqList;
-    }
+	static {
+		populateBoqLabelExpenseTypeMap();
+		populateExpenseTypeBoqLabelMap();
+	}
+
+	/**
+	 * 
+	 */
+	private static void populateBoqLabelExpenseTypeMap() {
+
+		Arrays.stream(BoqLabel.values()).forEach(f -> boqLabelExpenseTypeMap.put(f, f.getPoExpenseType()));
+	}
+
+	/**
+	 * 
+	 */
+	private static void populateExpenseTypeBoqLabelMap() {
+
+		Arrays.stream(BoqLabel.values()).forEach(f -> {
+			BoqLabel boqLabel = f;
+			List<String> expenseTypes = f.getPoExpenseType();
+
+			for (String expenseType : expenseTypes) {
+				if (!expenseTypeBoqLabelMap.containsKey(expenseType)) {
+					expenseTypeBoqLabelMap.put(expenseType, ImmutableSet.of(boqLabel));
+				} else {
+					expenseTypeBoqLabelMap.get(expenseType).add(boqLabel);
+				}
+			}
+		});
+	}
+
+	public static List<BoqLabel> getAllLabels() {
+		return Arrays.stream(BoqLabel.values())
+				.filter(boqLabel -> !boqLabel.equals(BoqLabel.NA))
+				.collect(Collectors.toList());
+	}
+
+	public static Set<BoqLabel> getBoqLabelsForExpenseType(String expenseType) {
+
+		return expenseTypeBoqLabelMap.getOrDefault(expenseType, new HashSet<BoqLabel>());
+	}
+
+	public static List<BoqLabel> getLabelsForExpenseType(String expenseType) {
+
+		List<BoqLabel> boqLabels = getBoqLabelsForExpenseType(expenseType).stream().collect(Collectors.toList());
+		boqLabels.add(BoqLabel.NA);
+
+		return boqLabels;
+	}
+
 }
