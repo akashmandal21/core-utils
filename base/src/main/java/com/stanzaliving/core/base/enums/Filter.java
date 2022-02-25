5cfa0519877be941f9622f616fa3d7d0606d1f22
Ayushi720 <ayushi.dwivedi@unthinkable.co>
2022-02-25 14:28:04 +0530

    Filter enum added

---
+++ base/src/main/java/com/stanzaliving/core/base/enums/Filter.java
@@ -0,0 +1,11 @@
+package com.stanzaliving.core.base.enums;
+
+import java.util.Collection;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public enum Filter {
+    Q_A,
+    MANAGER,
+    ROLE
+}
