51ffb86372f65e870bf746af4ec678858a73d734
Santhosh3579 <82016500+Santhosh3579@users.noreply.github.com>
2022-02-28 13:44:03 +0530

    added client api to fetch AddressBook

--- transformation-client/src/main/java/com/stanzaliving/core/transformation/client/api/InternalDataControllerApi.java
+++ transformation-client/src/main/java/com/stanzaliving/core/transformation/client/api/InternalDataControllerApi.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
+import com.stanzaliving.swim.AddressBookDto;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -1282,4 +1283,29 @@ public class InternalDataControllerApi {
         return null;
     }
 
+    public ResponseDto<List<AddressBookMetaDto>> getAllAddressBookInfo() {
+
+
+
+        Object postBody = null;
+
+        // create path and map variables
+        final Map<String, Object> uriVariables = new HashMap<>();
+
+        String path = UriComponentsBuilder.fromPath("/internal/get/addresses/all").buildAndExpand(uriVariables).toUriString();
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+        final HttpHeaders headerParams = new HttpHeaders();
+
+        final String[] accepts = {
+                "*/*"
+        };
+        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+        ParameterizedTypeReference<ResponseDto<List<AddressBookMetaDto>>> returnType = new ParameterizedTypeReference<ResponseDto<List<AddressBookMetaDto>>>() {
+        };
+        return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
+
+    }
 }
