f19339125d0e88fa3f9f0948b2b060ac38a62dfe
Ayushi720 <ayushi.dwivedi@unthinkable.co>
2022-03-01 19:20:51 +0530

    Filters on Property Listing Screen -FilterDisplayType enum added -display type added in response dto

--- pojo/src/main/java/com/stanzaliving/estate_v2/dto/reviewTemplate/filter/DynamicFilterResponseDTO.java
+++ pojo/src/main/java/com/stanzaliving/estate_v2/dto/reviewTemplate/filter/DynamicFilterResponseDTO.java
@@ -14,5 +14,6 @@ import java.util.List;
 @Setter
 public class DynamicFilterResponseDTO {
     private String dataSource;
+    private String displayType;
     private List<DynamicFiltersAnswersDTO> values;
 }
--- pojo/src/main/java/com/stanzaliving/estate_v2/dto/reviewTemplate/filter/DynamicFiltersAnswersDTO.java
+++ pojo/src/main/java/com/stanzaliving/estate_v2/dto/reviewTemplate/filter/DynamicFiltersAnswersDTO.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Comparator;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,4 +16,17 @@ public class DynamicFiltersAnswersDTO {
     String id;
     String value;
     Integer count;
+
+    public static Comparator<DynamicFiltersAnswersDTO> AnswersComparator = (answer1, answer2) -> {
+
+        String DynamicFiltersAnswersDTOName1
+                = answer1.getValue().toUpperCase();
+        String DynamicFiltersAnswersDTOName2
+                = answer2.getValue().toUpperCase();
+
+        // ascending order
+        return DynamicFiltersAnswersDTOName1.compareTo(
+                DynamicFiltersAnswersDTOName2);
+    };
+
 }
