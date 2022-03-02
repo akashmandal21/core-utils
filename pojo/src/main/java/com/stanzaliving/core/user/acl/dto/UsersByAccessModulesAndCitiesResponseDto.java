0b72164d1f8287ae5eba4bd2466cddaecf6cef90
Harpreet Singh <harpreet.singh@unthinkable.co>
2022-03-02 14:46:21 +0530

    Modified Dto

--- pojo/src/main/java/com/stanzaliving/core/user/acl/dto/UsersByAccessModulesAndCitiesResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/core/user/acl/dto/UsersByAccessModulesAndCitiesResponseDto.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,5 +16,5 @@ public class UsersByAccessModulesAndCitiesResponseDto {
     
     private String accessLevelEntityUuid;
     private String accessLevelEntityName;
-    private UserProfileDto userProfileDto;
+    private List<UserProfileDto> userProfileDtoList;
 }
