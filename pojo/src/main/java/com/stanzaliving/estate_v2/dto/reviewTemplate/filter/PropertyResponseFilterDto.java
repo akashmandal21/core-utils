5d17117fece57f0dfd8be874de1222092c94bf88
Bansal-Ansh <ansh.bansal@unthinkable.co>
2022-03-07 19:00:37 +0530

    Change In Property Response FilterDTO As per the requirement of frontend

--- pojo/src/main/java/com/stanzaliving/estate_v2/dto/reviewTemplate/filter/PropertyResponseFilterDto.java
+++ pojo/src/main/java/com/stanzaliving/estate_v2/dto/reviewTemplate/filter/PropertyResponseFilterDto.java
@@ -1,12 +1,10 @@
 package com.stanzaliving.estate_v2.dto.reviewTemplate.filter;
 
 import com.stanzaliving.core.leadership.dto.DateRange;
-import com.stanzaliving.core.leadership.dto.PropertyLocationFilter;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -18,6 +16,6 @@ public class PropertyResponseFilterDto {
     private List<String> propertyStatusName;
     private String city;
     private String microMarket;
-    private DateRange createdDateBetween;
+    private DateRange createdBetween;
     private DateRange propertyNamingBetween;
 }
