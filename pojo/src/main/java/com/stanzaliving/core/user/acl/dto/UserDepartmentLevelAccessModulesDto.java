ef5cf71242a8645c40609311387807e3fb5cbda3
Harpreet Singh <harpreet.singh@unthinkable.co>
2022-03-08 13:27:31 +0530

    Added new key

--- pojo/src/main/java/com/stanzaliving/core/user/acl/dto/UserDepartmentLevelAccessModulesDto.java
+++ pojo/src/main/java/com/stanzaliving/core/user/acl/dto/UserDepartmentLevelAccessModulesDto.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class UserDepartmentLevelAccessModulesDto {
     
     private AccessModule accessModule;
+    private String accessModuleName;
     private String roleUuid;
     private String userDepartmentLevelUuid;
     private AccessLevel accessLevel;
