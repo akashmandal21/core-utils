500f4245ed921dcc46df89af907d602ecfccbb75
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-03-22 15:40:16 +0530

    Create UserInterestSelectionDto.java

---
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/request/UserInterestSelectionDto.java
@@ -0,0 +1,22 @@
+package com.stanzaliving.wanda.request;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInterestSelectionDto {
+	
+	private List<Long> selectedCategoryIds;
+	private List<Long> selectedInterestIds;
+}
