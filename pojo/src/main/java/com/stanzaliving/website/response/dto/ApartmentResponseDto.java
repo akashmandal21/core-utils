17a1384b0b420ac70611887ea462f01e6faf7b3e
AgyeyaNarayan <agyeya.narayan@stanzaliving.com>
2022-03-23 14:00:14 +0530

    Update ApartmentResponseDto.java

--- pojo/src/main/java/com/stanzaliving/website/response/dto/ApartmentResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/ApartmentResponseDto.java
@@ -96,8 +96,6 @@ public class ApartmentResponseDto implements Serializable, Comparable<ApartmentR
 	
 	private Date created;
 	
-	private Integer imageCount;
-	
 	private List<ApartmentResponseDto> similarApartments;
 	
 	@Builder.Default
