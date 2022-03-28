245ecd4e27e2d7438bc689b97b4dfae5fb8fe1c2
Rahul.Tiwary <rahul.kumar1@stanzaliving.com>
2022-03-28 20:16:13 +0530

    code

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/enums/AlfredStep.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/enums/AlfredStep.java
@@ -54,7 +54,7 @@ public enum AlfredStep {
 	}
 	
 	static {
-		retentionNonBlockerStatus.addAll(Arrays.asList("REFUND INITIATED","ONBOARDING COMPLETED",
+		retentionNonBlockerStatus.addAll(Arrays.asList("EXPIRED","CANCELLED","DRAFT","IN_PROGRESS","REFUND INITIATED","ONBOARDING COMPLETED",
 				"VERIFICATION PENDING","ONBOARDING PENDING", "ONBOARDING IN PROGRESS"));
 	}
 
