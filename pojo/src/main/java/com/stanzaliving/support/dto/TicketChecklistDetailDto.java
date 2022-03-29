0c125328af51170211a351dabb701f194df75e6e
manishpareek <manishpareek.retainer@stanzaliving.com>
2022-03-29 18:54:41 +0530

    Update TicketChecklistDetailDto.java

--- pojo/src/main/java/com/stanzaliving/support/dto/TicketChecklistDetailDto.java
+++ pojo/src/main/java/com/stanzaliving/support/dto/TicketChecklistDetailDto.java
@@ -18,7 +18,7 @@ public class TicketChecklistDetailDto {
     private Boolean isMarkedNa;
     private Boolean isClosed;
     private Boolean isMandatory;
-    private Integer position;
+  //  private Integer position;
 //    private String attachmentLocation;
 //    private String attachmentType;
 //    private long fileSize;
