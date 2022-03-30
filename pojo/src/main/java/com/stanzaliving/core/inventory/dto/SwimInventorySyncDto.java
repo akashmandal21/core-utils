15fe6bfaecc15f7d5b646f1cb0920d165ea82766
Vivek.Parihar <vivek.parihar@Viveks-MacBook.local>
2022-03-30 18:35:07 +0530

    Added dummy addressFeild

--- pojo/src/main/java/com/stanzaliving/core/inventory/dto/SwimInventorySyncDto.java
+++ pojo/src/main/java/com/stanzaliving/core/inventory/dto/SwimInventorySyncDto.java
@@ -16,6 +16,7 @@ import java.util.Map;
 public class SwimInventorySyncDto {
 
     private String addressUuid;
+    private String dummyAddressUuid;
     private Map<String, ItemStatusDetail> itemStatusDetailMap;
 
     @Getter
