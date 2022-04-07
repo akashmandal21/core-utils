f0142816cf0cffa4d78da1b9040396d5b2fa7cb1
Prashant-unthinkable <prashant.chand@unthinkable.co>
2022-04-07 18:22:19 +0530

    added enum for food-service in ExceptionMarker

--- base/src/main/java/com/stanzaliving/core/base/exception/ExceptionMarker.java
+++ base/src/main/java/com/stanzaliving/core/base/exception/ExceptionMarker.java
@@ -6,7 +6,18 @@ public enum ExceptionMarker {
      DS002("Leftover submission failed "),
      DS003("No Data found"),
      DS004("No dispatched meal details found to initiate"),
-     DS005("No Food dispatched meal details found");
+     DS005("No Food dispatched meal details found"),
+     FS001("Unable to update dispatch summary"),
+     FS002("Unable to receive food"),
+     FS003("Failed to generate menu group"),
+     FS004("Failed to capture request"),
+     FS005("Failed to update status"),
+     FS006("Failed to generate QR"),
+     FS007("This is not a valid Stanza QR code."),
+     FS008("Failed to update user hostel"),
+     FS009("No Active Meal found for hostel"),
+     FS010("Fail to add special event in the calendar"),
+     FS011("Monthly budget planner details not found");
 
     private String message;
 
