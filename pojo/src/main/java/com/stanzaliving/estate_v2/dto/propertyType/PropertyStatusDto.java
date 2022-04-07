392c2e925d3a539f3d6a44316e3578f88dd59e3c
Bansal-Ansh <ansh.bansal@unthinkable.co>
2022-04-07 16:29:48 +0530

    Timer to be assigned to every approval stage -Initial Commit -Implemented timer functionality for all the statuses

--- pojo/src/main/java/com/stanzaliving/estate_v2/dto/propertyType/PropertyStatusDto.java
+++ pojo/src/main/java/com/stanzaliving/estate_v2/dto/propertyType/PropertyStatusDto.java
@@ -2,10 +2,12 @@ package com.stanzaliving.estate_v2.dto.propertyType;
 
 
 import com.stanzaliving.estate_v2.annotation.constraint.EmptyOrNull;
+import com.stanzaliving.estate_v2.dto.ReportToDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.ReportAsSingleViolation;
 import javax.validation.constraints.Pattern;
 import java.util.List;
 
@@ -31,4 +33,7 @@ public class PropertyStatusDto {
 
     private List<String> moduleVisible;
     private String statusDescription;
+    private Boolean isTimerEnabled;
+    private int deadlineForTimer;
+    private List<ReportToDTO> reportTo;
 }
