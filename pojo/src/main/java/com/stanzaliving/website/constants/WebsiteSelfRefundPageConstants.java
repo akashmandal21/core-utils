beefe019eabac5f33e8ca0194a4ec0d49fefe213
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-04-28 00:35:50 +0530

    Update WebsiteSelfRefundPageConstants.java

--- pojo/src/main/java/com/stanzaliving/website/constants/WebsiteSelfRefundPageConstants.java
+++ pojo/src/main/java/com/stanzaliving/website/constants/WebsiteSelfRefundPageConstants.java
@@ -12,7 +12,7 @@ public class WebsiteSelfRefundPageConstants {
 	public final String SELF_REFUND_REQUEST_FAILED_MESSAGE = "Unfortunately, your refund request was failed. We've initiatied another refund request. You'll receive the amount in 14 working days.";
 
 	public final String REFUND_SUCCESS_MESSAGE = "Refund successfully processed and the amount will reflect in your account in 14 days";
-	public final String CONTACT_STANZA_SUPPORT_MESSAGE = "Please contact stanza support team";
+	public final String CONTACT_STANZA_SUPPORT_MESSAGE = "Please contact stanza support team at operations@stanzaliving.com";
 	
 	public final String REFUND_INITIATED_MESSAGE = "Refund initiated successfully";
 
