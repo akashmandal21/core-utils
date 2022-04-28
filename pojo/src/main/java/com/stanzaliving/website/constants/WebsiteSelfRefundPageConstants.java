5a1de8297bb49b802f7c3a9e2f8380e06b0f1181
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-04-29 02:55:48 +0530

    Update WebsiteSelfRefundPageConstants.java

--- pojo/src/main/java/com/stanzaliving/website/constants/WebsiteSelfRefundPageConstants.java
+++ pojo/src/main/java/com/stanzaliving/website/constants/WebsiteSelfRefundPageConstants.java
@@ -5,15 +5,25 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class WebsiteSelfRefundPageConstants {
 
-	public final String SELF_REFUND_REQUEST_NOT_RAISED_MESSAGE = "You haven't raised any request for refund";
-	public final String ZERO_PREBOOKING_TRANSACTION_FOUND_MESSAGE = "You haven't prebooked yet";
-	public final String PREBOOKING_AMOUNT_ADJUSTED_IN_BOOKING_MESSAGE = "You do not have any pre-booking transaction available for refund as its already has been adjusted in your booking";
-	public final String SELF_REFUND_REQUEST_ALREADY_RAISED_MESSAGE = "You have already raised a refund request against your prebooking transaction and the amount will reflect in your account in 14 days";
-	public final String SELF_REFUND_REQUEST_FAILED_MESSAGE = "Unfortunately, your refund request was failed. We've initiatied another refund request. You'll receive the amount in 14 working days.";
+	public final String SELF_REFUND_REQUEST_NOT_RAISED_MESSAGE = "You haven’t raised any request for a refund for:";
+
+	public final String ZERO_PREBOOKING_TRANSACTION_FOUND_MESSAGE = "No bookings found in your name."
+			+ System.lineSeparator()
+			+ "Don't worry, it's not too late. Initiate the pre-booking process by clicking here.";
+
+	public final String PREBOOKING_AMOUNT_ADJUSTED_IN_BOOKING_MESSAGE = "The pre-booking amount you're requesting for refund has already been adjusted in your confirmed booking.";
+
+	public final String SELF_REFUND_REQUEST_ALREADY_RAISED_MESSAGE = "Your refund request already exists (Reference Number: 00000)."
+			+ System.lineSeparator() + "The amount will reach your account within the next 14 days.";
+
+	public final String SELF_REFUND_REQUEST_FAILED_MESSAGE = "Something went wrong. We are re-initiating your refund request and you'll receive the amount within the next 14 days.";
 
 	public final String REFUND_SUCCESS_MESSAGE = "Refund successfully processed and the amount will reflect in your account in 14 days";
-	public final String CONTACT_STANZA_SUPPORT_MESSAGE = "Please contact stanza support team";
 	
-	public final String REFUND_INITIATED_MESSAGE = "Refund initiated successfully";
+	public final String CONTACT_STANZA_SUPPORT_MESSAGE = "Please contact stanza support team at operations@stanzaliving.com";
+
+	public final String REFUND_INITIATED_MESSAGE = "Refund initiated successfully and you'll receive the amount within the next 14 days.";
+
+	public final String REFUND_PENDING_POST_14_DAYS = "Your refund request is still in process. Please write to us at operations@stanzaliving.com for further assistance.";
 
 }
