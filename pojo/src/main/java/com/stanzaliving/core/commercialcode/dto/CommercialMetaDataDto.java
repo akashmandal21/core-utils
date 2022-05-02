47ebce19266850dd07e38fe0a43e3b21377edcc7
rakshitKashyap <rakshit.k@urbanladder.com>
2022-05-02 16:28:18 +0530

    for pricing strategy in booking in update

--- pojo/src/main/java/com/stanzaliving/core/commercialcode/dto/CommercialMetaDataDto.java
+++ pojo/src/main/java/com/stanzaliving/core/commercialcode/dto/CommercialMetaDataDto.java
@@ -3,11 +3,14 @@ package com.stanzaliving.core.commercialcode.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Builder
 public class CommercialMetaDataDto {
     private String label;
     private String value;
     @Builder.Default
-    private String note = "[]";
+    private List note=new ArrayList<String>();
 }
