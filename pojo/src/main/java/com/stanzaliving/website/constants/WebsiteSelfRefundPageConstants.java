3cbc1272f6f22688ef40255ad9054cfac5d73fa5
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-05-05 15:16:11 +0530

    Update WebsiteSelfRefundPageConstants.java

--- pojo/src/main/java/com/stanzaliving/website/constants/WebsiteSelfRefundPageConstants.java
+++ pojo/src/main/java/com/stanzaliving/website/constants/WebsiteSelfRefundPageConstants.java
@@ -9,14 +9,11 @@ public class WebsiteSelfRefundPageConstants {
 
 	public final String SELF_REFUND_REQUEST_NOT_RAISED_MESSAGE = "You haven’t raised any request for a refund for:";
 
-	public final String ZERO_PREBOOKING_TRANSACTION_FOUND_MESSAGE = "No bookings found in your name."
-			+ System.lineSeparator()
-			+ "Don't worry, it's not too late. Initiate the pre-booking process by clicking here.";
+	public final String ZERO_PREBOOKING_TRANSACTION_FOUND_MESSAGE = "No bookings found in your name.<br />Don't worry, it's not too late. Initiate the pre-booking process by clicking here.";
 
 	public final String PREBOOKING_AMOUNT_ADJUSTED_IN_BOOKING_MESSAGE = "The pre-booking amount you're requesting for refund has already been adjusted in your confirmed booking.";
 
-	public final String SELF_REFUND_REQUEST_ALREADY_RAISED_MESSAGE = "Your refund request already exists (Reference Number: %s)."
-			+ System.lineSeparator() + "The amount will reach your account within the next %s days.";
+	public final String SELF_REFUND_REQUEST_ALREADY_RAISED_MESSAGE = "Your refund request already exists (Reference Number: %s).<br />The amount will reach your account within the next %s days.";
 
 	public final String SELF_REFUND_REQUEST_FAILED_MESSAGE = "Something went wrong. We are re-initiating your refund request and you'll receive the amount within the next %s days.";
 
