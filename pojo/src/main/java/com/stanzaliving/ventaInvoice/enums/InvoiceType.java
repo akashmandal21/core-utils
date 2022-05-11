3d4a4a888c476e5f6fd153088b8fd9236918a966
Muquitur.Rahman <muquitur.rahman@C02XP1WEJG5H.local>
2022-05-11 14:21:46 +0530

    code

--- pojo/src/main/java/com/stanzaliving/ventaInvoice/enums/InvoiceType.java
+++ pojo/src/main/java/com/stanzaliving/ventaInvoice/enums/InvoiceType.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 @Getter
 public enum InvoiceType {
     INVOICE("Invoice"),
-    CREDIT_NOTE("Credit Note"),
-    CANCELLATION_CHARGE("Cancellation Charge");
+    CREDIT_NOTE("Credit Note");
     private final String values;
 }
