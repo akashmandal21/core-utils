7f42b64e70d16a6bd789227715ae7cfc2f1b21cd
Hitesh.Singh <hitesh.singh@C02ZL33HLVCF.local>
2022-05-11 12:34:59 +0530

    adding approval service client

---
+++ pojo/src/main/java/com/stanzaliving/sfr/dto/AsisEntityDto.java
@@ -0,0 +1,20 @@
+package com.stanzaliving.sfr.dto;
+
+import com.stanzaliving.core.asis.enums.SiteVisitStatus;
+import com.stanzaliving.core.projectservice.enums.AsIsStatus;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AsisEntityDto {
+
+    private String propertyUuid;
+
+    private String moduleUuid;
+
+    private String questionTemplateId;
+    
+}
