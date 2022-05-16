5a576726a657a8c80906f232ae2aa7ff0a69ee3f
AgyeyaNarayan <agyeya.narayan@stanzaliving.com>
2022-05-16 18:20:30 +0530

    Update EnumFilterType.java

--- pojo/src/main/java/com/stanzaliving/website/enums/EnumFilterType.java
+++ pojo/src/main/java/com/stanzaliving/website/enums/EnumFilterType.java
@@ -2,5 +2,5 @@ package com.stanzaliving.website.enums;
 
 public enum EnumFilterType {
 
-	RADIO_BUTTON, CHECK_BOX, SLIDER, TOGGLE, BUTTON, DROPDOWN, CLICKABLE;
+	RADIO_BUTTON, CHECK_BOX, SLIDER, TOGGLE, BUTTON, CLICKABLE;
 }
