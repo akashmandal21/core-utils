fd2cb3275f31d40503739c5e2997f1afa0842d32
Santhosh3579 <82016500+Santhosh3579@users.noreply.github.com>
2022-05-26 00:13:28 +0530

    .

--- generic-validation-core/src/main/java/com/stanzaliving/core/generic/validation/enums/FieldOptionProvider.java
+++ generic-validation-core/src/main/java/com/stanzaliving/core/generic/validation/enums/FieldOptionProvider.java
@@ -26,7 +26,6 @@ public enum FieldOptionProvider {
 
     voucherTypeProvider,categoryProvider,verticalProvider,groupProvider,subGroupProvider,expenseTypeProvider,taxCodeProvider,tdsSectionProvider,tdsRateProvider,
 
-    FarCategoryProvider, HsnOrSacCodesProvider, ConsumableTagProvider,
-    poBOQLabelProvider
+    FarCategoryProvider, HsnOrSacCodesProvider, ConsumableTagProvider
 
 }
