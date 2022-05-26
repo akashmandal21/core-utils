53e145bf30981d6e7c19ddb8139cb3cae975d162
chicki9 <chahat.aggarwal@stanzaliving.com>
2022-05-26 18:59:42 +0530

    self onboarding changes

---
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/request/UpdateBookingDto.java
@@ -0,0 +1,13 @@
+package com.stanzaliving.wanda.venta.request;
+
+import com.stanzaliving.booking.enums.BookingSource;
+import com.stanzaliving.booking.enums.BookingStatus;
+import com.stanzaliving.booking.enums.BookingSubStatus;
+
+public class UpdateBookingDto {
+
+    private String userUuid;
+    private BookingStatus bookingStatus;
+    private BookingSource bookingSource;
+    private BookingSubStatus bookingSubStatus;
+}
