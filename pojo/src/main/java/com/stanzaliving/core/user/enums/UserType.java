a888ff888e43dd3781d3964cea106badadfe890e
yuktigupta2510 <yukti.gupta@stanzaliving.com>
2022-05-31 10:34:15 +0530

    added last transaction

--- pojo/src/main/java/com/stanzaliving/core/user/enums/UserType.java
+++ pojo/src/main/java/com/stanzaliving/core/user/enums/UserType.java
@@ -37,7 +37,8 @@ public enum UserType {
 	CENTRAL_TEAM("Central Team"),
 	EXTERNAL("External"),
 	GUEST("Guest"),
-	FOOD_DELIVERY_AGENT("Food Delivery Agent");
+    INVITED_GUEST("Invited Guest"),
+    FOOD_DELIVERY_AGENT("Food Delivery Agent");
 
 	public String typeName;
 
