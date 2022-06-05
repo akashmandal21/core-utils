55f84296bb4ab0a00851a45642f2504509bbe9b0
yuktigupta2510 <yukti.gupta@stanzaliving.com>
2022-06-05 11:44:18 +0530

    added bookings save

--- pojo/src/main/java/com/stanzaliving/booking/enums/BookingStatus.java
+++ pojo/src/main/java/com/stanzaliving/booking/enums/BookingStatus.java
@@ -168,6 +168,9 @@ public enum BookingStatus {
         bookingStatus.add(BookingStatus.ONBOARDING_PENDING.getDescription());
         bookingStatus.add(BookingStatus.ONBOARDING_IN_PROGRESS.getDescription());
         bookingStatus.add(BookingStatus.ONBOARDING_COMPLETED.getDescription());
+        bookingStatus.add(BookingStatus.ONBOARDING_COMPLETED.getDescription());
+        bookingStatus.add(BookingStatus.GUEST_ONBOARDING_PENDING.getDescription());
+        bookingStatus.add(BookingStatus.GUEST_ONBOARDING_COMPLETED.getDescription());
         return bookingStatus;
     }
 
@@ -177,6 +180,9 @@ public enum BookingStatus {
         bookingStatus.add(BookingStatus.ONBOARDING_PENDING.getDescription());
         bookingStatus.add(BookingStatus.ONBOARDING_IN_PROGRESS.getDescription());
         bookingStatus.add(BookingStatus.ONBOARDING_COMPLETED.getDescription());
+        bookingStatus.add(BookingStatus.GUEST_AGREEMENT_SENT.getDescription());
+        bookingStatus.add(BookingStatus.GUEST_ONBOARDING_PENDING.getDescription());
+        bookingStatus.add(BookingStatus.GUEST_ONBOARDING_COMPLETED.getDescription());
         return bookingStatus;
     }
 
