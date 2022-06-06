5a1fcdd89064828058dafcd424d7ccc07038ca43
chicki9 <chahat.aggarwal@stanzaliving.com>
2022-06-06 12:01:21 +0530

    added setter

--- pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/request/UpdateBookingDto.java
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/venta/request/UpdateBookingDto.java
@@ -3,7 +3,9 @@ package com.stanzaliving.wanda.venta.request;
 import com.stanzaliving.booking.enums.BookingSource;
 import com.stanzaliving.booking.enums.BookingStatus;
 import com.stanzaliving.booking.enums.BookingSubStatus;
+import lombok.Setter;
 
+@Setter
 public class UpdateBookingDto {
 
     private String userUuid;
