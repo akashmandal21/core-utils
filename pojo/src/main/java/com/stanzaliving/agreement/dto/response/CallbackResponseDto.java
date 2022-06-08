d03583f0e1a31e988fccdec5284e8e68a1df0059
Shivam Kesarwani <shivam.kesarwani@unthinkable.co>
2022-06-08 19:24:27 +0530

    changed list of string for files field

--- pojo/src/main/java/com/stanzaliving/agreement/dto/response/CallbackResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/agreement/dto/response/CallbackResponseDto.java
@@ -2,6 +2,8 @@ package com.stanzaliving.agreement.dto.response;
 
 import lombok.*;
 
+import java.util.List;
+
 @Builder
 @Getter
 @Setter
@@ -11,7 +13,7 @@ import lombok.*;
 public class CallbackResponseDto {
     private RequestDto request;
     private String irn;
-    private String files;
+    private List<String> files;
     private String documentId;
     private String mac;
     private VerificationDto verification;
