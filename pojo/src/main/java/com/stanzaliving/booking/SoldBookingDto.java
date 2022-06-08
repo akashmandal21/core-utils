254d210e7bfaa3b020b8bc105ecfac9b27ff6c7c
guruvachan jain <guruvachan.jain@stanzaliving.com>
2022-06-08 11:48:49 +0530

    change data type of lockIn duration

--- pojo/src/main/java/com/stanzaliving/booking/SoldBookingDto.java
+++ pojo/src/main/java/com/stanzaliving/booking/SoldBookingDto.java
@@ -17,7 +17,7 @@ public class SoldBookingDto {
     String roomUuid;
     Date contractStartDate;
     Date lockInEndDate;
-    Double lockInDuration;
+    Integer lockInDuration;
     Double beds;
     List<String> packageService;
 }
