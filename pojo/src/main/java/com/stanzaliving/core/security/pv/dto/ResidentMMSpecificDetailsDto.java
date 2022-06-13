ae57563053545613d211fef3670c971d5dc4935d
manishpareek <manishpareek.retainer@stanzaliving.com>
2022-06-13 05:50:16 +0530

    KycDocumentClient Added

---
+++ pojo/src/main/java/com/stanzaliving/core/security/pv/dto/ResidentMMSpecificDetailsDto.java
@@ -0,0 +1,15 @@
+package com.stanzaliving.core.security.pv.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResidentMMSpecificDetailsDto {
+
+	private KarnatakaPVDetailsDto karnatakaPVDetailsDto;
+}
