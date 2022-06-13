08961442a316cca781be6ce6f1d6d2e55ebb62a4
Prateek Ladha <>
2022-06-14 01:51:12 +0530

    Fomo tag - ONLYFIFTEENLEFT added

--- pojo/src/main/java/com/stanzaliving/website/enums/FomoTag.java
+++ pojo/src/main/java/com/stanzaliving/website/enums/FomoTag.java
@@ -1,5 +1,7 @@
 package com.stanzaliving.website.enums;
 
+import org.apache.commons.lang3.StringUtils;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,12 +9,26 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum FomoTag {
 
-	ALMOSTFULL("Almost Full", "#283264"), 
+	ALMOSTFULL("Almost Full", "#283264"),
 	TOPSELLING("Top Selling", "#645A87"),
 	FILLINGFAST("Filling Fast", "#FAB432"),
 	SOLDOUT("Sold Out", "#F05A78"),
-	NONE("None","#7D7A7A");
+	NONE("None","#7D7A7A"),
+	ONLYFIFTEENLEFT("Only 15 beds left","#FFF1F1");
 
 	public String fomoTagName;
 	public String fomoTagColour;
+
+	public static FomoTag fromString(String fomoTagName) {
+
+		if (StringUtils.isBlank(fomoTagName))
+			return null;
+
+		for (FomoTag fomoTag : FomoTag.values()) {
+			if (fomoTag.getFomoTagName().equalsIgnoreCase(fomoTagName)) {
+				return fomoTag;
+			}
+		}
+		return null;
+	}
 }
