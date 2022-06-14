f4b2bb2aaa770460b95215c3e5e502454c630b72
PruthiGarima <75316799+PruthiGarima@users.noreply.github.com>
2022-06-14 11:18:17 +0530

    cash recon changes

---
+++ pojo/src/main/java/com/stanzaliving/ledger/dto/CashReconLineItemResponseDTO.java
@@ -0,0 +1,43 @@
+package com.stanzaliving.ledger.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CashReconLineItemResponseDTO {
+
+    private Date date;
+
+    private String formattedDate;
+
+    private String title;
+
+    private String subTitle;
+
+    private String transactionType;
+
+    private Double amount;
+
+    private Double balance;
+
+    private String transactionColorCode;
+
+    public interface TRANSACTION_TYPE_COLOR {
+        String CREDIT_COLOR = "#F55F71";
+        String DEBIT_COLOR = "#60C3AD";
+        String PENDING_APPROVAL = "FFB701";
+    }
+
+}
+
+
