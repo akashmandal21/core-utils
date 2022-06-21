2dcfc5cc330c5ef635774b13a77bec35b0e7ab58
Amrit Patel <amrit.patel@stanzaliving.com>
2022-06-21 22:32:08 +0530

    JAR-396: config dto validation

--- pojo-ops/src/main/java/com/stanzaliving/core/security/policeverification/dto/PVConfigDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/security/policeverification/dto/PVConfigDto.java
@@ -8,6 +8,9 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @ToString(callSuper = true)
@@ -15,12 +18,16 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 public class PVConfigDto {
 
+    @NotBlank(message = "City Name is mandatory")
     private String cityName;
 
+    @NotBlank(message = "Micromarket Name is mandatory")
     private String micromarketName;
 
+    @NotNull(message = "Registration Type Type is mandatory")
     private RegistrationType registrationType;
 
+    @NotNull(message = "PVApplicable Type is mandatory")
     private PVApplicable pvApplicableType;
 
     private boolean panOrAadhaarRequired;
