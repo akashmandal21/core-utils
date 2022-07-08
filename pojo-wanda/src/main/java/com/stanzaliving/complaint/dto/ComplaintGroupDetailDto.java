d636f2e0318db0822503fa4716c6bd83deeb3960
Bhupendra.Varshney <bhupendra.varshney@ip-192-168-1-27.ap-south-1.compute.internal>
2022-07-08 11:21:27 +0530

    add new pojo

---
+++ pojo-wanda/src/main/java/com/stanzaliving/complaint/dto/ComplaintGroupDetailDto.java
@@ -0,0 +1,23 @@
+package com.stanzaliving.complaint.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class ComplaintGroupDetailDto {
+    private List<ComplaintInfo> complaintGroup;
+
+    @Data
+    public static class ComplaintInfo{
+        private String label;
+        private String key;
+        private List<ComplaintInfo> value;
+    }
+}
