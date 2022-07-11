7bf9ed04a4c8533b1bec14ef7c7dbd308b4dbe7d
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-07-11 16:38:54 +0530

    Create WebsiteSearchResultsDto.java

---
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/WebsiteSearchResultsDto.java
@@ -0,0 +1,34 @@
+package com.stanzaliving.website.response.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.stanzaliving.website.elasticsearch.index.dto.WebsitePlaceIndexDto;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+import java.util.List;
+
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class WebsiteSearchResultsDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@JsonInclude(value = Include.NON_NULL)
+	private List<ResidenceListingCardDto> residenceResponseShortDTOsPg;
+	
+	@JsonInclude(value = Include.NON_NULL)
+	private List<ResidenceListingCardDto> residenceResponseShortDTOsCoLiving;
+	
+	@JsonInclude(value = Include.NON_NULL)
+	private List<ApartmentListingCardDto> apartments;
+
+	private boolean isApartmentPage;
+	
+	private WebsitePlaceIndexDto placeIndexDto;
+}
