2823ad3b81305a4173a26be9bd48eb694f77bd33
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-07-12 17:45:43 +0530

    Update WebsiteSearchResultsDto.java

--- pojo/src/main/java/com/stanzaliving/website/response/dto/WebsiteSearchResultsDto.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/WebsiteSearchResultsDto.java
@@ -1,22 +1,27 @@
 package com.stanzaliving.website.response.dto;
 
+import java.io.Serializable;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.stanzaliving.core.base.common.dto.PageResponse;
 import com.stanzaliving.website.elasticsearch.index.dto.WebsitePlaceIndexDto;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
 
-import java.io.Serializable;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
-public class WebsiteSearchResultsDto extends PageResponse implements Serializable {
+@Builder
+public class WebsiteSearchResultsDto extends PageResponse<WebsiteSearchResultsDto> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
