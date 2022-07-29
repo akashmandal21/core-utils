d20b0cc055ff2f2a97c448648069bb27e70a7118
ayushi720 <ayushi.dwivedi@unthinkable.co>
2022-07-29 12:27:27 +0530

    visibility condition added in other information dto

--- pojo/src/main/java/com/stanzaliving/estate_v2/dto/reviewTemplate/OtherInformationDto.java
+++ pojo/src/main/java/com/stanzaliving/estate_v2/dto/reviewTemplate/OtherInformationDto.java
@@ -1,5 +1,6 @@
 package com.stanzaliving.estate_v2.dto.reviewTemplate;
 
+import com.stanzaliving.sfr.dto.questionTemplate.conditions.VisibilityConditionDto;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,4 +10,6 @@ public class OtherInformationDto {
     private String label;
     private String variableName;
     private String icon;
+    private VisibilityConditionDto visibilityCondition;
+
 }
