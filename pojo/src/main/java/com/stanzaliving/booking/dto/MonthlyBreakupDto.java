a94a06e32b3ab3555a20327ae4b717fbabeb1126
Prateek Ladha <>
2022-07-30 13:16:39 +0530

    handled null in case of licensefee, monthlyfee, cityheadbuffer, upsell

--- pojo/src/main/java/com/stanzaliving/booking/dto/MonthlyBreakupDto.java
+++ pojo/src/main/java/com/stanzaliving/booking/dto/MonthlyBreakupDto.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import javax.validation.Valid;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 @Builder
@@ -48,4 +49,43 @@ public class MonthlyBreakupDto implements Serializable {
 //    @Valid
     private ServicePriceDto cityBufferPriceDto;
 
+    public Double getMonthlyFee() {
+        if(Objects.isNull(this.monthlyFee))
+            return 0D;
+        return this.monthlyFee;
+    }
+
+    public Double getLicenseFee() {
+        if(Objects.isNull(this.licenseFee))
+            return 0D;
+        return this.licenseFee;
+    }
+
+    public Double getCityHeadBuffer() {
+        if(Objects.isNull(this.cityHeadBuffer))
+            return 0D;
+        return this.cityHeadBuffer;
+    }
+
+    public Double getUpsell() {
+        if(Objects.isNull(this.upsell))
+            return 0D;
+        return this.upsell;
+    }
+
+    public void setMonthlyFee(final Double monthlyFee) {
+        this.monthlyFee = monthlyFee;
+    }
+
+    public void setLicenseFee(final Double licenseFee) {
+        this.licenseFee = licenseFee;
+    }
+
+    public void setCityHeadBuffer(final Double cityHeadBuffer) {
+        this.cityHeadBuffer = cityHeadBuffer;
+    }
+
+    public void setUpsell(final Double upsell) {
+        this.upsell = upsell;
+    }
 }
