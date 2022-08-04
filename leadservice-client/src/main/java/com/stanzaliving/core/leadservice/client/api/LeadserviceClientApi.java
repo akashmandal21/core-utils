f06c8b5425d8086807818951616a1cfd7740139b
guruvachan jain <guruvachan.jain@stanzaliving.com>
2022-08-04 23:35:33 +0530

    fix

--- leadservice-client/src/main/java/com/stanzaliving/core/leadservice/client/api/LeadserviceClientApi.java
+++ leadservice-client/src/main/java/com/stanzaliving/core/leadservice/client/api/LeadserviceClientApi.java
@@ -682,23 +682,7 @@ public class LeadserviceClientApi {
         }
     }
 
-    public ResponseDto<Void> syncElasticData(LeadElasticDto leadElasticDto) {
-        log.debug("Lead client to lead elastic data");
-        Object postBody = leadElasticDto;
-        String path = UriComponentsBuilder.fromPath("/internal/lead/sync-elastic-data").toUriString();
-        final HttpHeaders headerParams = new HttpHeaders();
-        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
-        final String[] accepts = {"*/*"};
-        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
-        ParameterizedTypeReference<ResponseDto<Void>> returnType = new ParameterizedTypeReference<ResponseDto<Void>>() {
-        };
-        try {
-            return restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
-        } catch (Exception e) {
-            log.error("Exception caused while syncing lead elastic data", e);
-            return null;
-        }
-    }
+
 
     public ResponseDto<String> autoExpireReferralCode() {
         log.info("Expire referralCodes");
