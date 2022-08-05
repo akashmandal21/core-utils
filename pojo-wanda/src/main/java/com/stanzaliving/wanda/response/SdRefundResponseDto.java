aa89c6b716ed7775e41487a834c555cbac0d6838
chicki9 <chahat.aggarwal@stanzaliving.com>
2022-08-05 17:55:51 +0530

    added dto for sd refund

---
+++ pojo-wanda/src/main/java/com/stanzaliving/wanda/response/SdRefundResponseDto.java
@@ -0,0 +1,33 @@
+package com.stanzaliving.wanda.response;
+
+import com.stanzaliving.booking.enums.BookingStatus;
+import com.stanzaliving.booking.enums.BookingSubStatus;
+import com.stanzaliving.booking.enums.BookingType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SdRefundResponseDto {
+
+    private String bookingUuid;
+    private BookingStatus bookingStatus;
+    private BookingSubStatus bookingSubStatus;
+    private BookingType bookingType;
+    private String userUuid;
+    private String contractStartDate;
+    private String contractEndDate;
+    private String roomNumber;
+    private String occupancyName;
+    private String residentId;
+    private String residenceName;
+    private SdRefundStateDetails moveOutDate;
+    private SdRefundStateDetails roomAudit;
+    private SdRefundStateDetails refund;
+}
