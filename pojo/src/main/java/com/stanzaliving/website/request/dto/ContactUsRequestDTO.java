0910f6ad99e78e652f8053add4b2e180cccb31dd
SumitDutta1 <sumit.dutta@stanzaliving.com>
2022-08-08 22:24:46 +0530

    Added ContactUsDTOs

---
+++ pojo/src/main/java/com/stanzaliving/website/request/dto/ContactUsRequestDTO.java
@@ -0,0 +1,33 @@
+package com.stanzaliving.website.request.dto;
+
+import com.stanzaliving.core.enums.LookingFor;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ContactUsRequestDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int contactUsId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String mobile;
+
+    private String message;
+
+    private LookingFor lookingFor;
+
+    private String token;
+}
