ac919a83e5df3152b52622301000afcef9bd8bae
sudzzz <sudhirdaga1998@gmail.com>
2022-08-08 15:06:35 +0530

    - Added Questions for Competitor analysis

--- pojo/src/main/java/com/stanzaliving/core/estate/enums/SectionKey.java
+++ pojo/src/main/java/com/stanzaliving/core/estate/enums/SectionKey.java
@@ -43,12 +43,9 @@ public enum SectionKey {
 	CHICKEN("Chicken","chicken", TemplateParentKey.OTHERS, Boolean.FALSE, Boolean.FALSE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
 	PANEER("Paneer","paneer", TemplateParentKey.OTHERS, Boolean.FALSE, Boolean.FALSE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
 	FRUITS("Fruits","fruits", TemplateParentKey.OTHERS, Boolean.FALSE, Boolean.FALSE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
-	KITCHEN_NAME("Kitchen Name","kitchenName", TemplateParentKey.OTHERS, Boolean.TRUE, Boolean.TRUE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
-	KITCHEN_MENU_CONFIG("Kitchen Menu Config","kitchenMenuConfig", TemplateParentKey.OTHERS, Boolean.TRUE, Boolean.TRUE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
 	BREAD_BUTTER_AND_JAM("Bread, Butter & Jam","breadButterAndJam", TemplateParentKey.OTHERS, Boolean.FALSE, Boolean.FALSE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
 	KITCHEN_NAME("Kitchen Name","kitchenName", TemplateParentKey.OTHERS, Boolean.TRUE, Boolean.TRUE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
 	KITCHEN_MENU_CONFIG("Kitchen Menu Config","kitchenMenuConfig", TemplateParentKey.OTHERS, Boolean.TRUE, Boolean.TRUE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
-
 	ROOMS_WITH_AC_REQUIREMENT("Rooms with AC requirement","roomsWithAcRequirement", TemplateParentKey.OTHERS, Boolean.FALSE, Boolean.FALSE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
 	ROOMS_WITH_TV_REQUIREMENT("Rooms with TV requirement","roomsWithTvRequirement", TemplateParentKey.OTHERS, Boolean.FALSE, Boolean.FALSE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
 	POOL_TABLE_REQUIRED("Pool Table required","isPoolTableRequired", TemplateParentKey.OTHERS, Boolean.TRUE, Boolean.TRUE, Arrays.asList(SELECTED_OPTIONS,REMARKS,INPUT_VALUE,IMAGE)),
