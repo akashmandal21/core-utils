773c12104997c55352fc6a76d9f5142a74daa204
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-08-16 15:57:41 +0530

    code added

--- cloudinary-client/src/main/java/com/stanzaliving/core/cloudinary/CloudinaryClient.java
+++ cloudinary-client/src/main/java/com/stanzaliving/core/cloudinary/CloudinaryClient.java
@@ -1,8 +1,16 @@
 package com.stanzaliving.core.cloudinary;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.stanzaliving.core.cloudinary.constatnts.CloudinaryConstants;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Log4j2
 public class CloudinaryClient {
 
     private Cloudinary cloudinary;
@@ -11,11 +19,39 @@ public class CloudinaryClient {
         this.cloudinary = cloudinary;
     }
 
-    public String uploadImageAndGetUrl(MultipartFile file, int width, int height) {
+    public String uploadImageForWebsiteCmsAndGetUrl(MultipartFile file, int width, int height, String uploadFolderPath) {
 
+        String cloudinaryPath = null;
 
+        try {
 
+            Map properties = new HashMap<>();
 
-        return null;
+            if (StringUtils.isNotBlank(uploadFolderPath)) {
+                properties.put(CloudinaryConstants.FOLDER, uploadFolderPath);
+            }
+
+            if (width > 0 && height > 0) {
+                properties.put(CloudinaryConstants.TRANSFORMATION, new Transformation<>().crop(CloudinaryConstants.SCALE).width(width).height(height).fetchFormat(CloudinaryConstants.AUTO));
+            } else if (width > 0) {
+                properties.put(CloudinaryConstants.TRANSFORMATION, new Transformation<>().crop(CloudinaryConstants.SCALE).width(width).fetchFormat(CloudinaryConstants.AUTO));
+            } else if (height > 0) {
+                properties.put(CloudinaryConstants.TRANSFORMATION, new Transformation<>().crop(CloudinaryConstants.SCALE).height(height).fetchFormat(CloudinaryConstants.AUTO));
+            } else {
+                properties.put(CloudinaryConstants.TRANSFORMATION, new Transformation<>().fetchFormat(CloudinaryConstants.AUTO));
+            }
+
+            Map result = cloudinary.uploader().upload(file, properties);
+
+            cloudinaryPath = (String) result.get("secure_url");
+
+            cloudinaryPath = cloudinaryPath.replaceFirst( "image/upload", "image/upload/f_auto,q_auto");
+        } catch (Exception e) {
+            log.error("Exception while uploading file to cloudinary: {}", e);
+        }
+
+        log.info("Returning  cloudinaryFilePath as:", cloudinaryPath);
+
+        return cloudinaryPath;
     }
 }
--- cloudinary-client/src/main/java/com/stanzaliving/core/cloudinary/constatnts/CloudinaryConstants.java
+++ cloudinary-client/src/main/java/com/stanzaliving/core/cloudinary/constatnts/CloudinaryConstants.java
@@ -5,5 +5,20 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class CloudinaryConstants {
 
-    
+    public final String CLOUD_NAME = "cloud_name";
+
+    public final String API_KEY = "api_key";
+
+    public final String API_SECRET = "api_secret";
+
+    public final String FOLDER = "folder";
+
+    public final String TRANSFORMATION = "transformation";
+
+    public final String SCALE = "scale";
+
+    public final String AUTO = "auto";
+//    public final String FOLDER = "folder";
+//    public final String FOLDER = "folder";
+//    public final String FOLDER = "folder";
 }
