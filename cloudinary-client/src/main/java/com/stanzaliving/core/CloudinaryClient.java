23bc1b09d548b1e7ab1b80c6c0a5f20a68aa321b
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-08-16 14:21:42 +0530

    code added

---
+++ cloudinary-client/src/main/java/com/stanzaliving/core/CloudinaryClient.java
@@ -0,0 +1,19 @@
+package com.stanzaliving.core;
+
+import com.cloudinary.Cloudinary;
+
+public class CloudinaryClient {
+
+    private Cloudinary cloudinary;
+
+    public CloudinaryClient(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    public String uploadImageAndGetUrl(){
+
+        
+
+        return null;
+    }
+}
