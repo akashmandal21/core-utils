e0308764657e97be71ed4f910bf363f3f40c470a
Muquitur.Rahman <muquitur.rahman@C02XP1WEJG5H.local>
2022-08-18 19:10:35 +0530

    code

--- pojo/src/main/java/com/stanzaliving/website/dto/WebsiteResidenceResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/website/dto/WebsiteResidenceResponseDto.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class WebsiteResidenceResponseDto {
+public class WebsiteResidenceResponseDto implements Serializable {
     private String videoLink;
 
     private String googleMapLink;
