2038c426f9c3aa92e4770354061ea1d0d8e1fb41
manishpareek <manishpareek.retainer@stanzaliving.com>
2022-08-29 13:11:43 +0530

    PersonnelCategoryV2Dto updated for enum validation

--- pojo-ops/src/main/java/com/stanzaliving/operations/dto/servicemix/WashingService.java
+++ pojo-ops/src/main/java/com/stanzaliving/operations/dto/servicemix/WashingService.java
@@ -1,5 +1,7 @@
 package com.stanzaliving.operations.dto.servicemix;
 
+import java.io.Serializable;
+
 import com.stanzaliving.operations.enums.WashLimit;
 
 import lombok.Getter;
@@ -9,7 +11,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString(callSuper = true)
-public class WashingService extends BooleanService {
+public class WashingService extends BooleanService implements Serializable {
 	private Integer cloths;
 	private Double weight;
 	private WashLimit unitType;
