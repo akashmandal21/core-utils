68d136b47d6a9fb35855311aead2ecaa03138f23
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-08-29 19:00:27 +0530

    Update ImageLibraryDto.java

--- pojo/src/main/java/com/stanzaliving/website/dto/ImageLibraryDto.java
+++ pojo/src/main/java/com/stanzaliving/website/dto/ImageLibraryDto.java
@@ -2,12 +2,18 @@ package com.stanzaliving.website.dto;
 
 import com.stanzaliving.core.base.common.dto.AbstractDto;
 import lombok.*;
+import lombok.extern.jackson.Jacksonized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Jacksonized
 public class ImageLibraryDto extends AbstractDto {
 
     private String imageUrl;
@@ -15,5 +21,11 @@ public class ImageLibraryDto extends AbstractDto {
     private boolean sampleImage;
 
     @Builder.Default
-    private boolean clickable = true;
+    private boolean clickable = false;
+
+    @Builder.Default
+    private List<Integer> asasdf = new ArrayList<>();
+
+    @Builder.Default
+    private List<Integer> badf = Arrays.asList(1,2,3);
 }
