e35539a2863569921ea889be9c15382d764c5638
yuktigupta2510 <yukti.gupta@stanzaliving.com>
2022-08-30 13:24:52 +0530

    contract termination

--- pojo/src/main/java/com/stanzaliving/booking/dto/response/ContractTerminationResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/booking/dto/response/ContractTerminationResponseDto.java
@@ -1,12 +1,14 @@
 package com.stanzaliving.booking.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.stanzaliving.core.base.StanzaConstants;
+import com.stanzaliving.core.base.utils.DateUtil;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Builder
@@ -22,5 +24,6 @@ public class ContractTerminationResponseDto {
     private Double upcomingDues;
     private Double totalPendingDues;
     private Double totalRefundAmount;
+    @JsonFormat(pattern = DateUtil.yyyy_MM_dd_FORMAT, timezone = StanzaConstants.IST_TIMEZONE)
     private LocalDate auditDate;
 }
