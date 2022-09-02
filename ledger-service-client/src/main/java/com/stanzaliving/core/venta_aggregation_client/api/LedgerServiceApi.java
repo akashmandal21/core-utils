05aa64b428ccc3e4dc164ba3d4aa6b5a469f7690
Muquitur.Rahman <muquitur.rahman@ip-192-168-7-209.ap-south-1.compute.internal>
2022-09-03 00:48:07 +0530

    code

--- ledger-service-client/src/main/java/com/stanzaliving/core/venta_aggregation_client/api/LedgerServiceApi.java
+++ ledger-service-client/src/main/java/com/stanzaliving/core/venta_aggregation_client/api/LedgerServiceApi.java
@@ -383,4 +383,26 @@ public class LedgerServiceApi {
             return null;
         }
     }
+    public ResponseDto<Boolean> sdAutoRefund(AutoRefundDto autoRefundDto) {
+        Object postBody = autoRefundDto;
+        Map<String, Object> uriVariables = new HashMap<>();
+        String path = UriComponentsBuilder.fromPath("/internal/api/v1/sdAutoRefund")
+                .buildAndExpand(uriVariables).toUriString();
+
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+        HttpHeaders headerParams = new HttpHeaders();
+        String[] accepts = new String[]{"*/*"};
+        List<MediaType> accept = this.restClient.selectHeaderAccept(accepts);
+
+        ParameterizedTypeReference<ResponseDto<Boolean>> returnType = new ParameterizedTypeReference<ResponseDto<Boolean>>() {
+        };
+        try {
+            log.info("Executing Api for Auto Refund {}", path);
+            return this.restClient.invokeAPI(path, HttpMethod.POST, queryParams, postBody, headerParams, accept, returnType);
+        } catch (Exception e) {
+            log.error("Exception while Auto Refund, Exception is ", e);
+        }
+        return null;
+    }
 }
