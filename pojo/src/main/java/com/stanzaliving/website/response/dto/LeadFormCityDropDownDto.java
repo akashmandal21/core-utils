b9d7af53f58bf9cde3bf74d0065e8872b69d726a
SumitDutta1 <sumit.dutta@stanzaliving.com>
2022-09-07 18:00:45 +0530

    Update LeadFormCityDropDownDto.java
    added builder default false

--- pojo/src/main/java/com/stanzaliving/website/response/dto/LeadFormCityDropDownDto.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/LeadFormCityDropDownDto.java
@@ -24,8 +24,13 @@ public class LeadFormCityDropDownDto implements Serializable {
     private boolean comingSoonPg;
     private boolean comingSoonApartment;
     private String transformationUuid;
-    private boolean pgLpDropdownVisible;
-    private boolean apartmentLpDropdownVisible;
-    private boolean pgWebsiteDropdownVisible;
-    private boolean apartmentWebsiteDropdownVisible;
+
+    @Builder.Default
+    private boolean pgLpDropdownVisible = false;
+    @Builder.Default
+    private boolean apartmentLpDropdownVisible = false;
+    @Builder.Default
+    private boolean pgWebsiteDropdownVisible = false;
+    @Builder.Default
+    private boolean apartmentWebsiteDropdownVisible = false;
 }
