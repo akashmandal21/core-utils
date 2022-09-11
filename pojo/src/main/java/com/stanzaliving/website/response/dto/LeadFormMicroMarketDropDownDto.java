d293a80662a46941eeb568f2defe6b838df61de8
SumitDutta1 <sumit.dutta@stanzaliving.com>
2022-09-11 19:44:04 +0530

    Update LeadFormMicroMarketDropDownDto.java

--- pojo/src/main/java/com/stanzaliving/website/response/dto/LeadFormMicroMarketDropDownDto.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/LeadFormMicroMarketDropDownDto.java
@@ -14,10 +14,16 @@ public class LeadFormMicroMarketDropDownDto implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private int micromarketId;
-    private String name;
+    private String micromarketName;
     private int cityId;
     private String slug;
     private boolean enabled;
     private Integer micromarketAliasId;
+    private String micromarketAliasName;
     private String displayName;
+
+    private boolean pgLpDropdownVisible;
+    private boolean apartmentLpDropdownVisible;
+    private boolean pgWebsiteDropdownVisible;
+    private boolean apartmentWebsiteDropdownVisible;
 }
