41f0de5f7c97c657f4813da19f5b570ef963efbd
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2022-09-12 19:15:46 +0530

    Update ImageLibraryDto.java

--- pojo/src/main/java/com/stanzaliving/website/dto/ImageLibraryDto.java
+++ pojo/src/main/java/com/stanzaliving/website/dto/ImageLibraryDto.java
@@ -1,6 +1,7 @@
 package com.stanzaliving.website.dto;
 
 import com.stanzaliving.core.base.common.dto.AbstractDto;
+import com.stanzaliving.core.enums.PropertyEntityType;
 import lombok.*;
 
 @Builder
@@ -15,6 +16,8 @@ public class ImageLibraryDto extends AbstractDto {
     private boolean genericImage;
 
     private boolean dummyImage;
+
+    private PropertyEntityType propertyEntityType;
 
     @Builder.Default
     private boolean clickable = true;
