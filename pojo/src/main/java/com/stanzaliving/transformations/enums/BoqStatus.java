f3db2e9301b52416853ab29d927fbecd54ed00f3
Ayushi.Sharma <ayushi.sharma@stanzaliving.com>
2022-09-19 00:50:32 +0530

    adding status order in enum for handling draft and sent back case

--- pojo/src/main/java/com/stanzaliving/transformations/enums/BoqStatus.java
+++ pojo/src/main/java/com/stanzaliving/transformations/enums/BoqStatus.java
@@ -18,15 +18,15 @@ import java.util.Map;
 @Getter
 public enum BoqStatus {
 
-	NOT_STARTED("Not Available", "#e5e3e3",null,null,0, "Not Started", "#e5e3e3", ColorCode.BLACK.getColorCode()),
-	DRAFT_IN_PROGRESS("Under Draft", "#e5e3e3",UserType.DESIGN_COORDINATOR,Department.DESIGN,1, "In Draft", ColorCode.DARK_GRAYISH_BLUE.getColorCode(), ColorCode.LIGHT_GRAYISH_BLUE.getColorCode()),
-	SEND_FOR_APPROVAL("Sent For Approval to Zonal Head", "#FFC300",UserType.ZONAL_HEAD,Department.DESIGN,2, "L1 Approval Due", ColorCode.VIVID_ORANGE.getColorCode(), ColorCode.PALE_ORANGE.getColorCode()),
-	APPROVED_BY_ZH("Sent For Approval to Design Head", "#FFC300",UserType.NATIONAL_HEAD,Department.DESIGN,3, "L2 Approval Due", ColorCode.VIVID_ORANGE.getColorCode(), ColorCode.PALE_ORANGE.getColorCode()),
-	APPROVED_BY_NH("Sent For Approval to Projects Zonal Head", "#FFC300",UserType.MANAGER,Department.TRANSFORMATIONS,4, "L3 Approval Due", ColorCode.VIVID_ORANGE.getColorCode(), ColorCode.PALE_ORANGE.getColorCode()),
-	APPROVED_BY_PZH("Sent For Approval to Nitin", "#FFC300",UserType.ZONAL_HEAD,Department.PROJECTS,5, "L4 Approval Due", ColorCode.VIVID_ORANGE.getColorCode(), ColorCode.PALE_ORANGE.getColorCode()),
-	SENT_BACK("Sent Back", "#d41111",UserType.DESIGN_COORDINATOR,Department.DESIGN,6, "Sent Back", ColorCode.SOFT_RED.getColorCode(), ColorCode.VERY_PALE_MOSTLY_WHITE_RED.getColorCode()),
-	APPROVED_BY_NITIN("Sent For Approval to Leadership", "#77baa6",UserType.MANAGER,Department.TRANSFORMATIONS,7, "L5 Approval Due", ColorCode.VIVID_ORANGE.getColorCode(), ColorCode.PALE_ORANGE.getColorCode()),
-	APPROVED_BY_LEADERSHIP("Approved", "#77baa6",UserType.LEADERSHIP,Department.LEADERSHIP,8, "Approved", ColorCode.GREEN_CYAN.getColorCode(), ColorCode.SOFT_GREEN.getColorCode());
+	NOT_STARTED("Not Available", "#e5e3e3",null,null,0, "Not Started", "#e5e3e3", ColorCode.BLACK.getColorCode(), 0),
+	DRAFT_IN_PROGRESS("Under Draft", "#e5e3e3",UserType.DESIGN_COORDINATOR,Department.DESIGN,1, "In Draft", ColorCode.DARK_GRAYISH_BLUE.getColorCode(), ColorCode.LIGHT_GRAYISH_BLUE.getColorCode(), 1),
+	SEND_FOR_APPROVAL("Sent For Approval to Zonal Head", "#FFC300",UserType.ZONAL_HEAD,Department.DESIGN,2, "L1 Approval Due", ColorCode.VIVID_ORANGE.getColorCode(), ColorCode.PALE_ORANGE.getColorCode(), 3),
+	APPROVED_BY_ZH("Sent For Approval to Design Head", "#FFC300",UserType.NATIONAL_HEAD,Department.DESIGN,3, "L2 Approval Due", ColorCode.VIVID_ORANGE.getColorCode(), ColorCode.PALE_ORANGE.getColorCode(), 4),
+	APPROVED_BY_NH("Sent For Approval to Projects Zonal Head", "#FFC300",UserType.MANAGER,Department.TRANSFORMATIONS,4, "L3 Approval Due", ColorCode.VIVID_ORANGE.getColorCode(), ColorCode.PALE_ORANGE.getColorCode(), 5),
+	APPROVED_BY_PZH("Sent For Approval to Nitin", "#FFC300",UserType.ZONAL_HEAD,Department.PROJECTS,5, "L4 Approval Due", ColorCode.VIVID_ORANGE.getColorCode(), ColorCode.PALE_ORANGE.getColorCode(), 6),
+	SENT_BACK("Sent Back", "#d41111",UserType.DESIGN_COORDINATOR,Department.DESIGN,6, "Sent Back", ColorCode.SOFT_RED.getColorCode(), ColorCode.VERY_PALE_MOSTLY_WHITE_RED.getColorCode(), 2),
+	APPROVED_BY_NITIN("Sent For Approval to Leadership", "#77baa6",UserType.MANAGER,Department.TRANSFORMATIONS,7, "L5 Approval Due", ColorCode.VIVID_ORANGE.getColorCode(), ColorCode.PALE_ORANGE.getColorCode(), 7),
+	APPROVED_BY_LEADERSHIP("Approved", "#77baa6",UserType.LEADERSHIP,Department.LEADERSHIP,8, "Approved", ColorCode.GREEN_CYAN.getColorCode(), ColorCode.SOFT_GREEN.getColorCode(), 8);
 
 	private String desc;
 	private String color;
@@ -36,8 +36,9 @@ public enum BoqStatus {
 	private String approvalState;
 	private String textColor;
 	private String bgColor;
+	private Integer statusOrder;
 
-	BoqStatus(String desc, String color, UserType editableByUser, Department editableByDepartment, Integer order) {
+	BoqStatus(String desc, String color, UserType editableByUser, Department editableByDepartment, Integer order, Integer statusOrder) {
 		this.desc = desc;
 		this.color = color;
 		this.editableByUser = editableByUser;
@@ -46,9 +47,10 @@ public enum BoqStatus {
 		this.approvalState = null;
 		this.textColor = null;
 		this.bgColor = null;
+		this.statusOrder = statusOrder;
 	}
 
-	BoqStatus(String name, String color, UserType userType, Department department, int order, String approvalState, String textColor, String bgColor) {
+	BoqStatus(String name, String color, UserType userType, Department department, int order, String approvalState, String textColor, String bgColor, Integer statusOrder) {
 		this.desc = name;
 		this.color = color;
 		this.editableByUser = userType;
@@ -57,6 +59,7 @@ public enum BoqStatus {
 		this.approvalState=approvalState;
 		this.textColor=textColor;
 		this.bgColor=bgColor;
+		this.statusOrder = statusOrder;
 	}
 
 
@@ -64,7 +67,7 @@ public enum BoqStatus {
 
 	static{
 		for(BoqStatus boqStatus: BoqStatus.values()) {
-			boqStatusAndOrderMap.put(boqStatus.getOrder(), boqStatus);
+			boqStatusAndOrderMap.put(boqStatus.getStatusOrder(), boqStatus);
 		}
 	}
 }
