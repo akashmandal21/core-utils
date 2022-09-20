cba5dbc3c4a72d200ef0aaf8aa1dfd54c37647ab
manishpareek <manishpareek.retainer@stanzaliving.com>
2022-09-20 15:22:36 +0530

    Update WashingService.java

--- pojo-ops/src/main/java/com/stanzaliving/operations/dto/servicemix/WashingService.java
+++ pojo-ops/src/main/java/com/stanzaliving/operations/dto/servicemix/WashingService.java
@@ -1,17 +1,24 @@
 package com.stanzaliving.operations.dto.servicemix;
 
+import java.io.Serializable;
+
 import com.stanzaliving.operations.enums.WashLimit;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @ToString(callSuper = true)
-public class WashingService extends BooleanService {
-	private Integer cloths;
-	private Double weight;
-	private WashLimit unitType;
-	private int repetation;
+public class WashingService extends BooleanService implements Serializable {
+  private static final long serialVersionUID = 4712134052620448631L;
+  private Integer cloths;
+  private Double weight;
+  private WashLimit unitType;
+  private int repetation;
 }
