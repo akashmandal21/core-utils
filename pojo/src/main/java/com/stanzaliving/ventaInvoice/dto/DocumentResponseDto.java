e0db17d862402c6dc4989e0ae85c45dfed0b4aeb
SumitDutta1 <sumit.dutta@stanzaliving.com>
2022-09-28 10:31:08 +0530

    Update DocumentResponseDto.java

--- pojo/src/main/java/com/stanzaliving/ventaInvoice/dto/DocumentResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/ventaInvoice/dto/DocumentResponseDto.java
@@ -4,6 +4,7 @@ package com.stanzaliving.ventaInvoice.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.stanzaliving.ventaInvoice.enums.InvoiceType;
 import com.stanzaliving.ventaInvoice.enums.ReferenceType;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,6 +15,7 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
+@Builder
 public class DocumentResponseDto {
     private String uuid;
     private Long id;
