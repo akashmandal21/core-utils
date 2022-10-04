31477ad311cb7d194b226a5b6eb4df4e99e6d758
anirudh-code-08 <107861597+anirudh-code-08@users.noreply.github.com>
2022-10-04 14:11:36 +0530

    Add Residence Type

---
+++ pojo/src/main/java/com/stanzaliving/core/enums/JarvisResidenceType.java
@@ -0,0 +1,12 @@
+package com.stanzaliving.core.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum JarvisResidenceType {
+    ALL_RESIDENCE,
+    ALL_MANAGED_APPARTMENTS,
+    RESIDENCE_SPECIFIC;
+}
