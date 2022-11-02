d4a8ceecb455098169a2713aa880690a767be6a7
gaurav-likhar <gaurav.likhar@stanzaliving.com>
2022-11-02 14:48:39 +0530

    added two dtos, added client for request expiration

---
+++ pojo/src/main/java/com/stanzaliving/booking/dto/ExpiredPendingRequestsDto.java
@@ -0,0 +1,19 @@
+package com.stanzaliving.booking.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExpiredPendingRequestsDto {
+
+    List<String> requestUuidList;
+
+}
