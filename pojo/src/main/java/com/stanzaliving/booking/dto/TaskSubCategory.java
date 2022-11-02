3291bdbedff9151493b9f771356c29fb79455035
manu-stanza <manu.pathria@stanzaliving.com>
2022-11-02 18:00:50 +0530

    added dto's in core-utils

---
+++ pojo/src/main/java/com/stanzaliving/booking/dto/TaskSubCategory.java
@@ -0,0 +1,18 @@
+package com.stanzaliving.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum TaskSubCategory {
+    RECIEVE_MEALS("Receive meals"),
+    DAMAGE_CHECK("Damage Check"),
+    METER_READING("Enter Meter Reading"),
+    POLICE_VERIFICATION("Police Verification"),
+    RESOLVE_COMPLAINT("Resolve complaint"),
+    COMPLETE_EXIT("Complete Exit"),
+    COMPLETE_AUDIT("Complete Audit");
+
+    private String name;
+}
