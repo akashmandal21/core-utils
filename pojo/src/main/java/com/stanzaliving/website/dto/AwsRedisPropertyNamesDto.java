c7df96ba3bcfe50e55c3dc7a546b039902c278af
Navratan Choudhary <navratan.choudhary@stanzaliving.com>
2022-11-14 18:08:13 +0530

    Create AwsRedisPropertyNamesDto.java

---
+++ pojo/src/main/java/com/stanzaliving/website/dto/AwsRedisPropertyNamesDto.java
@@ -0,0 +1,20 @@
+package com.stanzaliving.website.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AwsRedisPropertyNamesDto implements Serializable {
+
+    Map<Integer, List<String>> bucketNumberPropertyNamesMap;
+
+    Date cacheBuildDate;
+}
