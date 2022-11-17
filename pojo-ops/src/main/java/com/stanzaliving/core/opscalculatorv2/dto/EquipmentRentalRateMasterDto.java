e3fd011f7a68de9122698aeb1de4b82f00205c62
manishpareekstanza <manish.pareek@stanzaliving.com>
2022-11-17 19:33:51 +0530

    dto updated

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/opscalculatorv2/dto/EquipmentRentalRateMasterDto.java
@@ -0,0 +1,31 @@
+
+package com.stanzaliving.core.opscalculatorv2.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author manish.pareek
+ *
+ */
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+public class EquipmentRentalRateMasterDto implements Serializable  {
+
+	private String cityUuid;
+	
+	private String microMarketUuid;
+
+	private String standardEquipRental;
+
+}
