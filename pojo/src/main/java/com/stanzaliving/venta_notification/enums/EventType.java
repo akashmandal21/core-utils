9677e69fcb9937e4bcfd9da614b9e2bd768254bc
Muquitur.Rahman <muquitur.rahman@ip-10-101-207-15.ap-south-1.compute.internal>
2022-11-17 19:42:44 +0530

    code

--- pojo/src/main/java/com/stanzaliving/venta_notification/enums/EventType.java
+++ pojo/src/main/java/com/stanzaliving/venta_notification/enums/EventType.java
@@ -17,7 +17,8 @@ public enum EventType {
     MODIFY_CONTRACT_REQUEST("Modify Contract Email"),
     SETTLE_LEDGER_REJECTED_REFUNDS("Settle Ledger Rejected Refunds"),
     REFUND_SUCCESSFULLY_PROCESSED("Refund successfully processed"),
-    CHECK_FOR_BALANCE_IN_RAZORPAY_REFUND("CHECK_FOR_BALANCE_IN_RAZORPAY_REFUND");
+    CHECK_FOR_BALANCE_IN_RAZORPAY_REFUND("CHECK_FOR_BALANCE_IN_RAZORPAY_REFUND"),
+    IMS_DIGEST_EMAIL("IMS Digest Email");
     private String commandText;
 
     EventType(String commandText)
