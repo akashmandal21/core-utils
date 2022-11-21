de1812be2c9120b270fb6b2538fcbf7c146e14f1
jayapavan42 <jaya.kadimetla@stanzaliving.com>
2022-11-21 14:49:36 +0530

    added changes

--- acl-client/src/main/java/com/stanzaliving/acl/client/Permissions.java
+++ acl-client/src/main/java/com/stanzaliving/acl/client/Permissions.java
@@ -1,5 +1,11 @@
 package com.stanzaliving.acl.client;
 
 public enum Permissions {
-    EDIT_BOQ_BOQ;
+    EDIT_BOQ_BOQ,
+    APPROVE_BOQ_L1_BOQ,
+    APPROVE_BOQ_L2_BOQ,
+    APPROVE_BOQ_L3_BOQ,
+    APPROVE_BOQ_L4_BOQ,
+    APPROVE_BOQ_L5_BOQ;
+
 }
