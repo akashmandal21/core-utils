41ecdea129f86a886c1941530aaad30ab6fda784
upadhyayamanS <aman.upadhyay@stanzaliving.com>
2022-12-06 12:37:32 +0530

    test

--- kafka/src/main/java/com/stanzaliving/core/kafka/producer/NotificationProducer.java
+++ kafka/src/main/java/com/stanzaliving/core/kafka/producer/NotificationProducer.java
@@ -5,6 +5,7 @@ package com.stanzaliving.core.kafka.producer;
 
 import java.util.Objects;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -79,6 +80,7 @@ public class NotificationProducer {
 
 						log.debug("Sent Message=[topic: " + record.topic() + ", partition: " + record.partition() + ", messageId: " + messageId + "] with offset="
 								+ result.getRecordMetadata().offset() + " and timestamp= " + result.getRecordMetadata().timestamp());
+						log.debug("kafka-server is {}", ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG);
 
 					}
 				}
