d8e9ffd22d28a1a8ee86fa9c932c2ddb4605d2fe
Ayushi.Sharma <ayushi.sharma@stanzaliving.com>
2022-12-09 14:12:44 +0530

    adding client core api to get property data

--- project-service-client/src/main/java/com/stanzaliving/core/projectservice/client/api/ProjectServiceClientApi.java
+++ project-service-client/src/main/java/com/stanzaliving/core/projectservice/client/api/ProjectServiceClientApi.java
@@ -315,4 +315,33 @@ public class ProjectServiceClientApi {
 
         return restClient.invokeAPI(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType);
     }
+
+    public ResponseDto<PropertyInfoDto> getPropertyDetails(String estateUuid) {
+        if (Objects.isNull(estateUuid)) {
+            return null;
+        }
+
+        Object postBody = null;
+
+        // create path and map variables
+        final Map<String, Object> uriVariables = new HashMap<>();
+
+        uriVariables.put("estateUuid", estateUuid);
+
+        String path = UriComponentsBuilder.fromPath("/internal/propertyDetails/{estateUuid}").buildAndExpand(uriVariables).toUriString();
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+
+        final HttpHeaders headerParams = new HttpHeaders();
+
+        final String[] accepts = {
+                "*/*"
+        };
+        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+        ParameterizedTypeReference<ResponseDto<PropertyInfoDto>> returnType = new ParameterizedTypeReference<ResponseDto<PropertyInfoDto>>() {
+        };
+        return restClient.invokeAPI(path, HttpMethod.GET, queryParams, postBody, headerParams, accept, returnType);
+    }
 }
