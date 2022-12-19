b16f5fd75f6ebcf214e816502a3d6cc0d238e784
jayapavan42 <jaya.kadimetla@stanzaliving.com>
2022-12-20 00:13:12 +0530

    added changes

--- acl-client/src/main/java/com/stanzaliving/acl/client/AbacResources.java
+++ acl-client/src/main/java/com/stanzaliving/acl/client/AbacResources.java
@@ -7,12 +7,4 @@ public class AbacResources {
 
     public static Map resourcePermissionMap;
 
-    static {
-        resourceAttributeMap = java.util.stream.Stream.of(
-                  new java.util.AbstractMap.SimpleEntry<String, java.util.List<String>>("Phoenix",java.util.Arrays.asList("propertyUuid|TEXT","moduleName|TEXT","parentId|NUMBER"))
-                ).collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-        resourcePermissionMap = java.util.stream.Stream.of(
-                  new java.util.AbstractMap.SimpleEntry<String, java.util.List<String>>("Phoenix",java.util.Arrays.asList("phoenix_perm1_Phoenix","pheonix_perm2_Phoenix","phoenix_perm3_Phoenix"))
-                ).collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
 }
