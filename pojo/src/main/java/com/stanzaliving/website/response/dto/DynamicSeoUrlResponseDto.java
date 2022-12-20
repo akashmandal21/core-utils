0278a6af5f6d2e2b145fe7a817e28a8919a05fe3
SumitDutta1 <sumit.dutta@stanzaliving.com>
2022-12-20 14:25:41 +0530

    Update DynamicSeoUrlResponseDto.java

--- pojo/src/main/java/com/stanzaliving/website/response/dto/DynamicSeoUrlResponseDto.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/DynamicSeoUrlResponseDto.java
@@ -1,6 +1,21 @@
 package com.stanzaliving.website.response.dto;
 
+import com.stanzaliving.core.enums.PropertyEntityType;
+import com.stanzaliving.core.user.enums.EnumListing;
+
+import java.util.Date;
+
 public class DynamicSeoUrlResponseDto {
 
-    
+    private int dynamicSeoUrlId;
+    private String dynamicSeoUrl;
+    private String dynamicSeoTitle;
+    private String dynamicSeoDescription;
+    private boolean status;
+    private boolean locationCheck;
+    private EnumListing<PropertyEntityType> dynamicSeoUrlType;
+    private Date createdAt;
+    private String createdBy;
+    private Date updatedAt;
+    private String updatedBy;
 }
