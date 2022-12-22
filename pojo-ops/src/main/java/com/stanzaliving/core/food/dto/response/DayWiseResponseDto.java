40a00fb60dc709b15566063752b27c48e4a705ae
manishpareekstanza <manish.pareek@stanzaliving.com>
2022-12-22 16:21:18 +0530

    DayWiseResponseDto updated

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/DayWiseResponseDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/response/DayWiseResponseDto.java
@@ -2,6 +2,7 @@ package com.stanzaliving.core.food.dto.response;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 import lombok.AllArgsConstructor;
@@ -19,6 +20,6 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 public class DayWiseResponseDto implements Serializable {
 	private static final long serialVersionUID = -769016461417491230L;
-	private Map<LocalDate, MealSummaryResponseDto> dayWiseMealPreferenceSummary;
+	private Map<LocalDate, List<MealSummaryResponseDto>> dayWiseMealPreferenceSummary;
 
 }
