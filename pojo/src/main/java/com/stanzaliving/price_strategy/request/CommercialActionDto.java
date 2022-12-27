93c00736013845f2e0896370cd1c9be4949b466e
guruvachan jain <guruvachan.jain@stanzaliving.com>
2022-12-27 12:59:59 +0530

    change double into int

--- pojo/src/main/java/com/stanzaliving/price_strategy/request/CommercialActionDto.java
+++ pojo/src/main/java/com/stanzaliving/price_strategy/request/CommercialActionDto.java
@@ -3,6 +3,8 @@ package com.stanzaliving.price_strategy.request;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Getter
@@ -14,10 +16,12 @@ public class CommercialActionDto {
 
     @NotBlank(message = "priceImpact can't be blank")
     private String priceImpact;
-    private Double advancedRentalLastMonth;
-
+    @Max(value = 6, message = "Value must be an integer value less than 6")
+    private Integer lastAdvancedRentalMonth;
+    @Min(value = 15, message = "Value must be an integer value grater than 15")
+    private Integer lastAdvancedRentalDays;
     @Builder.Default
-    private Double advanceRentalFirstMonths = 1.0;
+    private Integer advanceRentalFirstMonths = 1;
     private SecurityDepositDto securityDepositDto;
     private MaintenanceFeeDto maintenanceFeeDto;
 
