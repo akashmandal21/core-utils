d66b44566610fb2b02c742ec0557fc126f0bdf95
Navratan Choudhary <navratan.choudhary@stanzaliving.com>
2023-01-02 15:56:35 +0530

    code added

---
+++ osm-service-client/src/main/java/com/stanzaliving/core/api/OsmServiceClientApi.java
@@ -0,0 +1,13 @@
+package com.stanzaliving.core.api;
+
+import com.stanzaliving.core.base.http.StanzaRestClient;
+
+public class OsmServiceClientApi {
+
+    private StanzaRestClient restClient;
+
+    public OsmServiceClientApi(StanzaRestClient restClient) {
+        this.restClient = restClient;
+    }
+    
+}
