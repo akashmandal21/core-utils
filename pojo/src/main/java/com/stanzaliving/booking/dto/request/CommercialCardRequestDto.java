f6f045206d4ad3a3322c9c899b7fac4df6d83cab
Gaurav Likhar <gaurav.likhar@stanzaliving.com>
2023-01-02 17:43:03 +0530

    added superbuilder

--- pojo/src/main/java/com/stanzaliving/booking/dto/request/CommercialCardRequestDto.java
+++ pojo/src/main/java/com/stanzaliving/booking/dto/request/CommercialCardRequestDto.java
@@ -6,12 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
-@Builder
+@SuperBuilder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
