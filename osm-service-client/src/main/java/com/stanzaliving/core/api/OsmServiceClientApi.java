ec9b743545da4ab0569b6b93c03220c19f142bd2
Navratan Choudhary <navratan.choudhary@stanzaliving.com>
2023-01-09 15:06:47 +0530

    Update OsmServiceClientApi.java

--- osm-service-client/src/main/java/com/stanzaliving/core/api/OsmServiceClientApi.java
+++ osm-service-client/src/main/java/com/stanzaliving/core/api/OsmServiceClientApi.java
@@ -2,7 +2,6 @@ package com.stanzaliving.core.api;
 
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
-import com.stanzaliving.osm.dto.StanzaOsmPlaceDto;
 import com.stanzaliving.website.elasticsearch.index.dto.WebsitePlaceIndexDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
@@ -23,32 +22,6 @@ public class OsmServiceClientApi {
     public OsmServiceClientApi(StanzaRestClient restClient) {
         this.restClient = restClient;
     }
-
-//    public ResponseDto<List<StanzaOsmPlaceDto>> getOsmPlaceEntitiesByCity(String transformationCityUuid) {
-//
-//        try {
-//            String path = UriComponentsBuilder.fromPath("internal/website/osm/get/all/stanza/place/by").toUriString();
-//
-//            final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
-//
-//            queryParams.add("transformationCityUuid", transformationCityUuid);
-//
-//            final HttpHeaders headerParams = new HttpHeaders();
-//
-//            final String[] accepts = {"*/*"};
-//
-//            final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
-//
-//            ParameterizedTypeReference<ResponseDto<List<StanzaOsmPlaceDto>>> returnType = new ParameterizedTypeReference<ResponseDto<List<StanzaOsmPlaceDto>>>() {
-//            };
-//
-//            return restClient.invokeAPI(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType);
-//
-//        } catch (Exception e) {
-//            log.error("Error while Fetching Osm Place Entities By cityTransformationUuid: {}, {}", transformationCityUuid, e);
-//        }
-//        return null;
-//    }
 
     public ResponseDto<String> pushOldPlacesToNewElastic(List<WebsitePlaceIndexDto> websitePlaceIndexDtos) {
 
