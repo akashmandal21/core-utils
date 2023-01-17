665729e2ea702cd7cb4602a8b125e1c28682fa9d
SumitDutta1 <sumit.dutta@stanzaliving.com>
2023-01-17 17:27:07 +0530

    Update ResidenceResponseShortDTO.java

--- pojo/src/main/java/com/stanzaliving/website/response/dto/ResidenceResponseShortDTO.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/ResidenceResponseShortDTO.java
@@ -56,6 +56,10 @@ public class ResidenceResponseShortDTO implements Serializable {
 
 	private Integer unlockDiscountAmount;
 
+	private Integer discountPercentage;
+
+	private Integer discountedPrice;
+
 	private String preBookingMode;
 
 	private String googleMapLink;
