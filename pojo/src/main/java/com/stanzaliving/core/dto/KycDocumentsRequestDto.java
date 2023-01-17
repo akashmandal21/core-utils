ae1b126442f765cf3eb50fd7473cc90f421ee715
Anukul Jain <107670569+anukul00jain@users.noreply.github.com>
2023-01-17 11:30:03 +0530

    request dto

---
+++ pojo/src/main/java/com/stanzaliving/core/dto/KycDocumentsRequestDto.java
@@ -0,0 +1,20 @@
+package com.stanzaliving.core.dto;
+
+import com.stanzaliving.core.enums.DocumentTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KycDocumentsRequestDto {
+
+    @NotNull
+    private String bookingUuid;
+
+}
