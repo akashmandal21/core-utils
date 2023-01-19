46d53ed62b228020dec2554200c944f59400cb2b
aayush.kasaudhan <aayush.kasaudhan@stanzaliving.com>
2023-01-19 22:53:51 +0530

    added sowAtlDto

---
+++ pojo/src/main/java/com/stanzaliving/estate_v2/dto/SowAtlDataDto.java
@@ -0,0 +1,21 @@
+package com.stanzaliving.estate_v2.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SowAtlDataDto {
+    private String section;
+    private String subSection;
+    private String clauseDescription;
+    @Builder.Default
+    private String clauseAnswer="Yes";
+    private String reason;
+}
