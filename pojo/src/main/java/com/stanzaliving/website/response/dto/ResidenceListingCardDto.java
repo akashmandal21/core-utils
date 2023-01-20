6b32324e86b65f9fb685b73b1157806a36e2e92a
SumitDutta1 <sumit.dutta@stanzaliving.com>
2023-01-20 10:23:34 +0530

    Update ResidenceListingCardDto.java

--- pojo/src/main/java/com/stanzaliving/website/response/dto/ResidenceListingCardDto.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/ResidenceListingCardDto.java
@@ -55,6 +55,10 @@ public class ResidenceListingCardDto implements Serializable {
 
 	private Integer unlockDiscountAmount;
 
+	private Integer discountPercentage;
+
+	private Integer discountedPrice;
+
 	private String preBookingMode;
 
 	private String googleMapLink;
