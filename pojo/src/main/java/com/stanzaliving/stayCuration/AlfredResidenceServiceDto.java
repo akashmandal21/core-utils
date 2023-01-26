9b4f80bcf617dc77a0f5523084dacab69a136ac1
guruvachan jain <guruvachan.jain@stanzaliving.com>
2023-01-26 10:39:13 +0530

    add dto

---
+++ pojo/src/main/java/com/stanzaliving/stayCuration/AlfredResidenceServiceDto.java
@@ -0,0 +1,44 @@
+package com.stanzaliving.stayCuration;
+
+import lombok.*;
+
+import java.util.Map;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class AlfredResidenceServiceDto {
+
+    private String name;
+
+    private String category;
+
+    private String residenceUuid;
+
+    private Double jarvisPlanUuid;
+
+    private Double serviceMixUuid;
+
+    private Map<String, String> description;
+
+    private Double planAmountWithoutGst;
+
+    private Double planAmountWithGst;
+
+    private GstDto cgst;
+
+    private GstDto sgst;
+
+    private GstDto igst;
+
+    private String planBillingFrequency;
+
+    private double planAmountBreakageFee;
+
+    private LockInDto lockIn;
+
+}
