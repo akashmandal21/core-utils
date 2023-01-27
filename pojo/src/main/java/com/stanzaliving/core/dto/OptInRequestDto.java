fb118018da2597d75315a9c1aa5e1f5725ad89aa
yuktigupta2510 <yukti.gupta@stanzaliving.com>
2023-01-27 23:00:49 +0530

    dto changes

--- pojo/src/main/java/com/stanzaliving/core/dto/OptInRequestDto.java
+++ pojo/src/main/java/com/stanzaliving/core/dto/OptInRequestDto.java
@@ -2,6 +2,8 @@ package com.stanzaliving.core.dto;
 
 import lombok.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Builder
@@ -9,10 +11,9 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class PlanRequestDto {
+public class OptInRequestDto {
 
+    @Valid
+    @NotEmpty(message = "Plan Opt In Request List cannot be null or empty")
     List<PlanOptInRequestDto> planOptInRequestDtoList;
-
-    List<PlanOptOutRequestDto> planOptOutRequestDtoList;
-
 }
