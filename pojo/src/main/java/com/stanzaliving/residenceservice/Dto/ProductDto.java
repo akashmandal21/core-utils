b6c31af42d35d2be8d0dfcdd74394f2a443d4d1c
guruvachan jain <guruvachan.jain@stanzaliving.com>
2023-02-04 02:31:06 +0530

    add dto

---
+++ pojo/src/main/java/com/stanzaliving/residenceservice/Dto/ProductDto.java
@@ -0,0 +1,26 @@
+package com.stanzaliving.residenceservice.Dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+
+@Data
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@SuperBuilder
+public class ProductDto {
+
+    @JsonProperty("product_name")
+    private String productName;
+
+    @JsonProperty("product_status")
+    private int productStatus = 1;
+
+}
