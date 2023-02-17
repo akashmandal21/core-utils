8030b9a7cfe27b7cf50728652fafbe9dc9cc8d5f
Navratan Choudhary <navratan.choudhary@stanzaliving.com>
2023-02-17 11:42:50 +0530

    code added

--- osm-service-client/src/main/java/com/stanzaliving/core/api/OsmServiceClientApi.java
+++ osm-service-client/src/main/java/com/stanzaliving/core/api/OsmServiceClientApi.java
@@ -2,6 +2,7 @@ package com.stanzaliving.core.api;
 
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
+import com.stanzaliving.osm.dto.StanzaOsmPlaceDto;
 import com.stanzaliving.website.elasticsearch.index.dto.WebsitePlaceIndexDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
@@ -40,6 +41,31 @@ public class OsmServiceClientApi {
             };
 
             return restClient.invokeAPI(path, HttpMethod.POST, queryParams, websitePlaceIndexDtos, headerParams, accept, returnType);
+
+        } catch (Exception e) {
+            log.error("Error while pushing old places data to new elastic", e);
+        }
+        return null;
+    }
+
+    public ResponseDto<List<StanzaOsmPlaceDto>> fetchOsmPlacesForTheWholeCity(String transformationCityUuid) {
+
+        try {
+            String path = UriComponentsBuilder.fromPath("internal/website/osm/get/all/stanza/place/by").toUriString();
+
+            final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+            queryParams.add("transformationCityUuid", transformationCityUuid);
+
+            final HttpHeaders headerParams = new HttpHeaders();
+
+            final String[] accepts = {"*/*"};
+
+            final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+            ParameterizedTypeReference<ResponseDto<List<StanzaOsmPlaceDto>>> returnType = new ParameterizedTypeReference<ResponseDto<List<StanzaOsmPlaceDto>>>() {
+            };
+
+            return restClient.invokeAPI(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType);
 
         } catch (Exception e) {
             log.error("Error while pushing old places data to new elastic", e);
---
+++ pojo/src/main/java/com/stanzaliving/website/enums/DynamicUrlType.java
@@ -0,0 +1,8 @@
+package com.stanzaliving.website.enums;
+
+public enum DynamicUrlType {
+
+    OSM_URL,
+    STANZA_PLACE_URL,
+    FLAT_URL;
+}
