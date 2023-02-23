7f88468982aed3ab1a3de510a1983cb4608bfeb2
Navratan Choudhary <navratan.choudhary@stanzaliving.com>
2023-02-23 15:42:24 +0530

    Update DynamicSeoConstants.java

--- pojo/src/main/java/com/stanzaliving/website/constants/DynamicSeoConstants.java
+++ pojo/src/main/java/com/stanzaliving/website/constants/DynamicSeoConstants.java
@@ -5,8 +5,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class DynamicSeoConstants {
 
-    public final String NAME_HYPER_LINK = "<p><a href=\"#SL_HOUSE_URL\">#SL_HOUSE_NAME</a></p><br/>";
-    public final String HYPERLINK = "<p><a href=\"#SL_HOUSE_URL\">#SL_HOUSE_NAME</a></p><br/>";
+    public final String NAME_HYPER_LINK = "#SL_NAME_HYPER_LINK";
+    public final String HYPERLINK = "#SL_HYPERLINK";
     public final String HTML_HYPERLINK_PARA = "<p><a href=\"" + HYPERLINK + "\"></a>" + NAME_HYPER_LINK + "</p><br/>";
 
 }
