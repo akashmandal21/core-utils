5bd763113e8a1f06a0e1b4abbf3b3dd9d304a705
manishpareekstanza <manish.pareek@stanzaliving.com>
2023-02-27 10:20:48 +0530

    UserMenuCategoryDtoStayCuration added

---
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/UserMenuCategoryPlansDtoStayCuration.java
@@ -0,0 +1,33 @@
+package com.stanzaliving.core.food.dto;
+
+import java.io.Serializable;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import com.stanzaliving.core.operations.enums.MealType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class UserMenuCategoryPlansDtoStayCuration implements Serializable {
+
+    private static final long serialVersionUID = 6843282985331216007L;
+
+    private String menuId;
+    private LocalDate planStartDate;
+    private LocalDate planEndDate;
+
+    private Map<MealType, List<DayOfWeek>> mealDays;
+}
