b282de3947013f4682380e879554f8b70fe01b28
Navratan Choudhary <navratan.choudhary@stanzaliving.com>
2023-02-28 18:53:05 +0530

    Update DynamicSeoConstants.java

--- pojo/src/main/java/com/stanzaliving/website/constants/DynamicSeoConstants.java
+++ pojo/src/main/java/com/stanzaliving/website/constants/DynamicSeoConstants.java
@@ -13,7 +13,7 @@ public class DynamicSeoConstants {
     public final String HTML_HYPERLINK_PARA = "<p><a href=\"" + HYPERLINK + "\">" + NAME_HYPER_LINK + "</a></p><br/>";
     public final String SL_PLACE_NAME = "#SL_PLACE_NAME";
     public final String SL_PLACE_NAME_URL_SLUG = "#SL_PLACE_NAME_URL_SLUG";
-    public final String SL_CITY_NAME = "#SL_PLACE_NAME";
+    public final String SL_CITY_NAME = "#SL_CITY_NAME";
     public final String SL_MM_NAME = "#SL_MM_NAME";
     public final String SL_MM_DEFAULT_ALIAS = "#SL_MM_DEFAULT_ALIAS";
     public final String SL_NEARBY_MM1_NAME = "#SL_NEARBY_MM1_NAME";
