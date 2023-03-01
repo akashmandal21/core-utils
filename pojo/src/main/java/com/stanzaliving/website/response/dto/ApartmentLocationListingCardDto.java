4ef377aa242705dfc9359e170ee1c3acec85f680
SumitDutta1 <sumit.dutta@stanzaliving.com>
2023-03-01 14:07:44 +0530

    Update ApartmentLocationListingCardDto.java

--- pojo/src/main/java/com/stanzaliving/website/response/dto/ApartmentLocationListingCardDto.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/ApartmentLocationListingCardDto.java
@@ -20,7 +20,7 @@ public class ApartmentLocationListingCardDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private int locationId;
+    private long locationId;
 
     private String name;
 
