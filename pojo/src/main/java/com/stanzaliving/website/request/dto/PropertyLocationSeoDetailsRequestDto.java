14b81bd89408ae60f24d9e2267bab55b6c49ca71
SumitDutta1 <sumit.dutta@stanzaliving.com>
2023-03-02 19:07:32 +0530

    Create PropertyLocationSeoDetailsRequestDto.java

---
+++ pojo/src/main/java/com/stanzaliving/website/request/dto/PropertyLocationSeoDetailsRequestDto.java
@@ -0,0 +1,16 @@
+package com.stanzaliving.website.request.dto;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class PropertyLocationSeoDetailsRequestDto {
+
+    private Long locationId;
+    private String seoTitle;
+    private String seoDescription;
+}
