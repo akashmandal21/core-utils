145b07c55ed142b991df5dd0b089472f58c24817
Shubham.Chauhan <shubham.chauhan@ip-10-101-206-100.ap-south-1.compute.internal>
2023-03-03 12:37:06 +0530

    conflicts

--- sqljpa/src/main/java/com/stanzaliving/core/sqljpa/conveter/ListObjectConverter.java
+++ sqljpa/src/main/java/com/stanzaliving/core/sqljpa/conveter/ListObjectConverter.java
@@ -11,22 +11,9 @@ import org.apache.commons.lang3.StringUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-<<<<<<< HEAD
-import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
-
-import javax.persistence.AttributeConverter;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-=======
 
-import lombok.extern.log4j.Log4j2;
->>>>>>> 7cd5c7b649fb704ed0250d8bb5ea00d0c1e0c664
 
 @Log4j2
 public class ListObjectConverter implements AttributeConverter<List<Object>, String> {
