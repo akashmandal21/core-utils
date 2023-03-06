a8d137deeedf1d0d6cbd4da7391426dbc3e497b5
SumitDutta1 <sumit.dutta@stanzaliving.com>
2023-03-06 13:30:43 +0530

    Update PropertyLocationListResponseDTO.java

--- pojo/src/main/java/com/stanzaliving/website/response/dto/PropertyLocationListResponseDTO.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/PropertyLocationListResponseDTO.java
@@ -1,5 +1,4 @@
 package com.stanzaliving.website.response.dto;
-
 import lombok.*;
 
 import java.io.Serializable;
@@ -14,5 +13,20 @@ public class PropertyLocationListResponseDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-
+    private long locationId;
+    private String name;
+    private String slug;
+    private double latitude;
+    private double longitude;
+    private int micromarketId;
+    private String micromarketName;
+    private String micromarketSlug;
+    private String apartmentMicromarketSlug;
+    private int cityId;
+    private String cityName;
+    private String citySlug;
+    private String apartmentCitySlug;
+    private Integer preBookingAmount;
+    private Integer unlockDiscountAmount;
+    private boolean status;
 }
