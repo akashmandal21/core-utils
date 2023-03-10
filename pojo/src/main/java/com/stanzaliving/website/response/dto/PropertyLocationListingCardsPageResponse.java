076f0f3a880d962cdf65e10b6297f136e6b94b9f
SumitDutta1 <sumit.dutta@stanzaliving.com>
2023-03-10 18:17:15 +0530

    code added

--- pojo/src/main/java/com/stanzaliving/website/response/dto/PropertyLocationListingCardsPageResponse.java
+++ pojo/src/main/java/com/stanzaliving/website/response/dto/PropertyLocationListingCardsPageResponse.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Data
 @Builder
-public class ApartmentLocationListingCardsPageResponse extends PageResponse<ApartmentLocationListingCardsPageResponse> implements Serializable {
+public class PropertyLocationListingCardsPageResponse extends PageResponse<PropertyLocationListingCardsPageResponse> implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
