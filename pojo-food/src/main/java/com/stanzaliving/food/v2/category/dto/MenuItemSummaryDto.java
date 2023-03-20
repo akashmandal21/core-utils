5bdf2a98adbcff04e3a36a9ff726190d1b582524
manu-stanza <manu.pathria@stanzaliving.com>
2023-03-20 15:35:35 +0530

    added beverage and details dto in core utils

---
+++ pojo-food/src/main/java/com/stanzaliving/food/v2/category/dto/MenuItemSummaryDto.java
@@ -0,0 +1,22 @@
+package com.stanzaliving.food.v2.category.dto;
+
+import com.stanzaliving.core.food.dto.MenuItemDto;
+import java.io.Serializable;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+public class MenuItemSummaryDto implements Serializable {
+    private List<MenuItemDto> menuItemDtoList;
+    @Builder.Default
+    private String beverageHash = "";
+}
