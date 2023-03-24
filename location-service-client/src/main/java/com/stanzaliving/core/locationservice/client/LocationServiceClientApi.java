9828acdef6583f1d44d8745b6b7f7757639fd90d
SumitDutta1 <sumit.dutta@stanzaliving.com>
2023-03-24 13:53:44 +0530

    code added

--- location-service-client/src/main/java/com/stanzaliving/core/locationservice/client/LocationServiceClientApi.java
+++ location-service-client/src/main/java/com/stanzaliving/core/locationservice/client/LocationServiceClientApi.java
@@ -4,7 +4,9 @@ import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
 import com.stanzaliving.core.base.location.GeoPointDto;
 import com.stanzaliving.core.locationservice.dto.LocationDetailsDto;
+import com.stanzaliving.core.locationservice.dto.LocationDto;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -76,6 +78,38 @@ public class LocationServiceClientApi {
 
         } catch (Exception e) {
             log.error("Error while Fetching Location Details Dto For cityTransformationUuid: {}", cityTransformationUuid, e);
+        }
+        return null;
+    }
+
+    public ResponseDto<LocationDto> getSocietyLocation(String transformationUuid , String parentTypes) {
+
+        try {
+            final Map<String, Object> uriVariables = new HashMap<>();
+
+            uriVariables.put("transformationUuid", transformationUuid);
+
+            String path = UriComponentsBuilder.fromPath("/internal/residence/{transformationUuid}/parents").buildAndExpand(uriVariables).toUriString();
+
+            final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+            if (StringUtils.isNotBlank(parentTypes)) {
+                queryParams.add("parentTypes", parentTypes);
+            }
+
+            final HttpHeaders headerParams = new HttpHeaders();
+
+            final String[] accepts = {"*/*"};
+
+            final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+            ParameterizedTypeReference<ResponseDto<LocationDto>> returnType = new ParameterizedTypeReference<ResponseDto<LocationDto>>() {
+            };
+
+            return restClient.invokeAPI(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType);
+
+        } catch (Exception e) {
+            log.error("Error while fetching LocationDto For transformationUuid: {}", transformationUuid, e);
         }
         return null;
     }
---
+++ location-service-client/src/main/java/com/stanzaliving/core/locationservice/dto/LocationDto.java
@@ -0,0 +1,23 @@
+package com.stanzaliving.core.locationservice.dto;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String locationName;
+
+    private String locationServiceUuid;
+
+    private String locationType;
+}
