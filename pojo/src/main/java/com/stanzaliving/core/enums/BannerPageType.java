2ac1f0e7452451a93907d8415a43c3fdb94f8844
Navratan Choudhary <navratan.choudhary@stanzaliving.com>
2023-05-01 13:55:52 +0530

    Update BannerPageType.java

--- pojo/src/main/java/com/stanzaliving/core/enums/BannerPageType.java
+++ pojo/src/main/java/com/stanzaliving/core/enums/BannerPageType.java
@@ -7,12 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum BannerPageType {
 
-    CITY_PG_DEFAULT("City Pg Default"),
-    CITY_APARTMENT_DEFAULT("Apartment Pg Default"),
-    CITY_PG("City Pg"),
-    CITY_APARTMENT("City Apartment"),
-    MICROMARKET_PG("Micromarket Pg"),
-    MICROMARKET_APARTMENT("Micromarket Apartment");
+    CITY_PG_DEFAULT("City PG Page Default Banners"),
+    CITY_APARTMENT_DEFAULT("City Apartment Page Default Banners"),
+    CITY_PG("City PG Page Custom Banners"),
+    CITY_APARTMENT("City Apartment Page Custom Banners"),
+    MICROMARKET_PG("Micromarket PG Page Custom Banners"),
+    MICROMARKET_APARTMENT("Micromarket Apartment Page Custom Banners");
 
     private String bannerPageTypeName;
 }
