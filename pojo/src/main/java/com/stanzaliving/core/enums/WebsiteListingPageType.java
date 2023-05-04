05aca0a668fa2ef70d42ef782d0dc5de16321de6
Navratan Choudhary <navratan.choudhary@stanzaliving.com>
2023-05-04 15:10:13 +0530

    Update WebsiteListingPageType.java

--- pojo/src/main/java/com/stanzaliving/core/enums/WebsiteListingPageType.java
+++ pojo/src/main/java/com/stanzaliving/core/enums/WebsiteListingPageType.java
@@ -3,6 +3,9 @@ package com.stanzaliving.core.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 public enum WebsiteListingPageType {
@@ -15,4 +18,25 @@ public enum WebsiteListingPageType {
     MICROMARKET_APARTMENT_PAGE("Micromarket Apartment Page Configurations");
 
     private String websiteListingPageTypeName;
+
+    public static List<WebsiteListingPageType> getHierarchicalChildrenFromDefaultPageType(WebsiteListingPageType defaultListingPageType) {
+
+        if (WebsiteListingPageType.CITY_PG_PAGE_DEFAULT.equals(defaultListingPageType)) {
+            return Arrays.asList(WebsiteListingPageType.CITY_PG_PAGE, WebsiteListingPageType.MICROMARKET_PG_PAGE);
+        } else if (WebsiteListingPageType.CITY_APARTMENT_PAGE_DEFAULT.equals(defaultListingPageType)) {
+            return Arrays.asList(WebsiteListingPageType.CITY_APARTMENT_PAGE, WebsiteListingPageType.MICROMARKET_APARTMENT_PAGE);
+        }
+        return Arrays.asList(defaultListingPageType);
+    }
+
+    public static WebsiteListingPageType getHierarchicalChildrenFromParentPageType(WebsiteListingPageType parentPageType) {
+
+        if (WebsiteListingPageType.CITY_PG_PAGE.equals(parentPageType)) {
+            return WebsiteListingPageType.MICROMARKET_PG_PAGE;
+        } else if (WebsiteListingPageType.CITY_APARTMENT_PAGE.equals(parentPageType)) {
+            return WebsiteListingPageType.MICROMARKET_APARTMENT_PAGE;
+        } else {
+            return parentPageType;
+        }
+    }
 }
