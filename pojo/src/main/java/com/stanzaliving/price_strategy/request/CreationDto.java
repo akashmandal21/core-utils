48ce0cce0a8cfb2aa68b94fd405799f1e1ff374e
Guruvachan96 <guruvachan.jain@stanzaliving.com>
2023-05-24 10:18:28 +0530

    add validation

--- pojo/src/main/java/com/stanzaliving/price_strategy/request/CreationDto.java
+++ pojo/src/main/java/com/stanzaliving/price_strategy/request/CreationDto.java
@@ -7,6 +7,7 @@ import com.stanzaliving.commercialcard.enums.CommercialCodeType;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
@@ -25,6 +26,8 @@ public class CreationDto {
 
     private CommercialCodeType applyTo;
 
+    @NotNull(message = "applyToEntities can't be null")
+    @NotEmpty(message = "applyToEntities can't be empty")
     private List<String> applyToEntities;
 
     private Date applicabilityFrom;
@@ -33,12 +36,18 @@ public class CreationDto {
 
     private CommercialCardStatus strategyCardStatus;
 
+    @NotNull(message = "booking user type can't be null")
+    @NotEmpty(message = "booking user type can't be empty")
     private List<CommercialCardUserType> bookingTypes;
 
+    @NotNull(message = "commercial rules can't be null")
+    @NotEmpty(message = "commercial rules can't be empty")
     private List<ConditionsRule> conditionsRules;
 
     private CommercialActionDto commercialActionDto;
 
+    @NotNull(message = "payment rules can't be null")
+    @NotEmpty(message = "payment rules can't be empty")
     private List<PaymentActionDto> allowedFrequencies;
 
     private String bookingSubTypes;
