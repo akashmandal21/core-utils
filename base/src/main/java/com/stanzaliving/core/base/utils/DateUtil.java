3113b18007683afe298e0a3379cc80429055b059
SumitDutta1 <sumit.dutta@stanzaliving.com>
2023-05-28 23:33:21 +0530

    Update DateUtil.java

--- base/src/main/java/com/stanzaliving/core/base/utils/DateUtil.java
+++ base/src/main/java/com/stanzaliving/core/base/utils/DateUtil.java
@@ -1240,6 +1240,11 @@ public class DateUtil {
         return date.with(TemporalAdjusters.previousOrSame(day));
     }
 
+    public static LocalDate getPrevDayOfTheWeek(LocalDate date, DayOfWeek day) {
+
+        return date.with(TemporalAdjusters.previous(day));
+    }
+
     public static LocalDate getNextOrSameDayOfTheWeek(LocalDate date, DayOfWeek day) {
 
         return date.with(TemporalAdjusters.nextOrSame(day));
