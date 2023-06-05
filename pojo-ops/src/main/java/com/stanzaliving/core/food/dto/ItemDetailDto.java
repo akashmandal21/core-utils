40cb34eddefe2f3bb7eed50b62996543f3ba945a
SumitDutta1 <sumit.dutta@stanzaliving.com>
2023-06-05 11:52:14 +0530

    Revert "Update ItemDetailDto.java"
    This reverts commit 6eb637753bf28dfa038b4117d999bfb54a11a51d.

--- pojo-ops/src/main/java/com/stanzaliving/core/food/dto/ItemDetailDto.java
+++ pojo-ops/src/main/java/com/stanzaliving/core/food/dto/ItemDetailDto.java
@@ -10,16 +10,8 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemDetailDto {
-
 	private String itemName;
 	private Integer itemQty;
 	private String itemImg;
-
-	@Builder.Default
-	private Double itemGrammage = 0d;
-	private String itemId;
-	private String itemType;
-	private String unit;
-	private Double orderedWeight;
 
 }
