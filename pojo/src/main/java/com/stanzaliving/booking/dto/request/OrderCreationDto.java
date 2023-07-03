07da07b5f26074f2589829c26738ae1f2bcd023b
Guruvachan96 <guruvachan.jain@stanzaliving.com>
2023-07-03 12:39:34 +0530

    add serializable

--- pojo/src/main/java/com/stanzaliving/booking/dto/request/OrderCreationDto.java
+++ pojo/src/main/java/com/stanzaliving/booking/dto/request/OrderCreationDto.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class OrderCreationDto {
+public class OrderCreationDto implements Serializable {
 
     private String bundleId;
     private List<String> postpaidAddOnIds;
