faf960aeda641ec5006c13323440d3034d21f9b6
manishpareekstanza <manish.pareek@stanzaliving.com>
2023-07-14 18:32:26 +0530

    Update InternetVendor.java

--- pojo/src/main/java/com/stanzaliving/internet/enums/InternetVendor.java
+++ pojo/src/main/java/com/stanzaliving/internet/enums/InternetVendor.java
@@ -1,16 +1,20 @@
 package com.stanzaliving.internet.enums;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.stanzaliving.core.enums.helpers.EnumValue;
+import com.stanzaliving.core.user.enums.EnumListing;
 
 import lombok.Getter;
 
 @Getter
 public enum InternetVendor implements EnumValue {
 
-	SPECTRA("SPECTRA"),
-	JIO("Jio"),
-	ION("Ion"),
-	AIRTEL("Airtel"),
+	SPECTRA("SPECTRA"), 
+	JIO("Jio"), 
+	ION("Ion"), 
+	AIRTEL("Airtel"), 
 	OTHER("Other");
 
 	private String vendor;
@@ -26,6 +30,17 @@ public enum InternetVendor implements EnumValue {
 	@Override
 	public String getValue() {
 		return vendor;
+	}
+
+	private static List<EnumListing<InternetVendor>> internetVendorList = new ArrayList<>();
+	static {
+		for (InternetVendor internetVendor : InternetVendor.values()) {
+			internetVendorList.add(EnumListing.of(internetVendor, internetVendor.getVendor()));
+		}
+	}
+
+	public static List<EnumListing<InternetVendor>> getInternetVendorList() {
+		return internetVendorList;
 	}
 
 }
