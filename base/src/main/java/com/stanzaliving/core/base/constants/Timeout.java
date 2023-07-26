73b4e4d016bc144a943ec5dab78c8904ad3db605
manishpareekstanza <manish.pareek@stanzaliving.com>
2023-07-26 12:58:33 +0530

    Update Timeout.java

--- base/src/main/java/com/stanzaliving/core/base/constants/Timeout.java
+++ base/src/main/java/com/stanzaliving/core/base/constants/Timeout.java
@@ -1,6 +1,6 @@
 package com.stanzaliving.core.base.constants;
 
 public class Timeout {
-    public static final int DEFAULT_CONNECT_TIMEOUT_IN_MILLIS = 1000;
-    public static final int DEFAULT_READ_TIMEOUT_IN_MILLIS = 2000;
+    public static final int DEFAULT_CONNECT_TIMEOUT_IN_MILLIS = 10000;
+    public static final int DEFAULT_READ_TIMEOUT_IN_MILLIS = 10000;
 }
