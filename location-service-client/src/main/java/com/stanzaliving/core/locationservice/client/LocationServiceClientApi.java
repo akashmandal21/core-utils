8fcb94f65f67f798b5f27052a4d301339d189511
Navratan Choudhary <navratan.choudhary@stanzaliving.com>
2023-08-01 12:32:22 +0530

    code added

--- location-service-client/src/main/java/com/stanzaliving/core/locationservice/client/LocationServiceClientApi.java
+++ location-service-client/src/main/java/com/stanzaliving/core/locationservice/client/LocationServiceClientApi.java
@@ -3,6 +3,7 @@ package com.stanzaliving.core.locationservice.client;
 import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
 import com.stanzaliving.core.base.location.GeoPointDto;
+import com.stanzaliving.core.locationservice.dto.GeoPoint;
 import com.stanzaliving.core.locationservice.dto.LocationDetailsDto;
 import com.stanzaliving.core.locationservice.dto.LocationDto;
 import lombok.extern.log4j.Log4j2;
@@ -114,6 +115,36 @@ public class LocationServiceClientApi {
 
         } catch (Exception e) {
             log.error("Error while fetching LocationDto for transformationUuid: {}", transformationUuid, e);
+        }
+        return null;
+    }
+
+    public ResponseDto<LocationDto> getEnclosedAreaByTypeAndLatLong(double latitude, double longitude, String enclosedAreaType) {
+
+        GeoPoint geoPointDto = GeoPoint.builder().latitude(latitude).longitude(longitude).build();
+
+        try {
+            final Map<String, Object> uriVariables = new HashMap<>();
+
+            uriVariables.put("type", enclosedAreaType);
+
+            String path = UriComponentsBuilder.fromPath("/internal/residence/{type}/parents").buildAndExpand(uriVariables).toUriString();
+
+            final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+            final HttpHeaders headerParams = new HttpHeaders();
+
+            final String[] accepts = {"*/*"};
+
+            final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+            ParameterizedTypeReference<ResponseDto<LocationDto>> returnType = new ParameterizedTypeReference<ResponseDto<LocationDto>>() {
+            };
+
+            return restClient.invokeAPI(path, HttpMethod.POST, queryParams, geoPointDto, headerParams, accept, returnType);
+
+        } catch (Exception e) {
+            log.error("Error while Fetching Stanza Enclosed Area details from lat, long, geoPoint: {}", geoPointDto, e);
         }
         return null;
     }
