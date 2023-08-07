e6f321f407924a86d105cfc2da9349011afaec18
Anukul Jain <107670569+anukul00jain@users.noreply.github.com>
2023-08-07 15:43:32 +0530

    add api

--- bundle-client/src/main/java/com/stanzaliving/core/bundle/client/api/BundleDataControllerApi.java
+++ bundle-client/src/main/java/com/stanzaliving/core/bundle/client/api/BundleDataControllerApi.java
@@ -11,7 +11,6 @@ import com.stanzaliving.core.base.common.dto.ResponseDto;
 import com.stanzaliving.core.base.http.StanzaRestClient;
 import com.stanzaliving.residenceservice.enums.VasCategory;
 import com.stanzaliving.stayCuration.AlfredResidenceServiceDto;
-import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.core.ParameterizedTypeReference;
@@ -60,8 +59,32 @@ public class BundleDataControllerApi {
     }
 
     public Object getActiveBundle(String residenceId, String dealCategory, LocalDate fromDate) {
+        log.info("Bundle-Data-Controller::Fetching bundles for residenceId: {}, dealCategory: {}: fromDate: {}", residenceId, dealCategory, fromDate);
 
-        return null;
+        final Map<String, Object> uriVariables = new HashMap<>();
+        uriVariables.put("residenceUuid", residenceId);
+        uriVariables.put("dealCategory", dealCategory);
+        uriVariables.put("fromDate", fromDate);
+        String path = UriComponentsBuilder.fromPath("/find-first/{residenceUuid}/{dealCategory}/{validFrom}").buildAndExpand(uriVariables).toUriString();
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
+        final org.springframework.http.HttpHeaders headerParams = new HttpHeaders();
+
+        final String[] accepts = {
+                "*/*"
+        };
+        final List<MediaType> accept = restClient.selectHeaderAccept(accepts);
+
+        ParameterizedTypeReference<Object> returnType = new ParameterizedTypeReference<Object>() {
+        };
+
+        try {
+            return this.restClient.invokeAPI(path, HttpMethod.GET, queryParams, null, headerParams, accept, returnType);
+        } catch (Exception e) {
+            log.error("Exception while fetching bundle for residenceId: {}, dealCategory: {}: fromDate: {}", residenceId, dealCategory, fromDate, e);
+            return null;
+        }
     }
 
     public Object getBundle(String uuid) {
@@ -110,7 +133,7 @@ public class BundleDataControllerApi {
         };
 
         try {
-            return this.restClient.invokeAPI(path, HttpMethod.GET, queryParams, uuidList, headerParams, accept, returnType);
+            return this.restClient.invokeAPI(path, HttpMethod.POST, queryParams, uuidList, headerParams, accept, returnType);
         } catch (Exception e) {
             log.error("Exception while fetching bundle for bundleIds: {}", uuidList, e);
             return Collections.emptyList();
