1bbc395ce060c87b45b0693197d89f48a30ff8e0
Mayank Agrawal <107577957+mayank-sl@users.noreply.github.com>
2023-08-18 17:07:51 +0530

    Update GstDto.java

--- pojo/src/main/java/com/stanzaliving/stay_curation/dto/GstDto.java
+++ pojo/src/main/java/com/stanzaliving/stay_curation/dto/GstDto.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Getter
+@Setter
 public class GstDto {
     private Double amount;
     private Double percentage;
