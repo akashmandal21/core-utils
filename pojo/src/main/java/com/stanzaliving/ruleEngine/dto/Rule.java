79511006e538658ae4d3397863ed3ba65fdafa15
gaurav-likhar <gaurav.likhar@stanzaliving.com>
2023-09-10 18:02:26 +0530

    added superbuilder to fix compilation error

--- pojo/src/main/java/com/stanzaliving/ruleEngine/dto/Rule.java
+++ pojo/src/main/java/com/stanzaliving/ruleEngine/dto/Rule.java
@@ -7,13 +7,14 @@
 package com.stanzaliving.ruleEngine.dto;
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @Setter
 public class Rule extends BaseOperation{
     private List<Rule> rules;
